63633f4420b32f64bada0130e999333a579c028d
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-03-30 11:34:12 +0900

    Update levy + opposition in NMRA and PFA

--- mealpy/swarm_based/NMRA.py
+++ mealpy/swarm_based/NMRA.py
@@ -7,9 +7,7 @@
 #       Github:     https://github.com/thieunguyen5991                                                  %
 #-------------------------------------------------------------------------------------------------------%
 
-from numpy.random import uniform, choice, normal, randint
-from numpy import power, sin, pi, abs, zeros, sqrt, sign
-from math import gamma
+from numpy.random import uniform, choice, randint
 from copy import deepcopy
 from mealpy.root import Root
 
@@ -67,26 +65,6 @@ class LevyNMR(BaseNMR):
         BaseNMR.__init__(self, objective_func, problem_size, domain_range, log, epoch, pop_size, bp)
         self.pm = 0.025
 
-    def _levy_flight__(self, epoch, solution, prey):
-        beta = 1
-        # muy and v are two random variables which follow normal distribution
-        # sigma_muy : standard deviation of muy
-        sigma_muy = power(gamma(1 + beta) * sin(pi * beta / 2) / (gamma((1 + beta) / 2) * beta * power(2, (beta - 1) / 2)),1 / beta)
-        # sigma_v : standard deviation of v
-        sigma_v = 1
-        muy = normal(0, sigma_muy**2)
-        v = normal(0, sigma_v**2)
-        s = muy / power(abs(v), 1 / beta)
-        # D is a random solution
-        D = self._create_solution__(minmax=self.ID_MIN_PROB)
-        LB = 0.001 * s * (solution[self.ID_POS] - prey[self.ID_POS])
-
-        levy = D[self.ID_POS] * LB
-        return levy
-
-        # x_new = solution[0] + 1.0/sqrt(epoch+1) * sign(uniform() - 0.5) * levy
-        # return x_new
-
     def _crossover__(self, solution, g_best):
         start_point = randint(0, self.problem_size / 2)
         id1 = start_point
@@ -106,7 +84,7 @@ class LevyNMR(BaseNMR):
         id3 = int(self.problem_size)
 
         partner = pop[randint(0, self.pop_size)][self.ID_POS]
-        new_temp = zeros(self.problem_size)
+        new_temp = deepcopy(g_best[self.ID_POS])
         new_temp[0:id1] = g_best[self.ID_POS][0:id1]
         new_temp[id1:id2] = partner[id1:id2]
         new_temp[id2:id3] = g_best[self.ID_POS][id2:id3]
@@ -141,29 +119,16 @@ class LevyNMR(BaseNMR):
                         t1, t2 = choice(range(self.size_b, self.pop_size), 2, replace=False)
                         temp = pop[i][self.ID_POS] + uniform() * (pop[t1][self.ID_POS] - pop[t2][self.ID_POS])
                     else:
-                        temp = self._levy_flight__(epoch, pop[i], g_best)
+                        temp = self._levy_flight__(epoch, pop[i][self.ID_POS], g_best[self.ID_POS])
                     # Mutation
                     for id in range(0, self.problem_size):
                         if uniform() < self.pm:
                             temp = self._mutation_flip_point__(temp, id)
 
-                #temp = self._random_amend_solution_and_return__(temp)
+                temp = self._amend_solution_faster__(temp)
                 fit = self._fitness_model__(temp)
                 if fit < pop[i][self.ID_FIT]:
                     pop[i] = [temp, fit]
-                    # xichma = power((gamma(1 + 1.5) * sin(pi * 1.5 / 2.0)) / (gamma((1 + 1.5) * 1.5 * power(2, (1.5 - 1) / 2)) / 2.0), 1.0 / 1.5)
-                    # LF_D = 0.01 * uniform() * xichma / power(abs(uniform()), 1.0 / 1.5)
-                    # J = 2 * (1 - uniform())
-                    # w = 1 - (epoch + 1) * 1.0 / self.epoch
-                    # Y = g_best[self.ID_POS] - w * abs(J * g_best[self.ID_POS] - pop[i][self.ID_POS])
-                    # Z = Y + uniform(self.domain_range[0], self.domain_range[1], self.problem_size) * LF_D
-                    #
-                    # fit_Y = self._fitness_model__(Y)
-                    # fit_Z = self._fitness_model__(Z)
-                    # if fit_Y < pop[i][self.ID_FIT]:
-                    #     pop[i] = [Y, fit_Y]
-                    # if fit_Z < pop[i][self.ID_FIT]:
-                    #     pop[i] = [Z, fit_Z]
 
             pop, g_best = self._sort_pop_and_update_global_best__(pop, self.ID_MIN_PROB, g_best)
             self.loss_train.append(g_best[self.ID_FIT])
--- mealpy/swarm_based/PFA.py
+++ mealpy/swarm_based/PFA.py
@@ -25,7 +25,7 @@ LPFA: after 4 loop, it reaches value 100.0      (a huge improvement)
 IPFA: after 2 loop, it reaches value 100.0      (best improvement)
 """
 
-from numpy import exp, ones, sqrt, setxor1d, array, pi, power, abs, sin, sign
+from numpy import exp, sqrt, setxor1d, array, pi, power, abs, sin, sign
 from numpy.random import uniform, choice, normal, random
 from numpy.linalg import norm
 from copy import deepcopy
@@ -136,9 +136,7 @@ class OPFA(BasePFA):
                 if fit < pop[j][self.ID_FIT]:
                     pop[j] = [temp2, fit]
                 else:
-                    C_op = self.domain_range[1] * ones(self.problem_size) + self.domain_range[0] * \
-                           ones(self.problem_size) - gbest_present[self.ID_POS] + uniform() * \
-                           (gbest_present[self.ID_POS] - temp2)
+                    C_op = self._create_opposition_solution__(temp2, gbest_present[self.ID_POS])
                     fit_op = self._fitness_model__(C_op)
                     if fit_op < pop[j][self.ID_FIT]:
                         pop[j] = [C_op, fit_op]
@@ -156,25 +154,6 @@ class LPFA(BasePFA):
     def __init__(self, objective_func=None, problem_size=50, domain_range=(-1, 1), log=True, epoch=750, pop_size=100):
         BasePFA.__init__(self, objective_func, problem_size, domain_range, log, epoch, pop_size)
 
-    def _levy_flight__(self, epoch, solution, prey):
-        beta = 1
-        # muy and v are two random variables which follow normal distribution
-        # sigma_muy : standard deviation of muy
-        sigma_muy = power(gamma(1 + beta) * sin(pi * beta / 2) / (gamma((1 + beta) / 2) * beta * power(2, (beta - 1) / 2)),1 / beta)
-        # sigma_v : standard deviation of v
-        sigma_v = 1
-        muy = normal(0, sigma_muy)
-        v = normal(0, sigma_v)
-        s = muy / power(abs(v), 1 / beta)
-        # D is a random solution
-        D = self._create_solution__()
-        LB = 0.01 * s * (solution[self.ID_POS] - prey[self.ID_POS])
-
-        levy = D[self.ID_POS] * LB
-        return levy
-        #x_new = solution[self.ID_POS] + 1.0/sqrt(epoch+1) * sign(uniform() - 0.5) * levy
-        #return x_new
-
     def _train__(self):
         # Init pop and calculate fitness
         pop = [self._create_solution__(minmax=0) for _ in range(self.pop_size)]
@@ -183,9 +162,9 @@ class LPFA(BasePFA):
         pop, g_best = self._sort_pop_and_get_global_best__(pop, self.ID_FIT, self.ID_MIN_PROB)
         gbest_present = deepcopy(g_best)
 
-        for i in range(self.epoch):
+        for epoch in range(self.epoch):
             alpha, beta = uniform(1, 2, 2)
-            A = uniform(self.domain_range[0], self.domain_range[1]) * exp(-2 * (i + 1) / self.epoch)
+            A = uniform(self.domain_range[0], self.domain_range[1]) * exp(-2 * (epoch + 1) / self.epoch)
 
             ## Update the position of pathfinder and check the bound
             temp = gbest_present[self.ID_POS] + 2 * uniform() * (gbest_present[self.ID_POS] - g_best[self.ID_POS]) + A
@@ -197,38 +176,37 @@ class LPFA(BasePFA):
             pop[0] = deepcopy(gbest_present)
 
             ## Update positions of members, check the bound and calculate new fitness
-            for j in range(1, self.pop_size):
-                temp1 = deepcopy(pop[j][self.ID_POS])
+            for i in range(1, self.pop_size):
+                temp1 = deepcopy(pop[i][self.ID_POS])
                 temp2 = deepcopy(temp1)
                 if uniform() < 0.5:
                     t1 = beta * uniform() * (gbest_present[self.ID_POS] - temp1)
                     for k in range(1, self.pop_size):
                         dist = norm(pop[k][self.ID_POS] - temp1)
                         t2 = alpha * uniform() * (pop[k][self.ID_POS] - temp1)
-                        t3 = uniform(self.domain_range[0], self.domain_range[1], self.problem_size) * (1 - (i + 1) * 1.0 / self.epoch) * dist
+                        t3 = uniform(self.domain_range[0], self.domain_range[1], self.problem_size) * (1 - (epoch + 1) * 1.0 / self.epoch) * dist
                         temp2 += t2 + t3
                     temp2 += t1
                 else:
                     ## Using Levy-flight to boost algorithm's convergence speed
-                    temp2 = self._levy_flight__(i, pop[j], gbest_present)
+                    temp2 = self._levy_flight__(epoch, pop[i][self.ID_POS], gbest_present[self.ID_POS])
 
                 ## Update members
                 temp2 = self._amend_solution_faster__(temp2)
                 fit = self._fitness_model__(temp2)
-                if fit < pop[j][self.ID_FIT]:
-                    pop[j] = [temp2, fit]
+                if fit < pop[i][self.ID_FIT]:
+                    pop[i] = [temp2, fit]
 
             ## Update the best solution found so far (current pathfinder)
             gbest_present = self._update_global_best__(pop, self.ID_MIN_PROB, gbest_present)
             self.loss_train.append(gbest_present[self.ID_FIT])
             if self.log:
-                print("> Epoch: {}, Best fit: {}".format(i + 1, gbest_present[self.ID_FIT]))
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, gbest_present[self.ID_FIT]))
 
         return gbest_present[self.ID_POS], gbest_present[self.ID_FIT], self.loss_train
 
 
 class IPFA(LPFA):
-
     def __init__(self, objective_func=None, problem_size=50, domain_range=(-1, 1), log=True, epoch=750, pop_size=100):
         LPFA.__init__(self, objective_func, problem_size, domain_range, log, epoch, pop_size)
 
@@ -240,9 +218,9 @@ class IPFA(LPFA):
         pop, g_best = self._sort_pop_and_get_global_best__(pop, self.ID_FIT, self.ID_MIN_PROB)
         gbest_present = deepcopy(g_best)
 
-        for i in range(self.epoch):
+        for epoch in range(0, self.epoch):
             alpha, beta = uniform(1, 2, 2)
-            A = uniform(self.domain_range[0], self.domain_range[1]) * exp(-2 * (i + 1) / self.epoch)
+            A = uniform(self.domain_range[0], self.domain_range[1]) * exp(-2 * (epoch + 1) / self.epoch)
 
             ## Update the position of pathfinder and check the bound
             temp = gbest_present[self.ID_POS] + 2*uniform()*( gbest_present[self.ID_POS] - g_best[self.ID_POS]) + A
@@ -254,39 +232,37 @@ class IPFA(LPFA):
             pop[0] = deepcopy(gbest_present)
 
             ## Update positions of members, check the bound and calculate new fitness
-            for j in range(1, self.pop_size):
-                temp1 = deepcopy(pop[j][self.ID_POS])
+            for i in range(1, self.pop_size):
+                temp1 = deepcopy(pop[i][self.ID_POS])
                 temp2 = deepcopy(temp1)
                 if uniform() < 0.5:
                     t1 = beta * uniform() * (gbest_present[self.ID_POS] - temp1)
                     for k in range(1, self.pop_size):
                         dist = norm(pop[k][self.ID_POS] - temp1)
                         t2 = alpha * uniform() * (pop[k][self.ID_POS] - temp1)
-                        t3 = uniform(self.domain_range[0], self.domain_range[1], self.problem_size) * (1 - (i + 1) * 1.0 / self.epoch) * dist
+                        t3 = uniform(self.domain_range[0], self.domain_range[1], self.problem_size) * (1 - (epoch + 1) * 1.0 / self.epoch) * dist
                         temp2 += t2 + t3
                     temp2 += t1
                 else:
                     ## Using Levy-flight to boost algorithm's convergence speed
-                    temp2 = self._levy_flight__(i, pop[j], gbest_present)
+                    temp2 = self._levy_flight__(epoch, pop[i][self.ID_POS], gbest_present[self.ID_POS])
 
                 ## Update members based on Opposition-based learning
                 temp2 = self._amend_solution_faster__(temp2)
                 fit = self._fitness_model__(temp2)
-                if fit < pop[j][self.ID_FIT]:
-                    pop[j] = [temp2, fit]
+                if fit < pop[i][self.ID_FIT]:
+                    pop[i] = [temp2, fit]
                 else:
-                    C_op = self.domain_range[1] * ones(self.problem_size) + self.domain_range[0] * \
-                           ones(self.problem_size) - gbest_present[self.ID_POS] + uniform() * \
-                           (gbest_present[self.ID_POS] - temp2)
+                    C_op = self._create_opposition_solution__(temp, gbest_present[self.ID_POS])
                     fit_op = self._fitness_model__(C_op)
-                    if fit_op < pop[j][self.ID_FIT]:
-                        pop[j] = [C_op, fit_op]
+                    if fit_op < pop[i][self.ID_FIT]:
+                        pop[i] = [C_op, fit_op]
 
             ## Update the best solution found so far (current pathfinder)
             gbest_present = self._update_global_best__(pop, self.ID_MIN_PROB, gbest_present)
             self.loss_train.append(gbest_present[self.ID_FIT])
             if self.log:
-                print("> Epoch: {}, Best fit: {}".format(i + 1, gbest_present[self.ID_FIT]))
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, gbest_present[self.ID_FIT]))
 
         return gbest_present[self.ID_POS], gbest_present[self.ID_FIT], self.loss_train
 
@@ -353,25 +329,6 @@ class LevyDePFA(DePFA):
     """
     def __init__(self, objective_func=None, problem_size=50, domain_range=(-1, 1), log=True, epoch=750, pop_size=100):
         DePFA.__init__(self, objective_func, problem_size, domain_range, log, epoch, pop_size)
-
-    def _levy_flight__(self, epoch, solution, prey):
-        beta = 1
-        # muy and v are two random variables which follow normal distribution
-        # sigma_muy : standard deviation of muy
-        sigma_muy = power(gamma(1 + beta) * sin(pi * beta / 2) / (gamma((1 + beta) / 2) * beta * power(2, (beta - 1) / 2)),1 / beta)
-        # sigma_v : standard deviation of v
-        sigma_v = 1
-        muy = normal(0, sigma_muy)
-        v = normal(0, sigma_v)
-        s = muy / power(abs(v), 1 / beta)
-        # D is a random solution
-        D = self._create_solution__()
-        LB = 0.001 * s * (solution[self.ID_POS] - prey[self.ID_POS])
-
-        levy = D[self.ID_POS] * LB
-        return levy
-        #x_new = solution[self.ID_POS] + 1.0/sqrt(epoch+1) * sign(uniform() - 0.5) * levy
-        #return x_new
 
     def _caculate_xichma__(self, beta):
         up = gamma(1 + beta) * sin(pi * beta / 2)
