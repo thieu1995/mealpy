0b61634b5d7807270f25b16dd4da289b14dc92b1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-03-30 10:53:22 +0900

    Add levy_flight to root

--- mealpy/root.py
+++ mealpy/root.py
@@ -5,20 +5,22 @@
 #       Email:      nguyenthieu2102@gmail.com                                                           %
 #       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
 #       Github:     https://github.com/thieunguyen5991                                                  %
-#-------------------------------------------------------------------------------------------------------%
+# -------------------------------------------------------------------------------------------------------%
 
-import numpy as np
+from numpy import where, clip, logical_and, maximum, minimum, array, power, sin, abs, pi, sqrt, sign
+from numpy.random import uniform, random, normal
+from math import gamma
 from copy import deepcopy
 
 
 class Root:
     """ This is root of all Algorithms """
 
-    ID_MIN_PROB = 0             # min problem
-    ID_MAX_PROB = -1            # max problem
+    ID_MIN_PROB = 0  # min problem
+    ID_MAX_PROB = -1  # max problem
 
-    ID_POS = 0                  # Position
-    ID_FIT = 1                  # Fitness
+    ID_POS = 0  # Position
+    ID_FIT = 1  # Fitness
 
     EPSILON = 10E-10
 
@@ -47,7 +49,7 @@ class Root:
             0 - minimum problem, else - maximum problem
 
         """
-        solution = np.random.uniform(self.domain_range[0], self.domain_range[1], self.problem_size)
+        solution = uniform(self.domain_range[0], self.domain_range[1], self.problem_size)
         fitness = self._fitness_model__(solution=solution, minmax=minmax)
         return [solution, fitness]
 
@@ -77,25 +79,20 @@ class Root:
         return sorted_pop, deepcopy(sorted_pop[id_best])
 
     def _amend_solution__(self, solution=None):
-        return np.maximum(self.domain_range[0], np.minimum(self.domain_range[1], solution))
+        return maximum(self.domain_range[0], minimum(self.domain_range[1], solution))
 
     def _amend_solution_faster__(self, solution=None):
-        return np.clip(solution, self.domain_range[0], self.domain_range[1])
+        return clip(solution, self.domain_range[0], self.domain_range[1])
 
     def _amend_solution_random__(self, solution=None):
         for i in range(self.problem_size):
             if solution[i] < self.domain_range[0] or solution[i] > self.domain_range[1]:
-                solution[i] = np.random.uniform(self.domain_range[0], self.domain_range[1])
+                solution[i] = uniform(self.domain_range[0], self.domain_range[1])
         return solution
 
     def _amend_solution_random_faster__(self, solution=None):
-        return np.where(np.logical_and(self.domain_range[0] <= solution, solution <= self.domain_range[1]), solution,
-                        np.random.uniform(self.domain_range[0], self.domain_range[1]))
-
-    def _create_opposition_solution__(self, solution=None, g_best=None):
-        temp = [self.domain_range[0] + self.domain_range[1] - g_best[i] + np.random.random() * (g_best[i] - solution[i])
-                      for i in range(self.problem_size)]
-        return np.array(temp)
+        return where(logical_and(self.domain_range[0] <= solution, solution <= self.domain_range[1]), solution, uniform(self.domain_range[0],
+                                                                                                                        self.domain_range[1]))
 
     def _update_global_best__(self, pop=None, id_best=None, g_best=None):
         sorted_pop = sorted(pop, key=lambda temp: temp[self.ID_FIT])
@@ -107,6 +104,41 @@ class Root:
         current_best = sorted_pop[id_best]
         g_best = deepcopy(current_best) if current_best[self.ID_FIT] < g_best[self.ID_FIT] else deepcopy(g_best)
         return sorted_pop, g_best
+
+    def _create_opposition_solution__(self, solution=None, g_best=None):
+        temp = [self.domain_range[0] + self.domain_range[1] - g_best[i] + uniform() * (g_best[i] - solution[i]) for i in range(self.problem_size)]
+        return array(temp)
+
+    def _levy_flight__(self, epoch=None, solution=None, g_best=None, step=0.001, case=0):
+        """
+        Parameters
+        ----------
+        epoch : current iteration
+        solution : 1-D numpy array
+        g_best : 1-D numpy array
+        step : 0.001
+        type : 0, 1, 2
+        """
+        beta = 1
+        # muy and v are two random variables which follow normal distribution
+        # sigma_muy : standard deviation of muy
+        sigma_muy = power(gamma(1 + beta) * sin(pi * beta / 2) / (gamma((1 + beta) / 2) * beta * power(2, (beta - 1) / 2)), 1 / beta)
+        # sigma_v : standard deviation of v
+        sigma_v = 1
+        muy = normal(0, sigma_muy ** 2)
+        v = normal(0, sigma_v ** 2)
+        s = muy / power(abs(v), 1 / beta)
+        # D is a random solution
+        D = uniform(self.domain_range[0], self.domain_range[1], self.problem_size)
+        LB = step * s * (solution - g_best)
+        levy = D * LB
+
+        if case == 0:
+            return levy
+        elif case == 1:
+            return solution + 1.0 / sqrt(epoch + 1) * sign(random() - 0.5) * levy
+        elif case == 2:
+            return solution + 0.01 * levy
 
     def _train__(self):
         pass
