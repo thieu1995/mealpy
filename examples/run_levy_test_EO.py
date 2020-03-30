39bd7ce990095ae2bfee16e66d5f53d4ae229062
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-03-30 12:52:47 +0900

    Add levy version and examples of EO

---
+++ examples/run_levy_test_EO.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu Nguyen" at 12:51, 30/03/2020                                                        %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
+#       Github:     https://github.com/thieunguyen5991                                                  %
+#-------------------------------------------------------------------------------------------------------%
+
+from opfunu.type_based.uni_modal import Functions
+from mealpy.physics_based.EO import BaseEO, LevyEO
+
+t1 = Functions()
+
+## Setting parameters
+objective_func = t1._sum_squres__
+problem_size = 3000
+domain_range = [-15, 15]
+log = True
+epoch = 100
+pop_size = 50
+
+md1 = BaseEO(objective_func, problem_size, domain_range, log, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1._train__()
+print(best_fit1)
+print("========================================================")
+
+md3 = LevyEO(objective_func, problem_size, domain_range, log, epoch, pop_size)
+best_pos3, best_fit3, list_loss3 = md3._train__()
+print(best_fit3)
+
--- mealpy/physics_based/EO.py
+++ mealpy/physics_based/EO.py
@@ -24,12 +24,13 @@ class BaseEO(Root):
         Root.__init__(self, objective_func, problem_size, domain_range, log)
         self.epoch = epoch
         self.pop_size = pop_size
+        self.V = 1
+        self.a1 = 2
+        self.a2 = 1
+        self.GP = 0.5
 
     def _train__(self):
-        V = 1
-        a1 = 2
-        a2 = 1
-        GP = 0.5
+
         #c_eq1 = [None, float("inf")]                    # it is global best solution
         c_eq2 = [None, float("inf")]
         c_eq3 = [None, float("inf")]
@@ -59,19 +60,86 @@ class BaseEO(Root):
             c_pool = [c_eq1, c_eq2, c_eq3, c_eq4, [c_eq_ave, fit_ave]]
 
             # Eq. 9
-            t = (1 - epoch/self.epoch) ** (a2 * epoch / self.epoch)
+            t = (1 - epoch/self.epoch) ** (self.a2 * epoch / self.epoch)
 
             for i in range(0, self.pop_size):
                 lamda = uniform(0, 1, self.problem_size)                # lambda in Eq. 11
                 r = uniform(0, 1, self.problem_size)                    # r in Eq. 11
                 c_eq = c_pool[randint(0, len(c_pool))][self.ID_POS]     # random selection 1 of candidate from the pool
-                f = a1 * sign(r - 0.5) * (exp(-lamda * t) - 1.0)        # Eq. 11
+                f = self.a1 * sign(r - 0.5) * (exp(-lamda * t) - 1.0)        # Eq. 11
                 r1 = uniform()
                 r2 = uniform()                                                                  # r1, r2 in Eq. 15
-                gcp = 0.5 * r1 * ones(self.problem_size) * (r2 >= GP)                           # Eq. 15
+                gcp = 0.5 * r1 * ones(self.problem_size) * (r2 >= self.GP)                           # Eq. 15
                 g0 = gcp * (c_eq - lamda * pop[i][self.ID_POS])                                 # Eq. 14
                 g = g0 * f                                                                      # Eq. 13
-                temp = c_eq + (pop[i][self.ID_POS] - c_eq) * f + (g * V / lamda) * (1.0 - f)    # Eq. 16
+                temp = c_eq + (pop[i][self.ID_POS] - c_eq) * f + (g * self.V / lamda) * (1.0 - f)    # Eq. 16
+                fit = self._fitness_model__(temp)
+                pop[i] = [temp, fit]
+
+            g_best = self._update_global_best__(pop, self.ID_MIN_PROB, g_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.log:
+                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class LevyEO(BaseEO):
+    """
+        My modified version of: Equilibrium Optimizer (EO)
+    """
+
+    def __init__(self, objective_func=None, problem_size=50, domain_range=(-1, 1), log=True, epoch=750, pop_size=100):
+        BaseEO.__init__(self, objective_func, problem_size, domain_range, log, epoch, pop_size)
+
+    def _train__(self):
+        # c_eq1 = [None, float("inf")]                    # it is global best solution
+        c_eq2 = [None, float("inf")]
+        c_eq3 = [None, float("inf")]
+        c_eq4 = [None, float("inf")]
+
+        # ---------------- Memory saving-------------------
+        pop = [self._create_solution__() for _ in range(self.pop_size)]
+        g_best = self._get_global_best__(pop, self.ID_FIT, self.ID_MIN_PROB)
+        c_eq1 = deepcopy(g_best)
+
+        for epoch in range(0, self.epoch):
+
+            for i in range(0, self.pop_size):
+
+                if pop[i][self.ID_FIT] < c_eq1[self.ID_FIT]:
+                    c_eq1 = deepcopy(pop[i])
+                elif c_eq1[self.ID_FIT] < pop[i][self.ID_FIT] < c_eq2[self.ID_FIT]:
+                    c_eq2 = deepcopy(pop[i])
+                elif c_eq1[self.ID_FIT] < pop[i][self.ID_FIT] < c_eq3[self.ID_FIT] and c_eq2[self.ID_FIT] < pop[i][self.ID_FIT]:
+                    c_eq3 = deepcopy(pop[i])
+                elif c_eq1[self.ID_FIT] < pop[i][self.ID_FIT] and c_eq2[self.ID_FIT] < pop[i][self.ID_FIT] and \
+                        c_eq3[self.ID_FIT] < pop[i][self.ID_FIT] < c_eq4[self.ID_FIT]:
+                    c_eq4 = deepcopy(pop[i])
+
+            # make equilibrium pool
+            c_eq_ave = (c_eq1[self.ID_POS] + c_eq2[self.ID_POS] + c_eq3[self.ID_POS] + c_eq4[self.ID_POS]) / 4
+            fit_ave = self._fitness_model__(c_eq_ave)
+            c_pool = [c_eq1, c_eq2, c_eq3, c_eq4, [c_eq_ave, fit_ave]]
+
+            # Eq. 9
+            t = (1 - epoch / self.epoch) ** (self.a2 * epoch / self.epoch)
+
+            for i in range(0, self.pop_size):
+                if uniform() < 0.5:
+                    lamda = uniform(0, 1, self.problem_size)  # lambda in Eq. 11
+                    r = uniform(0, 1, self.problem_size)  # r in Eq. 11
+                    c_eq = c_pool[randint(0, len(c_pool))][self.ID_POS]  # random selection 1 of candidate from the pool
+                    f = self.a1 * sign(r - 0.5) * (exp(-lamda * t) - 1.0)  # Eq. 11
+                    r1 = uniform()
+                    r2 = uniform()  # r1, r2 in Eq. 15
+                    gcp = 0.5 * r1 * ones(self.problem_size) * (r2 >= self.GP)  # Eq. 15
+                    g0 = gcp * (c_eq - lamda * pop[i][self.ID_POS])  # Eq. 14
+                    g = g0 * f  # Eq. 13
+                    temp = c_eq + (pop[i][self.ID_POS] - c_eq) * f + (g * self.V / lamda) * (1.0 - f)  # Eq. 16
+                else:
+                    ## Idea: Sometimes, an unpredictable event happens, It make the status of equilibrium change.
+                    temp = self._levy_flight__(epoch, pop[i][self.ID_POS], g_best[self.ID_POS])
                 fit = self._fitness_model__(temp)
                 pop[i] = [temp, fit]
 
