6964fbe4e06525775d707ff7a3919c1ec6a24d69
Thieu Nguyen <nguyenthieu2102@gmail.com>
2020-10-19 17:43:54 +0700

    Added ModifiedEO by paper 'An efficient equilibrium optimizer with mutation strategy for numerical optimization'

--- examples/physics_based/run_test_EO.py
+++ examples/physics_based/run_test_EO.py
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------------------------------%
 
 from opfunu.cec_basic.cec2014_nobias import *
-from mealpy.physics_based.EO import BaseEO, LevyEO
+from mealpy.physics_based.EO import BaseEO, ModifiedEO, LevyEO
 
 ## Setting parameters
 obj_func = F11
@@ -16,10 +16,10 @@ obj_func = F11
 # ub = [15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3]
 lb = [-100]
 ub = [100]
-problem_size = 2000
+problem_size = 100
 batch_size = 25
 verbose = True
-epoch = 1000
+epoch = 100
 pop_size = 50
 
 md1 = BaseEO(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
@@ -27,6 +27,13 @@ best_pos1, best_fit1, list_loss1 = md1.train()
 print(md1.solution[0])
 print(md1.solution[1])
 print(md1.loss_train)
+
+md1 = ModifiedEO(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
+
 
 md1 = LevyEO(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
 best_pos1, best_fit1, list_loss1 = md1.train()
--- mealpy/physics_based/EO.py
+++ mealpy/physics_based/EO.py
@@ -8,7 +8,7 @@
 #-------------------------------------------------------------------------------------------------------%
 
 from numpy import exp, sign, ones, mean
-from numpy.random import uniform, randint
+from numpy.random import uniform, randint, normal, random, choice
 from copy import deepcopy
 from mealpy.root import Root
 
@@ -79,6 +79,95 @@ class BaseEO(Root):
                 pop[i] = [temp, fit]
 
             g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
+            self.loss_train.append(g_best[self.ID_FIT])
+            if self.verbose:
+                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
+        self.solution = g_best
+        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+
+
+class ModifiedEO(BaseEO):
+    """
+        Original version of: Modified Equilibrium Optimizer (MEO)
+            (An efficient equilibrium optimizer with mutation strategy for numerical optimization)
+    Link:
+        https://doi.org/10.1016/j.asoc.2020.106542
+    """
+
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True, epoch=750, pop_size=100):
+        BaseEO.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+
+    def _make_equilibrium_pool__(self, list_equilibrium=None):
+        pos_list = [item[self.ID_POS] for item in list_equilibrium]
+        pos_mean = mean(pos_list, axis=0)
+        fit = self.get_fitness_position(pos_mean)
+        list_equilibrium.append([pos_mean, fit])
+        return list_equilibrium
+
+    def train(self):
+        # Initialization
+        pop_len = int(self.pop_size/3)
+        pop = [self.create_solution() for _ in range(self.pop_size)]
+
+        # ---------------- Memory saving-------------------
+        # make equilibrium pool
+        pop_sorted = sorted(pop, key=lambda item: item[self.ID_FIT])
+        c_eq_list = deepcopy(pop_sorted[:4])
+        g_best = deepcopy(c_eq_list[0])
+        c_pool = self._make_equilibrium_pool__(c_eq_list)
+
+        for epoch in range(0, self.epoch):
+            # Eq. 5
+            t = (1 - epoch / self.epoch) ** (self.a2 * epoch / self.epoch)
+
+            for i in range(0, self.pop_size):
+                lamda = uniform(0, 1, self.problem_size)  # lambda in Eq. 4
+                r = uniform(0, 1, self.problem_size)  # r in Eq. 6
+                c_eq = c_pool[randint(0, len(c_pool))][self.ID_POS]  # random selection 1 of candidate from the pool
+                f = self.a1 * sign(r - 0.5) * (exp(-lamda * t) - 1.0)  # Eq. 4
+                r1 = uniform()
+                r2 = uniform()
+                gcp = 0.5 * r1 * ones(self.problem_size) * (r2 >= self.GP)  # Eq. 10
+                g0 = gcp * (c_eq - lamda * pop[i][self.ID_POS])  # Eq. 9
+                g = g0 * f
+                pos_new = c_eq + (pop[i][self.ID_POS] - c_eq) * f + (g * self.V / lamda) * (1.0 - f)  # Eq. 2
+                fit = self.get_fitness_position(pos_new)
+                pop[i] = [pos_new, fit]
+
+            ## Sort the updated population based on fitness
+            pop_sorted = sorted(pop, key=lambda item: item[self.ID_FIT])
+            pop_s1 = pop_sorted[:pop_len]
+            pop_s2 = deepcopy(pop_s1)
+            pop_s3 = deepcopy(pop_s1)
+
+            ## Mutation scheme
+            for i in range(0, pop_len):
+                pos_new = pop_s1[i][self.ID_POS] * (1 + normal(0, 1, self.problem_size))        # Eq. 12
+                fit = self.get_fitness_position(pos_new)
+                pop_s2[i] = [pos_new, fit]
+
+            ## Search Mechanism
+            pos_s1_list = [item[self.ID_POS] for item in pop_s1]
+            pos_s1_mean = mean(pos_s1_list, axis=0)
+            for i in range(0, pop_len):
+                pos_new = (c_pool[0][self.ID_POS] - pos_s1_mean) - random() * (self.lb + random() * (self.ub - self.lb))
+                fit = self.get_fitness_position(pos_new)
+                pop_s3[i] = [pos_new, fit]
+
+            ## Construct a new population
+            pop = pop_s1 + pop_s2 + pop_s3
+            temp = self.pop_size - len(pop)
+            idx_selected = choice(range(0, len(c_pool)), temp, replace=False)
+            for i in range(0, temp):
+                pop.append(c_pool[idx_selected[i]])
+
+            # Update the equilibrium pool
+            pop_sorted = sorted(pop, key=lambda item: item[self.ID_FIT])
+            c_eq_list = deepcopy(pop_sorted[:4])
+            c_pool = self._make_equilibrium_pool__(c_eq_list)
+
+            if pop_sorted[0][self.ID_FIT] < g_best[self.ID_FIT]:
+                g_best = deepcopy(pop_sorted[0])
             self.loss_train.append(g_best[self.ID_FIT])
             if self.verbose:
                 print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
