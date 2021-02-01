e7fec27cab9ed777e7e70606d80f552633630f65
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-02-01 17:19:07 +0700

    Update test case for FOA

---
+++ examples/swarm_based/run_test_FOA.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu" at 14:15, 16/11/2020                                                               %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
+#       Github:     https://github.com/thieu1995                                                        %
+# ------------------------------------------------------------------------------------------------------%
+
+from opfunu.cec_basic.cec2014_nobias import *
+from mealpy.swarm_based.FOA import BaseFOA, OriginalFOA, WFOA
+
+## Setting parameters
+obj_func = F20
+# lb = [-15, -10, -3, -15, -10, -3, -15, -10, -3, -15, -10, -3, -15, -10, -3]
+# ub = [15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3]
+lb = [-100]
+ub = [100]
+problem_size = 30
+batch_size = 25
+verbose = True
+epoch = 1000
+pop_size = 50
+
+md1 = BaseFOA(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
+
+md1 = WFOA(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
+
+md1 = WFOA(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
