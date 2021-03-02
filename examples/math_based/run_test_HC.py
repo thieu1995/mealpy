b6c81f0fa1c7402174ed71e4c35c3d60ad4ac2b6
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-03-02 11:37:23 +0700

    Add Hill Climbing (HC) to math-based group

---
+++ examples/math_based/run_test_HC.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu" at 10:29, 02/03/2021                                                               %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
+#       Github:     https://github.com/thieu1995                                                        %
+# ------------------------------------------------------------------------------------------------------%
+
+from opfunu.cec_basic.cec2014_nobias import *
+from mealpy.math_based.HC import OriginalHC, BaseHC
+
+## Setting parameters
+obj_func = F3
+# lb = [-15, -10, -3, -15, -10, -3, -15, -10, -3, -15, -10, -3, -15, -10, -3]
+# ub = [15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3, 15, 10, 3]
+lb = [-100]
+ub = [100]
+problem_size = 2000
+batch_size = 25
+verbose = True
+epoch = 1000
+pop_size = 50
+
+md1 = OriginalHC(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
+
+md1 = BaseHC(obj_func, lb, ub, problem_size, batch_size, verbose, epoch, pop_size)
+best_pos1, best_fit1, list_loss1 = md1.train()
+print(md1.solution[0])
+print(md1.solution[1])
+print(md1.loss_train)
