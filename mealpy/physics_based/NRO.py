7005c5da880f336436292b3b3b49c21526d5e3f5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-03-12 12:16:43 +0700

    Update parameters order of NRO algorithm in physics-based group

--- mealpy/physics_based/NRO.py
+++ mealpy/physics_based/NRO.py
@@ -4,7 +4,7 @@
 #                                                                                                       %
 #       Email:      nguyenthieu2102@gmail.com                                                           %
 #       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                  %
+#       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
 from numpy import sin, abs, sqrt, pi, subtract, array, exp
@@ -24,8 +24,8 @@ class BaseNRO(Root):
         Nuclear Reaction Optimization: A novel and powerful physics-based algorithm for global optimization
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True, epoch=750, pop_size=100):
-        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
+        Root.__init__(self, obj_func, lb, ub, verbose, kwargs)
         self.epoch = epoch
         self.pop_size = pop_size
 
