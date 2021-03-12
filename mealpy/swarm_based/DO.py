147fdbcdef38c5c203682f7808c1b5ebc625afb0
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-03-12 14:57:28 +0700

    Update parameters order of DO algorithm in swarm-based group

--- mealpy/swarm_based/DO.py
+++ mealpy/swarm_based/DO.py
@@ -21,8 +21,8 @@ class BaseDO(Root):
     Link:
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True, epoch=750, pop_size=100):
-        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
+        Root.__init__(self, obj_func, lb, ub, verbose, kwargs=kwargs)
         self.epoch = epoch
         self.pop_size = pop_size
 
