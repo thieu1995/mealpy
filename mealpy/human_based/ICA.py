6c447fb82e72ba5fbab489b77f49dc6a59793298
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-03-12 11:15:09 +0700

    Update parameters order of ICA algorithm in human-based group

--- mealpy/human_based/ICA.py
+++ mealpy/human_based/ICA.py
@@ -15,14 +15,14 @@ from mealpy.root import Root
 
 class BaseICA(Root):
     """
-    The original version of: Imperialist Competitive Algorithm (ICA)
+        The original version of: Imperialist Competitive Algorithm (ICA)
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=50, batch_size=10, verbose=True,
-                 epoch=750, pop_size=100, empire_count=5, selection_pressure=1, assimilation_coeff=1.5,
+    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100,
+                 empire_count=5, selection_pressure=1, assimilation_coeff=1.5,
                  revolution_prob=0.05, revolution_rate=0.1, revolution_step_size=0.1,
-                 revolution_step_size_damp=0.99, zeta=0.1):
-        Root.__init__(self, obj_func, lb, ub, problem_size, batch_size, verbose)
+                 revolution_step_size_damp=0.99, zeta=0.1, **kwargs):
+        Root.__init__(self, obj_func, lb, ub, verbose, kwargs)
         self.epoch = epoch
         self.pop_size = pop_size  # n: pop_size, m: clusters
         self.empire_count = empire_count                # Number of Empires (also Imperialists)
