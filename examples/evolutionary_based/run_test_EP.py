1bfddf7ce3baf7867713595da5ce0f596feeef13
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-10-21 14:07:31 +0700

    Restructure EP optimizer in Evolutionary-based group

--- examples/evolutionary_based/run_test_EP.py
+++ examples/evolutionary_based/run_test_EP.py
@@ -8,54 +8,196 @@
 #-------------------------------------------------------------------------------------------------------%
 
 from opfunu.cec_basic.cec2014_nobias import *
-from mealpy.evolutionary_based.EP import BaseEP, LevyEP
+from mealpy.evolutionary_based import EP
+from mealpy.problem import Problem
+from mealpy.utils.termination import Termination
 
-## Setting parameters
-obj_func = F5
-verbose = False
-epoch = 10
-pop_size = 50
+# Setting parameters
 
 # A - Different way to provide lower bound and upper bound. Here are some examples:
 
-## 1. When you have different lower bound and upper bound for each parameters
-lb1 = [-3, -5, 1, -10]
-ub1 = [5, 10, 100, 30]
+## A1. When you have different lower bound and upper bound for each parameters
+problem_dict1 = {
+    "obj_func": F5,
+    "lb": [-3, -5, 1, -10, ],
+    "ub": [5, 10, 100, 30, ],
+    "minmax": "min",
+    "verbose": True,
+}
+problem_obj1 = Problem(problem_dict1)
 
-md1 = BaseEP(obj_func, lb1, ub1, verbose, epoch, pop_size)
-best_pos1, best_fit1, list_loss1 = md1.train()
-print(md1.solution[1])
-
-## 2. When you have same lower bound and upper bound for each parameters, then you can use:
-##      + int or float: then you need to specify your problem size (number of dimensions)
-problemSize = 10
-lb2 = -5
-ub2 = 10
-md2 = BaseEP(obj_func, lb2, ub2, verbose, epoch, pop_size, problem_size=problemSize)  # Remember the keyword "problem_size"
-best_pos1, best_fit1, list_loss1 = md2.train()
-print(md2.solution[1])
+## A2. When you have same lower bound and upper bound for each parameters, then you can use:
+##      + int or float: then you need to specify your problem size / number of dimensions (n_dims)
+problem_dict2 = {
+    "obj_func": F5,
+    "lb": -10,
+    "ub": 30,
+    "minmax": "min",
+    "verbose": True,
+    "n_dims": 30,  # Remember the keyword "n_dims"
+}
+problem_obj2 = Problem(problem_dict2)
 
 ##      + array: 2 ways
-lb3 = [-5]
-ub3 = [10]
-md3 = BaseEP(obj_func, lb3, ub3, verbose, epoch, pop_size, problem_size=problemSize)  # Remember the keyword "problem_size"
-best_pos1, best_fit1, list_loss1 = md3.train()
-print(md3.solution[1])
+problem_dict3 = {
+    "obj_func": F5,
+    "lb": [-5],
+    "ub": [10],
+    "minmax": "min",
+    "verbose": True,
+    "n_dims": 30,  # Remember the keyword "n_dims"
+}
+problem_obj3 = Problem(problem_dict3)
 
-lb4 = [-5] * problemSize
-ub4 = [10] * problemSize
-md4 = BaseEP(obj_func, lb4, ub4, verbose, epoch, pop_size)  # No need the keyword "problem_size"
-best_pos1, best_fit1, list_loss1 = md4.train()
-print(md4.solution[1])
+n_dims = 100
+problem_dict4 = {
+    "obj_func": F5,
+    "lb": [-5] * n_dims,
+    "ub": [10] * n_dims,
+    "minmax": "min",
+    "verbose": True,
+}
 
-# B - Test with algorithm has batch size idea
+## Run the algorithm
+
+### Your parameter problem can be an instane of Problem class or just dict like above
+model1 = EP.BaseEP(problem_obj1, epoch=100, pop_size=50, bout_size=0.05)
+model1.solve()
+
+model2 = EP.BaseEP(problem_dict4, epoch=100, pop_size=50, bout_size=0.05)
+model2.solve()
+
+# B - Test with different Stopping Condition (Termination) by creating an Termination object
+
+## There are 4 termination cases:
+### 1. FE (Number of Function Evaluation)
+### 2. MG (Maximum Generations / Epochs): This is default in all algorithms
+### 3. ES (Early Stopping): Same idea in training neural network (If the global best solution not better an epsilon
+###     after K epoch then stop the program
+### 4. TB (Time Bound): You just want your algorithm run in K seconds. Especially when comparing different algorithms.
+
+termination_dict1 = {
+    "mode": "FE",
+    "quantity": 100000  # 100000 number of function evaluation
+}
+termination_dict2 = {  # When creating this object, it will override the default epoch you define in your model
+    "mode": "MG",
+    "quantity": 1000  # 1000 epochs
+}
+termination_dict3 = {
+    "mode": "ES",
+    "quantity": 30  # after 30 epochs, if the global best doesn't improve then we stop the program
+}
+termination_dict4 = {
+    "mode": "ES",
+    "quantity": 60  # 60 seconds = 1 minute to run this algorithm only
+}
+termination_obj1 = Termination(termination_dict1)
+termination_obj2 = Termination(termination_dict2)
+termination_obj3 = Termination(termination_dict3)
+termination_obj4 = Termination(termination_dict4)
+
+### Pass your termination object into your model as a addtional parameter with the keyword "termination"
+model3 = EP.BaseEP(problem_dict1, epoch=100, pop_size=50, bout_size=0.05, termination=termination_obj1)
+model3.solve()
+### Remember you can't pass termination dict, it only accept the Termination object
 
 
-## C - Test with different variants of this algorithm
+# C - Test with different training mode (sequential, threading parallelization, processing parallelization)
 
-md1 = LevyEP(obj_func, lb4, ub4, verbose, epoch, pop_size)
-best_pos1, best_fit1, list_loss1 = md1.train()
-print(md1.solution[0])
-print(md1.solution[1])
-print(md1.loss_train)
+## + sequential: Default for all algorithm (single core)
+## + thread: create multiple threading depend on your chip
+## + process: create multiple cores to run your algorithm.
+## Note: For windows, your program need the if __nam__ == "__main__" condition to avoid creating infinite processors
 
+model5 = EP.BaseEP(problem_dict1, epoch=100, pop_size=50, bout_size=0.05)
+model5.solve(mode='sequential')  # Default
+
+model6 = EP.BaseEP(problem_dict1, epoch=100, pop_size=50, bout_size=0.05)
+model6.solve(mode='thread')
+
+if __name__ == "__main__":
+    model7 = EP.BaseEP(problem_dict1, epoch=100, pop_size=50, pc=0.85, pm=0.15,
+                       p_local=0.5, max_local_gens=20, bits_per_param=16)
+    model7.solve(mode='process')
+
+# D - Drawing all available figures
+
+## There are 8 different figures for each algorithm.
+## D.1: Based on fitness value:
+##      1. Global best fitness chart
+##      2. Local best fitness chart
+## D.2: Based on objective value:
+##      3. Global objective chart
+##      4. Local objective chart
+## D.3: Based on runtime value (runtime for each epoch)
+##      5. Runtime chart
+## D.4: Based on exploration verse exploration value
+##      6. Exploration vs Exploitation chart
+## D.5: Based on diversity of population
+##      7. Diversity chart
+## D.6: Based on trajectory value (1D, 2D only)
+##      8. Trajectory chart
+
+model8 = EP.BaseEP(problem_dict1, epoch=100, pop_size=50, bout_size=0.05)
+model8.solve()
+
+## You can access them all via object "history" like this:
+model8.history.save_global_objectives_chart(filename="hello/goc")
+model8.history.save_local_objectives_chart(filename="hello/loc")
+model8.history.save_global_best_fitness_chart(filename="hello/gbfc")
+model8.history.save_local_best_fitness_chart(filename="hello/lbfc")
+model8.history.save_runtime_chart(filename="hello/rtc")
+model8.history.save_exploration_exploitation_chart(filename="hello/eec")
+model8.history.save_diversity_chart(filename="hello/dc")
+model8.history.save_trajectory_chart(list_agent_idx=[3, 5], list_dimensions=[3], filename="hello/tc")
+
+
+# E - Handling Multi-Objective function and Constraint Method
+
+## To handling Multi-Objective, mealpy is using weighting method which converting multiple objectives to a single target (fitness value)
+
+## Define your objective function, your constraint
+def obj_function(solution):
+    f1 = solution[0] ** 2
+    f2 = ((2 * solution[1]) / 5) ** 2
+    f3 = 0
+    for i in range(3, len(solution)):
+        f3 += (1 + solution[i] ** 2) ** 0.5
+    return [f1, f2, f3]
+
+
+## Define your objective weights. For example:
+###  f1: 50% important
+###  f2: 20% important
+###  f3: 30% important
+### Then weight = [0.5, 0.2, 0.3] ==> Fitness value = 0.5*f1 + 0.2*f2 + 0.3*f3
+### Default weight = [1, 1, 1]
+
+problem_dict9 = {
+    "obj_func": obj_function,
+    "lb": [-3, -5, 1, -10, ],
+    "ub": [5, 10, 100, 30, ],
+    "minmax": "min",
+    "verbose": True,
+    "obj_weight": [0.5, 0.2, 0.3]  # Remember the keyword "obj_weight"
+}
+problem_obj9 = Problem(problem_dict9)
+model9 = EP.BaseEP(problem_obj9, epoch=100, pop_size=50, bout_size=0.05)
+model9.solve()
+
+## To access the results, you can get the results by solve() method
+position, fitness_value = model9.solve()
+
+## To get all fitness value and all objective values, get it via "solution" attribute
+## A agent / solution format [position, [fitness, [obj1, obj2, ..., obj_n]]]
+position = model9.solution[0]
+fitness_value = model9.solution[1][0]
+objective_values = model9.solution[1][1]
+
+# F - Test with different variants of this algorithm
+
+
+problem_obj10 = Problem(problem_dict9)
+model10 = EP.LevyEP(problem_obj9, epoch=100, pop_size=50, bout_size=0.05)
+model10.solve()
--- mealpy/evolutionary_based/EP.py
+++ mealpy/evolutionary_based/EP.py
@@ -7,13 +7,12 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-from numpy.random import uniform, normal, choice, randint
-from numpy import abs
-from copy import deepcopy
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseEP(Root):
+class BaseEP(Optimizer):
     """
         The original version of: Evolutionary Programming (EP)
             (Clever Algorithms: Nature-Inspired Programming Recipes - Evolutionary Programming)
@@ -25,54 +24,89 @@ class BaseEP(Root):
     ID_STR = 2  # strategy
     ID_WIN = 3
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, bout_size=0.05, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, bout_size=0.05, **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size (miu in the paper), default = 100
+            n_child (float/int): if float number --> percentage of child agents, int --> number of child agents
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = True
+
         self.epoch = epoch
         self.pop_size = pop_size
-        if bout_size < 1:                               # Number of tried with tournament selection (5% of pop_size)
+        if bout_size < 1:                   # Number of tried with tournament selection (5% of pop_size)
             self.bout_size = int(bout_size * self.pop_size)
         else:
             self.bout_size = int(bout_size)
-        self.distance = 0.05 * (self.ub - self.lb)
+        self.distance = 0.05 * (self.problem.ub - self.problem.lb)
 
-    def create_solution(self, minmax=0):
-        pos = uniform(self.lb, self.ub)
-        fit = self.get_fitness_position(pos)
-        strategy = uniform(0, self.distance, self.problem_size)
+    def create_solution(self):
+        """
+        Returns:
+            The position position with 2 element: index of position/location and index of fitness wrapper
+            The general format: [position, [target, [obj1, obj2, ...]], strategy, times_win]
+
+        ## To get the position, fitness wrapper, target and obj list
+        ##      A[self.ID_POS]                  --> Return: position
+        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
+        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+        """
+        position = np.random.uniform(self.problem.lb, self.problem.ub)
+        fitness = self.get_fitness_position(position=position)
+        strategy = np.random.uniform(0, self.distance, self.problem.n_dims)
         times_win = 0
-        return [pos, fit, strategy, times_win]
+        return [position, fitness, strategy, times_win]
 
-    def _mutate_solution__(self, solution=None):
-        child = solution[self.ID_POS] + solution[self.ID_STR] * normal(0, 1.0, self.problem_size)
+    def create_child(self, agent_i):
+        child = agent_i[self.ID_POS] + agent_i[self.ID_STR] * np.random.normal(0, 1.0, self.problem.n_dims)
         child = self.amend_position_faster(child)
         fit = self.get_fitness_position(child)
-        s_old = solution[self.ID_STR] + normal(0, 1.0, self.problem_size) * abs(solution[self.ID_STR]) ** 0.5
+        s_old = agent_i[self.ID_STR] + np.random.normal(0, 1.0, self.problem.n_dims) * np.abs(agent_i[self.ID_STR]) ** 0.5
         return [child, fit, s_old, 0]
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-        for epoch in range(0, self.epoch):
-            children = [self._mutate_solution__(pop[i]) for i in range(0, self.pop_size)]
-            # Update the global best
-            children, g_best = self.update_sorted_population_and_global_best_solution(children, self.ID_MIN_PROB, g_best)
+        Returns:
+            [position, fitness value]
+        """
+        pop_copy = pop.copy()
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        else:
+            children = [self.create_child(agent) for agent in pop_copy]
 
-            pop = children + pop
-            for i in range(0, len(pop)):
-                ## Tournament winner (Tried with bout_size times)
-                for idx in range(0, self.bout_size):
-                    rand_idx = randint(0, len(pop))
-                    if pop[i][self.ID_FIT] < pop[rand_idx][self.ID_FIT]:
+        # Update the global best
+        children, g_best = self.update_global_best_solution(children)
+        pop = children + pop
+        for i in range(0, len(pop)):
+            ## Tournament winner (Tried with bout_size times)
+            for idx in range(0, self.bout_size):
+                rand_idx = np.random.randint(0, len(pop))
+                if self.problem.minmax == "min":
+                    if pop[i][self.ID_FIT][self.ID_TAR] < pop[rand_idx][self.ID_FIT][self.ID_TAR]:
                         pop[i][self.ID_WIN] += 1
-
-            pop = sorted(pop, key=lambda item: item[self.ID_WIN], reverse=True)
-            pop = pop[:self.pop_size]
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+                else:
+                    if pop[i][self.ID_FIT][self.ID_TAR] > pop[rand_idx][self.ID_FIT][self.ID_TAR]:
+                        pop[rand_idx][self.ID_WIN] += 1
+        pop = sorted(pop, key=lambda item: item[self.ID_WIN], reverse=True)
+        pop = pop[:self.pop_size]
+        return pop
 
 
 class LevyEP(BaseEP):
@@ -83,45 +117,68 @@ class LevyEP(BaseEP):
             + Change the flow and add more equations
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, bout_size=0.05, **kwargs):
-        BaseEP.__init__(self, obj_func, lb, ub, verbose, epoch, pop_size, bout_size, kwargs=kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, bout_size=0.05, **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size (miu in the paper), default = 100
+            n_child (float/int): if float number --> percentage of child agents, int --> number of child agents
+        """
+        super().__init__(problem, epoch, pop_size, bout_size, **kwargs)
+        self.nfe_per_epoch = 2 * pop_size
+        self.sort_flag = True
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-        for epoch in range(0, self.epoch):
-            children = [self._mutate_solution__(pop[i]) for i in range(0, self.pop_size)]
-            # Update the global best
-            children, g_best = self.update_sorted_population_and_global_best_solution(children, self.ID_MIN_PROB, g_best)
+        Returns:
+            [position, fitness value]
+        """
+        pop_copy = pop.copy()
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        else:
+            children = [self.create_child(agent) for agent in pop_copy]
 
-            pop = children + pop
-            for i in range(0, len(pop)):
-                ## Tournament winner (Tried with bout_size times)
-                for idx in range(0, self.bout_size):
-                    rand_idx = randint(0, len(pop))
-                    if pop[i][self.ID_FIT] < pop[rand_idx][self.ID_FIT]:
+        # Update the global best
+        children, g_best = self.update_global_best_solution(children)
+
+        pop = children + pop
+        for i in range(0, len(pop)):
+            ## Tournament winner (Tried with bout_size times)
+            for idx in range(0, self.bout_size):
+                rand_idx = np.random.randint(0, len(pop))
+                if self.problem.minmax == "min":
+                    if pop[i][self.ID_FIT][self.ID_TAR] < pop[rand_idx][self.ID_FIT][self.ID_TAR]:
                         pop[i][self.ID_WIN] += 1
+                else:
+                    if pop[i][self.ID_FIT][self.ID_TAR] > pop[rand_idx][self.ID_FIT][self.ID_TAR]:
+                        pop[rand_idx][self.ID_WIN] += 1
 
-            ## Keep the top population, but 50% of left population will make a comeback an take the good position
-            pop = sorted(pop, key=lambda item: item[self.ID_WIN], reverse=True)
-            pop_left = deepcopy(pop[self.pop_size:])
-            pop = pop[:self.pop_size]
+        ## Keep the top population, but 50% of left population will make a comeback an take the good position
+        pop = sorted(pop, key=lambda agent: agent[self.ID_WIN], reverse=True)
+        pop = pop[:self.pop_size]
+        pop_left = pop[self.pop_size:].copy()
 
-            ## Choice random 50% of population left
-            pop_comeback = []
-            idx_list = choice(range(0, len(pop_left)), int(0.5*len(pop_left)), replace=False)
-            for idx in idx_list:
-                pos_new = self.levy_flight(epoch, pop_left[idx][self.ID_POS], g_best[self.ID_POS])
-                fit = self.get_fitness_position(pos_new)
-                strategy = self.distance = 0.05 * (self.ub - self.lb)
-                pop_comeback.append([pos_new, fit, strategy, 0])
-
-            pop = pop + pop_comeback
-            pop, g_best = self.update_sorted_population_and_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-            pop = pop[:self.pop_size]
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+        ## Choice random 50% of population left
+        pop_comeback = []
+        idx_list = np.random.choice(range(0, len(pop_left)), int(0.5 * len(pop_left)), replace=False)
+        for idx in idx_list:
+            pos_new = self.levy_flight(epoch, pop_left[idx][self.ID_POS], g_best[self.ID_POS])
+            fit = self.get_fitness_position(pos_new)
+            strategy = self.distance = 0.05 * (self.problem.ub - self.problem.lb)
+            pop_comeback.append([pos_new, fit, strategy, 0])
+        pop = pop + pop_comeback
+        pop = sorted(pop, key=lambda agent: agent[self.ID_FIT][self.ID_TAR])
+        return pop[:self.pop_size]
