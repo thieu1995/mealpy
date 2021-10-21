21a15f58d03ae51783ce16784451000ff4a50025
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-10-21 11:21:37 +0700

    Restructure ES algorithm in evolutionary-based group

--- examples/bio_based/run_test_SMA2.py
+++ examples/bio_based/run_test_SMA2.py
@@ -12,7 +12,7 @@ from mealpy.bio_based import SMA
 from mealpy.swarm_based import HGS
 from mealpy.human_based import GSKA
 from mealpy.physics_based import EO
-from mealpy.evolutionary_based import MA, FPA
+from mealpy.evolutionary_based import MA, FPA, ES
 from mealpy.problem import Problem
 from mealpy.utils.termination import Termination
 
@@ -32,5 +32,5 @@ problem_dict1 = {
 if __name__ == "__main__":
     problem_obj1 = Problem(problem_dict1)
     ### Your parameter problem can be an instane of Problem class or just dict like above
-    model1 = FPA.BaseFPA(problem_obj1, epoch=100, pop_size=50)
-    model1.solve(mode="sequential")
+    model1 = ES.BaseES(problem_obj1, epoch=100, pop_size=50)
+    model1.solve(mode="thread")
--- mealpy/evolutionary_based/ES.py
+++ mealpy/evolutionary_based/ES.py
@@ -7,8 +7,9 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
+import concurrent.futures as parallel
+from functools import partial
 import numpy as np
-import time
 from mealpy.optimizer import Optimizer
 
 
@@ -23,63 +24,77 @@ class BaseES(Optimizer):
     ID_FIT = 1
     ID_STR = 2      # strategy
 
-    def __init__(self, problem: dict, epoch=1000, pop_size=100, n_child=0.75):
+    def __init__(self, problem, epoch=10000, pop_size=100, n_child=0.75, **kwargs):
         """
-        pop_size = miu, n_child = lamda
         Args:
-            problem (dict): a dictionary of your problem
-            epoch (int): maximum number of iterations, default = 1000
-            pop_size (int): number of population size, default = 100
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size (miu in the paper), default = 100
             n_child (float/int): if float number --> percentage of child agents, int --> number of child agents
         """
-        super().__init__(problem)
+        super().__init__(problem, kwargs)
         self.epoch = epoch
-        self.pop_size = pop_size        # miu
-        if n_child < 1:                 # lamda, 75% of pop_size
+        self.pop_size = pop_size
+        if n_child < 1:             # lamda, 75% of pop_size
             self.n_child = int(n_child * self.pop_size)
         else:
             self.n_child = int(n_child)
-        self.distance = 0.05 * (self.ub - self.lb)
+        self.distance = 0.05 * (self.problem.ub - self.problem.lb)
 
-    def create_solution(self, minmax=0):
-        pos = np.random.uniform(self.lb, self.ub)
-        fit = self.get_fitness_position(pos)
+        self.nfe_per_epoch = pop_size + self.n_child
+        self.sort_flag = True
+
+    def create_solution(self):
+        """
+        Returns:
+            The position position with 2 element: index of position/location and index of fitness wrapper
+            The general format: [position, [target, [obj1, obj2, ...]]]
+
+        ## To get the position, fitness wrapper, target and obj list
+        ##      A[self.ID_POS]                  --> Return: position
+        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
+        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+        """
+        position = np.random.uniform(self.problem.lb, self.problem.ub)
+        fitness = self.get_fitness_position(position=position)
         strategy = np.random.uniform(0, self.distance)
-        return [pos, fit, strategy]
+        return [position, fitness, strategy]
 
-    def _mutate_solution_(self, solution=None):
-        pos_new = solution[self.ID_POS] + solution[self.ID_STR] * np.random.normal(0, 1.0, self.problem_size)
+    def create_child(self, agent_i):
+        pos_new = agent_i[self.ID_POS] + agent_i[self.ID_STR] * np.random.normal(0, 1.0, self.problem.n_dims)
         pos_new = self.amend_position_faster(pos_new)
         fit_new = self.get_fitness_position(pos_new)
-        tau = np.sqrt(2.0 * self.problem_size) ** -1.0
-        tau_p = np.sqrt(2.0 * np.sqrt(self.problem_size)) ** -1.0
-        strategy = np.exp(tau_p * np.random.normal(0, 1.0, self.problem_size) + tau * np.random.normal(0, 1.0, self.problem_size))
+        tau = np.sqrt(2.0 * self.problem.n_dims) ** -1.0
+        tau_p = np.sqrt(2.0 * np.sqrt(self.problem.n_dims)) ** -1.0
+        strategy = np.exp(tau_p * np.random.normal(0, 1.0, self.problem.n_dims) + tau * np.random.normal(0, 1.0, self.problem.n_dims))
         return [pos_new, fit_new, strategy]
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop, g_best = self.get_global_best_solution(pop)  # We sort the population
-        self.history_list_g_best = [g_best]
-        self.history_list_c_best = self.history_list_g_best.copy()
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-        for epoch in range(0, self.epoch):
-            time_start = time.time()
+        Returns:
+            [position, fitness value]
+        """
+        pop_copy = pop[:self.n_child].copy()
 
-            children = [self._mutate_solution_(pop[i]) for i in range(0, self.n_child)]
-            pop = pop + children
-            pop = self.update_global_best_solution(pop)
-            pop = pop[:self.pop_size]
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            children = [x for x in pop_child]
+        else:
+            children = [self.create_child(agent) for agent in pop_copy]
 
-            ## Additional information for the framework
-            time_start = time.time() - time_start
-            self.history_list_epoch_time.append(time_start)
-            self.print_epoch(epoch + 1, time_start)
-            self.history_list_pop.append(pop.copy())
-
-        ## Additional information for the framework
-        self.solution = self.history_list_g_best[-1]
-        self.save_data()
-        return self.solution[self.ID_POS], self.solution[self.ID_FIT][self.ID_TAR]
+        pop = sorted(pop + children, key=lambda agent: agent[self.ID_FIT][self.ID_TAR])
+        return pop[:self.pop_size]
 
 
 class LevyES(BaseES):
@@ -89,48 +104,59 @@ class LevyES(BaseES):
             + Applied levy-flight
             + Change the flow of algorithm
     """
-    ID_POS = 0
-    ID_FIT = 1
-    ID_STR = 2  # strategy
 
-    def __init__(self, problem: dict, epoch=750, pop_size=100, n_child=0.75):
-        BaseES.__init__(self, problem, epoch, pop_size, n_child)
+    def __init__(self, problem, epoch=10000, pop_size=100, n_child=0.75, **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size (miu in the paper), default = 100
+            n_child (float/int): if float number --> percentage of child agents, int --> number of child agents
+        """
+        super().__init__(problem, epoch, pop_size, n_child, **kwargs)
+        self.nfe_per_epoch = 2 * pop_size
 
-    def __create_levy_population__(self, pop=None):
-        children = []
-        for agent in pop:
-            levy = self.get_step_size_levy_flight(multiplier=0.01, case=-1)
-            pos_new = np.random.uniform(self.lb, self.ub) * levy * (agent[self.ID_POS] - self.history_list_g_best[-1][self.ID_POS])
-            fit_new = self.get_fitness_position(pos_new)
-            tau = np.sqrt(2.0 * self.problem_size) ** -1.0
-            tau_p = np.sqrt(2.0 * np.sqrt(self.problem_size)) ** -1.0
-            stdevs = np.array([np.exp(tau_p * np.random.normal(0, 1.0) + tau * np.random.normal(0, 1.0)) for _ in range(self.problem_size)])
-            children.append([pos_new, fit_new, stdevs])
-        return children
+    def create_levy_child(self, agent_i, g_best):
+        levy = self.get_levy_flight_step(multiplier=0.01, case=-1)
+        pos_new = agent_i[self.ID_POS] + np.random.uniform(self.problem.lb, self.problem.ub) * \
+                  levy * (agent_i[self.ID_POS] - g_best[self.ID_POS])
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        tau = np.sqrt(2.0 * self.problem.n_dims) ** -1.0
+        tau_p = np.sqrt(2.0 * np.sqrt(self.problem.n_dims)) ** -1.0
+        stdevs = np.array([np.exp(tau_p * np.random.normal(0, 1.0) + tau * np.random.normal(0, 1.0)) for _ in range(self.problem.n_dims)])
+        return [pos_new, fit_new, stdevs]
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop, g_best = self.get_global_best_solution(pop)  # We sort the population
-        self.history_list_g_best = [g_best]
-        self.history_list_c_best = self.history_list_g_best.copy()
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-        for epoch in range(0, self.epoch):
-            time_start = time.time()
+        Returns:
+            [position, fitness value]
+        """
+        pop_copy = pop[:self.n_child].copy()
+        pop_copy_levy = pop[self.n_child:].copy()
 
-            children = [self._mutate_solution_(pop[i]) for i in range(0, self.n_child)]
-            children_levy = self.__create_levy_population__(pop[self.n_child:])
-            pop = pop + children + children_levy
-            pop = self.update_global_best_solution(pop)
-            pop = pop[:self.pop_size]
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child_levy = executor.map(partial(self.create_levy_child, g_best=g_best), pop_copy_levy)
+            children = [x for x in pop_child] + [x for x in pop_child_levy]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(self.create_child, pop_copy)
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child_levy = executor.map(partial(self.create_levy_child, g_best=g_best), pop_copy_levy)
+            children = [x for x in pop_child] + [x for x in pop_child_levy]
+        else:
+            pop_child = [self.create_child(agent) for agent in pop_copy]
+            pop_child_levy = [self.create_levy_child(agent, g_best) for agent in pop_copy_levy]
+            children = pop_child + pop_child_levy
 
-            ## Additional information for the framework
-            time_start = time.time() - time_start
-            self.history_list_epoch_time.append(time_start)
-            self.print_epoch(epoch + 1, time_start)
-            self.history_list_pop.append(pop.copy())
-
-            ## Additional information for the framework
-        self.solution = self.history_list_g_best[-1]
-        self.save_data()
-        return self.solution[self.ID_POS], self.solution[self.ID_FIT][self.ID_TAR]
+        pop = sorted(pop + children, key=lambda agent: agent[self.ID_FIT][self.ID_TAR])
+        return pop[:self.pop_size]
 
