e02f82b0bd4f7bc26376f9c83089e3d0fd1b51f2
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-10-29 11:37:05 +0700

    Restructure SalpSO algorithm in swarm-based group

--- mealpy/swarm_based/SalpSO.py
+++ mealpy/swarm_based/SalpSO.py
@@ -7,58 +7,79 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-from numpy.random import random
-from numpy import exp, where
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseSalpSO(Root):
+class BaseSalpSO(Optimizer):
     """
     The original version of: Salp Swarm Optimization (SalpSO)
     Link:
         https://doi.org/10.1016/j.advengsoft.2017.07.002
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = self.pop_size
+        self.sort_flag = True
+
         self.epoch = epoch
         self.pop_size = pop_size
 
-    def train(self):
-        # Initial population and find the global best solution
-        pop = [self.create_solution() for _ in range(0, self.pop_size)]
-        pop, g_best = self.get_sorted_pop_and_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+    def create_child(self, idx, pop, g_best, c1):
+        if idx < self.pop_size / 2:
+            c2_list = np.random.random(self.problem.n_dims)
+            c3_list = np.random.random(self.problem.n_dims)
+            pos_new_1 = g_best[self.ID_POS] + c1 * ((self.problem.ub - self.problem.lb) * c2_list + self.problem.lb)
+            pos_new_2 = g_best[self.ID_POS] - c1 * ((self.problem.ub - self.problem.lb) * c2_list + self.problem.lb)
+            pos_new = np.where(c3_list < 0.5, pos_new_1, pos_new_2)
+        else:
+            # Eq. (3.4) in the paper
+            pos_new = (pop[idx][self.ID_POS] + pop[idx - 1][self.ID_POS]) / 2
 
-        # Main loop
-        for epoch in range(0, self.epoch):
+        # Check if salps go out of the search space and bring it back then re-calculate its fitness value
+        # for i in range(0, self.pop_size):
+        #     pos_new = self.amend_position_faster(pop[i][self.ID_POS])
+        #     fit_new = self.get_fitness_position(pos_new)
+        #     pop[i] = [pos_new, fit_new]
 
-            c1 = 2 * exp(-((4 * (epoch+1) / self.epoch) ** 2))
-            ## Eq. (3.2) in the paper
-            for i in range(0, self.pop_size):
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        if self.compare_agent([pos_new, fit_new], pop[idx]):
+            return [pos_new, fit_new]
+        return pop[idx].copy()
 
-                if i < self.pop_size / 2:
-                    c2_list = random(self.problem_size)
-                    c3_list = random(self.problem_size)
-                    pos_new_1 = g_best[self.ID_POS] + c1 * ((self.ub - self.lb) * c2_list + self.lb)
-                    pos_new_2 = g_best[self.ID_POS] - c1 * ((self.ub - self.lb) * c2_list + self.lb)
-                    pos_new = where(c3_list < 0.5, pos_new_1, pos_new_2)
-                else:
-                    # Eq. (3.4) in the paper
-                    pos_new = (pop[i][self.ID_POS] + pop[i-1][self.ID_POS]) / 2
-                pos_new = self.amend_position_faster(pos_new)
-                fit_new = self.get_fitness_position(pos_new)
-                if fit_new < pop[i][self.ID_FIT]:
-                    pop[i] = [pos_new, fit_new]
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-            # Check if salps go out of the search space and bring it back then re-calculate its fitness value
-            # for i in range(0, self.pop_size):
-            #     pos_new = self.amend_position_faster(pop[i][self.ID_POS])
-            #     fit_new = self.get_fitness_position(pos_new)
-            #     pop[i] = [pos_new, fit_new]
-
-            pop, g_best = self.update_sorted_population_and_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+        Returns:
+            [position, fitness value]
+        """
+        ## Eq. (3.2) in the paper
+        c1 = 2 * np.exp(-((4 * (epoch + 1) / self.epoch) ** 2))
+        pop_idx = np.array(range(0, self.pop_size))
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, c1=c1), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, c1=c1), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, g_best, c1) for idx in pop_idx]
+        return child
