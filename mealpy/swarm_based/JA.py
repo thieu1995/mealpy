287240ebcd8b0ced2a91ef1acf8f2030da76555b
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-10-29 15:55:21 +0700

    Restructure JA optimizer in swarm-based group

--- mealpy/swarm_based/JA.py
+++ mealpy/swarm_based/JA.py
@@ -7,12 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-from numpy.random import uniform, normal, rand
-from numpy import abs
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseJA(Root):
+class BaseJA(Optimizer):
     """
         My original version of: Jaya Algorithm (JA)
             (A simple and new optimization algorithm for solving constrained and unconstrained optimization problems)
@@ -20,33 +21,56 @@ class BaseJA(Root):
             https://www.researchgate.net/publication/282532308_Jaya_A_simple_and_new_optimization_algorithm_for_solving_constrained_and_unconstrained_optimization_problems
         Notes:
             + Remove all third loop in algorithm
-            + Change the second random variable r2 to Gaussian instead of uniform
+            + Change the second random variable r2 to Gaussian instead of np.random.uniform
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
+
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
+
         self.epoch = epoch
         self.pop_size = pop_size
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        g_best, g_worst = self.get_global_best_global_worst_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+    def create_child(self, idx, pop, g_best, g_worst):
+        pos_new = pop[idx][self.ID_POS] + np.random.uniform() * (g_best[self.ID_POS] - np.abs(pop[idx][self.ID_POS])) + \
+                  np.random.normal() * (g_worst[self.ID_POS] - np.abs(pop[idx][self.ID_POS]))
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        return [pos_new, fit_new]
 
-        for epoch in range(self.epoch):
-            for i in range(self.pop_size):
-                po = pop[i][self.ID_POS]
-                pos_new = po + uniform() * (g_best[self.ID_POS] - abs(po)) + normal() * (g_worst[self.ID_POS] - abs(po))
-                pos_new = self.amend_position_faster(pos_new)
-                fit = self.get_fitness_position(pos_new)
-                pop[i] = [pos_new, fit]
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-            ## Update global best and global worst
-            g_best, g_worst = self.update_global_best_global_worst_solution(pop, self.ID_MIN_PROB, self.ID_MAX_PROB, g_best)
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+        Returns:
+            [position, fitness value]
+        """
+        _, best, worst = self.get_special_solutions(pop, best=1, worst=1)
+        pop_idx = np.array(range(0, self.pop_size))
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=best[0], g_worst=worst[0]), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=best[0], g_worst=worst[0]), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, best[0], worst[0]) for idx in pop_idx]
+        return child
 
 
 class OriginalJA(BaseJA):
@@ -57,32 +81,28 @@ class OriginalJA(BaseJA):
             http://www.growingscience.com/ijiec/Vol7/IJIEC_2015_32.pdf
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
-        BaseJA.__init__(self, obj_func, lb, ub, verbose, epoch, pop_size, kwargs=kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        g_best, g_worst = self.get_global_best_global_worst_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            **kwargs ():
+        """
+        super().__init__(problem, epoch, pop_size, **kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
 
-        for epoch in range(self.epoch):
-            for i in range(self.pop_size):
-                po = pop[i][self.ID_POS]
-                for j in range(0, self.problem_size):
-                    pos_new = po[j] + uniform()*(g_best[self.ID_POS][j] - abs(po[j])) - uniform()*(g_worst[self.ID_POS][j] - abs(po[j]))
-                pos_new = self.amend_position_faster(pos_new)
-                fit = self.get_fitness_position(pos_new)
-                pop[i] = [pos_new, fit]
-
-            ## Update global best and global worst
-            g_best, g_worst = self.update_global_best_global_worst_solution(pop, self.ID_MIN_PROB, self.ID_MAX_PROB, g_best)
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+    def create_child(self, idx, pop, g_best, g_worst):
+        pos_new = pop[idx][self.ID_POS] + np.random.uniform(0, 1, self.problem.n_dims) * (g_best[self.ID_POS] - np.abs(pop[idx][self.ID_POS])) - \
+            np.random.uniform(0, 1, self.problem.n_dims) * (g_worst[self.ID_POS] - np.abs(pop[idx][self.ID_POS]))
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        return [pos_new, fit_new]
 
 
-class LJA(BaseJA):
+class LevyJA(BaseJA):
     """
         The original version of: Levy-flight Jaya Algorithm (LJA)
             (An improved Jaya optimization algorithm with Levy flight)
@@ -92,29 +112,25 @@ class LJA(BaseJA):
             + This version I still remove all third loop in algorithm
             + The beta value of Levy-flight equal to 1.8 as the best value in the paper.
     """
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
-        BaseJA.__init__(self, obj_func, lb, ub, verbose, epoch, pop_size, kwargs=kwargs)
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            **kwargs ():
+        """
+        super().__init__(problem, epoch, pop_size, **kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        g_best, g_worst = self.get_global_best_global_worst_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
-
-        for epoch in range(self.epoch):
-            for i in range(self.pop_size):
-                L1 = self.step_size_by_levy_flight(multiplier=1.0, beta=1.0, case=-1)
-                L2 = self.step_size_by_levy_flight(multiplier=1.0, beta=1.0, case=-1)
-                po = pop[i][self.ID_POS]
-                pos_new = po + abs(L1) * (g_best[self.ID_POS] - abs(po)) - abs(L2) * (g_worst[self.ID_POS] - abs(po))
-                pos_new = self.amend_position_faster(pos_new)
-                fit = self.get_fitness_position(pos_new)
-                pop[i] = [pos_new, fit]
-
-            ## Update global best and global worst
-            g_best, g_worst = self.update_global_best_global_worst_solution(pop, self.ID_MIN_PROB, self.ID_MAX_PROB, g_best)
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
+    def create_child(self, idx, pop, g_best, g_worst):
+        L1 = self.get_levy_flight_step(multiplier=1.0, beta=1.0, case=-1)
+        L2 = self.get_levy_flight_step(multiplier=1.0, beta=1.0, case=-1)
+        pos_new = pop[idx][self.ID_POS] + np.abs(L1) * (g_best[self.ID_POS] - np.abs(pop[idx][self.ID_POS])) - \
+                  np.abs(L2) * (g_worst[self.ID_POS] - np.abs(pop[idx][self.ID_POS]))
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        return [pos_new, fit_new]
 
