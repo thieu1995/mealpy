3e9bd048a7c0cd1f772aea9e1f4346abbb78fed2
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-10-29 17:14:28 +0700

    Restructure GOA optimizer in swarm-based group

--- mealpy/swarm_based/GOA.py
+++ mealpy/swarm_based/GOA.py
@@ -7,12 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-from numpy import exp, zeros, sqrt, remainder, sum
-from numpy.random import normal
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseGOA(Root):
+class BaseGOA(Optimizer):
     """
     The original version of: Grasshopper Optimization Algorithm (GOA)
         (Grasshopper Optimisation Algorithm: Theory and Application Advances in Engineering Software)
@@ -20,54 +21,72 @@ class BaseGOA(Root):
         http://dx.doi.org/10.1016/j.advengsoft.2017.01.004
         https://www.mathworks.com/matlabcentral/fileexchange/61421-grasshopper-optimisation-algorithm-goa
     Notes:
-        + I added normal() component to Eq, 2.7
+        + I added np.random.normal() component to Eq, 2.7
         + Changed the way to calculate distance between two location
-        + Used batch-size idea
     """
+    def __init__(self, problem, epoch=10000, pop_size=100, c_minmax=(0.00004, 1), **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            c_minmax (list): coefficient c
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, c_minmax=(0.00004, 1), **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
         self.epoch = epoch
         self.pop_size = pop_size
-        self.c_minmax = c_minmax    # coefficient c
+        self.c_minmax = c_minmax
 
     def _s_function__(self, r_vector=None):
         f = 0.5
         l = 1.5
-        return f * exp(-r_vector / l) - exp(-r_vector)        # Eq.(2.3) in the paper
+        # Eq.(2.3) in the paper
+        return f * np.exp(-r_vector / l) - np.exp(-r_vector)
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        g_best = self.get_global_best_solution(pop=pop, id_fit=self.ID_FIT, id_best=self.ID_MIN_PROB)
+    def create_child(self, idx, pop, g_best, c):
+        S_i_total = np.zeros(self.problem.n_dims)
+        for j in range(0, self.pop_size):
+            dist = np.sqrt(np.sum((pop[idx][self.ID_POS] - pop[j][self.ID_POS]) ** 2))
+            r_ij_vector = (pop[idx][self.ID_POS] - pop[j][self.ID_POS]) / (dist + self.EPSILON)  # xj - xi / dij in Eq.(2.7)
+            xj_xi = 2 + np.remainder(dist, 2)  # |xjd - xid| in Eq. (2.7)
+            ## The first part inside the big bracket in Eq. (2.7)   16 955 230 764    212 047 193 643
+            ran = (c / 2) * (self.problem.ub - self.problem.lb)
+            s_ij = ran * self._s_function__(xj_xi) * r_ij_vector
+            S_i_total += s_ij
+        x_new = c * np.random.normal() * S_i_total + g_best[self.ID_POS]  # Eq. (2.7) in the paper
+        pos_new = self.amend_position_faster(x_new)
+        fit_new = self.get_fitness_position(pos_new)
+        if self.compare_agent([pos_new, fit_new], pop[idx]):
+            return [pos_new, fit_new]
+        return pop[idx].copy()
 
-        for epoch in range(self.epoch):
-            # Eq.(2.8) in the paper
-            c = self.c_minmax[1] - epoch * ((self.c_minmax[1] - self.c_minmax[0]) / self.epoch)
-            for i in range(0, self.pop_size):
-                S_i_total = zeros(self.problem_size)
-                for j in range(0, self.pop_size):
-                    dist = sqrt(sum((pop[i][self.ID_POS] - pop[j][self.ID_POS])**2))
-                    r_ij_vector = (pop[i][self.ID_POS] - pop[j][self.ID_POS]) / (dist + self.EPSILON)    # xj - xi / dij in Eq.(2.7)
-                    xj_xi = 2 + remainder(dist, 2)  # |xjd - xid| in Eq. (2.7)
-                    ## The first part inside the big bracket in Eq. (2.7)   16 955 230 764    212 047 193 643
-                    ran = (c / 2) * (self.ub - self.lb)
-                    s_ij = ran * self._s_function__(xj_xi) * r_ij_vector
-                    S_i_total += s_ij
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-                x_new = c * normal() * S_i_total + g_best[self.ID_POS]     # Eq. (2.7) in the paper
-                x_new = self.amend_position_faster(x_new)
-                fit = self.get_fitness_position(x_new)
-                pop[i] = [x_new, fit]
+        Returns:
+            [position, fitness value]
+        """
+        # Eq.(2.8) in the paper
+        c = self.c_minmax[1] - epoch * ((self.c_minmax[1] - self.c_minmax[0]) / self.epoch)
+        pop_idx = np.array(range(0, self.pop_size))
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, c=c), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, c=c), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, g_best=g_best, c=c) for idx in pop_idx]
+        return child
 
-                # batch size idea
-                if self.batch_idea:
-                    if (i + 1) % self.batch_size == 0:
-                        g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-                else:
-                    if (i + 1) % self.pop_size == 0:
-                        g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
