fddf103b2b38e950d704b08250c48c33c0bed16a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-01 16:53:14 +0700

    Restructure Firefly Algorithm in swarm-based group

--- mealpy/swarm_based/FireflyA.py
+++ mealpy/swarm_based/FireflyA.py
@@ -7,14 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-from numpy import sqrt, exp, matmul
-from numpy.random import uniform
-from scipy.linalg import norm
-from copy import deepcopy
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseFireflyA(Root):
+class BaseFireflyA(Optimizer):
     """
         The original version of: Firefly Algorithm (FireflyA)
             Firefly Algorithm for Optimization Problem
@@ -23,64 +22,89 @@ class BaseFireflyA(Root):
             https://www.researchgate.net/publication/259472546_Firefly_Algorithm_for_Optimization_Problem
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100,
-                 gamma=1, beta_base=1, alpha=0.2, alpha_damp=0.99, delta=0.05, m=2, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100,
+                 gamma=0.001, beta_base=2, alpha=0.2, alpha_damp=0.99, delta=0.05, exponent=2, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            gamma (float): Light Absorption Coefficient, default = 0.001
+            beta_base (float): Attraction Coefficient Base Value, default = 2
+            alpha (float): Mutation Coefficient, default = 0.2
+            alpha_damp (float): Mutation Coefficient Damp Rate, default = 0.99
+            delta (float): Mutation Step Size, default = 0.05
+            exponent (int): Exponent (m in the paper), default = 2
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = int(pop_size * (pop_size + 1) / 2 * 0.5)
+        self.sort_flag = False
+
         self.epoch = epoch
         self.pop_size = pop_size
-        self.gamma = gamma              # Light Absorption Coefficient
-        self.beta_base = beta_base      # Attraction Coefficient Base Value
-        self.alpha = alpha              # Mutation Coefficient
-        self.alpha_damp = alpha_damp    # Mutation Coefficient Damp Rate
-        self.delta = delta              # Mutation Step Size
-        self.m = m                      # Exponent
+        self.gamma = gamma
+        self.beta_base = beta_base
+        self.alpha = alpha
+        self.alpha_damp = alpha_damp
+        self.delta = delta
+        self.exponent = exponent
 
-    def train(self):
+        ## Dynamic variable
+        self.dyn_alpha = alpha   # Initial Value of Mutation Coefficient
 
-        # Initialization population
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        g_best = self.get_global_best_solution(pop=pop, id_fit=self.ID_FIT, id_best=self.ID_MIN_PROB)
+    def create_child(self, idx, pop, dmax):
+        agent = pop[idx].copy()
+        for j in range(idx + 1, self.pop_size):
+            # Move Towards Better Solutions
+            if self.compare_agent(pop[j], agent):
+                # Calculate Radius and Attraction Level
+                rij = np.linalg.norm(agent[self.ID_POS] - pop[j][self.ID_POS]) / dmax
+                beta = self.beta_base * np.exp(-self.gamma * rij ** self.exponent)
 
-        # Initial Value of Mutation Coefficient
-        alpha = self.alpha
+                # Mutation Vector
+                mutation_vector = self.delta * np.random.uniform(0, 1, self.problem.n_dims)
+                temp = np.matmul((pop[j][self.ID_POS] - agent[self.ID_POS]), np.random.uniform(0, 1, (self.problem.n_dims, self.problem.n_dims)))
+                # print(temp)
+                pos_new = agent[self.ID_POS] + self.dyn_alpha * mutation_vector + beta * temp
 
-        for epoch in range(self.epoch):
-            # Maximum Distance
-            dmax = sqrt(self.problem_size)
-            # Decision Vector Size
+                pos_new = self.amend_position_faster(pos_new)
+                fit_new = self.get_fitness_position(pos_new)
 
-            pop_new = deepcopy(pop)
-            for i in range(0, self.pop_size-1):
+                # Compare to Previous Solution
+                if self.compare_agent([pos_new, fit_new], agent):
+                    agent = [pos_new, fit_new]
+        return agent
 
-                for j in range(i+1, self.pop_size):
-                    # Move Towards Better Solutions
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-                    if pop[j][self.ID_FIT] < pop[i][self.ID_FIT]:
-                        # Calculate Radius and Attraction Level
-                        rij = norm(pop[i][self.ID_POS] - pop[j][self.ID_POS]) / dmax
-                        beta = self.beta_base * exp(-self.gamma * rij ** self.m)
+        Returns:
+            [position, fitness value]
+        """
+        # Maximum Distance
+        dmax = np.sqrt(self.problem.n_dims)
+        pop_idx = np.array(range(0, self.pop_size-1))
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, dmax=dmax), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, dmax=dmax), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, dmax) for idx in pop_idx]
+        child.append(g_best)
 
-                        # Mutation Vector
-                        e = self.delta * uniform(-1, 1, self.problem_size)
+        # Merge, Sort and Selection, update global best
+        # pop = self.get_sorted_strim_population(pop + child, self.pop_size)
+        self.dyn_alpha = self.alpha_damp * self.alpha
+        return child
 
-                        pos_new = pop[i][self.ID_POS] + alpha * e +\
-                                  beta * matmul((pop[j][self.ID_POS] - pop[i][self.ID_POS]), uniform(0, 1, (self.problem_size, self.problem_size)))
-                        pos_new = self.amend_position_faster(pos_new)
-                        fit_new = self.get_fitness_position(pos_new)
 
-                        # Compare to Previous Solution
-                        if fit_new < pop[i][self.ID_FIT]:
-                            pop_new[i] = [pos_new, fit_new]
-
-            # Merge, Sort and Selection, update global best
-            pop, g_best = self.update_sorted_population_and_global_best_solution(pop + pop_new, self.ID_MIN_PROB, g_best)
-            pop = pop[:self.pop_size]
-
-            alpha = self.alpha_damp * alpha
-
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print(">Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
