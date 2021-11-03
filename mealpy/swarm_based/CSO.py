05a6ed3f9f0d5343d1f8881c73e750fe800665ca
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-03 17:53:05 +0700

    Restructure BSO algorithm in swarm-based group

--- mealpy/swarm_based/CSO.py
+++ mealpy/swarm_based/CSO.py
@@ -7,14 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-from numpy.random import uniform, random
-from numpy import where
-from random import sample, choice
-from copy import deepcopy
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseCSO(Root):
+class BaseCSO(Optimizer):
     """
         The original version of: Cat Swarm Optimization (CSO)
         Link:
@@ -26,20 +25,26 @@ class BaseCSO(Root):
     ID_VEL = 2      # velocity
     ID_FLAG = 3     # status
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100,
-                 mixture_ratio=0.15, smp=10, spc=False, cdc=0.8, srd=0.15, c1=0.4, w_minmax=(0.4, 0.9), selected_strategy=1, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, mixture_ratio=0.15, smp=5, spc=False,
+                 cdc=0.8, srd=0.15, c1=0.4, w_minmax=(0.4, 0.9), selected_strategy=1, **kwargs):
         """
-        # mixture_ratio - joining seeking mode with tracing mode
-        # smp - seeking memory pool, 10 clones  (larger is better but time-consuming)
-        # spc - self-position considering
-        # cdc - counts of dimension to change  (larger is more diversity but slow convergence)
-        # srd - seeking range of the selected dimension (smaller is better but slow convergence)
-        # w_minmax - same in PSO
-        # c1 - same in PSO
-        # selected_strategy : 0: best fitness, 1: tournament, 2: roulette wheel, else: random  (decrease by quality)
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            mixture_ratio (): joining seeking mode with tracing mode
+            smp (): seeking memory pool, 10 clones  (larger is better but time-consuming)
+            spc (): self-position considering
+            cdc (): counts of dimension to change  (larger is more diversity but slow convergence)
+            srd (): seeking range of the selected dimension (smaller is better but slow convergence)
+            c1 (): same in PSO
+            w_minmax (): same in PSO
+            selected_strategy ():  0: best fitness, 1: tournament, 2: roulette wheel, else: random  (decrease by quality)
+            **kwargs ():
         """
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
-        self.epoch =  epoch
+
+        super().__init__(problem, kwargs)
+        self.epoch = epoch
         self.pop_size = pop_size
         self.mixture_ratio = mixture_ratio
         self.smp = smp
@@ -51,85 +56,99 @@ class BaseCSO(Root):
         self.w_max = w_minmax[1]
         self.selected_strategy = selected_strategy
 
-    def create_solution(self, minmax=0):
+    def create_solution(self):
         """
-                x: current position of cat
-                v: vector v of cat (same amount of dimension as x)
-                flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching)
-        """
-        x = uniform(self.lb, self.ub)
-        v = uniform(self.lb, self.ub)
-        fitness = self.get_fitness_position(x, minmax)
-        flag = False                        # False: seeking mode , True: tracing mode
-        if random() < self.mixture_ratio:
-            flag = True
-        return [x, fitness, v, flag]
+        Returns:
+            The position position with 2 element: index of position/location and index of fitness wrapper
+            The general format: [position, [target, [obj1, obj2, ...]]]
 
-    def _seeking_mode__(self, cat):
+        ## To get the position, fitness wrapper, target and obj list
+        ##      A[self.ID_POS]                  --> Return: position
+        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
+        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+
+        x: current position of cat
+        v: vector v of cat (same amount of dimension as x)
+        flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching)
+        # False: seeking mode , True: tracing mode
+        """
+        position = np.random.uniform(self.problem.lb, self.problem.ub)
+        fitness = self.get_fitness_position(position=position)
+        velocity = np.random.uniform(self.problem.lb, self.problem.ub)
+        flag = True if np.random.uniform() < self.mixture_ratio else False
+        return [position, fitness, velocity, flag]
+
+    def _seeking_mode__(self, mode, cat):
         candidate_cats = []
-        clone_cats = [deepcopy(cat) for _ in range(self.smp)]
+        clone_cats = self.create_population(mode, self.smp)
         if self.spc:
-            candidate_cats.append(deepcopy(cat))
-            clone_cats = [deepcopy(cat) for _ in range(self.smp - 1)]
+            candidate_cats.append(cat.copy())
+            clone_cats = [cat.copy() for _ in range(self.smp - 1)]
 
         for clone in clone_cats:
-            idx = sample(range(0, self.problem_size), int(self.cdc * self.problem_size))
+            idx = np.random.choice(range(0, self.problem.n_dims), int(self.cdc * self.problem.n_dims), replace=False)
             pos_new1 = clone[self.ID_POS] * (1 + self.srd)
             pos_new2 = clone[self.ID_POS] * (1 - self.srd)
 
-            pos_new = where(uniform(0, 1, self.problem_size) < 0.5, pos_new1, pos_new2)
+            pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < 0.5, pos_new1, pos_new2)
             pos_new[idx] = clone[self.ID_POS][idx]
             pos_new = self.amend_position_faster(pos_new)
-            fit_new = self.get_fitness_position(pos_new)
-            candidate_cats.append([pos_new, fit_new, clone[self.ID_VEL], clone[self.ID_FLAG]])
+            candidate_cats.append([pos_new, None, clone[self.ID_VEL], clone[self.ID_FLAG]])
+        candidate_cats = self.update_fitness_population(mode, candidate_cats)
 
         if self.selected_strategy == 0:                # Best fitness-self
-            cat = sorted(candidate_cats, key=lambda cat: cat[self.ID_FIT])[0]
+            _, cat = self.get_global_best_solution(candidate_cats)
         elif self.selected_strategy == 1:              # Tournament
             k_way = 4
-            idx = sample(range(0, self.smp), k_way)
+            idx = np.random.choice(range(0, self.smp), k_way, replace=False)
             cats_k_way = [candidate_cats[_] for _ in idx]
-            cat = sorted(cats_k_way, key=lambda cat: cat[self.ID_FIT])[0]
+            _, cat = self.get_global_best_solution(cats_k_way)
         elif self.selected_strategy == 2:              ### Roul-wheel selection
-            list_fitness = [candidate_cats[u][self.ID_FIT] for u in range(0, len(candidate_cats))]
+            list_fitness = [candidate_cats[u][self.ID_FIT][self.ID_TAR] for u in range(0, len(candidate_cats))]
             idx = self.get_index_roulette_wheel_selection(list_fitness)
             cat = candidate_cats[idx]
         else:
-            cat = choice(candidate_cats)                # Random
+            idx = np.random.choice(range(0, len(candidate_cats)))
+            cat = candidate_cats[idx]               # Random
         return cat
 
+    def create_child(self, idx, pop, g_best, w, mode):
+        # tracing mode
+        if pop[idx][self.ID_FLAG]:
+            pos_new = pop[idx][self.ID_POS] + w * pop[idx][self.ID_VEL] + \
+                      np.random.uniform() * self.c1 * (g_best[self.ID_POS] - pop[idx][self.ID_POS])
+            fit_new = self.get_fitness_position(pos_new)
+            pop[idx][self.ID_POS] = pos_new
+            pop[idx][self.ID_FIT] = fit_new
+        else:
+            pop[idx] = self._seeking_mode__(mode, pop[idx])
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(0, self.pop_size)]
-        g_best = self.get_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
+        pop[idx][self.ID_FLAG] = True if np.random.uniform() < self.mixture_ratio else False
+        return pop[idx].copy()
 
-        for epoch in range(self.epoch):
-            w = (self.epoch - epoch) / self.epoch * (self.w_max - self.w_min) + self.w_min
-            for i in range(0, self.pop_size):
-                if pop[i][self.ID_FLAG]:            # tracing mode
-                    pos_new = pop[i][self.ID_POS] + w * pop[i][self.ID_VEL] + uniform() * self.c1 * (g_best[self.ID_POS] - pop[i][self.ID_POS])
-                    fit_new = self.get_fitness_position(pos_new)
-                    pop[i][self.ID_POS] = pos_new
-                    pop[i][self.ID_FIT] = fit_new
-                else:
-                    pop[i] = self._seeking_mode__(pop[i])
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-                ## batch size idea
-                if self.batch_idea:
-                    if (i + 1) % self.batch_size == 0:
-                        g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-                else:
-                    if (i + 1) % self.pop_size == 0:
-                        g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
+        Returns:
+            [position, fitness value]
+        """
+        w = (self.epoch - epoch) / self.epoch * (self.w_max - self.w_min) + self.w_min
+        pop_idx = np.array(range(0, self.pop_size))
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, w=w, mode=mode), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, w=w, mode=mode), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, g_best=g_best, w=w, mode=mode) for idx in pop_idx]
+        return child
 
-            for i in range(0, self.pop_size):
-                if uniform() < self.mixture_ratio:
-                    pop[i][self.ID_FLAG] = True
-                else:
-                    pop[i][self.ID_FLAG] = False
-
-            self.loss_train.append(g_best[self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch+1, g_best[self.ID_FIT]))
-        self.solution = g_best
-        return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
