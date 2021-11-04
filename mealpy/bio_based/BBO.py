3a4684e8a07ced6ebb6ad63ab1434a32e73f465c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-04 11:07:40 +0700

    Restructure BBO algorithm in bio-based group

--- mealpy/bio_based/BBO.py
+++ mealpy/bio_based/BBO.py
@@ -7,13 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-from numpy.random import uniform
-from numpy import array, where
-from copy import deepcopy
-from mealpy.optimizer import Root
+import concurrent.futures as parallel
+from functools import partial
+import numpy as np
+from mealpy.optimizer import Optimizer
 
 
-class BaseBBO(Root):
+class BaseBBO(Optimizer):
     """
     My version of: Biogeography-based optimization (BBO)
         Biogeography-Based Optimization
@@ -21,60 +21,73 @@ class BaseBBO(Root):
         https://ieeexplore.ieee.org/abstract/document/4475427
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, p_m=0.01, elites=2, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
+    def __init__(self, problem, epoch=10000, pop_size=100, p_m=0.01, elites=2, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            p_m (): mutation probability
+            elites (): Number of elites will be keep for next generation
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
+
         self.epoch = epoch
         self.pop_size = pop_size
-        self.p_m = p_m              # mutation probability
-        self.elites = elites        # Number of elites will be keep for next generation
+        self.p_m = p_m
+        self.elites = elites
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop_sorted = sorted(pop, key=lambda temp: temp[self.ID_FIT])
-        # Save the best solutions and costs in the elite arrays
-        pop_elites = deepcopy(pop_sorted[:self.elites])
-        # Compute migration rates, assuming the population is sorted from most train to least train
-        mu = (self.pop_size + 1 - array(range(1, self.pop_size + 1))) / (self.pop_size + 1)
-        mr = 1 - mu
-        for epoch in range(self.epoch):
+        self.mu = (self.pop_size + 1 - np.array(range(1, self.pop_size + 1))) / (self.pop_size + 1)
+        self.mr = 1 - self.mu
 
-            # Use migration rates to decide how much information to share between solutions
-            pop_new = deepcopy(pop)
-            for i in range(self.pop_size):
+    def create_child(self, idx, pop, list_fitness):
+        # Probabilistic migration to the i-th position
+        # Pick a position from which to emigrate (roulette wheel selection)
+        idx_selected = self.get_index_roulette_wheel_selection(list_fitness)
+        # this is the migration step
+        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.mr[idx], pop[idx_selected][self.ID_POS], pop[idx][self.ID_POS])
+        # Mutation
+        temp = np.random.uniform(self.problem.lb, self.problem.ub)
+        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, temp, pos_new)
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        return [pos_new, fit_new]
 
-                # Probabilistic migration to the i-th position
-                list_fitness = [item[self.ID_FIT] for item in pop]
-                pos_old = pop_new[i][self.ID_POS]
+    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+        """
+        Args:
+            mode (str): 'sequential', 'thread', 'process'
+                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
+                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
+                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
 
-                # Pick a position from which to emigrate (roulette wheel selection)
-                idx_selected = self.get_index_roulette_wheel_selection(list_fitness)
+        Returns:
+            [position, fitness value]
+        """
+        pop_elites, local_best = self.get_global_best_solution(pop)
+        list_fitness = [agent[self.ID_FIT][self.ID_TAR] for agent in pop]
+        pop_idx = np.array(range(0, self.pop_size))
+        # Use migration rates to decide how much information to share between solutions
+        if mode == "thread":
+            with parallel.ThreadPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, list_fitness=list_fitness), pop_idx)
+            child = [x for x in pop_child]
+        elif mode == "process":
+            with parallel.ProcessPoolExecutor() as executor:
+                pop_child = executor.map(partial(self.create_child, pop=pop, list_fitness=list_fitness), pop_idx)
+            child = [x for x in pop_child]
+        else:
+            child = [self.create_child(idx, pop, list_fitness) for idx in pop_idx]
 
-                # this is the migration step
-                pos_new = where(uniform(0, 1, self.problem_size) < mr[i], pop_new[idx_selected][self.ID_POS], pos_old)
-
-                # Mutation
-                temp = uniform(self.lb, self.ub)
-                pos_new = where(uniform(0, 1, self.problem_size) < self.p_m, temp, pos_new)
-
-                # Re-calculated fitness
-                pop_new[i] = [pos_new, self.get_fitness_position(pos_new)]
-
-            # replace the solutions with their new migrated and mutated versions then Merge Populations
-            pop = sorted(deepcopy(pop_new + pop_elites), key=lambda temp: temp[self.ID_FIT])
-            pop = pop[:self.pop_size]
-
-            # Update all elite solutions
-            for i in range(self.elites):
-                if pop_elites[i][self.ID_FIT] > pop[i][self.ID_FIT]:
-                    pop_elites[i] = deepcopy(pop[i])
-            self.loss_train.append(pop_elites[self.ID_MIN_PROB][self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, pop_elites[self.ID_MIN_PROB][self.ID_FIT]))
-        self.solution = pop_elites[self.ID_MIN_PROB]
-        return pop_elites[self.ID_MIN_PROB][self.ID_POS], pop_elites[self.ID_MIN_PROB][self.ID_FIT], self.loss_train
+        # replace the solutions with their new migrated and mutated versions then Merge Populations
+        pop = self.get_sorted_strim_population(child + pop_elites, self.pop_size)
+        return pop
 
 
-class OriginalBBO(Root):
+class OriginalBBO(BaseBBO):
     """
     The original version of: Biogeography-based optimization (BBO)
         Biogeography-Based Optimization
@@ -82,60 +95,35 @@ class OriginalBBO(Root):
         https://ieeexplore.ieee.org/abstract/document/4475427
     """
 
-    def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, p_m=0.01, elites=2, **kwargs):
-        super().__init__(obj_func, lb, ub, verbose, kwargs)
-        self.epoch = epoch
-        self.pop_size = pop_size
-        self.p_m = p_m                  # mutation probability
-        self.elites = elites            # Number of elites will be keep for next generation
+    def __init__(self, problem, epoch=10000, pop_size=100, p_m=0.01, elites=2, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            p_m (): mutation probability
+            elites (): Number of elites will be keep for next generation
+            **kwargs ():
+        """
+        super().__init__(problem, epoch, pop_size, **kwargs)
 
-    def train(self):
-        pop = [self.create_solution() for _ in range(self.pop_size)]
-        pop_sorted = sorted(pop, key=lambda temp: temp[self.ID_FIT])
-        # Save the best solutions and costs in the elite arrays
-        pop_elites = deepcopy(pop_sorted[:self.elites])
-        # Compute migration rates, assuming the population is sorted from most train to least train
-        mu = (self.pop_size + 1 - array(range(1, self.pop_size+1))) / (self.pop_size + 1)
-        mr = 1 - mu
-        for epoch in range(self.epoch):
+    def create_child(self, idx, pop, list_fitness):
+        # Probabilistic migration to the i-th position
+        pos_new = pop[idx][self.ID_POS].copy()
+        for j in range(self.problem.n_dims):
+            if np.random.uniform() < self.mr[idx]:  # Should we immigrate?
+                # Pick a position from which to emigrate (roulette wheel selection)
+                random_number = np.random.uniform() * np.sum(self.mu)
+                select = self.mu[0]
+                select_index = 0
+                while (random_number > select) and (select_index < self.pop_size - 1):
+                    select_index += 1
+                    select += self.mu[select_index]
+                # this is the migration step
+                pos_new[j] = pop[select_index][self.ID_POS][j]
 
-            # Use migration rates to decide how much information to share between solutions
-            pop_new = deepcopy(pop)
-            for i in range(self.pop_size):
-
-                # Probabilistic migration to the i-th position
-                for j in range(self.problem_size):
-
-                    if uniform() < mr[i]:     # Should we immigrate?
-                        # Pick a position from which to emigrate (roulette wheel selection)
-                        random_number = uniform() * sum(mu)
-                        select = mu[0]
-                        select_index = 0
-                        while (random_number > select) and (select_index < self.pop_size - 1):
-                            select_index += 1
-                            select += mu[select_index]
-                        # this is the migration step
-                        pop_new[i][self.ID_POS][j] = pop[select_index][self.ID_POS][j]
-
-            # Mutation
-            for i in range(self.pop_size):
-                temp = uniform(self.lb, self.ub)
-                pos_new = where(uniform(0, 1, self.problem_size) < self.p_m, temp, pop_new[i][self.ID_POS])
-                # Re-calculated fitness
-                pop_new[i][self.ID_FIT] = self.get_fitness_position(pos_new)
-
-            # replace the solutions with their new migrated and mutated versions then Merge Populations
-            pop = deepcopy(pop_new)
-            pop = pop + pop_elites
-            pop = sorted(pop, key=lambda temp: temp[self.ID_FIT])
-            pop = pop[:self.pop_size]
-
-            # Update all elite solutions
-            for i in range(self.elites):
-                if pop_elites[i][self.ID_FIT] > pop[i][self.ID_FIT]:
-                    pop_elites[i] = deepcopy(pop[i])
-            self.loss_train.append(pop_elites[self.ID_MIN_PROB][self.ID_FIT])
-            if self.verbose:
-                print("> Epoch: {}, Best fit: {}".format(epoch + 1, pop_elites[self.ID_MIN_PROB][self.ID_FIT]))
-        self.solution = pop_elites[self.ID_MIN_PROB]
-        return pop_elites[self.ID_MIN_PROB][self.ID_POS], pop_elites[self.ID_MIN_PROB][self.ID_FIT], self.loss_train
+        noise = np.random.uniform(self.problem.lb, self.problem.ub)
+        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, noise, pos_new)
+        pos_new = self.amend_position_faster(pos_new)
+        fit_new = self.get_fitness_position(pos_new)
+        return [pos_new, fit_new]
