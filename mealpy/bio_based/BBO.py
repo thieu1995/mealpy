d83a7ce18ab39911f91a3e96ee9202a8120497a5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-11 15:26:20 +0700

    Update BBO optimizer

--- mealpy/bio_based/BBO.py
+++ mealpy/bio_based/BBO.py
@@ -7,15 +7,13 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from mealpy.optimizer import Optimizer
 
 
-class BaseBBO(Optimizer):
+class OriginalBBO(Optimizer):
     """
-    My version of: Biogeography-based optimization (BBO)
+    The original version of: Biogeography-based optimization (BBO)
         Biogeography-Based Optimization
     Link:
         https://ieeexplore.ieee.org/abstract/document/4475427
@@ -27,8 +25,8 @@ class BaseBBO(Optimizer):
             problem ():
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            p_m (): mutation probability
-            elites (): Number of elites will be keep for next generation
+            p_m (float): mutation probability, default=0.01
+            elites (int): Number of elites will be keep for next generation, default=2
             **kwargs ():
         """
         super().__init__(problem, kwargs)
@@ -43,53 +41,41 @@ class BaseBBO(Optimizer):
         self.mu = (self.pop_size + 1 - np.array(range(1, self.pop_size + 1))) / (self.pop_size + 1)
         self.mr = 1 - self.mu
 
-    def create_child(self, idx, pop, list_fitness):
-        # Probabilistic migration to the i-th position
-        # Pick a position from which to emigrate (roulette wheel selection)
-        idx_selected = self.get_index_roulette_wheel_selection(list_fitness)
-        # this is the migration step
-        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.mr[idx], pop[idx_selected][self.ID_POS], pop[idx][self.ID_POS])
-        # Mutation
-        temp = np.random.uniform(self.problem.lb, self.problem.ub)
-        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, temp, pos_new)
-        pos_new = self.amend_position_faster(pos_new)
-        fit_new = self.get_fitness_position(pos_new)
-        return [pos_new, fit_new]
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_elites, local_best = self.get_global_best_solution(pop)
-        list_fitness = [agent[self.ID_FIT][self.ID_TAR] for agent in pop]
-        pop_idx = np.array(range(0, self.pop_size))
-        # Use migration rates to decide how much information to share between solutions
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, list_fitness=list_fitness), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, list_fitness=list_fitness), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, list_fitness) for idx in pop_idx]
+        _, pop_elites, _ = self.get_special_solutions(self.pop, best=self.elites)
+        pop = []
+        for idx in range(0, self.pop_size):
+            # Probabilistic migration to the i-th position
+            pos_new = self.pop[idx][self.ID_POS].copy()
+            for j in range(self.problem.n_dims):
+                if np.random.uniform() < self.mr[idx]:  # Should we immigrate?
+                    # Pick a position from which to emigrate (roulette wheel selection)
+                    random_number = np.random.uniform() * np.sum(self.mu)
+                    select = self.mu[0]
+                    select_index = 0
+                    while (random_number > select) and (select_index < self.pop_size - 1):
+                        select_index += 1
+                        select += self.mu[select_index]
+                    # this is the migration step
+                    pos_new[j] = self.pop[select_index][self.ID_POS][j]
 
+            noise = np.random.uniform(self.problem.lb, self.problem.ub)
+            pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, noise, pos_new)
+            pos_new = self.amend_position_faster(pos_new)
+            pop.append([pos_new, None])
+
+        pop = self.update_fitness_population(pop)
         # replace the solutions with their new migrated and mutated versions then Merge Populations
-        pop = self.get_sorted_strim_population(child + pop_elites, self.pop_size)
-        return pop
+        self.pop = self.get_sorted_strim_population(pop + pop_elites, self.pop_size)
 
 
-class OriginalBBO(BaseBBO):
+class BaseBBO(OriginalBBO):
     """
-    The original version of: Biogeography-based optimization (BBO)
+    My version of: Biogeography-based optimization (BBO)
         Biogeography-Based Optimization
     Link:
         https://ieeexplore.ieee.org/abstract/document/4475427
@@ -101,29 +87,32 @@ class OriginalBBO(BaseBBO):
             problem ():
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            p_m (): mutation probability
-            elites (): Number of elites will be keep for next generation
+            p_m (float): mutation probability, default=0.01
+            elites (int): Number of elites will be keep for next generation, default=2
             **kwargs ():
         """
-        super().__init__(problem, epoch, pop_size, **kwargs)
+        super().__init__(problem, epoch, pop_size, p_m, elites, **kwargs)
 
-    def create_child(self, idx, pop, list_fitness):
-        # Probabilistic migration to the i-th position
-        pos_new = pop[idx][self.ID_POS].copy()
-        for j in range(self.problem.n_dims):
-            if np.random.uniform() < self.mr[idx]:  # Should we immigrate?
-                # Pick a position from which to emigrate (roulette wheel selection)
-                random_number = np.random.uniform() * np.sum(self.mu)
-                select = self.mu[0]
-                select_index = 0
-                while (random_number > select) and (select_index < self.pop_size - 1):
-                    select_index += 1
-                    select += self.mu[select_index]
-                # this is the migration step
-                pos_new[j] = pop[select_index][self.ID_POS][j]
+    def evolve(self, epoch):
+        """
+        Args:
+            epoch (int): The current iteration
+        """
+        _, pop_elites, _ = self.get_special_solutions(self.pop, best=self.elites)
+        list_fitness = [agent[self.ID_FIT][self.ID_TAR] for agent in self.pop]
+        pop = []
+        for idx in range(0, self.pop_size):
+            # Probabilistic migration to the i-th position
+            # Pick a position from which to emigrate (roulette wheel selection)
+            idx_selected = self.get_index_roulette_wheel_selection(list_fitness)
+            # this is the migration step
+            pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.mr[idx], self.pop[idx_selected][self.ID_POS], self.pop[idx][self.ID_POS])
+            # Mutation
+            temp = np.random.uniform(self.problem.lb, self.problem.ub)
+            pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, temp, pos_new)
+            pos_new = self.amend_position_faster(pos_new)
+            pop.append([pos_new, None])
+        pop = self.update_fitness_population(pop)
+        # Replace the solutions with their new migrated and mutated versions then merge populations
+        self.pop = self.get_sorted_strim_population(pop + pop_elites, self.pop_size)
 
-        noise = np.random.uniform(self.problem.lb, self.problem.ub)
-        pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < self.p_m, noise, pos_new)
-        pos_new = self.amend_position_faster(pos_new)
-        fit_new = self.get_fitness_position(pos_new)
-        return [pos_new, fit_new]
