9f0fe77aaadf924715dda4d2c0573d1621c54f82
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-11 17:43:08 +0700

    Update HC optimizer

--- mealpy/math_based/HC.py
+++ mealpy/math_based/HC.py
@@ -36,30 +36,19 @@ class OriginalHC(Optimizer):
         self.pop_size = pop_size
         self.neighbour_size = neighbour_size
 
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
+        self.nfe_per_epoch = self.neighbour_size
         step_size = np.mean(self.problem.ub - self.problem.lb) * np.exp(-2 * (epoch + 1) / self.epoch)
-
-        if mode != "sequential":
-            print("Original HC algorithm only support sequential process!")
-            exit(0)
         pop_neighbours = []
         for i in range(0, self.neighbour_size):
-            pos_new = pos_new = g_best[self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * step_size
+            pos_new = self.g_best[self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * step_size
             pos_new = self.amend_position_faster(pos_new)
-            fit_new = self.get_fitness_position(pos_new)
-            pop_neighbours.append([pos_new, fit_new])
-        pop_neighbours.append(g_best)
-        return pop_neighbours
+            pop_neighbours.append([pos_new, None])
+        self.pop = self.update_fitness_population(pop_neighbours)
 
 
 class BaseHC(OriginalHC):
@@ -84,50 +73,23 @@ class BaseHC(OriginalHC):
         self.nfe_per_epoch = pop_size
         self.sort_flag = True
 
-        self.epoch = epoch
-        self.pop_size = pop_size
-        self.neighbour_size = neighbour_size
-
-    def create_child(self, idx, pop, g_best, step_size, ranks):
-        ss = step_size * ranks[idx]
-        pop_neighbours = []
-        for j in range(0, self.neighbour_size):
-            pos_new = pop[idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * ss
-            pos_new = self.amend_position_faster(pos_new)
-            fit_new = self.get_fitness_position(pos_new)
-            pop_neighbours.append([pos_new, fit_new])
-        pop_neighbours.append(g_best)
-        _, agent = self.get_global_best_solution(pop_neighbours)
-        return agent
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_copy = pop.copy()
-        pop_idx = np.array(range(0, self.pop_size))
-
         ranks = np.array(list(range(1, self.pop_size + 1)))
         ranks = ranks / sum(ranks)
         step_size = np.mean(self.problem.ub - self.problem.lb) * np.exp(-2 * (epoch + 1) / self.epoch)
 
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop_copy, g_best=g_best, step_size=step_size, ranks=ranks), pop_idx)
-            pop_new = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop_copy, g_best=g_best, step_size=step_size, ranks=ranks), pop_idx)
-            pop_new = [x for x in pop_child]
-        else:
-            pop_new = [self.create_child(idx, pop_copy, g_best, step_size, ranks) for idx in pop_idx]
-        return pop_new
-
+        for idx in range(0, self.pop_size):
+            ss = step_size * ranks[idx]
+            pop_neighbours = []
+            for j in range(0, self.neighbour_size):
+                pos_new = self.pop[idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * ss
+                pos_new = self.amend_position_faster(pos_new)
+                pop_neighbours.append([pos_new, None])
+            pop_neighbours = self.update_fitness_population(pop_neighbours)
+            _, agent = self.get_global_best_solution(pop_neighbours)
+            self.pop[idx] = agent
 
