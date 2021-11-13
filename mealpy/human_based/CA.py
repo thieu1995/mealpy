8558668e698cdf4d5bc3d9fab22263f71e2192d6
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-13 10:04:46 +0700

    Update CA optimizer

--- mealpy/human_based/CA.py
+++ mealpy/human_based/CA.py
@@ -7,8 +7,6 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from mealpy.optimizer import Optimizer
 
@@ -42,10 +40,6 @@ class OriginalCA(Optimizer):
         self.dyn_accepted_num = int(self.accepted_rate * self.pop_size)
         # update situational knowledge (g_best here is a element inside belief space)
 
-    def binary_tournament(self, population):
-        id1, id2 = np.random.choice(list(range(0, len(population))), 2, replace=False)
-        return population[id1] if (population[id1][self.ID_FIT] < population[id2][self.ID_FIT]) else population[id2]
-
     def create_faithful(self, lb, ub):
         position = np.random.uniform(lb, ub)
         fitness = self.get_fitness_position(position=position)
@@ -57,36 +51,28 @@ class OriginalCA(Optimizer):
         belief_space["ub"] = np.max(pos_list, axis=0)
         return belief_space
 
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        if mode != "sequential":
-            print("CA optimizer is support sequential mode only!")
-            exit(0)
-
         # create next generation
         pop_child = [self.create_faithful(self.dyn_belief_space["lb"], self.dyn_belief_space["ub"]) for _ in range(0, self.pop_size)]
 
         # select next generation
-        pop = [self.binary_tournament(pop + pop_child) for _ in range(0, self.pop_size)]
-
-        if self.problem.minmax == "min":
-            pop = sorted(pop, key=lambda agent: agent[self.ID_FIT][self.ID_TAR])
-        else:
-            pop = sorted(pop, key=lambda agent: agent[self.ID_FIT][self.ID_TAR], reverse=True)
+        pop_new = []
+        pop_full = self.pop + pop_child
+        size_new = len(pop_full)
+        for _ in range(0, self.pop_size):
+            id1, id2 = np.random.choice(list(range(0, size_new)), 2, replace=False)
+            if self.compare_agent(pop_full[id1], pop_full[id2]):
+                pop_new.append(pop_full[id1])
+            else:
+                pop_new.append(pop_full[id2])
+        self.pop = self.get_sorted_strim_population(pop_new)
 
         # Get accepted faithful
-        accepted = pop[:self.dyn_accepted_num]
+        accepted = self.pop[:self.dyn_accepted_num]
 
         # Update belief_space
         self.dyn_belief_space = self.update_belief_space(self.dyn_belief_space, accepted)
-
-        return pop
