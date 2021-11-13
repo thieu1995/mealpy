68da88176682f0a4dcb73c7f71db57f9206516d3
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-13 16:08:01 +0700

    Update TLO optimizer

--- mealpy/human_based/TLO.py
+++ mealpy/human_based/TLO.py
@@ -7,8 +7,7 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial, reduce
+from functools import reduce
 import numpy as np
 from mealpy.optimizer import Optimizer
 import time
@@ -26,9 +25,12 @@ class BaseTLO(Optimizer):
 
     def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
         """
+
         Args:
+            problem ():
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
+            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = 2 * pop_size
@@ -37,57 +39,37 @@ class BaseTLO(Optimizer):
         self.epoch = epoch
         self.pop_size = pop_size
 
-    def create_child(self, idx, pop, g_best):
-        ## Teaching Phrase
-        TF = np.random.randint(1, 3)  # 1 or 2 (never 3)
-        list_pos = np.array([item[self.ID_POS] for item in pop])
-        DIFF_MEAN = np.random.rand(self.problem.n_dims) * (g_best[self.ID_POS] - TF * np.mean(list_pos, axis=0))
-        temp = pop[idx][self.ID_POS] + DIFF_MEAN
-        pos_new = self.amend_position_faster(temp)
-        fit_new = self.get_fitness_position(temp)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            pop[idx] = [pos_new, fit_new]
-
-        ## Learning Phrase
-        temp = pop[idx][self.ID_POS].copy()
-        id_partner = np.random.choice(np.setxor1d(np.array(range(self.pop_size)), np.array([idx])))
-        # arr_random = np.random.rand(self.problem.n_dims)
-        if pop[idx][self.ID_FIT] < pop[id_partner][self.ID_FIT]:
-            temp += np.random.rand(self.problem.n_dims) * (pop[idx][self.ID_POS] - pop[id_partner][self.ID_POS])
-        else:
-            temp += np.random.rand(self.problem.n_dims) * (pop[id_partner][self.ID_POS] - pop[idx][self.ID_POS])
-        pos_new = self.amend_position_faster(temp)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            return [pos_new, fit_new]
-        return pop[idx].copy()
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_copy = pop.copy()
-        pop_idx = np.array(range(0, self.pop_size))
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            ## Teaching Phrase
+            TF = np.random.randint(1, 3)  # 1 or 2 (never 3)
+            list_pos = np.array([item[self.ID_POS] for item in self.pop])
+            DIFF_MEAN = np.random.rand(self.problem.n_dims) * (self.g_best[self.ID_POS] - TF * np.mean(list_pos, axis=0))
+            temp = self.pop[idx][self.ID_POS] + DIFF_MEAN
+            pos_new = self.amend_position_faster(temp)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        pop_new = self.greedy_selection_population(self.pop, pop_new)
 
-        ## Reproduction
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop_copy, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop_copy, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop_copy, g_best=g_best) for idx in pop_idx]
-        return child
+        pop_child = []
+        for idx in range(0, self.pop_size):
+            ## Learning Phrase
+            temp = pop_new[idx][self.ID_POS].copy()
+            id_partner = np.random.choice(np.setxor1d(np.array(range(self.pop_size)), np.array([idx])))
+            # arr_random = np.random.rand(self.problem.n_dims)
+            if self.compare_agent(pop_new[idx], pop_new[id_partner]):
+                temp += np.random.rand(self.problem.n_dims) * (pop_new[idx][self.ID_POS] - pop_new[id_partner][self.ID_POS])
+            else:
+                temp += np.random.rand(self.problem.n_dims) * (pop_new[id_partner][self.ID_POS] - pop_new[idx][self.ID_POS])
+            pos_new = self.amend_position_faster(temp)
+            pop_child.append([pos_new, None])
+        pop_child = self.update_fitness_population(pop_child)
+        self.pop = self.greedy_selection_population(pop_new, pop_child)
 
 
 class OriginalTLO(BaseTLO):
@@ -110,31 +92,36 @@ class OriginalTLO(BaseTLO):
         self.nfe_per_epoch = 2 * pop_size
         self.sort_flag = False
 
-    def create_child(self, idx, pop, g_best):
-        ## Teaching Phrase
-        TF = np.random.randint(1, 3)  # 1 or 2 (never 3)
-        #### Remove third loop here
-        list_pos = np.array([item[self.ID_POS] for item in pop])
-        pos_new = pop[idx][self.ID_POS] + np.random.uniform(0, 1, self.problem.n_dims) * (g_best[self.ID_POS] - TF * np.mean(list_pos, axis=0))
-        pos_new = self.amend_position_faster(pos_new)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            pop[idx] = [pos_new, fit_new]
+    def evolve(self, epoch):
+        """
+        Args:
+            epoch (int): The current iteration
+        """
+        for idx in range(0, self.pop_size):
+            ## Teaching Phrase
+            TF = np.random.randint(1, 3)  # 1 or 2 (never 3)
+            #### Remove third loop here
+            list_pos = np.array([item[self.ID_POS] for item in self.pop])
+            pos_new = self.pop[idx][self.ID_POS] + np.random.uniform(0, 1, self.problem.n_dims) * \
+                      (self.g_best[self.ID_POS] - TF * np.mean(list_pos, axis=0))
+            pos_new = self.amend_position_faster(pos_new)
+            fit_new = self.get_fitness_position(pos_new)
+            if self.compare_agent([pos_new, fit_new], self.pop[idx]):
+                self.pop[idx] = [pos_new, fit_new]
 
-        ## Learning Phrase
-        id_partner = np.random.choice(np.setxor1d(np.array(range(self.pop_size)), np.array([idx])))
+            ## Learning Phrase
+            id_partner = np.random.choice(np.setxor1d(np.array(range(self.pop_size)), np.array([idx])))
 
-        #### Remove third loop here
-        if self.compare_agent(pop[idx], pop[id_partner]):
-            diff = pop[idx][self.ID_POS] - pop[id_partner][self.ID_POS]
-        else:
-            diff = pop[id_partner][self.ID_POS] - pop[idx][self.ID_POS]
-        pos_new = pop[idx][self.ID_POS] + np.random.uniform(0, 1, self.problem.n_dims) * diff
-        pos_new = self.amend_position_faster(pos_new)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            return [pos_new, fit_new]
-        return pop[idx].copy()
+            #### Remove third loop here
+            if self.compare_agent(self.pop[idx], self.pop[id_partner]):
+                diff = self.pop[idx][self.ID_POS] - self.pop[id_partner][self.ID_POS]
+            else:
+                diff = self.pop[id_partner][self.ID_POS] - self.pop[idx][self.ID_POS]
+            pos_new = self.pop[idx][self.ID_POS] + np.random.uniform(0, 1, self.problem.n_dims) * diff
+            pos_new = self.amend_position_faster(pos_new)
+            fit_new = self.get_fitness_position(pos_new)
+            if self.compare_agent([pos_new, fit_new], self.pop[idx]):
+                self.pop[idx] = [pos_new, fit_new]
 
 
 class ITLO(BaseTLO):
@@ -159,10 +146,10 @@ class ITLO(BaseTLO):
         self.n_teachers = n_teachers  # Number of teams / group
         self.n_students = pop_size - n_teachers
         self.n_students_in_team = int(self.n_students / self.n_teachers)
+        self.teachers, self.teams = None, None
 
     def classify(self, pop):
-        sorted_pop = sorted(pop, key=lambda item: item[self.ID_FIT])
-        best = sorted_pop[0].copy()
+        sorted_pop, best = self.get_global_best_solution(pop)
         teachers = sorted_pop[:self.n_teachers]
         sorted_pop = sorted_pop[self.n_teachers:]
         idx_list = np.random.permutation(range(0, self.n_students))
@@ -175,100 +162,58 @@ class ITLO(BaseTLO):
             teams.append(group)
         return teachers, teams, best
 
-    def solve(self, mode='sequential'):
+    def initialization(self):
+        self.pop = self.create_population(self.pop_size)
+        self.teachers, self.teams, self.g_best = self.classify(self.pop)
+
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        if mode != "sequential":
-            print("ImprovedTLO is support sequential mode only!")
-            exit(0)
-        self.termination_start()
-        pop = self.create_population(mode, self.pop_size)
-        teachers, teams, g_best = self.classify(pop)
-        self.history.save_initial_best(g_best)
+        for id_teach, teacher in enumerate(self.teachers):
+            team = self.teams[id_teach]
+            list_pos = np.array([student[self.ID_POS] for student in self.teams[id_teach]])  # Step 7
+            mean_team = np.mean(list_pos, axis=0)
+            pop_new = []
+            for id_stud, student in enumerate(team):
+                if teacher[self.ID_FIT][self.ID_TAR] == 0:
+                    TF = 1
+                else:
+                    TF = student[self.ID_FIT][self.ID_TAR] / teacher[self.ID_FIT][self.ID_TAR]
+                diff_mean = np.random.rand() * (teacher[self.ID_POS] - TF * mean_team)  # Step 8
 
-        for epoch in range(0, self.epoch):
-            time_epoch = time.time()
+                id2 = np.random.choice(list(set(range(0, self.n_teachers)) - {id_teach}))
+                if self.compare_agent(teacher, team[id2]):
+                    pos_new = (student[self.ID_POS] + diff_mean) + np.random.rand() * (team[id2][self.ID_POS] - student[self.ID_POS])
+                else:
+                    pos_new = (student[self.ID_POS] + diff_mean) + np.random.rand() * (student[self.ID_POS] - team[id2][self.ID_POS])
+                pos_new = self.amend_position_faster(pos_new)
+                pop_new.append([pos_new, None])
+            pop_new = self.update_fitness_population(pop_new)
+            self.teams[id_teach] = self.greedy_selection_population(team, pop_new)
 
-            for id_teach, teacher in enumerate(teachers):
-                team = teams[id_teach]
-                list_pos = np.array([student[self.ID_POS] for student in teams[id_teach]])  # Step 7
-                mean_team = np.mean(list_pos, axis=0)
-                for id_stud, student in enumerate(team):
-                    if teacher[self.ID_FIT][self.ID_TAR] == 0:
-                        TF = 1
-                    else:
-                        TF = student[self.ID_FIT][self.ID_TAR] / teacher[self.ID_FIT][self.ID_TAR]
-                    diff_mean = np.random.rand() * (teacher[self.ID_POS] - TF * mean_team)  # Step 8
+        for id_teach, teacher in enumerate(self.teachers):
+            ef = round(1 + np.random.rand())
+            team = self.teams[id_teach]
+            pop_new = []
+            for id_stud, student in enumerate(team):
+                id2 = np.random.choice(list(set(range(0, self.n_students_in_team)) - {id_stud}))
+                if self.compare_agent(student, team[id2]):
+                    pos_new = student[self.ID_POS] + np.random.rand() * (student[self.ID_POS] - team[id2][self.ID_POS]) + \
+                              np.random.rand() * (teacher[self.ID_POS] - ef * team[id2][self.ID_POS])
+                else:
+                    pos_new = student[self.ID_POS] + np.random.rand() * (team[id2][self.ID_POS] - student[self.ID_POS]) + \
+                              np.random.rand() * (teacher[self.ID_POS] - ef * student[self.ID_POS])
+                pos_new = self.amend_position_faster(pos_new)
+                pop_new.append([pos_new, None])
+            pop_new = self.update_fitness_population(pop_new)
+            self.teams[id_teach] = self.greedy_selection_population(team, pop_new)
 
-                    id2 = np.random.choice(list(set(range(0, self.n_teachers)) - {id_teach}))
-                    if self.compare_agent(teacher, team[id2]):
-                        pos_new = (student[self.ID_POS] + diff_mean) + np.random.rand() * (team[id2][self.ID_POS] - student[self.ID_POS])
-                    else:
-                        pos_new = (student[self.ID_POS] + diff_mean) + np.random.rand() * (student[self.ID_POS] - team[id2][self.ID_POS])
-                    pos_new = self.amend_position_faster(pos_new)
-                    fit_new = self.get_fitness_position(pos_new)
-                    if self.compare_agent([pos_new, fit_new], student):
-                        teams[id_teach][id_stud] = [pos_new, fit_new]
+        for id_teach, teacher in enumerate(self.teachers):
+            team = self.teams[id_teach] + [teacher]
+            team, local_best = self.get_global_best_solution(team)
+            self.teachers[id_teach] = local_best
+            self.teams[id_teach] = team[1:]
 
-            for id_teach, teacher in enumerate(teachers):
-                ef = round(1 + np.random.rand())
-                team = teams[id_teach]
-                for id_stud, student in enumerate(team):
-                    id2 = np.random.choice(list(set(range(0, self.n_students_in_team)) - {id_stud}))
-                    if self.compare_agent(student, team[id2]):
-                        pos_new = student[self.ID_POS] + np.random.rand() * (student[self.ID_POS] - team[id2][self.ID_POS]) + \
-                                  np.random.rand() * (teacher[self.ID_POS] - ef * team[id2][self.ID_POS])
-                    else:
-                        pos_new = student[self.ID_POS] + np.random.rand() * (team[id2][self.ID_POS] - student[self.ID_POS]) + \
-                                  np.random.rand() * (teacher[self.ID_POS] - ef * student[self.ID_POS])
-                    pos_new = self.amend_position_faster(pos_new)
-                    fit_new = self.get_fitness_position(pos_new)
-                    if self.compare_agent([pos_new, fit_new], student):
-                        teams[id_teach][id_stud] = [pos_new, fit_new]
-
-            for id_teach, teacher in enumerate(teachers):
-                team = teams[id_teach] + [teacher]
-                team = sorted(team, key=lambda item: item[self.ID_FIT])
-                teachers[id_teach] = team[0]
-                teams[id_teach] = team[1:]
-
-            pop = teachers + reduce(lambda x, y: x + y, teams)
-            # update global best position
-            _, g_best = self.update_global_best_solution(pop)
-
-            ## Additional information for the framework
-            time_epoch = time.time() - time_epoch
-            self.history.list_epoch_time.append(time_epoch)
-            self.history.list_population.append(pop.copy())
-            self.print_epoch(epoch + 1, time_epoch)
-            if self.termination_flag:
-                if self.termination.mode == 'TB':
-                    if time.time() - self.count_terminate >= self.termination.quantity:
-                        self.termination.logging(self.verbose)
-                        break
-                elif self.termination.mode == 'FE':
-                    self.count_terminate += self.nfe_per_epoch
-                    if self.count_terminate >= self.termination.quantity:
-                        self.termination.logging(self.verbose)
-                        break
-                elif self.termination.mode == 'MG':
-                    if epoch >= self.termination.quantity:
-                        self.termination.logging(self.verbose)
-                        break
-                else:  # Early Stopping
-                    temp = self.count_terminate + self.history.get_global_repeated_times(self.ID_FIT, self.ID_TAR, self.EPSILON)
-                    if temp >= self.termination.quantity:
-                        self.termination.logging(self.verbose)
-                        break
-
-        ## Additional information for the framework
-        self.save_optimization_process()
-        return self.solution[self.ID_POS], self.solution[self.ID_FIT][self.ID_TAR]
+        self.pop = self.teachers + reduce(lambda x, y: x + y, self.teams)
