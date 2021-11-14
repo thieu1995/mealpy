8f648174208129892b1b47e03fa5749a7f627dec
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-14 19:09:54 +0700

    Update WDO optimizer

--- mealpy/physics_based/WDO.py
+++ mealpy/physics_based/WDO.py
@@ -7,8 +7,6 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from mealpy.optimizer import Optimizer
 
@@ -53,48 +51,22 @@ class BaseWDO(Optimizer):
         ## Dynamic variable
         self.dyn_list_velocity = self.max_v * np.random.uniform(self.problem.lb, self.problem.ub, (self.pop_size, self.problem.n_dims))
 
-    def create_child(self, idx, pop, g_best):
-        rand_dim = np.random.randint(0, self.problem.n_dims)
-        temp = self.dyn_list_velocity[idx][rand_dim] * np.ones(self.problem.n_dims)
-        vel = (1 - self.alp) * self.dyn_list_velocity[idx] - self.g_c * pop[idx][self.ID_POS] + \
-              (1 - 1.0 / (idx + 1)) * self.RT * (g_best[self.ID_POS] - pop[idx][self.ID_POS]) + self.c_e * temp / (idx + 1)
-        vel = np.clip(vel, -self.max_v, self.max_v)
-
-        # Update air parcel positions, check the bound and calculate pressure (fitness)
-        self.dyn_list_velocity[idx] = vel
-        pos = pop[idx][self.ID_POS] + vel
-        pos_new = self.amend_position_faster(pos)
-        fit_new = self.get_fitness_position(pos_new)
-        return [pos_new, fit_new]
-
-        # ## batch size idea
-        # if self.batch_idea:
-        #     if (i + 1) % self.batch_size == 0:
-        #         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-        # else:
-        #     if (i + 1) % self.pop_size == 0:
-        #         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, g_best) for idx in pop_idx]
-        return child
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            rand_dim = np.random.randint(0, self.problem.n_dims)
+            temp = self.dyn_list_velocity[idx][rand_dim] * np.ones(self.problem.n_dims)
+            vel = (1 - self.alp) * self.dyn_list_velocity[idx] - self.g_c * self.pop[idx][self.ID_POS] + \
+                  (1 - 1.0 / (idx + 1)) * self.RT * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]) + self.c_e * temp / (idx + 1)
+            vel = np.clip(vel, -self.max_v, self.max_v)
+
+            # Update air parcel positions, check the bound and calculate pressure (fitness)
+            self.dyn_list_velocity[idx] = vel
+            pos = self.pop[idx][self.ID_POS] + vel
+            pos_new = self.amend_position_faster(pos)
+            pop_new.append([pos_new, None])
+        self.pop = self.update_fitness_population(pop_new)
