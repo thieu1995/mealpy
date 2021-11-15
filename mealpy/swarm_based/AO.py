ec0b0280fe0094a1a7510cf7b6cc82254ad4f045
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-15 12:19:27 +0700

    Update AO optimizer

--- mealpy/swarm_based/AO.py
+++ mealpy/swarm_based/AO.py
@@ -7,8 +7,6 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from math import gamma
 from mealpy.optimizer import Optimizer
@@ -39,37 +37,18 @@ class OriginalAO(Optimizer):
         self.alpha = 0.1
         self.delta = 0.1
 
-    def create_child(self, idx, pop, epoch, g_best, x, y, g1, g2, QF):
-        x_mean = np.mean(np.array([item[self.ID_FIT][self.ID_TAR] for item in pop]), axis=0)
-        if (epoch + 1) <= (2 / 3) * self.epoch:  # Eq. 3, 4
-            if np.random.rand() < 0.5:
-                pos_new = g_best[self.ID_POS] * (1 - (epoch + 1) / self.epoch) + np.random.rand() * (x_mean - g_best[self.ID_POS])
-            else:
-                idx = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
-                pos_new = g_best[self.ID_POS] * self.get_simple_levy_step() + pop[idx][self.ID_POS] + np.random.rand() * (y - x)  # Eq. 5
-        else:
-            if np.random.rand() < 0.5:
-                pos_new = self.alpha * (g_best[self.ID_POS] - x_mean) - np.random.rand() * \
-                          (np.random.rand() * (self.problem.ub - self.problem.lb) + self.problem.lb) * self.delta  # Eq. 13
-            else:
-                pos_new = QF * g_best[self.ID_POS] - (g2 * pop[idx][self.ID_POS] *
-                                        np.random.rand()) - g2 * self.get_simple_levy_step() + np.random.rand() * g1  # Eq. 14
-        pos_new = self.amend_position_faster(pos_new)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            return [pos_new, fit_new]
-        return pop[idx].copy()
+    def get_simple_levy_step(self):
+        beta = 1.5
+        sigma = (gamma(1 + beta) * np.sin(np.pi * beta / 2) / (gamma((1 + beta) / 2) * beta * 2 ** ((beta - 1) / 2))) ** (1 / beta)
+        u = np.random.normal(0, 1, self.problem.n_dims) * sigma
+        v = np.random.normal(1, self.problem.n_dims)
+        step = u / abs(v) ** (1 / beta)
+        return step
 
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
         g1 = 2 * np.random.rand() - 1  # Eq. 16
         g2 = 2 * (1 - epoch / self.epoch)  # Eq. 17
@@ -85,26 +64,27 @@ class OriginalAO(Optimizer):
         y = r * np.cos(phi)  # Eq.(10)
         QF = (epoch + 1) ** ((2 * np.random.rand() - 1) / (1 - self.epoch) ** 2)  # Eq.(15)        Quality function
 
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, epoch=epoch,
-                                                 g_best=g_best, x=x, y=y, g1=g1, g2=g2, QF=QF), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, epoch=epoch,
-                                                 g_best=g_best, x=x, y=y, g1=g1, g2=g2, QF=QF), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, epoch, g_best, x, y, g1, g2, QF) for idx in pop_idx]
-        return pop + child
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            x_mean = np.mean(np.array([item[self.ID_FIT][self.ID_TAR] for item in self.pop]), axis=0)
+            if (epoch + 1) <= (2 / 3) * self.epoch:  # Eq. 3, 4
+                if np.random.rand() < 0.5:
+                    pos_new = self.g_best[self.ID_POS] * (1 - (epoch + 1) / self.epoch) + \
+                              np.random.rand() * (x_mean - self.g_best[self.ID_POS])
+                else:
+                    idx = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
+                    pos_new = self.g_best[self.ID_POS] * self.get_simple_levy_step() + \
+                              self.pop[idx][self.ID_POS] + np.random.rand() * (y - x)  # Eq. 5
+            else:
+                if np.random.rand() < 0.5:
+                    pos_new = self.alpha * (self.g_best[self.ID_POS] - x_mean) - np.random.rand() * \
+                              (np.random.rand() * (self.problem.ub - self.problem.lb) + self.problem.lb) * self.delta  # Eq. 13
+                else:
+                    pos_new = QF * self.g_best[self.ID_POS] - (g2 * self.pop[idx][self.ID_POS] *
+                            np.random.rand()) - g2 * self.get_simple_levy_step() + np.random.rand() * g1  # Eq. 14
+            pos_new = self.amend_position_faster(pos_new)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        self.pop = self.greedy_selection_population(self.pop, pop_new)
 
-    def get_simple_levy_step(self):
-        beta = 1.5
-        sigma = (gamma(1 + beta) * np.sin(np.pi * beta / 2) / (gamma((1 + beta) / 2) * beta * 2 ** ((beta - 1) / 2))) ** (1 / beta)
-        u = np.random.normal(0, 1, self.problem.n_dims) * sigma
-        v = np.random.normal(1, self.problem.n_dims)
-        step = u / abs(v) ** (1 / beta)
-        return step
 
