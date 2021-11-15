77eddcf6ad6fe1e912b1746502e95b441fc6d8b9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-15 15:29:07 +0700

    Update BA optimizer

--- mealpy/swarm_based/BA.py
+++ mealpy/swarm_based/BA.py
@@ -7,8 +7,6 @@
 #       Github:     https://github.com/thieu1995                                                        %
 #-------------------------------------------------------------------------------------------------------%
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from mealpy.optimizer import Optimizer
 
@@ -68,51 +66,33 @@ class BasicBA(Optimizer):
         pulse_frequency = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()
         return [position, fitness, velocity, loudness, pulse_rate, pulse_frequency]
 
-    def create_child(self, idx, pop, g_best, epoch, mean_a):
-        agent = pop[idx].copy()
-        agent[self.ID_VEC] = agent[self.ID_VEC] + pop[idx][self.ID_PFRE] * (pop[idx][self.ID_POS] - g_best[self.ID_POS])
-        x = pop[idx][self.ID_POS] + agent[self.ID_VEC]
-        ## Local Search around g_best position
-        if np.random.uniform() > agent[self.ID_PRAT]:
-            # print(f"{epoch}, {mean_a}, {self.dyn_r}")
-            x = g_best[self.ID_POS] + mean_a * np.random.normal(-1, 1)
-        pos_new = self.amend_position_faster(x)
-        fit_new = self.get_fitness_position(pos_new)
-
-        ## Replace the old position by the new one when its has better fitness.
-        ##  and then update loudness and emission rate
-        if self.compare_agent([pos_new, fit_new], pop[idx]) and np.random.rand() < agent[self.ID_LOUD]:
-            agent[self.ID_POS] = pos_new
-            agent[self.ID_FIT] = fit_new
-            agent[self.ID_LOUD] = self.alpha * agent[self.ID_LOUD]
-            agent[self.ID_PRAT] = agent[self.ID_PRAT] * (1 - np.exp(-self.gamma * (epoch + 1)))
-            return agent
-        return pop[idx].copy()
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        mean_a = np.mean([agent[self.ID_LOUD] for agent in pop])
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, epoch=epoch, mean_a=mean_a), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, epoch=epoch, mean_a=mean_a), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, g_best, epoch, mean_a) for idx in pop_idx]
-        return child
+        mean_a = np.mean([agent[self.ID_LOUD] for agent in self.pop])
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            agent = self.pop[idx].copy()
+            agent[self.ID_VEC] = agent[self.ID_VEC] + self.pop[idx][self.ID_PFRE] * (self.pop[idx][self.ID_POS] - self.g_best[self.ID_POS])
+            x = self.pop[idx][self.ID_POS] + agent[self.ID_VEC]
+            ## Local Search around g_best position
+            if np.random.uniform() > agent[self.ID_PRAT]:
+                # print(f"{epoch}, {mean_a}, {self.dyn_r}")
+                x = self.g_best[self.ID_POS] + mean_a * np.random.normal(-1, 1)
+            pos_new = self.amend_position_faster(x)
+            agent[self.ID_POS] = pos_new
+            pop_new.append(agent)
+        pop_new = self.update_fitness_population(pop_new)
+
+        for idx in range(0, self.pop_size):
+            ## Replace the old position by the new one when its has better fitness.
+            ##  and then update loudness and emission rate
+            if self.compare_agent(pop_new[idx], self.pop[idx]) and np.random.rand() < pop_new[idx][self.ID_LOUD]:
+                pop_new[idx][self.ID_LOUD] = self.alpha * pop_new[idx][self.ID_LOUD]
+                pop_new[idx][self.ID_PRAT] = pop_new[idx][self.ID_PRAT] * (1 - np.exp(-self.gamma * (epoch + 1)))
+                self.pop[idx] = pop_new[idx].copy()
 
 
 class BaseBA(Optimizer):
@@ -150,46 +130,40 @@ class BaseBA(Optimizer):
 
         self.dyn_list_velocity = np.zeros((self.pop_size, self.problem.n_dims))
 
-    def create_child(self, idx, pop, g_best):
-        pf = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()  # Eq. 2
-        self.dyn_list_velocity[idx] = np.random.uniform() * self.dyn_list_velocity[idx] + (g_best[self.ID_POS] - pop[idx][self.ID_POS]) * pf  # Eq. 3
-        x = pop[idx][self.ID_POS] + self.dyn_list_velocity[idx]  # Eq. 4
-        pos_new = self.amend_position_faster(x)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            return [pos_new, fit_new]
-        else:
-            if np.random.uniform() > self.pulse_rate:
-                x = g_best[self.ID_POS] + 0.01 * np.random.uniform(self.problem.lb, self.problem.ub)
-                pos_new = self.amend_position_faster(x)
-                fit_new = self.get_fitness_position(x)
-                if self.compare_agent([pos_new, fit_new], pop[idx]):
-                    return [pos_new, fit_new]
-            return pop[idx].copy()
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, g_best) for idx in pop_idx]
-        return child
+        nfe_epoch = 0
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            pf = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()  # Eq. 2
+            self.dyn_list_velocity[idx] = np.random.uniform() * self.dyn_list_velocity[idx] + \
+                                          (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]) * pf  # Eq. 3
+            x = self.pop[idx][self.ID_POS] + self.dyn_list_velocity[idx]  # Eq. 4
+            pos_new = self.amend_position_faster(x)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        nfe_epoch += self.pop_size
+        pop_child_idx = []
+        pop_child = []
+        for idx in range(0, self.pop_size):
+            if self.compare_agent(pop_new[idx], self.pop[idx]):
+                self.pop[idx] = pop_new[idx].copy()
+            else:
+                if np.random.random() > self.pulse_rate:
+                    x = self.g_best[self.ID_POS] + 0.01 * np.random.uniform(self.problem.lb, self.problem.ub)
+                    pos_new = self.amend_position_faster(x)
+                    pop_child_idx.append(idx)
+                    pop_child.append([pos_new, None])
+                    nfe_epoch += 1
+        pop_child = self.update_fitness_population(pop_child)
+        for idx, idx_selected in enumerate(pop_child_idx):
+            if self.compare_agent(pop_child[idx], pop_new[idx_selected]):
+                pop_new[idx_selected] = pop_child[idx].copy()
+        self.pop = pop_new
+        self.nfe_per_epoch = nfe_epoch
 
 
 class OriginalBA(Optimizer):
@@ -242,45 +216,27 @@ class OriginalBA(Optimizer):
         pulse_frequency = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()
         return [position, fitness, velocity, pulse_frequency]
 
-    def create_child(self, idx, pop, g_best):
-        agent = pop[idx].copy()
-        agent[self.ID_VEC] = agent[self.ID_VEC] + pop[idx][self.ID_PFRE] * (pop[idx][self.ID_POS] - g_best[self.ID_POS])
-        x = pop[idx][self.ID_POS] + agent[self.ID_VEC]
-        ## Local Search around g_best position
-        if np.random.uniform() > self.pulse_rate:
-            # print(f"{epoch}, {mean_a}, {self.dyn_r}")
-            x = g_best[self.ID_POS] + 0.001 * np.random.normal(self.problem.n_dims)  # gauss
-        pos_new = self.amend_position_faster(x)
-        fit_new = self.get_fitness_position(pos_new)
-
-        ## Replace the old position by the new one when its has better fitness.
-        ##  and then update loudness and emission rate
-        if self.compare_agent([pos_new, fit_new], pop[idx]) and np.random.rand() < self.loudness:
-            agent[self.ID_POS] = pos_new
-            agent[self.ID_FIT] = fit_new
-            return agent
-        return pop[idx].copy()
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, g_best) for idx in pop_idx]
-        return child
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            agent = self.pop[idx].copy()
+            agent[self.ID_VEC] = agent[self.ID_VEC] + self.pop[idx][self.ID_PFRE] * (self.pop[idx][self.ID_POS] - self.g_best[self.ID_POS])
+            x = self.pop[idx][self.ID_POS] + agent[self.ID_VEC]
+            ## Local Search around g_best position
+            if np.random.uniform() > self.pulse_rate:
+                x = self.g_best[self.ID_POS] + 0.0001 * np.random.normal(self.problem.n_dims)  # gauss
+            pos_new = self.amend_position_faster(x)
+            agent[self.ID_POS] = pos_new
+            pop_new.append(agent)
+        pop_new = self.update_fitness_population(pop_new)
+        for idx in range(self.pop_size):
+            ## Replace the old position by the new one when its has better fitness.
+            ##  and then update loudness and emission rate
+            if self.compare_agent(pop_new[idx], self.pop[idx]) and np.random.rand() < self.loudness:
+                self.pop[idx] = pop_new[idx].copy()
+
+
