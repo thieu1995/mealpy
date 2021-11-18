ac3c185bf9e853552f7c300e847a12f332e92b20
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-18 11:07:40 +0700

    Udpate SHO optimizer

--- mealpy/swarm_based/SHO.py
+++ mealpy/swarm_based/SHO.py
@@ -7,8 +7,6 @@
 #       Github:     https://github.com/thieu1995                                                        #
 #-------------------------------------------------------------------------------------------------------#
 
-import concurrent.futures as parallel
-from functools import partial
 import numpy as np
 from mealpy.optimizer import Optimizer
 
@@ -42,60 +40,45 @@ class BaseSHO(Optimizer):
         self.M = M
         self.N_tried = N_tried
 
-    def create_child(self, idx, pop, g_best, epoch):
-        h = 5 - (epoch + 1.0) * (5 / self.epoch)
-        rd1 = np.random.uniform(0, 1, self.problem.n_dims)
-        rd2 = np.random.uniform(0, 1, self.problem.n_dims)
-        B = 2 * rd1
-        E = 2 * h * rd2 - h
-
-        if np.random.rand() < 0.5:
-            D_h = np.abs(np.dot(B, g_best[self.ID_POS]) - pop[idx][self.ID_POS])
-            x_new = g_best[self.ID_POS] - np.dot(E, D_h)
-        else:
-            N = 0
-            for i in range(0, self.N_tried):
-                pos_new = g_best[self.ID_POS] + np.random.uniform(self.M[0], self.M[1]) * np.random.uniform(self.problem.lb, self.problem.ub)
-                pos_new = self.amend_position_faster(pos_new)
-                fit_new = self.get_fitness_position(pos_new)
-                if self.compare_agent(g_best, [pos_new, fit_new]):
-                    N += 1
-                    break
-                N += 1
-            circle_list = []
-            idx_list = np.random.choice(range(0, self.pop_size), N, replace=False)
-            for j in range(0, N):
-                D_h = np.abs(np.dot(B, g_best[self.ID_POS]) - pop[idx_list[j]][self.ID_POS])
-                p_k = g_best[self.ID_POS] - np.dot(E, D_h)
-                circle_list.append(p_k)
-            x_new = np.mean(np.array(circle_list))
-        pos_new = self.amend_position_faster(x_new)
-        fit_new = self.get_fitness_position(pos_new)
-        if self.compare_agent([pos_new, fit_new], pop[idx]):
-            return [pos_new, fit_new]
-        return pop[idx].copy()
-
-    def evolve(self, mode='sequential', epoch=None, pop=None, g_best=None):
+    def evolve(self, epoch):
         """
         Args:
-            mode (str): 'sequential', 'thread', 'process'
-                + 'sequential': recommended for simple and small task (< 10 seconds for calculating objective)
-                + 'thread': recommended for IO bound task, or small computing task (< 2 minutes for calculating objective)
-                + 'process': recommended for hard and big task (> 2 minutes for calculating objective)
-
-        Returns:
-            [position, fitness value]
+            epoch (int): The current iteration
         """
-        pop_idx = np.array(range(0, self.pop_size))
-        if mode == "thread":
-            with parallel.ThreadPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, epoch=epoch), pop_idx)
-            child = [x for x in pop_child]
-        elif mode == "process":
-            with parallel.ProcessPoolExecutor() as executor:
-                pop_child = executor.map(partial(self.create_child, pop=pop, g_best=g_best, epoch=epoch), pop_idx)
-            child = [x for x in pop_child]
-        else:
-            child = [self.create_child(idx, pop, g_best, epoch=epoch) for idx in pop_idx]
-        return child
+        nfe_epoch = 0
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            h = 5 - (epoch + 1.0) * (5 / self.epoch)
+            rd1 = np.random.uniform(0, 1, self.problem.n_dims)
+            rd2 = np.random.uniform(0, 1, self.problem.n_dims)
+            B = 2 * rd1
+            E = 2 * h * rd2 - h
 
+            if np.random.rand() < 0.5:
+                D_h = np.abs(np.dot(B, self.g_best[self.ID_POS]) - self.pop[idx][self.ID_POS])
+                pos_new = self.g_best[self.ID_POS] - np.dot(E, D_h)
+            else:
+                N = 0
+                for i in range(0, self.N_tried):
+                    pos_new = self.g_best[self.ID_POS] + np.random.uniform(self.M[0], self.M[1]) * \
+                              np.random.uniform(self.problem.lb, self.problem.ub)
+                    pos_new = self.amend_position_faster(pos_new)
+                    fit_new = self.get_fitness_position(pos_new)
+                    if self.compare_agent(self.g_best, [pos_new, fit_new]):
+                        N += 1
+                        nfe_epoch += 1
+                        break
+                    N += 1
+                circle_list = []
+                idx_list = np.random.choice(range(0, self.pop_size), N, replace=False)
+                for j in range(0, N):
+                    D_h = np.abs(np.dot(B, self.g_best[self.ID_POS]) - self.pop[idx_list[j]][self.ID_POS])
+                    p_k = self.g_best[self.ID_POS] - np.dot(E, D_h)
+                    circle_list.append(p_k)
+                pos_new = np.mean(np.array(circle_list))
+            pos_new = self.amend_position_faster(pos_new)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        self.pop = self.greedy_selection_population(self.pop, pop_new)
+        nfe_epoch += self.pop_size
+        self.nfe_per_epoch = nfe_epoch
