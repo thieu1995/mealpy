fe4893a161909a5c9b58c72dddfb57cdee1a40d8
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-11-18 09:25:55 +0700

    Rename SalpSO to SSO and update SSO

---
+++ mealpy/swarm_based/SSO.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu" at 11:38, 02/03/2021                                                               %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
+#       Github:     https://github.com/thieu1995                                                        %
+# ------------------------------------------------------------------------------------------------------%
+
+import numpy as np
+from mealpy.optimizer import Optimizer
+
+
+class BaseSSO(Optimizer):
+    """
+    The original version of: Salp Swarm Optimization (SalpSO)
+    Link:
+        https://doi.org/10.1016/j.advengsoft.2017.07.002
+    """
+
+    def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = True
+
+        self.epoch = epoch
+        self.pop_size = pop_size
+
+    def evolve(self, epoch):
+        """
+        Args:
+            epoch (int): The current iteration
+        """
+        ## Eq. (3.2) in the paper
+        c1 = 2 * np.exp(-((4 * (epoch + 1) / self.epoch) ** 2))
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            if idx < self.pop_size / 2:
+                c2_list = np.random.random(self.problem.n_dims)
+                c3_list = np.random.random(self.problem.n_dims)
+                pos_new_1 = self.g_best[self.ID_POS] + c1 * ((self.problem.ub - self.problem.lb) * c2_list + self.problem.lb)
+                pos_new_2 = self.g_best[self.ID_POS] - c1 * ((self.problem.ub - self.problem.lb) * c2_list + self.problem.lb)
+                pos_new = np.where(c3_list < 0.5, pos_new_1, pos_new_2)
+            else:
+                # Eq. (3.4) in the paper
+                pos_new = (self.pop[idx][self.ID_POS] + self.pop[idx - 1][self.ID_POS]) / 2
+
+            # Check if salps go out of the search space and bring it back then re-calculate its fitness value
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        self.pop = self.greedy_selection_population(self.pop, pop_new)
