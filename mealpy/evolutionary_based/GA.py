3aeace0d47a28d0388f309310644ccc667e12ad1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-27 21:56:52 +0700

    Update docs for GA in evolutionary_based group

--- mealpy/evolutionary_based/GA.py
+++ mealpy/evolutionary_based/GA.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 09:33, 16/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-# ------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 09:33, 16/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from mealpy.optimizer import Optimizer
@@ -13,16 +10,51 @@ from mealpy.optimizer import Optimizer
 
 class BaseGA(Optimizer):
     """
-        Genetic Algorithm (GA)
-    Link:
-        https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
-        https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
-        https://www.analyticsvidhya.com/blog/2017/07/introduction-to-genetic-algorithm/
+    The original version of: Genetic Algorithm (GA)
+
+    Links:
+        1. https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
+        2. https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
+        3. https://www.analyticsvidhya.com/blog/2017/07/introduction-to-genetic-algorithm/
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + pc (float): [0.7, 0.95], cross-over probability, default = 0.95
+        + pm (float): [0.01, 0.2], mutation probability, default = 0.025
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.evolutionary_based.GA import BaseGA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pc = 0.9
+    >>> pm = 0.05
+    >>> model = BaseGA(problem_dict1, epoch, pop_size, pc, pm)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Whitley, D., 1994. A genetic algorithm tutorial. Statistics and computing, 4(2), pp.65-85.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, pc=0.95, pm=0.025, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
             pc (float): cross-over probability, default = 0.95
@@ -39,6 +71,8 @@ class BaseGA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
