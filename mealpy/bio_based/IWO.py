0150e5900a8407d1ba740be1034c8cdb8876c4e5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-27 15:43:18 +0700

    Update docs for IWO in bio_based group

--- mealpy/bio_based/IWO.py
+++ mealpy/bio_based/IWO.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 12:17, 18/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-#-------------------------------------------------------------------------------------------------------%
+# Created by "Thieu" at 12:17, 18/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from mealpy.optimizer import Optimizer
@@ -13,24 +10,62 @@ from mealpy.optimizer import Optimizer
 
 class OriginalIWO(Optimizer):
     """
-    Original version of: Invasive Weed Optimization (IWO)
-        A novel numerical optimization algorithm inspired from weed colonization
-        (https://pdfs.semanticscholar.org/734c/66e3757620d3d4016410057ee92f72a9853d.pdf)
-    Noted:
-        + Better to use normal distribution instead of uniform distribution
-        + Updating population by sorting both parent population and child population
+    The original version of: Invasive Weed Optimization (IWO)
+
+    Links:
+        1. https://pdfs.semanticscholar.org/734c/66e3757620d3d4016410057ee92f72a9853d.pdf
+
+    Notes
+    ~~~~~
+    Better to use normal distribution instead of uniform distribution, updating population by sorting
+    both parent population and child population
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + seeds (list): (min_value, max_value) -> ([1, 3], [5, 10]), Number of Seeds
+        + exponent (int): [2, 4], Variance Reduction Exponent
+        + sigma (list): (initial_value, final_value), ([0.5, 0.9], [0.001, 0.1]), Value of Standard Deviation
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.bio_based.IWO import OriginalIWO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> seeds = [3, 9]
+    >>> exponent = 3
+    >>> sigma = [0.6, 0.01]
+    >>> model = OriginalIWO(problem_dict1, epoch, pop_size, seeds, exponent, sigma)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Mehrabian, A.R. and Lucas, C., 2006. A novel numerical optimization algorithm inspired from weed colonization.
+    Ecological informatics, 1(4), pp.355-366.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, seeds=(2, 10), exponent=2, sigma=(0.5, 0.001), **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
             seeds (list): (Min, Max) Number of Seeds
             exponent (int): Variance Reduction Exponent
             sigma (list): (Initial, Final) Value of Standard Deviation
-            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -44,6 +79,8 @@ class OriginalIWO(Optimizer):
 
     def evolve(self, epoch=None):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
