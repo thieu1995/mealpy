3c924954c6c9abdea650b9e03496b804d485fe5f
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-27 22:45:12 +0700

    Update docs for CA in human_based group

--- mealpy/human_based/CA.py
+++ mealpy/human_based/CA.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu" at 12:09, 02/03/2021                                                               %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-# ------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 12:09, 02/03/2021 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from mealpy.optimizer import Optimizer
@@ -14,15 +11,50 @@ from mealpy.optimizer import Optimizer
 class OriginalCA(Optimizer):
     """
     The original version of: Culture Algorithm (CA)
-        Based on Ruby version in the book: Clever Algorithm (Jason Brown)
+
+    Links:
+        1. https://github.com/clever-algorithms/CleverAlgorithms
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + accepted_rate (float): [0.1, 0.5], probability of accepted rate, default: 0.15
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.human_based.CA import OriginalCA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> accepted_rate = 0.15
+    >>> model = OriginalCA(problem_dict1, epoch, pop_size, accepted_rate)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Chen, B., Zhao, L. and Lu, J.H., 2009, April. Wind power forecast using RBF network and culture algorithm.
+    In 2009 International Conference on Sustainable Power Generation and Supply (pp. 1-6). IEEE.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, accepted_rate=0.15, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            accepted_rate (float): probability of accepted rate, Default: 0.15,
+            accepted_rate (float): probability of accepted rate, default: 0.15
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -53,6 +85,8 @@ class OriginalCA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
