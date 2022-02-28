a3f2e0e7247df6e2930412cc8c1c568f42a335c9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 21:23:45 +0700

    Update docs for NMRA in swarm_based group

--- mealpy/swarm_based/NMRA.py
+++ mealpy/swarm_based/NMRA.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 14:52, 17/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-#-------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 14:52, 17/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -14,21 +11,51 @@ from mealpy.optimizer import Optimizer
 
 class BaseNMR(Optimizer):
     """
-    The original version of: Naked Mole-rat Algorithm (NMRA)
-        (The naked mole-rat algorithm)
-    Link:
-        https://www.doi.org10.1007/s00521-019-04464-7
+    The original version of: Naked Mole-Rat Algorithm (NMRA)
+
+    Links:
+        1. https://www.doi.org10.1007/s00521-019-04464-7
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + pb (float): [0.5, 0.95], probability of breeding, default = 0.75
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.NMRA import BaseNMR
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pb = 0.75
+    >>> model = BaseNMR(problem_dict1, epoch, pop_size, pb)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Salgotra, R. and Singh, U., 2019. The naked mole-rat algorithm.
+    Neural Computing and Applications, 31(12), pp.8837-8857.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, bp=0.75, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, pb=0.75, **kwargs):
         """
-
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            bp (float): breeding probability (0.75)
-            **kwargs ():
+            pb (float): probability of breeding, default = 0.75
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = self.pop_size
@@ -37,11 +64,12 @@ class BaseNMR(Optimizer):
         self.epoch = epoch
         self.pop_size = pop_size
         self.size_b = int(self.pop_size / 5)
-        self.size_w = self.pop_size - self.size_b
-        self.bp = bp
+        self.pb = pb
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -49,7 +77,7 @@ class BaseNMR(Optimizer):
         for idx in range(0, self.pop_size):
             pos_new = deepcopy(self.pop[idx][self.ID_POS])
             if idx < self.size_b:  # breeding operators
-                if np.random.uniform() < self.bp:
+                if np.random.uniform() < self.pb:
                     alpha = np.random.uniform()
                     pos_new = (1 - alpha) * self.pop[idx][self.ID_POS] + alpha * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
             else:  # working operators
@@ -61,29 +89,67 @@ class BaseNMR(Optimizer):
         self.pop = self.greedy_selection_population(self.pop, pop_new)
 
 
-class ImprovedNMR(BaseNMR):
+class ImprovedNMR(Optimizer):
     """
-    My improved version of: Naked Mole-rat Algorithm (NMRA)
-        (The naked mole-rat algorithm)
+    The original version of: Naked Mole-Rat Algorithm (I-NMRA)
+
     Notes:
-        + Using mutation probability
-        + Using levy-flight
-        + Using crossover operator
+    + Use mutation probability idea
+    + Use crossover operator
+    + Use Levy-flight technique
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + pb (float): [0.5, 0.95], probability of breeding, default = 0.75
+        + pm (float): [0.01, 0.1], probability of mutation, default = 0.01
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.NMRA import ImprovedNMR
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pb = 0.75
+    >>> pm = 0.01
+    >>> model = ImprovedNMR(problem_dict1, epoch, pop_size, pb, pm)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Salgotra, R. and Singh, U., 2019. The naked mole-rat algorithm.
+    Neural Computing and Applications, 31(12), pp.8837-8857.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, bp=0.75, pm=0.01, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, pb=0.75, pm=0.01, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            bp (float): breeding probability (0.75)
-            pm ():
-            **kwargs ():
+            pb (float): breeding probability, default = 0.75
+            pm (float): probability of mutation, default = 0.01
         """
-        super().__init__(problem, epoch, pop_size, bp, **kwargs)
+        super().__init__(problem, kwargs)
         self.nfe_per_epoch = self.pop_size
         self.sort_flag = True
+
+        self.epoch = epoch
+        self.pop_size = pop_size
+        self.size_b = int(self.pop_size / 5)
+        self.pb = pb
         self.pm = pm
 
     def _crossover_random(self, pop, g_best):
@@ -101,6 +167,8 @@ class ImprovedNMR(BaseNMR):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -108,7 +176,7 @@ class ImprovedNMR(BaseNMR):
         for idx in range(0, self.pop_size):
             # Exploration
             if idx < self.size_b:  # breeding operators
-                if np.random.uniform() < self.bp:
+                if np.random.uniform() < self.pb:
                     pos_new = self.pop[idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * \
                               (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
                 else:
