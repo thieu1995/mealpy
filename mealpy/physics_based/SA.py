6320e2d0a2159f758807a3a14c577c22527ce206
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 10:09:35 +0700

    Update docs for SA in physics_based group

--- mealpy/physics_based/SA.py
+++ mealpy/physics_based/SA.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu" at 22:08, 01/03/2021                                                               %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-# ------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 22:08, 01/03/2021 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -14,24 +11,67 @@ from mealpy.optimizer import Optimizer
 
 class BaseSA(Optimizer):
     """
-        The original version of: Simulated Annealing (SA)
+    The original version of: Simulated Annealing (SA)
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + max_sub_iter (int): [5, 10, 15], Maximum Number of Sub-Iteration (within fixed temperature), default=5
+        + t0 (int): Fixed parameter, Initial Temperature, default=1000
+        + t1 (int): Fixed parameter, Final Temperature, default=1
+        + move_count (int): [5, 20], Move Count per Individual Solution, default=5
+        + mutation_rate (float): [0.01, 0.2], Mutation Rate, default=0.1
+        + mutation_step_size (float): [0.05, 0.1, 0.15], Mutation Step Size, default=0.1
+        + mutation_step_size_damp (float): [0.8, 0.99], Mutation Step Size Damp, default=0.99
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.physics_based.SA import BaseSA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> max_sub_iter = 5
+    >>> t0 = 1000
+    >>> t1 = 1
+    >>> move_count = 5
+    >>> mutation_rate = 0.1
+    >>> mutation_step_size = 0.1
+    >>> mutation_step_size_damp = 0.99
+    >>> model = BaseSA(problem_dict1, epoch, pop_size, max_sub_iter, t0, t1, move_count, mutation_rate, mutation_step_size, mutation_step_size_damp)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Van Laarhoven, P.J. and Aarts, E.H., 1987. Simulated annealing. In Simulated
+    annealing: Theory and applications (pp. 7-15). Springer, Dordrecht.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, max_sub_iter=5, t0=1000, t1=1, move_count=5,
                  mutation_rate=0.1, mutation_step_size=0.1, mutation_step_size_damp=0.99, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            max_sub_iter (int): Maximum Number of Sub-Iteration (within fixed temperature)
-            t0 (int): Initial Temperature
-            t1 (int): Final Temperature
-            move_count (int): Move Count per Individual Solution
-            mutation_rate (float): Mutation Rate
-            mutation_step_size (float): Mutation Step Size
-            mutation_step_size_damp (float): Mutation Step Size Damp
-            **kwargs ():
+            max_sub_iter (int): Maximum Number of Sub-Iteration (within fixed temperature), default=5
+            t0 (int): Initial Temperature, default=1000
+            t1 (int): Final Temperature, default=1
+            move_count (int): Move Count per Individual Solution, default=5
+            mutation_rate (float): Mutation Rate, default=0.1
+            mutation_step_size (float): Mutation Step Size, default=0.1
+            mutation_step_size_damp (float): Mutation Step Size Damp, default=0.99
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size * max_sub_iter * move_count
@@ -68,6 +108,8 @@ class BaseSA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
