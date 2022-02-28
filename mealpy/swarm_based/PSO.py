fe7e80d67d69426335751c83a66947a6ca6e1b25
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 21:51:50 +0700

    Update docs for PSO in swarm_based group

--- mealpy/swarm_based/PSO.py
+++ mealpy/swarm_based/PSO.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 09:49, 17/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-#-------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 09:49, 17/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -14,23 +11,61 @@ from mealpy.optimizer import Optimizer
 
 class BasePSO(Optimizer):
     """
-        The original version of: Particle Swarm Optimization (PSO)
+    The original version of: Particle Swarm Optimization (PSO)
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + c1 (float): [1, 3], local coefficient, default = 2.05
+        + c2 (float): [1, 3], global coefficient, default = 2.05
+        + w_min (float): [0.1, 0.5], Weight min of bird, default = 0.4
+        + w_max (float): [0.8, 2.0], Weight max of bird, default = 0.9
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.PSO import BasePSO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> c1 = 2.05
+    >>> c2 = 2.05
+    >>> w_min = 0.4
+    >>> w_max = 0.9
+    >>> model = BasePSO(problem_dict1, epoch, pop_size, c1, c2, w_min, w_max)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Kennedy, J. and Eberhart, R., 1995, November. Particle swarm optimization. In Proceedings of
+    ICNN'95-international conference on neural networks (Vol. 4, pp. 1942-1948). IEEE.
     """
-    ID_VEC = 2      # Velocity
-    ID_LOP = 3      # Local position
-    ID_LOF = 4      # Local fitness
+
+    ID_VEC = 2  # Velocity
+    ID_LOP = 3  # Local position
+    ID_LOF = 4  # Local fitness
 
     def __init__(self, problem, epoch=10000, pop_size=100, c1=2.05, c2=2.05, w_min=0.4, w_max=0.9, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
             c1 (float): [0-2] local coefficient
             c2 (float): [0-2] global coefficient
             w_min (float): Weight min of bird, default = 0.4
             w_max (float): Weight max of bird, default = 0.9
-            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -48,15 +83,14 @@ class BasePSO(Optimizer):
 
     def create_solution(self):
         """
-        Returns:
-            The position position with 2 element: index of position/location and index of fitness wrapper
-            The general format: [position, [target, [obj1, obj2, ...]]]
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
 
-        ## To get the position, fitness wrapper, target and obj list
-        ##      A[self.ID_POS]                  --> Return: position
-        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
-        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
-        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+        Returns:
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, local_pos, local_fit]
         """
         position = np.random.uniform(self.problem.lb, self.problem.ub)
         fitness = self.get_fitness_position(position=position)
@@ -67,6 +101,8 @@ class BasePSO(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -96,12 +132,41 @@ class BasePSO(Optimizer):
 
 class PPSO(Optimizer):
     """
-        A variant version of PSO: Phasor particle swarm optimization: a simple and efficient variant of PSO
-    Link:
-        Phasor particle swarm optimization: a simple and efficient variant of PSO
-    Notes:
-        This code is converted from matlab code (sent from author: Ebrahim Akbari)
+    The original version of: Phasor Particle Swarm Optimization (P-PSO)
+
+    Notes
+    ~~~~~
+    + This code is converted from matlab code (sent from author: Ebrahim Akbari)
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.PSO import PPSO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = PPSO(problem_dict1, epoch, pop_size)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Ghasemi, M., Akbari, E., Rahimnejad, A., Razavi, S.E., Ghavidel, S. and Li, L., 2019.
+    Phasor particle swarm optimization: a simple and efficient variant of PSO. Soft Computing, 23(19), pp.9701-9718.
     """
+
     ID_VEC = 2  # Velocity
     ID_LOP = 3  # Local position
     ID_LOF = 4  # Local fitness
@@ -109,10 +174,9 @@ class PPSO(Optimizer):
     def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -127,6 +191,16 @@ class PPSO(Optimizer):
         self.dyn_delta_list = np.random.uniform(0, 2 * np.pi, self.pop_size)
 
     def create_solution(self):
+        """
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+
+        Returns:
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, local_pos, local_fit]
+        """
         position = np.random.uniform(self.problem.lb, self.problem.ub)
         fitness = self.get_fitness_position(position=position)
         velocity = np.random.uniform(self.v_min, self.v_max)
@@ -136,6 +210,8 @@ class PPSO(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -172,21 +248,55 @@ class PPSO(Optimizer):
 
 class HPSO_TVAC(PPSO):
     """
-        The variant version of PSO:
-            Self-organising Hierarchical PSO with Time-Varying Acceleration Coefficients (HPSO_TVAC)
-        Link:
-            New self-organising hierarchical PSO with jumping time-varying acceleration coefficients
-        Note:
-            This code is converted from matlab code (sent from author: Ebrahim Akbari)
+    The original version of: Ant Colony Optimization Continuous (HPSO-TVAC)
+
+    Notes
+    ~~~~~
+    + This code is converted from matlab code (sent from author: Ebrahim Akbari)
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + ci (float): [0.3, 1.0], c initial, default = 0.5
+        + cf (float): [0.0, 0.3], c final, default = 0.0
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.PSO import HPSO_TVAC
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> ci = 0.5
+    >>> cf = 0.0
+    >>> model = HPSO_TVAC(problem_dict1, epoch, pop_size, ci, cf)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Ghasemi, M., Aghaei, J. and Hadipour, M., 2017. New self-organising hierarchical PSO with
+    jumping time-varying acceleration coefficients. Electronics Letters, 53(20), pp.1360-1362.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, ci=0.5, cf=0.0, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            **kwargs ():
+            ci (float): c initial, default = 0.5
+            cf (float): c final, default = 0.0
         """
         super().__init__(problem, epoch, pop_size, **kwargs)
         self.nfe_per_epoch = pop_size
@@ -199,6 +309,8 @@ class HPSO_TVAC(PPSO):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -208,14 +320,14 @@ class HPSO_TVAC(PPSO):
             agent = deepcopy(self.pop[i])
             idx_k = np.random.randint(0, self.pop_size)
             w = np.random.normal()
-            while (np.abs(w - 1.0) < 0.01):
+            while np.abs(w - 1.0) < 0.01:
                 w = np.random.normal()
             c1_it = np.abs(w) ** (c_it * w)
             c2_it = np.abs(1 - w) ** (c_it / (1 - w))
 
             #################### HPSO
             v_new = c1_it * np.random.uniform(0, 1, self.problem.n_dims) * (self.pop[i][self.ID_LOP] - self.pop[i][self.ID_POS]) + \
-                        c2_it * np.random.uniform(0, 1, self.problem.n_dims) * \
+                    c2_it * np.random.uniform(0, 1, self.problem.n_dims) * \
                     (self.g_best[self.ID_POS] + self.pop[idx_k][self.ID_LOP] - 2 * self.pop[i][self.ID_POS])
 
             np.where(v_new == 0, np.sign(0.5 - np.random.uniform()) * np.random.uniform() * self.v_max, v_new)
@@ -243,22 +355,57 @@ class HPSO_TVAC(PPSO):
 
 class C_PSO(BasePSO):
     """
-        Version: Chaos Particle Swarm Optimization (C-PSO)
-        Link
-            Improved particle swarm optimization combined with chaos
+    The original version of: Chaos Particle Swarm Optimization (C-PSO)
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + c1 (float): [1.0, 3.0] local coefficient, default = 2.05
+        + c2 (float): [1.0, 3.0] global coefficient, default = 2.05
+        + w_min (float): [0.1, 0.4], Weight min of bird, default = 0.4
+        + w_max (float): [0.4, 2.0], Weight max of bird, default = 0.9
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.PSO import C_PSO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> c1 = 2.05
+    >>> c2 = 2.05
+    >>> w_min = 0.4
+    >>> w_max = 0.9
+    >>> model = C_PSO(problem_dict1, epoch, pop_size, c1, c2, w_min, w_max)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Liu, B., Wang, L., Jin, Y.H., Tang, F. and Huang, D.X., 2005. Improved particle swarm optimization
+    combined with chaos. Chaos, Solitons & Fractals, 25(5), pp.1261-1271.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, c1=1.2, c2=1.2, w_min=0.2, w_max=1.2, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, c1=2.05, c2=2.05, w_min=0.4, w_max=0.9, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            c1 (float): [0-2] local coefficient
-            c2 (float): [0-2] global coefficient
-            w_min (float): Weight min of bird, default = 0.2
-            w_max (float): Weight max of bird, default = 1.2
-            **kwargs ():
+            c1 (float): [0-2] local coefficient, default = 2.05
+            c2 (float): [0-2] global coefficient, default = 2.05
+            w_min (float): Weight min of bird, default = 0.4
+            w_max (float): Weight max of bird, default = 0.9
         """
         super().__init__(problem, epoch, pop_size, c1, c2, w_min, w_max, **kwargs)
         self.nfe_per_epoch = pop_size
@@ -289,6 +436,8 @@ class C_PSO(BasePSO):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -344,10 +493,47 @@ class C_PSO(BasePSO):
 
 class CL_PSO(Optimizer):
     """
-        Version: Comprehensive Learning Particle Swarm Optimization (CL-PSO)
-        Link
-            Comprehensive Learning Particle Swarm Optimizer for Global Optimization of Multimodal Functions
+    The original version of: Comprehensive Learning Particle Swarm Optimization (CL-PSO)
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + c_local (float): [1.0, 3.0], local coefficient, default = 1.2
+        + w_min (float): [0.1, 0.5], Weight min of bird, default = 0.4
+        + w_max (float): [0.7, 2.0], Weight max of bird, default = 0.9
+        + max_flag (int): [5, 20], Number of times, default = 7
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.PSO import CL_PSO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> c_local = 1.2
+    >>> w_min = 0.4
+    >>> w_max = 0.9
+    >>> max_flag = 7
+    >>> model = CL_PSO(problem_dict1, epoch, pop_size, c_local, w_min, w_max, max_flag)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Liang, J.J., Qin, A.K., Suganthan, P.N. and Baskar, S., 2006. Comprehensive learning particle swarm optimizer
+    for global optimization of multimodal functions. IEEE transactions on evolutionary computation, 10(3), pp.281-295.
     """
+
     ID_VEC = 2
     ID_LOP = 3
     ID_LOF = 4
@@ -355,15 +541,13 @@ class CL_PSO(Optimizer):
     def __init__(self, problem, epoch=10000, pop_size=100, c_local=1.2, w_min=0.4, w_max=0.9, max_flag=7, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            c1 (float): [0-2] local coefficient
-            c2 (float): [0-2] global coefficient
-            w_min (float): Weight min of bird, default = 0.2
-            w_max (float): Weight max of bird, default = 1.2
+            c_local (float): local coefficient, default = 1.2
+            w_min (float): Weight min of bird, default = 0.4
+            w_max (float): Weight max of bird, default = 0.9
             max_flag (int): Number of times, default = 7
-            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -383,6 +567,16 @@ class CL_PSO(Optimizer):
         self.flags = np.zeros(self.pop_size)
 
     def create_solution(self):
+        """
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+
+        Returns:
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, local_pos, local_fit]
+        """
         position = np.random.uniform(self.problem.lb, self.problem.ub)
         fitness = self.get_fitness_position(position=position)
         velocity = np.random.uniform(self.v_min, self.v_max)
@@ -392,6 +586,8 @@ class CL_PSO(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -413,7 +609,7 @@ class CL_PSO(Optimizer):
                 else:
                     id1, id2 = np.random.choice(list(set(range(0, self.pop_size)) - {i}), 2, replace=False)
                     if self.compare_agent(self.pop[id1], self.pop[id2]):
-                        vj = wk * self.pop[i][self.ID_VEC][j] + self.c_local * np.random.rand() *\
+                        vj = wk * self.pop[i][self.ID_VEC][j] + self.c_local * np.random.rand() * \
                              (self.pop[id1][self.ID_LOP][j] - self.pop[i][self.ID_POS][j])
                     else:
                         vj = wk * self.pop[i][self.ID_VEC][j] + self.c_local * np.random.rand() * \
