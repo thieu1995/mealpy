bf1e8346150355c092de54a4e78ff2fe4213c0d5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 17:00:55 +0700

    Update docs for CSO in swarm_based group

--- mealpy/swarm_based/CSO.py
+++ mealpy/swarm_based/CSO.py
@@ -5,7 +5,7 @@
 #       Email:      nguyenthieu2102@gmail.com                                                           %
 #       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
 #       Github:     https://github.com/thieu1995                                                        %
-#-------------------------------------------------------------------------------------------------------%
+# -------------------------------------------------------------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -14,32 +14,79 @@ from mealpy.optimizer import Optimizer
 
 class BaseCSO(Optimizer):
     """
-        The original version of: Cat Swarm Optimization (CSO)
-        Link:
-            https://link.springer.com/chapter/10.1007/978-3-540-36668-3_94
-            https://www.hindawi.com/journals/cin/2020/4854895/
+    The original version of: Cat Swarm Optimization (CSO)
+
+    Links:
+        1. https://link.springer.com/chapter/10.1007/978-3-540-36668-3_94
+        2. https://www.hindawi.com/journals/cin/2020/4854895/
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + mixture_ratio (float): joining seeking mode with tracing mode, default=0.15
+        + smp (int): seeking memory pool, default=5 clones (larger is better but time-consuming)
+        + spc (bool): self-position considering, default=False
+        + cdc (float): counts of dimension to change (larger is more diversity but slow convergence), default=0.8
+        + srd (float): seeking range of the selected dimension (smaller is better but slow convergence), default=0.15
+        + c1 (float): same in PSO, default=0.4
+        + w_minmax (list): same in PSO, default=(0.4, 0.9)
+        + selected_strategy (int):  0: best fitness, 1: tournament, 2: roulette wheel, else: random (decrease by quality)
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.CSO import BaseCSO
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> mixture_ratio = 0.15
+    >>> smp = 5
+    >>> spc = False
+    >>> cdc = 0.8
+    >>> srd = 0.15
+    >>> c1 = 0.4
+    >>> w_minmax = [0.4, 0.9]
+    >>> selected_strategy = 1
+    >>> model = BaseCSO(problem_dict1, epoch, pop_size, mixture_ratio, smp, spc, cdc, srd, c1, w_minmax, selected_strategy)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Chu, S.C., Tsai, P.W. and Pan, J.S., 2006, August. Cat swarm optimization. In Pacific Rim
+    international conference on artificial intelligence (pp. 854-858). Springer, Berlin, Heidelberg.
     """
-    ID_POS = 0      # position of the cat
-    ID_FIT = 1      # fitness
-    ID_VEL = 2      # velocity
-    ID_FLAG = 3     # status
+
+    ID_POS = 0  # position of the cat
+    ID_FIT = 1  # fitness
+    ID_VEL = 2  # velocity
+    ID_FLAG = 3  # status
 
     def __init__(self, problem, epoch=10000, pop_size=100, mixture_ratio=0.15, smp=5, spc=False,
                  cdc=0.8, srd=0.15, c1=0.4, w_minmax=(0.4, 0.9), selected_strategy=1, **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            mixture_ratio (): joining seeking mode with tracing mode
-            smp (): seeking memory pool, 10 clones  (larger is better but time-consuming)
-            spc (): self-position considering
-            cdc (): counts of dimension to change  (larger is more diversity but slow convergence)
-            srd (): seeking range of the selected dimension (smaller is better but slow convergence)
-            c1 (): same in PSO
-            w_minmax (): same in PSO
-            selected_strategy ():  0: best fitness, 1: tournament, 2: roulette wheel, else: random  (decrease by quality)
-            **kwargs ():
+            mixture_ratio (float): joining seeking mode with tracing mode
+            smp (int): seeking memory pool, 10 clones  (larger is better but time-consuming)
+            spc (bool): self-position considering
+            cdc (float): counts of dimension to change  (larger is more diversity but slow convergence)
+            srd (float): seeking range of the selected dimension (smaller is better but slow convergence)
+            c1 (float): same in PSO
+            w_minmax (list): same in PSO
+            selected_strategy (int):  0: best fitness, 1: tournament, 2: roulette wheel, else: random (decrease by quality)
         """
 
         super().__init__(problem, kwargs)
@@ -50,27 +97,25 @@ class BaseCSO(Optimizer):
         self.spc = spc
         self.cdc = cdc
         self.srd = srd
-        self.c1 = c1         # Still using c1 and r1 but not c2, r2
+        self.c1 = c1  # Still using c1 and r1 but not c2, r2
         self.w_min = w_minmax[0]
         self.w_max = w_minmax[1]
         self.selected_strategy = selected_strategy
 
     def create_solution(self):
         """
+        + x: current position of cat
+        + v: vector v of cat (same amount of dimension as x)
+        + flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching) => False: seeking mode , True: tracing mode
+
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+
         Returns:
-            The position position with 2 element: index of position/location and index of fitness wrapper
-            The general format: [position, [target, [obj1, obj2, ...]]]
-
-        ## To get the position, fitness wrapper, target and obj list
-        ##      A[self.ID_POS]                  --> Return: position
-        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
-        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
-        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
-
-        x: current position of cat
-        v: vector v of cat (same amount of dimension as x)
-        flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching)
-        # False: seeking mode , True: tracing mode
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, flag]
         """
         position = np.random.uniform(self.problem.lb, self.problem.ub)
         fitness = self.get_fitness_position(position=position)
@@ -96,24 +141,26 @@ class BaseCSO(Optimizer):
             candidate_cats.append([pos_new, None, clone[self.ID_VEL], clone[self.ID_FLAG]])
         candidate_cats = self.update_fitness_population(candidate_cats)
 
-        if self.selected_strategy == 0:                # Best fitness-self
+        if self.selected_strategy == 0:  # Best fitness-self
             _, cat = self.get_global_best_solution(candidate_cats)
-        elif self.selected_strategy == 1:              # Tournament
+        elif self.selected_strategy == 1:  # Tournament
             k_way = 4
             idx = np.random.choice(range(0, self.smp), k_way, replace=False)
             cats_k_way = [candidate_cats[_] for _ in idx]
             _, cat = self.get_global_best_solution(cats_k_way)
-        elif self.selected_strategy == 2:              ### Roul-wheel selection
+        elif self.selected_strategy == 2:  ### Roul-wheel selection
             list_fitness = [candidate_cats[u][self.ID_FIT][self.ID_TAR] for u in range(0, len(candidate_cats))]
             idx = self.get_index_roulette_wheel_selection(list_fitness)
             cat = candidate_cats[idx]
         else:
             idx = np.random.choice(range(0, len(candidate_cats)))
-            cat = candidate_cats[idx]               # Random
+            cat = candidate_cats[idx]  # Random
         return cat[self.ID_POS]
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
