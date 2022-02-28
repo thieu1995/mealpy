8684579dcc6f7d9a0f329b5306ee94a49f7767e0
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 15:45:38 +0700

    Update docs for BA in swarm_based group

--- mealpy/swarm_based/BA.py
+++ mealpy/swarm_based/BA.py
@@ -5,36 +5,70 @@
 #       Email:      nguyenthieu2102@gmail.com                                                           %
 #       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
 #       Github:     https://github.com/thieu1995                                                        %
-#-------------------------------------------------------------------------------------------------------%
+# -------------------------------------------------------------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
 from mealpy.optimizer import Optimizer
 
 
-class BasicBA(Optimizer):
+class OriginalBA(Optimizer):
     """
-        The original version of: Bat-Inspired Algorithm (BA)
-            A New Metaheuristic Bat-Inspired Algorithm
-        Notes:
-            The value of A and r are changing after each iteration
-    """
-    ID_VEC = 2      # Velocity
-    ID_LOUD = 3     # Loudness
-    ID_PRAT = 4     # Pulse Rate
-    ID_PFRE = 5     # Pulse Frequency
+    The original version of: Bat-inspired Algorithm (BA)
 
-    def __init__(self, problem, epoch=10000, pop_size=100, loudness=(1.0, 2.0),
-                 pulse_rate=(0.15, 0.85), pulse_frequency=(0, 10), **kwargs):
+    Notes
+    ~~~~~
+    + The value of A and r parameters are constant
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + loudness (float): (1.0, 2.0), loudness, default = 0.8
+        + pulse_rate (float): (0.15, 0.85), pulse rate / emission rate, default = 0.95
+        + pulse_frequency (list): (pf_min, pf_max) -> ([0, 3], [5, 20]), pulse frequency, default = (0, 10)
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.BA import OriginalBA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> loudness = 0.8
+    >>> pulse_rate = 0.95
+    >>> pulse_frequency = [0, 10]
+    >>> model = OriginalBA(problem_dict1, epoch, pop_size, loudness, pulse_rate, pulse_frequency)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Yang, X.S., 2010. A new metaheuristic bat-inspired algorithm. In Nature inspired cooperative
+    strategies for optimization (NICSO 2010) (pp. 65-74). Springer, Berlin, Heidelberg.
+    """
+
+    ID_VEC = 2  # Velocity
+    ID_PFRE = 3  # Pulse Frequency
+
+    def __init__(self, problem, epoch=10000, pop_size=100, loudness=0.8, pulse_rate=0.95, pulse_frequency=(0, 10), **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            loudness (): (A_min, A_max): loudness, default = (1.0, 2.0)
-            pulse_rate (): (r_min, r_max): pulse rate / emission rate, default = (0.15, 0.85)
-            pulse_frequency (): (pf_min, pf_max): pulse frequency, default = (0, 10)
-            **kwargs ():
+            loudness (float): (A_min, A_max): loudness, default = 0.8
+            pulse_rate (float): (r_min, r_max): pulse rate / emission rate, default = 0.95
+            pulse_frequency (list): (pf_min, pf_max): pulse frequency, default = (0, 10)
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -49,15 +83,129 @@ class BasicBA(Optimizer):
 
     def create_solution(self):
         """
-        Returns:
-            The position position with 2 element: index of position/location and index of fitness wrapper
-            The general format: [position, [target, [obj1, obj2, ...]]]
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
 
-        ## To get the position, fitness wrapper, target and obj list
-        ##      A[self.ID_POS]                  --> Return: position
-        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
-        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
-        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+        Returns:
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, pulse_frequency]
+        """
+        position = np.random.uniform(self.problem.lb, self.problem.ub)
+        fitness = self.get_fitness_position(position=position)
+        velocity = np.random.uniform(self.problem.lb, self.problem.ub)
+        pulse_frequency = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()
+        return [position, fitness, velocity, pulse_frequency]
+
+    def evolve(self, epoch):
+        """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
+        Args:
+            epoch (int): The current iteration
+        """
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            agent = deepcopy(self.pop[idx])
+            agent[self.ID_VEC] = agent[self.ID_VEC] + self.pop[idx][self.ID_PFRE] * (self.pop[idx][self.ID_POS] - self.g_best[self.ID_POS])
+            x = self.pop[idx][self.ID_POS] + agent[self.ID_VEC]
+            ## Local Search around g_best position
+            if np.random.uniform() > self.pulse_rate:
+                x = self.g_best[self.ID_POS] + 0.0001 * np.random.normal(self.problem.n_dims)  # gauss
+            pos_new = self.amend_position_faster(x)
+            agent[self.ID_POS] = pos_new
+            pop_new.append(agent)
+        pop_new = self.update_fitness_population(pop_new)
+        for idx in range(self.pop_size):
+            ## Replace the old position by the new one when its has better fitness.
+            ##  and then update loudness and emission rate
+            if self.compare_agent(pop_new[idx], self.pop[idx]) and np.random.rand() < self.loudness:
+                self.pop[idx] = deepcopy(pop_new[idx])
+
+
+class BaseBA(Optimizer):
+    """
+    The original version of: Bat-inspired Algorithm (BA)
+
+    Notes
+    ~~~~~
+    + The value of A and r are changing after each iteration
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + loudness (float): (A_min, A_max) -> ([0.5, 1.5], [1.0, 3.0]): loudness, default = (1.0, 2.0)
+        + pulse_rate (float): (r_min, r_max) -> ([0.1, 0.5], [0.5, 0.95]), pulse rate / emission rate, default = (0.15, 0.85)
+        + pulse_frequency (list): (pf_min, pf_max) -> ([0, 3], [5, 20]), pulse frequency, default = (0, 10)
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.BA import BaseBA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> loudness = [1.0, 2.0]
+    >>> pulse_rate = [0.15, 0.85]
+    >>> pulse_frequency = [0, 10]
+    >>> model = BaseBA(problem_dict1, epoch, pop_size, loudness, pulse_rate, pulse_frequency)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Yang, X.S., 2010. A new metaheuristic bat-inspired algorithm. In Nature inspired cooperative
+    strategies for optimization (NICSO 2010) (pp. 65-74). Springer, Berlin, Heidelberg.
+    """
+
+    ID_VEC = 2  # Velocity
+    ID_LOUD = 3  # Loudness
+    ID_PRAT = 4  # Pulse Rate
+    ID_PFRE = 5  # Pulse Frequency
+
+    def __init__(self, problem, epoch=10000, pop_size=100, loudness=(1.0, 2.0),
+                 pulse_rate=(0.15, 0.85), pulse_frequency=(0, 10), **kwargs):
+        """
+        Args:
+            problem (dict): The problem dictionary
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            loudness (): (A_min, A_max): loudness, default = (1.0, 2.0)
+            pulse_rate (): (r_min, r_max): pulse rate / emission rate, default = (0.15, 0.85)
+            pulse_frequency (): (pf_min, pf_max): pulse frequency, default = (0, 10)
+        """
+        super().__init__(problem, kwargs)
+        self.nfe_per_epoch = pop_size
+        self.sort_flag = False
+
+        self.epoch = epoch
+        self.pop_size = pop_size
+        self.loudness = loudness
+        self.pulse_rate = pulse_rate
+        self.pulse_frequency = pulse_frequency
+        self.alpha = self.gamma = 0.9
+
+    def create_solution(self):
+        """
+        To get the position, fitness wrapper, target and obj list
+            + A[self.ID_POS]                  --> Return: position
+            + A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
+            + A[self.ID_FIT][self.ID_TAR]     --> Return: target
+            + A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+
+        Returns:
+            list: wrapper of solution with format [position, [target, [obj1, obj2, ...]], velocity, loudness, pulse_rate, pulse_frequency]
         """
         position = np.random.uniform(self.problem.lb, self.problem.ub)
         fitness = self.get_fitness_position(position=position)
@@ -69,6 +217,8 @@ class BasicBA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -96,28 +246,56 @@ class BasicBA(Optimizer):
                 self.pop[idx] = deepcopy(pop_new[idx])
 
 
-class BaseBA(Optimizer):
+class ModifiedBA(Optimizer):
     """
-    My modified version of: Bat-inspired Algorithm (BA)
-    (A little bit better than both 2 original version)
-    - No need A parameter
-    - I changed the process.
+    My modified version of: Bat-inspired Algorithm (MBA)
+
+    Notes
+    ~~~~~
+    + Removes A (loudness) parameter
+    + Changed processes
         + 1st: We proceed exploration phase (using frequency)
         + 2nd: If new position has better fitness we replace the old position
         + 3rd: Otherwise, we proceed exploitation phase (using finding around the best position so far)
-    Link:
-        https://link.springer.com/chapter/10.1007/978-3-642-12538-6_6
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + pulse_rate (float): [0.7, 1.0], pulse rate / emission rate, default = 0.95
+        + pulse_frequency (list): (pf_min, pf_max) -> ([0, 3], [5, 20]), pulse frequency, default = (0, 10)
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.BA import ModifiedBA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pulse_rate = 0.95
+    >>> pulse_frequency = [0, 10]
+    >>> model = ModifiedBA(problem_dict1, epoch, pop_size, pulse_rate, pulse_frequency)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, pulse_rate=0.95, pulse_frequency=(0, 10), **kwargs):
         """
         Args:
-            problem ():
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            pulse_rate (float): (r_min, r_max): pulse rate / emission rate, default = (0.15, 0.85)
+            pulse_rate (float): pulse rate / emission rate, default = 0.95
             pulse_frequency (list): (pf_min, pf_max): pulse frequency, default = (0, 10)
-            **kwargs ():
         """
         super().__init__(problem, kwargs)
         self.nfe_per_epoch = pop_size
@@ -133,6 +311,8 @@ class BaseBA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -165,79 +345,3 @@ class BaseBA(Optimizer):
                 pop_new[idx_selected] = deepcopy(pop_child[idx])
         self.pop = pop_new
         self.nfe_per_epoch = nfe_epoch
-
-
-class OriginalBA(Optimizer):
-    """
-        The original version of: Bat-Inspired Algorithm (BA)
-            A New Metaheuristic Bat-Inspired Algorithm
-        Notes:
-            The value of A and r parameters are constant
-    """
-    ID_VEC = 2  # Velocity
-    ID_PFRE = 3  # Pulse Frequency
-
-    def __init__(self, problem, epoch=10000, pop_size=100, loudness=0.8, pulse_rate=0.95, pulse_frequency=(0, 10), **kwargs):
-        """
-        Args:
-            problem ():
-            epoch (int): maximum number of iterations, default = 10000
-            pop_size (int): number of population size, default = 100
-            loudness (float): (A_min, A_max): loudness, default = (1.0, 2.0)
-            pulse_rate (float): (r_min, r_max): pulse rate / emission rate, default = (0.15, 0.85)
-            pulse_frequency (list): (pf_min, pf_max): pulse frequency, default = (0, 10)
-            **kwargs ():
-        """
-        super().__init__(problem, kwargs)
-        self.nfe_per_epoch = pop_size
-        self.sort_flag = False
-
-        self.epoch = epoch
-        self.pop_size = pop_size
-        self.loudness = loudness
-        self.pulse_rate = pulse_rate
-        self.pulse_frequency = pulse_frequency
-        self.alpha = self.gamma = 0.9
-
-    def create_solution(self):
-        """
-        Returns:
-            The position position with 2 element: index of position/location and index of fitness wrapper
-            The general format: [position, [target, [obj1, obj2, ...]]]
-
-        ## To get the position, fitness wrapper, target and obj list
-        ##      A[self.ID_POS]                  --> Return: position
-        ##      A[self.ID_FIT]                  --> Return: [target, [obj1, obj2, ...]]
-        ##      A[self.ID_FIT][self.ID_TAR]     --> Return: target
-        ##      A[self.ID_FIT][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
-        """
-        position = np.random.uniform(self.problem.lb, self.problem.ub)
-        fitness = self.get_fitness_position(position=position)
-        velocity = np.random.uniform(self.problem.lb, self.problem.ub)
-        pulse_frequency = self.pulse_frequency[0] + (self.pulse_frequency[1] - self.pulse_frequency[0]) * np.random.uniform()
-        return [position, fitness, velocity, pulse_frequency]
-
-    def evolve(self, epoch):
-        """
-        Args:
-            epoch (int): The current iteration
-        """
-        pop_new = []
-        for idx in range(0, self.pop_size):
-            agent = deepcopy(self.pop[idx])
-            agent[self.ID_VEC] = agent[self.ID_VEC] + self.pop[idx][self.ID_PFRE] * (self.pop[idx][self.ID_POS] - self.g_best[self.ID_POS])
-            x = self.pop[idx][self.ID_POS] + agent[self.ID_VEC]
-            ## Local Search around g_best position
-            if np.random.uniform() > self.pulse_rate:
-                x = self.g_best[self.ID_POS] + 0.0001 * np.random.normal(self.problem.n_dims)  # gauss
-            pos_new = self.amend_position_faster(x)
-            agent[self.ID_POS] = pos_new
-            pop_new.append(agent)
-        pop_new = self.update_fitness_population(pop_new)
-        for idx in range(self.pop_size):
-            ## Replace the old position by the new one when its has better fitness.
-            ##  and then update loudness and emission rate
-            if self.compare_agent(pop_new[idx], self.pop[idx]) and np.random.rand() < self.loudness:
-                self.pop[idx] = deepcopy(pop_new[idx])
-
-
