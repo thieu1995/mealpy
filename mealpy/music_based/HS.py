c3e46e7ce45e6cb14b676e45494015cf51b43b99
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 08:27:42 +0700

    Update docs for HS in music_based group

--- mealpy/music_based/HS.py
+++ mealpy/music_based/HS.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 17:48, 18/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                  %
-#-------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 17:48, 18/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from mealpy.optimizer import Optimizer
@@ -13,18 +10,52 @@ from mealpy.optimizer import Optimizer
 
 class BaseHS(Optimizer):
     """
-    My version of: Harmony Search (HS)
-    Noted:
-        - Using global best in the harmony memories
-        - Remove third for loop
+    My changed version of: Harmony Search (HS)
+
+    Links:
+        1. https://doi.org/10.1177/003754970107600201
+
+    Notes
+    ~~~~~
+    - Used the global best in the harmony memories
+    - Removed third for loop
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + c_r (float): [0.1, 0.5], Harmony Memory Consideration Rate), default = 0.15
+        + pa_r (float): [0.3, 0.8], Pitch Adjustment Rate, default=0.5
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.music_based.HS import BaseHS
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> c_r = 0.95
+    >>> pa_r = 0.05
+    >>> model = BaseHS(problem_dict1, epoch, pop_size, c_r, pa_r)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, n_new=50, c_r=0.95, pa_r=0.05, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, c_r=0.95, pa_r=0.05, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            n_new (int): Number of New Harmonies, default = 0.85
             c_r (float): Harmony Memory Consideration Rate, default = 0.15
             pa_r (float): Pitch Adjustment Rate, default=0.5
         """
@@ -34,16 +65,17 @@ class BaseHS(Optimizer):
 
         self.epoch = epoch
         self.pop_size = pop_size
-        self.n_new = n_new
         self.c_r = c_r
         self.pa_r = pa_r
         self.fw = 0.0001 * (self.problem.ub - self.problem.lb)  # Fret Width (Bandwidth)
-        self.fw_damp = 0.9995                                   # Fret Width Damp Ratio
+        self.fw_damp = 0.9995  # Fret Width Damp Ratio
 
         self.dyn_fw = self.fw
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -71,27 +103,63 @@ class BaseHS(Optimizer):
 
 class OriginalHS(BaseHS):
     """
-    Original version of: Harmony Search (HS)
-        A New Heuristic Optimization Algorithm: Harmony Search
-    Link:
+    The original version of: Harmony Search (HS)
 
+    Links:
+        1. https://doi.org/10.1177/003754970107600201
+
+    Hyper-parameters should fine tuned in approximate range to get faster convergen toward the global optimum:
+        + c_r (float): [0.1, 0.5], Harmony Memory Consideration Rate), default = 0.15
+        + pa_r (float): [0.3, 0.8], Pitch Adjustment Rate, default=0.5
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.music_based.HS import OriginalHS
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> c_r = 0.95
+    >>> pa_r = 0.05
+    >>> model = OriginalHS(problem_dict1, epoch, pop_size, c_r, pa_r)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Geem, Z.W., Kim, J.H. and Loganathan, G.V., 2001. A new heuristic
+    optimization algorithm: harmony search. simulation, 76(2), pp.60-68.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, n_new=50, c_r=0.95, pa_r=0.05, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, c_r=0.95, pa_r=0.05, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
-            n_new (int): Number of New Harmonies, default = 0.85
             c_r (float): Harmony Memory Consideration Rate), default = 0.15
             pa_r (float): Pitch Adjustment Rate, default=0.5
         """
-        super().__init__(problem, epoch, pop_size, n_new, c_r, pa_r, **kwargs)
+        super().__init__(problem, epoch, pop_size, c_r, pa_r, **kwargs)
         self.nfe_per_epoch = pop_size
         self.sort_flag = False
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
