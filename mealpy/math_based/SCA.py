5eceff5a35ed706ee185a4362185f8cba00237d0
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-02-28 07:15:26 +0700

    Update docs for SCA in math_based group

--- mealpy/math_based/SCA.py
+++ mealpy/math_based/SCA.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 17:44, 18/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                  %
-#-------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 17:44, 18/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -15,19 +12,40 @@ from mealpy.optimizer import Optimizer
 class BaseSCA(Optimizer):
     """
     The original version of: Sine Cosine Algorithm (SCA)
-        A Sine Cosine Algorithm for solving optimization problems
-    Link:
-        https://doi.org/10.1016/j.knosys.2015.12.022
-        https://www.mathworks.com/matlabcentral/fileexchange/54948-sca-a-sine-cosine-algorithm
-    Notes:
-        + I changed the flow as well as the equations.
-        + Removed third loop for faster computational time
-        + Batch size ideas
+
+    Notes
+    ~~~~~
+    + The flow and few equations is changed
+    + Removed third loop for faster computational time
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.math_based.SCA import BaseSCA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = BaseSCA(problem_dict1, epoch, pop_size)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
         """
@@ -40,6 +58,8 @@ class BaseSCA(Optimizer):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -61,27 +81,47 @@ class BaseSCA(Optimizer):
         pop_new = self.update_fitness_population(pop_new)
         self.pop = self.greedy_selection_population(self.pop, pop_new)
 
-            # ## Update the global best
-            # if self.batch_idea:
-            #     if (i + 1) % self.batch_size == 0:
-            #         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-            # else:
-            #     if (i + 1) * self.pop_size == 0:
-            #         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-
 
 class OriginalSCA(BaseSCA):
     """
-    Original version of: Sine Cosine Algorithm (SCA)
-        A Sine Cosine Algorithm for solving optimization problems
-    Link:
-        https://doi.org/10.1016/j.knosys.2015.12.022
-        https://www.mathworks.com/matlabcentral/fileexchange/54948-sca-a-sine-cosine-algorithm
+    The original version of: Sine Cosine Algorithm (SCA)
+
+    Links:
+        1. https://doi.org/10.1016/j.knosys.2015.12.022
+        2. https://www.mathworks.com/matlabcentral/fileexchange/54948-sca-a-sine-cosine-algorithm
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.math_based.SCA import OriginalSCA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "obj_func": fitness_function,
+    >>>     "n_dims": 5,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>>     "verbose": True,
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = OriginalSCA(problem_dict1, epoch, pop_size)
+    >>> best_position, best_fitness = model.solve()
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Mirjalili, S., 2016. SCA: a sine cosine algorithm for solving optimization problems. Knowledge-based systems, 96, pp.120-133.
     """
 
     def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
         """
         Args:
+            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
         """
@@ -91,6 +131,8 @@ class OriginalSCA(BaseSCA):
 
     def evolve(self, epoch):
         """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
         Args:
             epoch (int): The current iteration
         """
@@ -114,131 +156,3 @@ class OriginalSCA(BaseSCA):
             pos_new = self.amend_position_random(pos_new)
             pop_new.append([pos_new, None])
         self.pop = self.update_fitness_population(pop_new)
-
-
-# class FasterSCA(BaseSCA):
-#     """
-#         A Sine Cosine Algorithm for solving optimization problems (SCA)
-#     Link:
-#         https://doi.org/10.1016/j.knosys.2015.12.022
-#         https://www.mathworks.com/matlabcentral/fileexchange/54948-sca-a-sine-cosine-algorithm
-#
-#     This is my version of SCA. The original version of SCA is not working. So I changed the flow of algorithm
-#     """
-#     def __init__(self, problem, epoch=10000, pop_size=100, **kwargs):
-#         """
-#         Args:
-#             epoch (int): maximum number of iterations, default = 10000
-#             pop_size (int): number of population size, default = 100
-#         """
-#         super().__init__(problem, epoch, pop_size, **kwargs)
-#         self.nfe_per_epoch = pop_size
-#         self.sort_flag = False
-#
-#     def create_solution(self, minmax=0):
-#         position = uniform(self.lb, self.ub)
-#         fitness = self.get_fitness_position(position=position, minmax=minmax)
-#         return {0: position, 1: fitness}
-#
-#     def train(self):
-#         pop = {i: self.create_solution() for i in range(self.pop_size)}
-#         pop_sorted = {k: v for k, v in sorted(pop.items(), key=lambda encoded: encoded[1][self.ID_FIT])}
-#         g_best = next(iter(pop_sorted.values()))
-#
-#         for epoch in range(self.epoch):
-#             # Update the position of solutions with respect to destination
-#             for i, (idx, item) in enumerate(pop.items()):  # i-th position
-#                 # Eq 3.4, r1 decreases linearly from a to 0
-#                 a = 2.0
-#                 r1 = a - (epoch + 1) * (a / self.epoch)
-#                 # Update r2, r3, and r4 for Eq. (3.3), remove third loop here
-#                 r2 = 2 * pi * uniform(0, 1, self.problem_size)
-#                 r3 = 2 * uniform(0, 1, self.problem_size)
-#                 # Eq. 3.3, 3.1 and 3.2
-#                 pos_new1 = pop[i][self.ID_POS] + r1 * sin(r2) * abs(r3 * g_best[self.ID_POS] - pop[i][self.ID_POS])
-#                 pos_new2 = pop[i][self.ID_POS] + r1 * cos(r2) * abs(r3 * g_best[self.ID_POS] - pop[i][self.ID_POS])
-#                 pos_new = where(uniform(0, 1, self.problem_size) < 0.5, pos_new1, pos_new2)
-#                 # Check the bound
-#                 pos_new = self.amend_position_random(pos_new)
-#                 fit = self.get_fitness_position(pos_new)
-#                 if fit < item[self.ID_FIT]:  # My improved part
-#                     pop[idx] = {0: pos_new, 1: fit}
-#
-#                 ## Update the global best
-#                 if self.batch_idea:
-#                     if (i + 1) % self.batch_size == 0:
-#                         pop_sorted = {k: v for k, v in sorted(pop.items(), key=lambda encoded: encoded[1][self.ID_FIT])}
-#                         current_best = next(iter(pop_sorted.values()))
-#                         if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
-#                             g_best = deepcopy(current_best)
-#                 else:
-#                     if (i + 1) * self.pop_size == 0:
-#                         pop_sorted = {k: v for k, v in sorted(pop.items(), key=lambda encoded: encoded[1][self.ID_FIT])}
-#                         current_best = next(iter(pop_sorted.values()))
-#                         if current_best[self.ID_FIT] < g_best[self.ID_FIT]:
-#                             g_best = deepcopy(current_best)
-#
-#             self.loss_train.append(g_best[self.ID_FIT])
-#             if self.verbose:
-#                 print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-#         self.solution = g_best
-#         return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
-#
-#
-# class FastestSCA(Root):
-#     """
-#         A Sine Cosine Algorithm for solving optimization problems (SCA)
-#     Link:
-#         https://doi.org/10.1016/j.knosys.2015.12.022
-#         https://www.mathworks.com/matlabcentral/fileexchange/54948-sca-a-sine-cosine-algorithm
-#
-#     This is my version of SCA. The original version of SCA is not working. So I changed the flow of algorithm
-#     """
-#
-#     def __init__(self, obj_func=None, lb=None, ub=None, verbose=True, epoch=750, pop_size=100, **kwargs):
-#         super().__init__(obj_func, lb, ub, verbose, kwargs)
-#         self.epoch = epoch
-#         self.pop_size = pop_size
-#
-#     def create_solution(self, minmax=0):
-#         position = uniform(self.lb, self.ub)
-#         fitness = self.get_fitness_position(position=position, minmax=minmax)
-#         return {0: position, 1: fitness}
-#
-#     def train(self):
-#         pop = [self.create_solution() for i in range(self.pop_size)]
-#         g_best = self.get_global_best_solution(pop, self.ID_FIT, self.ID_MIN_PROB)
-#
-#         for epoch in range(self.epoch):
-#             # Update the position of solutions with respect to destination
-#             for i in range(self.pop_size):  # i-th position
-#                 # Eq 3.4, r1 decreases linearly from a to 0
-#                 a = 2.0
-#                 r1 = a - (epoch + 1) * (a / self.epoch)
-#                 # Update r2, r3, and r4 for Eq. (3.3), remove third loop here
-#                 r2 = 2 * pi * uniform(0, 1, self.problem_size)
-#                 r3 = 2 * uniform(0, 1, self.problem_size)
-#                 # Eq. 3.3, 3.1 and 3.2
-#                 pos_new1 = pop[i][self.ID_POS] + r1 * sin(r2) * abs(r3 * g_best[self.ID_POS] - pop[i][self.ID_POS])
-#                 pos_new2 = pop[i][self.ID_POS] + r1 * cos(r2) * abs(r3 * g_best[self.ID_POS] - pop[i][self.ID_POS])
-#                 pos_new = where(uniform(0, 1, self.problem_size) < 0.5, pos_new1, pos_new2)
-#                 # Check the bound
-#                 pos_new = self.amend_position_random(pos_new)
-#                 fit = self.get_fitness_position(pos_new)
-#                 if fit < pop[i][self.ID_FIT]:  # My improved part
-#                     pop[i] = {0: pos_new, 1: fit}
-#
-#                 ## Update the global best
-#                 if self.batch_idea:
-#                     if (i + 1) % self.batch_size == 0:
-#                         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-#                 else:
-#                     if (i + 1) * self.pop_size == 0:
-#                         g_best = self.update_global_best_solution(pop, self.ID_MIN_PROB, g_best)
-#
-#             self.loss_train.append(g_best[self.ID_FIT])
-#             if self.verbose:
-#                 print("> Epoch: {}, Best fit: {}".format(epoch + 1, g_best[self.ID_FIT]))
-#         self.solution = g_best
-#         return g_best[self.ID_POS], g_best[self.ID_FIT], self.loss_train
-#
