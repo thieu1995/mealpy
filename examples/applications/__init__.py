7f1804bfdeec31aac9b82f9f40d792c641c0aca1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-02 10:03:33 +0700

    Update examples for mealpy's applications

---
+++ examples/applications/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python
+# Created by "Thieu" at 17:33, 06/11/2021 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
