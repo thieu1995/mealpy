98168e263d533194f79c979904f7ff4e2d03e9f2
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-14 21:13:21 +0700

    Update help function

--- mealpy/__init__.py
+++ mealpy/__init__.py
@@ -3,8 +3,32 @@
 #       Email: nguyenthieu2102@gmail.com            %
 #       Github: https://github.com/thieu1995        %
 # --------------------------------------------------%
+#
+# Examples:
+# >>>
+# >>> from mealpy.swarm_based import PSO
+# >>> import numpy as np
+# >>>
+# >>> def fitness_function(solution):
+# >>>     return np.sum(solution ** 2)
+# >>>
+# >>> problem = {
+# >>>    "fit_func": fitness_function,
+# >>>    "lb": [-100, ] * 30,
+# >>>    "ub": [100, ] * 30,
+# >>>    "minmax": "min",
+# >>>    "save_population": False,
+# >>>    "log_to": "file",
+# >>>    "log_file": "mealpy.log"
+# >>> }
+# >>>
+# >>> ## Run the algorithm
+# >>> model = PSO.C_PSO(problem, epoch=5, pop_size=50, name="C-PSO", fit_name="Square")
+# >>> best_position, best_fitness = model.solve()
+# >>> print(f"Best solution: {best_position}, Best fitness: {best_fitness}")
 
-__version__ = "2.3.1"
+
+__version__ = "2.4.0"
 
 from . import bio_based
 from . import evolutionary_based
