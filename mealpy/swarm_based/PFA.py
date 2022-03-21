e84561daa21b02dc14076bafac74aedaa491f44e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-03-21 10:51:33 +0700

    Set up validator and tests for PFA

--- mealpy/swarm_based/PFA.py
+++ mealpy/swarm_based/PFA.py
@@ -51,11 +51,11 @@ class BasePFA(Optimizer):
             pop_size (int): number of population size, default = 100
         """
         super().__init__(problem, kwargs)
-        self.nfe_per_epoch = pop_size
-        self.sort_flag = True
+        self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
 
-        self.epoch = epoch
-        self.pop_size = pop_size
+        self.nfe_per_epoch = self.pop_size
+        self.sort_flag = True
 
     def evolve(self, epoch):
         """
