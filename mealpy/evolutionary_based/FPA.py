33d366aa4a5d9024eaafbdabda85212714f3c563
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-01 18:49:16 +0700

    Update training mode in FPA

--- mealpy/evolutionary_based/FPA.py
+++ mealpy/evolutionary_based/FPA.py
@@ -17,7 +17,7 @@ class BaseFPA(Optimizer):
 
     Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
         + p_s (float): [0.5, 0.95], switch probability, default = 0.8
-        + levy_multiplier: [0.2, 0.5], mutiplier factor of Levy-flight trajectory
+        + levy_multiplier: [0.1, 0.5], mutiplier factor of Levy-flight trajectory
 
     Examples
     ~~~~~~~~
@@ -48,7 +48,7 @@ class BaseFPA(Optimizer):
     conference on unconventional computing and natural computation (pp. 240-249). Springer, Berlin, Heidelberg.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, p_s=0.8, levy_multiplier=0.2, **kwargs):
+    def __init__(self, problem, epoch=10000, pop_size=100, p_s=0.8, levy_multiplier=0.1, **kwargs):
         """
         Args:
             problem (dict): The problem dictionary
@@ -75,7 +75,9 @@ class BaseFPA(Optimizer):
         Returns:
             Amended position (make the position is in bound)
         """
-        return np.where(np.logical_and(lb <= position, position <= ub), position, np.random.uniform(lb, ub))
+        condition = np.logical_and(lb <= position, position <= ub)
+        random_pos = np.random.uniform(lb, ub)
+        return np.where(condition, position, random_pos)
 
     def evolve(self, epoch):
         """
@@ -87,7 +89,7 @@ class BaseFPA(Optimizer):
         pop = []
         for idx in range(0, self.pop_size):
             if np.random.uniform() < self.p_s:
-                levy = self.get_levy_flight_step(multiplier=self.levy_multiplier, case=-1)
+                levy = self.get_levy_flight_step(multiplier=self.levy_multiplier, size=self.problem.n_dims, case=-1)
                 pos_new = self.pop[idx][self.ID_POS] + 1.0 / np.sqrt(epoch + 1) * np.sign(np.random.random() - 0.5) * \
                           levy * (self.pop[idx][self.ID_POS] - self.g_best[self.ID_POS])
             else:
@@ -95,4 +97,10 @@ class BaseFPA(Optimizer):
                 pos_new = self.pop[idx][self.ID_POS] + np.random.uniform() * (self.pop[id1][self.ID_POS] - self.pop[id2][self.ID_POS])
             pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             pop.append([pos_new, None])
-        self.pop = self.update_target_wrapper_population(pop)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                pop[-1] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop = self.update_target_wrapper_population(pop)
+            pop = self.greedy_selection_population(self.pop, pop)
+        self.pop = pop
