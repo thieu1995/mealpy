2bc31681b90ac48785b84927e9e7acd845cebea1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-02 22:31:30 +0700

    Update training mode in HC

--- mealpy/math_based/HC.py
+++ mealpy/math_based/HC.py
@@ -17,7 +17,7 @@ class OriginalHC(Optimizer):
     + The number of neighbour solutions are equal to user defined
     + The step size to calculate neighbour is randomized
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + neighbour_size (int): [pop_size/2, pop_size], fixed parameter, sensitive exploitation parameter, Default: 50
 
     Examples
@@ -73,11 +73,14 @@ class OriginalHC(Optimizer):
         self.nfe_per_epoch = self.neighbour_size
         step_size = np.mean(self.problem.ub - self.problem.lb) * np.exp(-2 * (epoch + 1) / self.epoch)
         pop_neighbours = []
-        for i in range(0, self.neighbour_size):
+        for idx in range(0, self.neighbour_size):
             pos_new = self.g_best[self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * step_size
             pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             pop_neighbours.append([pos_new, None])
-        self.pop = self.update_target_wrapper_population(pop_neighbours)
+            if self.mode not in self.AVAILABLE_MODES:
+                pop_neighbours[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+        pop_neighbours = self.update_target_wrapper_population(pop_neighbours)
+        self.pop = pop_neighbours
 
 
 class BaseHC(OriginalHC):
@@ -93,7 +96,7 @@ class BaseHC(OriginalHC):
         + Imagination: exploration when far from global best, and exploitation when near global best
     + Who on top of mountain first will be the winner. (global optimal)
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + neighbour_size (int): [pop_size/2, pop_size], fixed parameter, sensitive exploitation parameter, Default: 50
 
     Examples
@@ -146,6 +149,9 @@ class BaseHC(OriginalHC):
                 pos_new = self.pop[idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims) * ss
                 pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
                 pop_neighbours.append([pos_new, None])
+                if self.mode not in self.AVAILABLE_MODES:
+                    pop_neighbours[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+            pop_neighbours = self.update_target_wrapper_population(pop_neighbours)
             pop_neighbours = self.update_target_wrapper_population(pop_neighbours)
             _, agent = self.get_global_best_solution(pop_neighbours)
             self.pop[idx] = agent
