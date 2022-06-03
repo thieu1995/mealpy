1327bdce3f3170483099af3ac1b73d45089ab988
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-03 19:29:23 +0700

    Update training mode in WDO

--- mealpy/physics_based/WDO.py
+++ mealpy/physics_based/WDO.py
@@ -21,7 +21,7 @@ class BaseWDO(Optimizer):
     + air parcel: is the set of gas atoms. Each atom represents a dimension in position and has its own velocity
     + pressure represented by fitness value
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + RT (int): [2, 3, 4], RT coefficient, default = 3
         + g_c (float): [0.1, 0.5], gravitational constant, default = 0.2
         + alp (float): [0.3, 0.8], constants in the update equation, default=0.4
@@ -108,4 +108,10 @@ class BaseWDO(Optimizer):
             pos = self.pop[idx][self.ID_POS] + vel
             pos_new = self.amend_position(pos, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        self.pop = self.update_target_wrapper_population(pop_new)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                pop_new[-1] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.greedy_selection_population(pop_new, self.pop)
+        self.pop = pop_new
