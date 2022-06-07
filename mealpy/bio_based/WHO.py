9cc0a6b4f100ce6af01bbb676831326cc0aef7a4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-07 18:02:30 +0700

    Fix missing training mode in WHO

--- mealpy/bio_based/WHO.py
+++ mealpy/bio_based/WHO.py
@@ -103,6 +103,7 @@ class BaseWHO(Optimizer):
         pop_new = []
         for idx in range(0, self.pop_size):
             ### 1. Local movement (Milling behaviour)
+            nfe_epoch += self.n_s
             local_list = []
             for j in range(0, self.n_s):
                 temp = self.pop[idx][self.ID_POS] + self.eta * np.random.uniform() * np.random.uniform(self.problem.lb, self.problem.ub)
@@ -117,40 +118,39 @@ class BaseWHO(Optimizer):
             pop_new.append([pos_new, None])
             if self.mode not in self.AVAILABLE_MODES:
                 target = self.get_target_wrapper(pos_new)
-                pop_new[-1] = self.get_better_solution([pos_new, target, np.zeros(self.problem.n_dims)], self.pop[idx])
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
         if self.mode in self.AVAILABLE_MODES:
             pop_new = self.update_target_wrapper_population(pop_new)
-            pop_new = self.greedy_selection_population(self.pop, pop_new)
-        nfe_epoch += self.pop_size
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
 
         for idx in range(0, self.pop_size):
             ### 2. Herd instinct
             idr = np.random.choice(range(0, self.pop_size))
-            if self.compare_agent(pop_new[idr], pop_new[idx]) and np.random.rand() < self.p_hi:
-                temp = self.global_move[0] * pop_new[idx][self.ID_POS] + self.global_move[1] * pop_new[idr][self.ID_POS]
+            if self.compare_agent(self.pop[idr], self.pop[idx]) and np.random.rand() < self.p_hi:
+                temp = self.global_move[0] * self.pop[idx][self.ID_POS] + self.global_move[1] * self.pop[idr][self.ID_POS]
                 pos_new = self.amend_position(temp, self.problem.lb, self.problem.ub)
                 target = self.get_target_wrapper(pos_new)
                 nfe_epoch += 1
-                if self.compare_agent([pos_new, target], pop_new[idx]):
-                    pop_new[idx] = [pos_new, target]
+                if self.compare_agent([pos_new, target], self.pop[idx]):
+                    self.pop[idx] = [pos_new, target]
 
-        _, best, worst = self.get_special_solutions(pop_new, best=1, worst=1)
+        _, best, worst = self.get_special_solutions(self.pop, best=1, worst=1)
         g_best, g_worst = best[0], worst[0]
 
         pop_child = []
         for idx in range(0, self.pop_size):
-            dist_to_worst = np.linalg.norm(pop_new[idx][self.ID_POS] - g_worst[self.ID_POS])
-            dist_to_best = np.linalg.norm(pop_new[idx][self.ID_POS] - g_best[self.ID_POS])
+            dist_to_worst = np.linalg.norm(self.pop[idx][self.ID_POS] - g_worst[self.ID_POS])
+            dist_to_best = np.linalg.norm(self.pop[idx][self.ID_POS] - g_best[self.ID_POS])
 
             ### 3. Starvation avoidance
             if dist_to_worst < self.delta[0]:
-                temp = pop_new[idx][self.ID_POS] + np.random.uniform() * (self.problem.ub - self.problem.lb) * \
+                temp = self.pop[idx][self.ID_POS] + np.random.uniform() * (self.problem.ub - self.problem.lb) * \
                        np.random.uniform(self.problem.lb, self.problem.ub)
                 pos_new = self.amend_position(temp, self.problem.lb, self.problem.ub)
                 pop_child.append([pos_new, None])
                 if self.mode not in self.AVAILABLE_MODES:
                     target = self.get_target_wrapper(pos_new)
-                    pop_child[-1] = self.get_better_solution([pos_new, target], pop_new[idx])
+                    self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
 
             ### 4. Population pressure
             if 1.0 < dist_to_best and dist_to_best < self.delta[1]:
@@ -159,7 +159,7 @@ class BaseWHO(Optimizer):
                 pop_child.append([pos_new, None])
                 if self.mode not in self.AVAILABLE_MODES:
                     target = self.get_target_wrapper(pos_new)
-                    pop_child[-1] = self.get_better_solution([pos_new, target], pop_new[idx])
+                    self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
 
             ### 5. Herd social memory
             for j in range(0, self.n_e):
@@ -168,10 +168,10 @@ class BaseWHO(Optimizer):
                 pop_child.append([pos_new, None])
                 if self.mode not in self.AVAILABLE_MODES:
                     target = self.get_target_wrapper(pos_new)
-                    pop_child[-1] = self.get_better_solution([pos_new, target], pop_new[idx])
-        nfe_epoch += len(pop_child)
-        self.nfe_per_epoch = nfe_epoch
+                    self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
         if self.mode in self.AVAILABLE_MODES:
             pop_child = self.update_target_wrapper_population(pop_child)
-        pop_child = self.get_sorted_strim_population(pop_child, self.pop_size)
-        self.pop = self.greedy_selection_population(pop_child, pop_new)
+            pop_child = self.get_sorted_strim_population(pop_child, self.pop_size)
+            self.pop = self.greedy_selection_population(pop_child, self.pop)
+        nfe_epoch += len(pop_child)
+        self.nfe_per_epoch = nfe_epoch
