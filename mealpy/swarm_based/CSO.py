c150f867df1eeda7dbca8bcae62a1c7b671d5d2c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-08 20:49:35 +0700

    Add single training mode in CSO

--- mealpy/swarm_based/CSO.py
+++ mealpy/swarm_based/CSO.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# ------------------------------------------------------------------------------------------------------%
-# Created by "Thieu Nguyen" at 10:09, 17/03/2020                                                        %
-#                                                                                                       %
-#       Email:      nguyenthieu2102@gmail.com                                                           %
-#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
-#       Github:     https://github.com/thieu1995                                                        %
-# -------------------------------------------------------------------------------------------------------%
+# !/usr/bin/env python
+# Created by "Thieu" at 10:09, 17/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
 
 import numpy as np
 from copy import deepcopy
@@ -20,7 +17,7 @@ class BaseCSO(Optimizer):
         1. https://link.springer.com/chapter/10.1007/978-3-540-36668-3_94
         2. https://www.hindawi.com/journals/cin/2020/4854895/
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + mixture_ratio (float): joining seeking mode with tracing mode, default=0.15
         + smp (int): seeking memory pool, default=5 clones (larger is better but time-consuming)
         + spc (bool): self-position considering, default=False
@@ -101,29 +98,25 @@ class BaseCSO(Optimizer):
         self.selected_strategy = self.validator.check_int("selected_strategy", selected_strategy, [0, 4])
         self.sort_flag = False
 
-    def create_solution(self, lb=None, ub=None):
+    def create_solution(self, lb=None, ub=None, pos=None):
         """
+        Overriding method in Optimizer class
         + x: current position of cat
         + v: vector v of cat (same amount of dimension as x)
         + flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching) => False: seeking mode , True: tracing mode
 
-        To get the position, fitness wrapper, target and obj list
-            + A[self.ID_POS]                  --> Return: position
-            + A[self.ID_TAR]                  --> Return: [target, [obj1, obj2, ...]]
-            + A[self.ID_TAR][self.ID_FIT]     --> Return: target
-            + A[self.ID_TAR][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
-
         Returns:
             list: wrapper of solution with format [position, target, velocity, flag]
         """
-        position = self.generate_position(lb, ub)
-        position = self.amend_position(position, lb, ub)
+        if pos is None:
+            pos = self.generate_position(lb, ub)
+        position = self.amend_position(pos, lb, ub)
         target = self.get_target_wrapper(position)
         velocity = np.random.uniform(lb, ub)
         flag = True if np.random.uniform() < self.mixture_ratio else False
         return [position, target, velocity, flag]
 
-    def _seeking_mode__(self, cat):
+    def seeking_mode__(self, cat):
         candidate_cats = []
         clone_cats = self.create_population(self.smp)
         if self.spc:
@@ -135,10 +128,12 @@ class BaseCSO(Optimizer):
             pos_new1 = clone[self.ID_POS] * (1 + self.srd)
             pos_new2 = clone[self.ID_POS] * (1 - self.srd)
 
-            pos_new = np.where(np.random.uniform(0, 1, self.problem.n_dims) < 0.5, pos_new1, pos_new2)
+            pos_new = np.where(np.random.random(self.problem.n_dims) < 0.5, pos_new1, pos_new2)
             pos_new[idx] = clone[self.ID_POS][idx]
             pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             candidate_cats.append([pos_new, None, clone[self.ID_VEL], clone[self.ID_FLAG]])
+            if self.mode not in self.AVAILABLE_MODES:
+                candidate_cats[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
         candidate_cats = self.update_target_wrapper_population(candidate_cats)
 
         if self.selected_strategy == 0:  # Best fitness-self
@@ -166,6 +161,7 @@ class BaseCSO(Optimizer):
         """
         w = (self.epoch - epoch) / self.epoch * (self.w_max - self.w_min) + self.w_min
         pop_new = []
+        nfe_epoch = 0
         for idx in range(0, self.pop_size):
             agent = deepcopy(self.pop[idx])
             # tracing mode
@@ -173,10 +169,14 @@ class BaseCSO(Optimizer):
                 pos_new = self.pop[idx][self.ID_POS] + w * self.pop[idx][self.ID_VEL] + \
                           np.random.uniform() * self.c1 * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
                 pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+                nfe_epoch += 1
             else:
-                pos_new = self._seeking_mode__(self.pop[idx])
+                pos_new = self.seeking_mode__(self.pop[idx])
+                nfe_epoch += self.smp
             agent[self.ID_POS] = pos_new
             agent[self.ID_FLAG] = True if np.random.uniform() < self.mixture_ratio else False
             pop_new.append(agent)
+            if self.mode not in self.AVAILABLE_MODES:
+                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
         self.pop = self.update_target_wrapper_population(pop_new)
-        self.nfe_per_epoch = self.pop_size
+        self.nfe_per_epoch = nfe_epoch
