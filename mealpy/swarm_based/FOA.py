a1ed2bd4df655a47727fafa0408b13098812634e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-08 21:28:03 +0700

    Add single training mode to FOA

--- mealpy/swarm_based/FOA.py
+++ mealpy/swarm_based/FOA.py
@@ -60,26 +60,23 @@ class OriginalFOA(Optimizer):
         self.nfe_per_epoch = self.pop_size
         self.sort_flag = False
 
-    def norm_consecutive_adjacent(self, position=None):
+    def norm_consecutive_adjacent__(self, position=None):
         return np.array([np.linalg.norm([position[x], position[x + 1]]) for x in range(0, self.problem.n_dims - 1)] + \
                         [np.linalg.norm([position[-1], position[0]])])
 
-    def create_solution(self, lb=None, ub=None):
+    def create_solution(self, lb=None, ub=None, pos=None):
         """
-        To get the position, fitness wrapper, target and obj list
-            + A[self.ID_POS]                  --> Return: position
-            + A[self.ID_TAR]                  --> Return: [target, [obj1, obj2, ...]]
-            + A[self.ID_TAR][self.ID_FIT]     --> Return: target
-            + A[self.ID_TAR][self.ID_OBJ]     --> Return: [obj1, obj2, ...]
+        Overriding method in Optimizer class
 
         Returns:
             list: a solution with format [position, target]
         """
-        position = np.random.uniform(self.problem.lb, self.problem.ub)
-        s = self.norm_consecutive_adjacent(position)
+        if pos is None:
+            pos = self.generate_position(self.problem.lb, self.problem.ub)
+        s = self.norm_consecutive_adjacent__(pos)
         pos = self.amend_position(s, self.problem.lb, self.problem.ub)
         target = self.get_target_wrapper(pos)
-        return [position, target]
+        return [pos, target]
 
     def evolve(self, epoch):
         """
@@ -91,10 +88,15 @@ class OriginalFOA(Optimizer):
         pop_new = []
         for idx in range(0, self.pop_size):
             pos_new = self.pop[idx][self.ID_POS] + np.random.normal(self.problem.lb, self.problem.ub)
-            pos_new = self.norm_consecutive_adjacent(pos_new)
+            pos_new = self.norm_consecutive_adjacent__(pos_new)
             pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        self.pop = self.update_target_wrapper_population(pop_new)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop)
 
 
 class BaseFOA(OriginalFOA):
@@ -147,12 +149,16 @@ class BaseFOA(OriginalFOA):
         """
         pop_new = []
         for idx in range(0, self.pop_size):
-            pos_new = self.pop[idx][self.ID_POS] + np.random.normal(self.problem.lb, self.problem.ub)
-            pos_new = np.random.normal() * self.norm_consecutive_adjacent(pos_new)
+            pos_new = self.pop[idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims)
+            pos_new = np.random.normal() * self.norm_consecutive_adjacent__(pos_new)
             pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        pop_new = self.update_target_wrapper_population(pop_new)
-        self.pop = self.greedy_selection_population(self.pop, pop_new)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop)
 
 
 class WhaleFOA(OriginalFOA):
@@ -227,7 +233,12 @@ class WhaleFOA(OriginalFOA):
             else:
                 D1 = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
                 pos_new = D1 * np.exp(b * l) * np.cos(2 * np.pi * l) + self.g_best[self.ID_POS]
-            smell = self.norm_consecutive_adjacent(pos_new)
+            smell = self.norm_consecutive_adjacent__(pos_new)
             pos_new = self.amend_position(smell, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        self.pop = self.update_target_wrapper_population(pop_new)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop)
