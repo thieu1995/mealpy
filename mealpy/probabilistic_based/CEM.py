5e426e7b42589bb17506fab1e0ce04988b320e13
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-08 11:24:28 +0700

    Update CEM

--- mealpy/probabilistic_based/CEM.py
+++ mealpy/probabilistic_based/CEM.py
@@ -93,8 +93,7 @@ class BaseCEM(Optimizer):
             pop_new.append([pos_new, None])
             if self.mode not in self.AVAILABLE_MODES:
                 target = self.get_target_wrapper(pos_new)
-                pop_new[-1] = self.get_better_solution([pos_new, target], self.pop[idx])
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
         if self.mode in self.AVAILABLE_MODES:
             pop_new = self.update_target_wrapper_population(pop_new)
-            pop_new = self.greedy_selection_population(self.pop, pop_new)
-        self.pop = pop_new
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
