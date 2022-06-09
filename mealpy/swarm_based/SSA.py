183256119a17b67ee3c55bf4e0c958b4a3711786
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-09 09:54:53 +0700

    Add single training mode to SSA

--- mealpy/swarm_based/SSA.py
+++ mealpy/swarm_based/SSA.py
@@ -19,7 +19,7 @@ class BaseSSA(Optimizer):
     + In Eq. 4, Instead of using A+ and L, I used np.random.normal()
     + Some components (g_best_position, fitness updated) are missing in Algorithm 1 (paper)
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + ST (float): ST in [0.5, 1.0], safety threshold value, default = 0.8
         + PD (float): number of producers (percentage), default = 0.2
         + SD (float): number of sparrows who perceive the danger, default = 0.1
@@ -77,9 +77,6 @@ class BaseSSA(Optimizer):
 
     def amend_position(self, position=None, lb=None, ub=None):
         """
-        Depend on what kind of problem are we trying to solve, there will be an different amend_position
-        function to rebound the position of agent into the valid range.
-
         Args:
             position: vector position (location) of the solution.
             lb: list of lower bound values
@@ -88,7 +85,9 @@ class BaseSSA(Optimizer):
         Returns:
             Amended position (make the position is in bound)
         """
-        return np.where(np.logical_and(lb <= position, position <= ub), position, np.random.uniform(lb, ub))
+        condition = np.logical_and(lb <= position, position <= ub)
+        pos_rand = np.random.uniform(lb, ub)
+        return np.where(condition, position, pos_rand)
 
     def evolve(self, epoch):
         """
@@ -119,25 +118,32 @@ class BaseSSA(Optimizer):
                     x_new = g_best[self.ID_POS] + np.abs(self.pop[idx][self.ID_POS] - g_best[self.ID_POS]) * np.random.normal()
             pos_new = self.amend_position(x_new, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        pop_new = self.update_target_wrapper_population(pop_new)
-        pop_new = self.greedy_selection_population(self.pop, pop_new)
-        pop_new, best, worst = self.get_special_solutions(pop_new, best=1, worst=1)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution(self.pop[idx], [pos_new, target])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
+        self.pop, best, worst = self.get_special_solutions(self.pop, best=1, worst=1)
         g_best, g_worst = best[0], worst[0]
-        pop2 = deepcopy(pop_new[self.n2:])
+        pop2 = deepcopy(self.pop[self.n2:])
         child = []
         for idx in range(0, len(pop2)):
             #  Using equation (5) update the sparrow’s location;
             if self.compare_agent(self.pop[idx], g_best):
-                x_new = pop2[idx][self.ID_POS] + \
-                        np.random.uniform(-1, 1) * (np.abs(pop2[idx][self.ID_POS] - g_worst[self.ID_POS]) /
-                                                    (pop2[idx][self.ID_TAR][self.ID_FIT] - g_worst[self.ID_TAR][self.ID_FIT] + self.EPSILON))
+                x_new = pop2[idx][self.ID_POS] + np.random.uniform(-1, 1) * (np.abs(pop2[idx][self.ID_POS] - g_worst[self.ID_POS]) /
+                        (pop2[idx][self.ID_TAR][self.ID_FIT] - g_worst[self.ID_TAR][self.ID_FIT] + self.EPSILON))
             else:
                 x_new = g_best[self.ID_POS] + np.random.normal() * np.abs(pop2[idx][self.ID_POS] - g_best[self.ID_POS])
             pos_new = self.amend_position(x_new, self.problem.lb, self.problem.ub)
             child.append([pos_new, None])
-        child = self.update_target_wrapper_population(child)
-        child = self.greedy_selection_population(pop2, child)
-        self.pop = pop_new[:self.n2] + child
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                pop2[idx] = self.get_better_solution(pop2[idx], [pos_new, target])
+        if self.mode in self.AVAILABLE_MODES:
+            child = self.update_target_wrapper_population(child)
+            pop2 = self.greedy_selection_population(pop2, child)
+        self.pop = self.pop[:self.n2] + pop2
 
 
 class OriginalSSA(BaseSSA):
@@ -151,7 +157,7 @@ class OriginalSSA(BaseSSA):
     ~~~~~
     + The paper contains some unclear equations and symbol
 
-    Hyper-parameters should fine tuned in approximate range to get faster convergence toward the global optimum:
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + ST (float): ST in [0.5, 1.0], safety threshold value, default = 0.8
         + PD (float): number of producers (percentage), default = 0.2
         + SD (float): number of sparrows who perceive the danger, default = 0.1
@@ -230,22 +236,29 @@ class OriginalSSA(BaseSSA):
                     x_new = g_best[self.ID_POS] + np.matmul(np.abs(self.pop[idx][self.ID_POS] - g_best[self.ID_POS]), A1)
             pos_new = self.amend_position(x_new, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
-        pop_new = self.update_target_wrapper_population(pop_new)
-        pop_new = self.greedy_selection_population(self.pop, pop_new)
-        pop_new, best, worst = self.get_special_solutions(pop_new, best=1, worst=1)
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution(self.pop[idx], [pos_new, target])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
+        self.pop, best, worst = self.get_special_solutions(self.pop, best=1, worst=1)
         g_best, g_worst = best[0], worst[0]
-        pop2 = pop_new[self.n2:]
+        pop2 = self.pop[self.n2:]
         child = []
         for idx in range(0, len(pop2)):
             #  Using equation (5) update the sparrow’s location;
             if self.compare_agent(self.pop[idx], g_best):
-                x_new = pop2[idx][self.ID_POS] + \
-                        np.random.uniform(-1, 1) * (np.abs(pop2[idx][self.ID_POS] - g_worst[self.ID_POS]) /
-                                                    (pop2[idx][self.ID_TAR][self.ID_FIT] - g_worst[self.ID_TAR][self.ID_FIT] + self.EPSILON))
+                x_new = pop2[idx][self.ID_POS] + np.random.uniform(-1, 1) * (np.abs(pop2[idx][self.ID_POS] - g_worst[self.ID_POS]) /
+                    (pop2[idx][self.ID_TAR][self.ID_FIT] - g_worst[self.ID_TAR][self.ID_FIT] + self.EPSILON))
             else:
                 x_new = g_best[self.ID_POS] + np.random.normal() * np.abs(pop2[idx][self.ID_POS] - g_best[self.ID_POS])
             pos_new = self.amend_position(x_new, self.problem.lb, self.problem.ub)
             child.append([pos_new, None])
-        child = self.update_target_wrapper_population(child)
-        child = self.greedy_selection_population(pop2, child)
-        self.pop = pop_new[:self.n2] + child
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                pop2[idx] = self.get_better_solution(pop2[idx], [pos_new, target])
+        if self.mode in self.AVAILABLE_MODES:
+            child = self.update_target_wrapper_population(child)
+            pop2 = self.greedy_selection_population(pop2, child)
+        self.pop = self.pop[:self.n2] + pop2
