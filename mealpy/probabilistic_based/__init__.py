79c304a5ecbfb4372ff60790ae9d2295c9dafd27
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-06-22 16:27:28 +0700

    Merge probabilistic group to math-based group

--- mealpy/probabilistic_based/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# Created by "Thieu" at 00:08, 27/04/2020 ----------%
-#       Email: nguyenthieu2102@gmail.com            %
-#       Github: https://github.com/thieu1995        %
-# --------------------------------------------------%
-
-from . import CEM
