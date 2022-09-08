2350f890b3afbf8297c7bede2bdc9d05dcde0e31
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-09-08 14:13:41 +0700

    Refactor GA class

--- mealpy/evolutionary_based/GA.py
+++ mealpy/evolutionary_based/GA.py
@@ -45,31 +45,30 @@ class BaseGA(Optimizer):
     >>> pop_size = 50
     >>> pc = 0.9
     >>> pm = 0.05
-    >>> model1 = BaseGA(problem_dict1, epoch, pop_size, pc, pm)
-    >>> best_position, best_fitness = model1.solve()
+    >>> model1 = BaseGA(epoch, pop_size, pc, pm)
+    >>> best_position, best_fitness = model1.solve(problem_dict1)
     >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
     >>>
-    >>> model2 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, selection="tournament", k_way=0.4, crossover="multi_points")
+    >>> model2 = BaseGA(epoch, pop_size, pc, pm, selection="tournament", k_way=0.4, crossover="multi_points")
     >>>
-    >>> model3 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, crossover="one_point", mutation="scramble")
+    >>> model3 = BaseGA(epoch, pop_size, pc, pm, crossover="one_point", mutation="scramble")
     >>>
-    >>> model4 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, crossover="arithmetic", mutation_multipoints=True, mutation="swap")
+    >>> model4 = BaseGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation_multipoints=True, mutation="swap")
     >>>
-    >>> model5 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, selection="roulette", crossover="multi_points")
+    >>> model5 = BaseGA(epoch, pop_size, pc, pm, selection="roulette", crossover="multi_points")
     >>>
-    >>> model6 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, selection="random", mutation="inversion")
+    >>> model6 = BaseGA(epoch, pop_size, pc, pm, selection="random", mutation="inversion")
     >>>
-    >>> model7 = BaseGA(problem_dict1, epoch, pop_size, pc, pm, crossover="arithmetic", mutation="flip")
+    >>> model7 = BaseGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation="flip")
 
     References
     ~~~~~~~~~~
     [1] Whitley, D., 1994. A genetic algorithm tutorial. Statistics and computing, 4(2), pp.65-85.
     """
 
-    def __init__(self, problem, epoch=10000, pop_size=100, pc=0.95, pm=0.025, **kwargs):
+    def __init__(self, epoch=10000, pop_size=100, pc=0.95, pm=0.025, **kwargs):
         """
         Args:
-            problem (dict): The problem dictionary
             epoch (int): maximum number of iterations, default = 10000
             pop_size (int): number of population size, default = 100
             pc (float): cross-over probability, default = 0.95
@@ -80,12 +79,13 @@ class BaseGA(Optimizer):
             mutation_multipoints (bool): Optional, True or False, effect on mutation process, default = False
             mutation (str): Optional, can be ["flip", "swap"] for multipoints and can be ["flip", "swap", "scramble", "inversion"] for one-point, default="flip"
         """
-        super().__init__(problem, kwargs)
+        super().__init__(**kwargs)
 
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
-        self.pc = self.validator.check_float("p_c", pc, (0, 1.0))
-        self.pm = self.validator.check_float("p_m", pm, (0, 1.0))
+        self.pc = self.validator.check_float("pc", pc, (0, 1.0))
+        self.pm = self.validator.check_float("pm", pm, (0, 1.0))
+        self.set_parameters(["epoch", "pop_size", "pc", "pm"])
 
         self.nfe_per_epoch = self.pop_size
         self.sort_flag = False
@@ -267,3 +267,227 @@ class BaseGA(Optimizer):
         ### Survivor Selection
         pop_new = self.update_target_wrapper_population(pop_new)
         self.pop = self.survivor_process__(self.pop, pop_new)
+
+
+class SingleGA(BaseGA):
+    """
+    The developed single-point mutation of: Genetic Algorithm (GA)
+
+    Links:
+        1. https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
+        2. https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
+        3. https://www.analyticsvidhya.com/blog/2017/07/introduction-to-genetic-algorithm/
+
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
+        + pc (float): [0.7, 0.95], cross-over probability, default = 0.95
+        + pm (float): [0.01, 0.2], mutation probability, default = 0.025
+        + selection (str): Optional, can be ["roulette", "tournament", "random"], default = "tournament"
+        + crossover (str): Optional, can be ["one_point", "multi_points", "uniform", "arithmetic"], default = "uniform"
+        + mutation (str): Optional, can be ["flip", "swap", "scramble", "inversion"] for one-point
+        + k_way (float): Optional, set it when use "tournament" selection, default = 0.2
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.evolutionary_based.GA import SingleGA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "fit_func": fitness_function,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pc = 0.9
+    >>> pm = 0.8
+    >>> selection = "roulette"
+    >>> crossover = "uniform"
+    >>> mutation = "swap"
+    >>> model1 = SingleGA(epoch, pop_size, pc, pm, selection, crossover, mutation)
+    >>> best_position, best_fitness = model1.solve(problem_dict1)
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>>
+    >>> model2 = SingleGA(epoch, pop_size, pc, pm, selection="tournament", k_way=0.4, crossover="multi_points")
+    >>>
+    >>> model3 = SingleGA(epoch, pop_size, pc, pm, crossover="one_point", mutation="scramble")
+    >>>
+    >>> model4 = SingleGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation="swap")
+    >>>
+    >>> model5 = SingleGA(epoch, pop_size, pc, pm, selection="roulette", crossover="multi_points")
+    >>>
+    >>> model6 = SingleGA(epoch, pop_size, pc, pm, selection="random", mutation="inversion")
+    >>>
+    >>> model7 = SingleGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation="flip")
+
+    References
+    ~~~~~~~~~~
+    [1] Whitley, D., 1994. A genetic algorithm tutorial. Statistics and computing, 4(2), pp.65-85.
+    """
+
+    def __init__(self, epoch=10000, pop_size=100, pc=0.95, pm=0.8, selection="roulette", crossover="uniform", mutation="swap", **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            pc (float): cross-over probability, default = 0.95
+            pm (float): mutation probability, default = 0.8
+            selection (str): Optional, can be ["roulette", "tournament", "random"], default = "tournament"
+            crossover (str): Optional, can be ["one_point", "multi_points", "uniform", "arithmetic"], default = "uniform"
+            mutation (str): Optional, can be ["flip", "swap", "scramble", "inversion"], default="flip"
+            k_way (float): Optional, set it when use "tournament" selection, default = 0.2
+        """
+        super().__init__(epoch, pop_size, pc, pm, **kwargs)
+        self.selection = self.validator.check_str("selection", selection, ["tournament", "random", "roulette"])
+        self.crossover = self.validator.check_str("crossover", crossover, ["one_point", "multi_points", "uniform", "arithmetic"])
+        self.mutation = self.validator.check_str("mutation", mutation, ["flip", "swap", "scramble", "inversion"])
+        self.set_parameters(["epoch", "pop_size", "pc", "pm", "selection", "crossover", "mutation"])
+        if "k_way" in kwargs:
+            self.k_way = self.validator.check_float("k_way", kwargs["k_way"], (0, 1.0))
+
+        self.nfe_per_epoch = self.pop_size
+        self.sort_flag = False
+
+    def mutation_process__(self, child):
+        """
+        + https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_mutation.htm
+        + The mutation process is effected by parameter: pm
+            + flip --> should set the pm large such as: [0.5 -> 0.9]
+            + swap --> same as flip: pm in range [0.5 -> 0.9]
+            + scramble --> should set the pm small enough such as: [0.4 -> 0.6]
+            + inversion --> like scramble [0.4 -> 0.6]
+
+        Args:
+            child (np.array): The position of the child
+
+        Returns:
+            np.array: The mutated vector of the child
+        """
+        if self.mutation == "swap":
+            idx1, idx2 = np.random.choice(range(0, self.problem.n_dims), 2, replace=False)
+            child[idx1], child[idx2] = child[idx2], child[idx1]
+            return child
+        elif self.mutation == "inversion":
+            cut1, cut2 = np.random.choice(range(0, self.problem.n_dims), 2, replace=False)
+            temp = child[cut1:cut2]
+            temp = temp[::-1]
+            child[cut1:cut2] = temp
+            return child
+        elif self.mutation == "scramble":
+            cut1, cut2 = np.random.choice(range(0, self.problem.n_dims), 2, replace=False)
+            temp = child[cut1:cut2]
+            np.random.shuffle(temp)
+            child[cut1:cut2] = temp
+            return child
+        else:   # "flip"
+            idx = np.random.randint(0, self.problem.n_dims)
+            child[idx] = np.random.uniform(self.problem.lb[idx], self.problem.ub[idx])
+            return child
+
+
+class MultiGA(BaseGA):
+    """
+    The developed multipoints-mutation version of: Genetic Algorithm (GA)
+
+    Links:
+        1. https://blog.sicara.com/getting-started-genetic-algorithms-python-tutorial-81ffa1dd72f9
+        2. https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
+        3. https://www.analyticsvidhya.com/blog/2017/07/introduction-to-genetic-algorithm/
+
+    Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
+        + pc (float): [0.7, 0.95], cross-over probability, default = 0.95
+        + pm (float): [0.01, 0.2], mutation probability, default = 0.025
+        + selection (str): Optional, can be ["roulette", "tournament", "random"], default = "tournament"
+        + k_way (float): Optional, set it when use "tournament" selection, default = 0.2
+        + crossover (str): Optional, can be ["one_point", "multi_points", "uniform", "arithmetic"], default = "uniform"
+        + mutation (str): Optional, can be ["flip", "swap"] for multipoints
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.evolutionary_based.GA import MultiGA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "fit_func": fitness_function,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> pc = 0.9
+    >>> pm = 0.05
+    >>> selection = "roulette"
+    >>> crossover = "uniform"
+    >>> mutation = "swap"
+    >>> model1 = MultiGA(epoch, pop_size, pc, pm, selection, crossover, mutation)
+    >>> best_position, best_fitness = model1.solve(problem_dict1)
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>>
+    >>> model2 = MultiGA(epoch, pop_size, pc, pm, selection="tournament", k_way=0.4, crossover="multi_points")
+    >>>
+    >>> model3 = MultiGA(epoch, pop_size, pc, pm, crossover="one_point", mutation="flip")
+    >>>
+    >>> model4 = MultiGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation_multipoints=True, mutation="swap")
+    >>>
+    >>> model5 = MultiGA(epoch, pop_size, pc, pm, selection="roulette", crossover="multi_points")
+    >>>
+    >>> model6 = MultiGA(epoch, pop_size, pc, pm, selection="random", mutation="swap")
+    >>>
+    >>> model7 = MultiGA(epoch, pop_size, pc, pm, crossover="arithmetic", mutation="flip")
+
+    References
+    ~~~~~~~~~~
+    [1] Whitley, D., 1994. A genetic algorithm tutorial. Statistics and computing, 4(2), pp.65-85.
+    """
+
+    def __init__(self, epoch=10000, pop_size=100, pc=0.95, pm=0.025, selection="roulette", crossover="arithmetic", mutation="flip", **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            pc (float): cross-over probability, default = 0.95
+            pm (float): mutation probability, default = 0.025
+            selection (str): Optional, can be ["roulette", "tournament", "random"], default = "tournament"
+            crossover (str): Optional, can be ["one_point", "multi_points", "uniform", "arithmetic"], default = "uniform"
+            mutation (str): Optional, can be ["flip", "swap"] for multipoints
+            k_way (float): Optional, set it when use "tournament" selection, default = 0.2
+        """
+        super().__init__(epoch, pop_size, pc, pm, **kwargs)
+        self.selection = self.validator.check_str("selection", selection, ["tournament", "random", "roulette"])
+        self.crossover = self.validator.check_str("crossover", crossover, ["one_point", "multi_points", "uniform", "arithmetic"])
+        self.mutation = self.validator.check_str("mutation", mutation, ["flip", "swap"])
+        self.set_parameters(["epoch", "pop_size", "pc", "pm", "selection", "crossover", "mutation"])
+        if "k_way" in kwargs:
+            self.k_way = self.validator.check_float("k_way", kwargs["k_way"], (0, 1.0))
+
+    def mutation_process__(self, child):
+        """
+        + https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_mutation.htm
+        + Mutated on the whole vector is effected by parameter: pm
+            + flip --> (default in this case) should set the pm small such as: [0.01 -> 0.2]
+            + swap --> should set the pm small such as: [0.01 -> 0.2]
+
+        Args:
+            child (np.array): The position of the child
+
+        Returns:
+            np.array: The mutated vector of the child
+        """
+        if self.mutation == "swap":
+            for idx in range(self.problem.n_dims):
+                idx_swap = np.random.choice(list(set(range(0, self.problem.n_dims)) - {idx}))
+                child[idx], child[idx_swap] = child[idx_swap], child[idx]
+                return child
+        else:       # "flip"
+            mutation_child = self.generate_position(self.problem.lb, self.problem.ub)
+            flag_child = np.random.uniform(0, 1, self.problem.n_dims) < self.pm
+            return np.where(flag_child, mutation_child, child)
