7f24d0226126e98d43ca1c0b7044821d2b3f53ad
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-09-11 21:04:31 +0700

    Refactor Multitask

--- mealpy/utils/app/multitask.py
+++ mealpy/utils/app/multitask.py
@@ -9,6 +9,9 @@ from pathlib import Path
 from mealpy.optimizer import Optimizer
 from mealpy.utils.problem import Problem
 from mealpy.utils.validator import Validator
+from functools import partial
+import concurrent.futures as parallel
+import os
 
 
 class Multitask:
@@ -20,32 +23,45 @@ class Multitask:
     Attributes:
         algorithms (list, tuple): List of algorithms to run
         problems (list, tuple): List of problems to run
-        n_trials (int): Number of repetitions
 
     """
-    def __init__(self, algorithms=(), problems=(), n_trials=2):
+    def __init__(self, algorithms=(), problems=(), **kwargs):
+        self.__set_keyword_arguments(kwargs)
         self.validator = Validator(log_to="console", log_file=None)
         self.algorithms = self.validator.check_list_tuple("algorithms", algorithms, "Optimizer")
         self.problems = self.validator.check_list_tuple("problems", problems, "Problem")
-        self.n_trials = self.validator.check_int("n_trials", n_trials, [1, 100000])
 
-    def export_to_dataframe(self, result: pd.DataFrame, save_path: str):
+    def __set_keyword_arguments(self, kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @staticmethod
+    def export_to_dataframe(result: pd.DataFrame, save_path: str):
         result.to_pickle(f"{save_path}.pkl")
 
-    def export_to_json(self, result: pd.DataFrame, save_path: str):
+    @staticmethod
+    def export_to_json(result: pd.DataFrame, save_path: str):
         result.to_json(f"{save_path}.json")
 
-    def export_to_csv(self, result: pd.DataFrame, save_path: str):
+    @staticmethod
+    def export_to_csv(result: pd.DataFrame, save_path: str):
         result.to_csv(f"{save_path}.csv", header=True, index=False)
 
-    def get_export_function(self, export_type="csv"):
-        et = self.validator.check_str("export_type", export_type, ["csv", "json", "dataframe"])
-        return getattr(self, f"export_to_{et}")
+    def __run__(self, id_trial, model, problem):
+        _, best_fitness = model.solve(problem)
+        return {
+            "id_trial": id_trial,
+            "best_fitness": best_fitness,
+            "convergence": model.history.list_global_best_fit
+        }
 
-    def execute(self, save_path="", save_as="csv", save_convergence=False, verbose=False):
+    def execute(self, n_trials=2, mode="sequential", n_workers=2, save_path="history", save_as="csv", save_convergence=False, verbose=False):
         """Execute multitask utility.
 
         Args:
+            n_trials (int): Number of repetitions
+            mode (str): Execute problem using "sequential" or "parallel" mode, default = "sequential"
+            n_workers (int): Number of processes if mode is "parallel"
             save_path (str): The path to the folder that hold results
             save_as (str): Saved file type (e.g. dataframe, json, csv) (default: "csv")
             save_convergence (bool): Save the error (convergence/fitness) during generations (default: False)
@@ -55,8 +71,15 @@ class Multitask:
             TypeError: Raises TypeError if export type is not supported
 
         """
+        n_trials = self.validator.check_int("n_trials", n_trials, [1, 100000])
+        mode = self.validator.check_str("mode", mode, ["parallel", "sequential"])
+        if mode == "process":
+            n_workers = self.validator.check_int("n_workers", n_workers, [2, min(61, os.cpu_count() - 1)])
+        else:
+            n_workers = None
         ## Get export function
-        export_function = self.get_export_function(save_as)
+        save_as = self.validator.check_str("save_as", save_as, ["csv", "json", "dataframe"])
+        export_function = getattr(self, f"export_to_{save_as}")
 
         for id_model, model in enumerate(self.algorithms):
             if not isinstance(model, Optimizer):
@@ -80,15 +103,24 @@ class Multitask:
 
                 convergence_trials = {}
                 best_fit_trials = []
-                for id_trial in range(1, self.n_trials + 1):
 
-                    if verbose:
-                        print(f"Solving problem: {problem.get_name()} using algorithm: {model.get_name()}, on the: {id_trial} trial")
+                trial_list = list(range(1, n_trials+1))
 
-                    _, best_fitness = model.solve(problem)
-
-                    convergence_trials[f"trial_{id_trial}"] = model.history.list_global_best_fit
-                    best_fit_trials.append(best_fitness)
+                if mode == "parallel":
+                    with parallel.ProcessPoolExecutor(n_workers) as executor:
+                        list_results = executor.map(partial(self.__run__, model=model, problem=problem), trial_list)
+                        for result in list_results:
+                            convergence_trials[f"trial_{result['id_trial']}"] = result['convergence']
+                            best_fit_trials.append(result['best_fitness'])
+                            if verbose:
+                                print(f"Solving problem: {problem.get_name()} using algorithm: {model.get_name()}, on the: {result['id_trial']} trial")
+                else:
+                    for idx in trial_list:
+                        result = self.__run__(idx, model, problem)
+                        convergence_trials[f"trial_{result['id_trial']}"] = result['convergence']
+                        best_fit_trials.append(result['best_fitness'])
+                        if verbose:
+                            print(f"Solving problem: {problem.get_name()} using algorithm: {model.get_name()}, on the: {result['id_trial']} trial")
 
                 best_fit_model_results[problem.get_name()] = best_fit_trials
                 if save_convergence:
