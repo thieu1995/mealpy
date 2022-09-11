2787a12487556ca926a072de24eca2d0e25621f5
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-09-11 22:35:01 +0700

    Refactor path

--- examples/run_multitask.py
+++ examples/run_multitask.py
@@ -4,12 +4,18 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
+#### Using multiple algorithm to solve multiple problems with multiple trials
+
+
+## Import libraries
+
 from opfunu.cec_based.cec2017 import F52017, F102017, F292017
 from mealpy.bio_based import BBO
 from mealpy.evolutionary_based import DE
-from mealpy.utils.app import Multitask
+from mealpy.multitask import Multitask
 
-#### Using multiple algorithm to solve multiple problems with multiple trials
+
+## Define your own problems
 
 f1 = F52017(30, f_bias=0)
 f2 = F102017(30, f_bias=0)
@@ -48,6 +54,8 @@ model1 = BBO.BaseBBO(epoch=10, pop_size=50)
 model2 = BBO.OriginalBBO(epoch=10, pop_size=50)
 model3 = DE.BaseDE(epoch=10, pop_size=50)
 
+
+## Define and run Multitask
 
 if __name__ == "__main__":
     multitask = Multitask(algorithms=(model1, model2, model3), problems=(p1, p2, p3))
--- mealpy/utils/app/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Created by "Thieu" at 22:21, 06/09/2022 ----------%                                                                               
-#       Email: nguyenthieu2102@gmail.com            %                                                    
-#       Github: https://github.com/thieu1995        %                         
-# --------------------------------------------------%
-
-from .multitask import Multitask
