37c5e708e9c49626aad5242464b8d72d24301831
Thieu Nguyen <nguyenthieu2102@gmail.com>
2022-10-13 17:38:44 +0700

    Add Hybrid Grey Wolf - Whale Optimization Algorithm (GWO_WOA)

--- mealpy/swarm_based/GWO.py
+++ mealpy/swarm_based/GWO.py
@@ -174,3 +174,82 @@ class RW_GWO(Optimizer):
             pop_new = self.update_target_wrapper_population(pop_new)
             self.pop = self.greedy_selection_population(self.pop, pop_new)
         self.pop = self.get_sorted_strim_population(self.pop + leaders, self.pop_size)
+
+
+class GWO_WOA(OriginalGWO):
+    """
+    The original version of: Hybrid Grey Wolf - Whale Optimization Algorithm (GWO_WOA)
+
+    Links:
+        1. https://sci-hub.se/https://doi.org/10.1177/10775463211003402
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.GWO import GWO_WOA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "fit_func": fitness_function,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = GWO_WOA(epoch, pop_size)
+    >>> best_position, best_fitness = model.solve(problem_dict1)
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Obadina, O. O., Thaha, M. A., Althoefer, K., & Shaheed, M. H. (2022). Dynamic characterization of a master–slave
+    robotic manipulator using a hybrid grey wolf–whale optimization algorithm. Journal of Vibration and Control, 28(15-16), 1992-2003.
+    """
+
+    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+        """
+        super().__init__(epoch, pop_size, **kwargs)
+        self.b = 1.0
+        self.nfe_per_epoch = self.pop_size
+        self.sort_flag = False
+
+    def evolve(self, epoch):
+        """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
+        Args:
+            epoch (int): The current iteration
+        """
+        # linearly decreased from 2 to 0
+        a = 2 - (epoch + 1) / self.epoch
+        _, list_best, _ = self.get_special_solutions(self.pop, best=3)
+
+        pop_new = []
+        for idx in range(0, self.pop_size):
+            A1, A2, A3 = a * (2 * np.random.uniform() - 1), a * (2 * np.random.uniform() - 1), a * (2 * np.random.uniform() - 1)
+            C1, C2, C3 = 2 * np.random.uniform(), 2 * np.random.uniform(), 2 * np.random.uniform()
+            if np.random.random() < 0.5:
+                da = np.random.random() * np.abs(C1 * list_best[0][self.ID_POS] - self.pop[idx][self.ID_POS])
+            else:
+                P, L = np.random.random(), np.random.uniform(-1, 1)
+                da = P * np.exp(self.b * L) * np.cos(2*np.pi*L) * np.abs(C1 * list_best[0][self.ID_POS] - self.pop[idx][self.ID_POS])
+            X1 = list_best[0][self.ID_POS] - A1 * da
+            X2 = list_best[1][self.ID_POS] - A2 * np.abs(C2 * list_best[1][self.ID_POS] - self.pop[idx][self.ID_POS])
+            X3 = list_best[2][self.ID_POS] - A3 * np.abs(C3 * list_best[2][self.ID_POS] - self.pop[idx][self.ID_POS])
+            pos_new = (X1 + X2 + X3) / 3.0
+            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            pop_new.append([pos_new, None])
+            if self.mode not in self.AVAILABLE_MODES:
+                target = self.get_target_wrapper(pos_new)
+                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_wrapper_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new)
