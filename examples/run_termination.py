c4e9650e2a0743a50c17332d8effcc8b4bb77c2c
godot <yuezhang9810@gmail.com>
2022-11-29 01:49:48 +0800

    Fixed arguments missing for model.solve() function and a comment error.

--- examples/run_termination.py
+++ examples/run_termination.py
@@ -25,9 +25,9 @@ model = HGSO.OriginalHGSO(epoch=200, pop_size=50)
 ## 1. Epoch/Maximum Generation (default)
 term_dict1 = {
     "mode": "MG",
-    "quantity": 50  # 100000 number of function evaluation
+    "quantity": 50  # 50 epochs
 }
-best_position, best_fitness = model.solve(termination=term_dict1)
+best_position, best_fitness = model.solve(problem_dict1, termination=term_dict1)
 print(f"Solution: {best_position}, Fitness: {best_fitness}")
 
 
@@ -36,7 +36,7 @@ term_dict2 = {
     "mode": "FE",
     "quantity": 2000  # 2000 number of function evaluation
 }
-best_position, best_fitness = model.solve(termination=term_dict2)
+best_position, best_fitness = model.solve(problem_dict1, termination=term_dict2)
 print(f"Solution: {best_position}, Fitness: {best_fitness}")
 
 
@@ -45,7 +45,7 @@ term_dict3 = {
     "mode": "TB",
     "quantity": 15  # 15 seconds
 }
-best_position, best_fitness = model.solve(termination=term_dict3)
+best_position, best_fitness = model.solve(problem_dict1, termination=term_dict3)
 print(f"Solution: {best_position}, Fitness: {best_fitness}")
 
 
@@ -54,5 +54,5 @@ term_dict4 = {
     "mode": "ES",
     "quantity": 10  # 10 patients
 }
-best_position, best_fitness = model.solve(termination=term_dict4)
+best_position, best_fitness = model.solve(problem_dict1, termination=term_dict4)
 print(f"Solution: {best_position}, Fitness: {best_fitness}")
