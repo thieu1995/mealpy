1b91f0e15b429fa13b4388c196d07381e27481f7
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-03-11 21:58:31 +0700

    Add Serval Optimization Algorithm (ServalOA) to swarm-based group

---
+++ mealpy/swarm_based/ServalOA.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 21:34, 11/03/2023 ----------%
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from mealpy.optimizer import Optimizer
+
+
+class OriginalServalOA(Optimizer):
+    """
+    The original version of: Serval Optimization Algorithm (ServalOA)
+
+    Links:
+        1. https://www.mdpi.com/2313-7673/7/4/204
+
+    Notes (Plagiarism):
+        0. It's really horrible, it's like he's copying and pasting all the garbage algorithms that he invented. They are almost the same.
+        1. Algorithm design is very similar to Zebra Optimization Algorithm (ZOA), Osprey Optimization Algorithm (OOA), Coati Optimization Algorithm (CoatiOA),
+        Siberian Tiger Optimization (STO), Language Education Optimization (LEO), Pelican Optimization Algorithm (POA), Walrus Optimization Algorithm (WOA),
+        Fennec Fox Optimization (FFO), Three-periods optimization algorithm (TPOA), Teamwork optimization algorithm (TOA), Northern goshawk optimization (NGO),
+        Tasmanian devil optimization (TDO), Archery algorithm (AA), Cat and mouse based optimizer (CMBO)
+        2. Check the matlab code of all above algorithms
+        2. Same authors, self-plagiarized article with kinda same algorithm with different meta-metaphors
+        4. Check the results of benchmark functions in the papers, they are mostly make up results
+
+    Examples
+    ~~~~~~~~
+    >>> import numpy as np
+    >>> from mealpy.swarm_based.ServalOA import OriginalServalOA
+    >>>
+    >>> def fitness_function(solution):
+    >>>     return np.sum(solution**2)
+    >>>
+    >>> problem_dict1 = {
+    >>>     "fit_func": fitness_function,
+    >>>     "lb": [-10, -15, -4, -2, -8],
+    >>>     "ub": [10, 15, 12, 8, 20],
+    >>>     "minmax": "min",
+    >>> }
+    >>>
+    >>> epoch = 1000
+    >>> pop_size = 50
+    >>> model = OriginalServalOA(epoch, pop_size)
+    >>> best_position, best_fitness = model.solve(problem_dict1)
+    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] Dehghani, M., & Trojovský, P. (2022). Serval Optimization Algorithm: A New Bio-Inspired
+    Approach for Solving Optimization Problems. Biomimetics, 7(4), 204.
+    """
+    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+        """
+        Args:
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+        """
+        super().__init__(**kwargs)
+        self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.set_parameters(["epoch", "pop_size"])
+        self.support_parallel_modes = False
+        self.nfe_per_epoch = 2 * self.pop_size
+        self.sort_flag = False
+
+    def evolve(self, epoch):
+        """
+        The main operations (equations) of algorithm. Inherit from Optimizer class
+
+        Args:
+            epoch (int): The current iteration
+        """
+        kk = np.random.permutation(self.pop_size)[0]
+        for idx in range(0, self.pop_size):
+            # Phase 1: Prey Selection and Attacking (Exploration)
+            pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * \
+                      (self.pop[kk][self.ID_POS] - np.random.randint(1, 3, self.problem.n_dims) * self.pop[idx][self.ID_POS])
+            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            tar_new = self.get_target_wrapper(pos_new)
+            if self.compare_agent([pos_new, tar_new], self.pop[idx]):
+                self.pop[idx] = [pos_new, tar_new]
+
+            # Phase 2: Chase Process (Exploitation)
+            pos_new = self.pop[idx][self.ID_POS] + np.random.randint(1, 3, self.problem.n_dims) * (self.problem.ub - self.problem.lb) / (epoch+1)   # Eq. 6
+            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            tar_new = self.get_target_wrapper(pos_new)
+            if self.compare_agent([pos_new, tar_new], self.pop[idx]):
+                self.pop[idx] = [pos_new, tar_new]
