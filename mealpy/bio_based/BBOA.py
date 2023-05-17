601a35032dbcdca02eacc1f07e5eeae181cfb62c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-17 14:31:05 +0700

    Update BBOA algorithm

--- mealpy/bio_based/BBOA.py
+++ mealpy/bio_based/BBOA.py
@@ -92,6 +92,7 @@ class OriginalBBOA(Optimizer):
                 pos_new = self.pop[idx][self.ID_POS] + np.random.rand() * (self.pop[idx][self.ID_POS] - self.pop[kk][self.ID_POS])
             else:
                 pos_new = self.pop[idx][self.ID_POS] + np.random.rand() * (self.pop[kk][self.ID_POS] - self.pop[idx][self.ID_POS])
+            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
             pop_new.append([pos_new, None])
             if self.mode not in self.AVAILABLE_MODES:
                 target = self.get_target_wrapper(pos_new)
