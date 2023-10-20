c4b259d872cbb7fdc69a42ac16d1dae5051ef3d8
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-21 00:16:27 +0700

    Convert ABC

--- mealpy/swarm_based/ABC.py
+++ mealpy/swarm_based/ABC.py
@@ -21,42 +21,39 @@ class OriginalABC(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.ABC import OriginalABC
+    >>> from mealpy import FloatVar, ABC
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> n_limits = 50
-    >>> model = OriginalABC(epoch, pop_size, n_limits)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = ABC.OriginalABC(epoch=1000, pop_size=50, n_limits = 50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] B. Basturk, D. Karaboga, An artificial bee colony (ABC) algorithm for numeric function optimization,
     in: IEEE Swarm Intelligence Symposium 2006, May 12–14, Indianapolis, IN, USA, 2006.
     """
-    def __init__(self, epoch=10000, pop_size=100, n_limits=25, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, n_limits: int = 25, **kwargs: object) -> None:
         """
         Args:
-            epoch (int): maximum number of iterations, default = 10000
-            pop_size (int): number of population size = onlooker bees = employed bees, default = 100
-            n_limits (int): Limit of trials before abandoning a food source, default=25
+            epoch: maximum number of iterations, default = 10000
+            pop_size: number of population size = onlooker bees = employed bees, default = 100
+            n_limits: Limit of trials before abandoning a food source, default=25
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.n_limits = self.validator.check_int("n_limits", n_limits, [1, 1000])
-        self.support_parallel_modes = False
+        self.is_parallelizable = False
         self.set_parameters(["epoch", "pop_size", "n_limits"])
         self.sort_flag = False
 
@@ -72,34 +69,34 @@ class OriginalABC(Optimizer):
         """
         for idx in range(0, self.pop_size):
             # Choose a random employed bee to generate a new solution
-            t = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
+            rdx = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
             # Generate a new solution by the equation x_{ij} = x_{ij} + phi_{ij} * (x_{tj} - x_{ij})
-            phi = np.random.uniform(low=-1, high=1, size=self.problem.n_dims)
-            pos_new = self.pop[idx][self.ID_POS] + phi * (self.pop[t][self.ID_POS] - self.pop[idx][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            target = self.get_target_wrapper(pos_new)
-            if self.compare_agent([pos_new, target], self.pop[idx]):
-                self.pop[idx] = [pos_new, target]
+            phi = self.generator.uniform(low=-1, high=1, size=self.problem.n_dims)
+            pos_new = self.pop[idx].solution + phi * (self.pop[rdx].solution - self.pop[idx].solution)
+            pos_new = self.correct_solution(pos_new)
+            target = self.get_target(pos_new)
+            if self.compare_target(target, self.pop[idx].target):
+                self.pop[idx].update(solution=pos_new, target=target)
                 self.trials[idx] = 0
             else:
                 self.trials[idx] += 1
 
         # Onlooker bees phase
         # Calculate the probabilities of each employed bee
-        employed_fits = np.array([agent[self.ID_TAR][self.ID_FIT] for agent in self.pop])
+        employed_fits = np.array([agent.target.fitness for agent in self.pop])
         # probabilities = employed_fits / np.sum(employed_fits)
         for idx in range(0, self.pop_size):
             # Select an employed bee using roulette wheel selection
             selected_bee = self.get_index_roulette_wheel_selection(employed_fits)
             # Choose a random employed bee to generate a new solution
-            t = np.random.choice(list(set(range(0, self.pop_size)) - {idx, selected_bee}))
+            rdx = self.generator.choice(list(set(range(0, self.pop_size)) - {idx, selected_bee}))
             # Generate a new solution by the equation x_{ij} = x_{ij} + phi_{ij} * (x_{tj} - x_{ij})
-            phi = np.random.uniform(low=-1, high=1, size=self.problem.n_dims)
-            pos_new = self.pop[selected_bee][self.ID_POS] + phi * (self.pop[t][self.ID_POS] - self.pop[selected_bee][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            target = self.get_target_wrapper(pos_new)
-            if self.compare_agent([pos_new, target], self.pop[selected_bee]):
-                self.pop[selected_bee] = [pos_new, target]
+            phi = self.generator.uniform(low=-1, high=1, size=self.problem.n_dims)
+            pos_new = self.pop[selected_bee].solution + phi * (self.pop[rdx].solution - self.pop[selected_bee].solution)
+            pos_new = self.correct_solution(pos_new)
+            target = self.get_target(pos_new)
+            if self.compare_target(target, self.pop[selected_bee].target):
+                self.pop[selected_bee].update(solution=pos_new, target=target)
                 self.trials[selected_bee] = 0
             else:
                 self.trials[selected_bee] += 1
@@ -108,5 +105,5 @@ class OriginalABC(Optimizer):
         # Check the number of trials for each employed bee and abandon the food source if the limit is exceeded
         abandoned = np.where(self.trials >= self.n_limits)[0]
         for idx in abandoned:
-            self.pop[idx] = self.create_solution(self.problem.lb, self.problem.ub)
+            self.pop[idx] = self.generate_agent()
             self.trials[idx] = 0
