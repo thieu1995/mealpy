48ba8b2f539350263361b07c77f5ee57a0f77c18
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-21 00:26:51 +0700

    Convert ACOR

--- mealpy/swarm_based/ACOR.py
+++ mealpy/swarm_based/ACOR.py
@@ -20,31 +20,28 @@ class OriginalACOR(Optimizer):
     Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + sample_count (int): [2, 10000], Number of Newly Generated Samples, default = 25
         + intent_factor (float): [0.2, 1.0], Intensification Factor (Selection Pressure), (q in the paper), default = 0.5
-        + zeta (float): [1, 2, 3], Deviation-Distance Ratio, default = 1
+        + sample_count (int): Number of Newly Generated Samples, default = 25
+        + intent_factor (float): Intensification Factor (Selection Pressure) (q in the paper), default = 0.5
+        + zeta (float): [1, 2, 3], Deviation-Distance Ratio, default = 1.0
 
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.ACOR import OriginalACOR
+    >>> from mealpy import FloatVar, ACOR
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> sample_count = 25
-    >>> intent_factor = 0.5
-    >>> zeta = 1.0
-    >>> model = OriginalACOR(epoch, pop_size, sample_count, intent_factor, zeta)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = ACOR.OriginalACOR(epoch=1000, pop_size=50, sample_count = 25, intent_factor = 0.5, zeta = 1.0)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -52,18 +49,19 @@ class OriginalACOR(Optimizer):
     European journal of operational research, 185(3), pp.1155-1173.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, sample_count=25, intent_factor=0.5, zeta=1.0, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, sample_count: int = 25,
+                 intent_factor: float = 0.5, zeta: float = 1.0, **kwargs: object) -> None:
         """
         Args:
-            epoch (int): maximum number of iterations, default = 10000
-            pop_size (int): number of population size, default = 100
-            sample_count (int): Number of Newly Generated Samples, default = 25
-            intent_factor (float): Intensification Factor (Selection Pressure) (q in the paper), default = 0.5
-            zeta (float): Deviation-Distance Ratio, default = 1.0
+            epoch: maximum number of iterations, default = 10000
+            pop_size: number of population size, default = 100
+            sample_count: Number of Newly Generated Samples, default = 25
+            intent_factor: Intensification Factor (Selection Pressure) (q in the paper), default = 0.5
+            zeta: Deviation-Distance Ratio, default = 1.0
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.sample_count = self.validator.check_int("sample_count", sample_count, [2, 10000])
         self.intent_factor = self.validator.check_float("intent_factor", intent_factor, (0, 1.0))
         self.zeta = self.validator.check_float("zeta", zeta, (0, 5))
@@ -84,10 +82,10 @@ class OriginalACOR(Optimizer):
         matrix_p = matrix_w / np.sum(matrix_w)  # Normalize to find the probability.
 
         # Means and Standard Deviations
-        matrix_pos = np.array([solution[self.ID_POS] for solution in self.pop])
+        matrix_pos = np.array([agent.solution for agent in self.pop])
         matrix_sigma = []
-        for i in range(0, self.pop_size):
-            matrix_i = np.repeat(self.pop[i][self.ID_POS].reshape((1, -1)), self.pop_size, axis=0)
+        for idx in range(0, self.pop_size):
+            matrix_i = np.repeat(self.pop[idx].solution.reshape((1, -1)), self.pop_size, axis=0)
             D = np.sum(np.abs(matrix_pos - matrix_i), axis=0)
             temp = self.zeta * D / (self.pop_size - 1)
             matrix_sigma.append(temp)
@@ -97,12 +95,13 @@ class OriginalACOR(Optimizer):
         pop_new = []
         for i in range(0, self.sample_count):
             child = np.zeros(self.problem.n_dims)
-            for j in range(0, self.problem.n_dims):
-                idx = self.get_index_roulette_wheel_selection(matrix_p)
-                child[j] = self.pop[idx][self.ID_POS][j] + np.random.normal() * matrix_sigma[idx, j]  # (1)
-            pos_new = self.amend_position(child, self.problem.lb, self.problem.ub)  # (2)
-            pop_new.append([pos_new, None])
+            for jdx in range(0, self.problem.n_dims):
+                rdx = self.get_index_roulette_wheel_selection(matrix_p)
+                child[jdx] = self.pop[rdx].solution[jdx] + self.generator.normal() * matrix_sigma[rdx, jdx]  # (1)
+            pos_new = self.correct_solution(child)      # (2)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
-        self.pop = self.get_sorted_strim_population(self.pop + pop_new, self.pop_size)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
+        self.pop = self.get_sorted_and_trimmed_population(self.pop + pop_new, self.pop_size)
