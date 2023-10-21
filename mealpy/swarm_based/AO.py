fc12c8200f31bb5e126eda04020dec19949fa898
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-21 08:25:14 +0700

    Convert AO

--- mealpy/swarm_based/AO.py
+++ mealpy/swarm_based/AO.py
@@ -18,23 +18,21 @@ class OriginalAO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.AO import OriginalAO
+    >>> from mealpy import FloatVar, AO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalAO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = AO.OriginalAO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -50,7 +48,7 @@ class OriginalAO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -62,9 +60,8 @@ class OriginalAO(Optimizer):
             epoch (int): The current iteration
         """
         alpha = delta = 0.1
-        g1 = 2 * np.random.rand() - 1  # Eq. 16
+        g1 = 2 * self.generator.random() - 1  # Eq. 16
         g2 = 2 * (1 - epoch / self.epoch)  # Eq. 17
-
         dim_list = np.array(list(range(1, self.problem.n_dims + 1)))
         miu = 0.00565
         r0 = 10
@@ -74,31 +71,30 @@ class OriginalAO(Optimizer):
         phi = -w * dim_list + phi0
         x = r * np.sin(phi)  # Eq.(9)
         y = r * np.cos(phi)  # Eq.(10)
-        QF = (epoch + 1) ** ((2 * np.random.rand() - 1) / (1 - self.epoch) ** 2)  # Eq.(15)        Quality function
-
+        QF = epoch ** ((2 * self.generator.random() - 1) / (1 - self.epoch) ** 2)  # Eq.(15)        Quality function
         pop_new = []
         for idx in range(0, self.pop_size):
-            x_mean = np.mean(np.array([item[self.ID_TAR][self.ID_FIT] for item in self.pop]), axis=0)
+            x_mean = np.mean(np.array([agent.target.fitness for agent in self.pop]), axis=0)
             levy_step = self.get_levy_flight_step(beta=1.5, multiplier=1.0, case=-1)
-            if (epoch + 1) <= (2 / 3) * self.epoch:  # Eq. 3, 4
-                if np.random.rand() < 0.5:
-                    pos_new = self.g_best[self.ID_POS] * (1 - (epoch + 1) / self.epoch) + \
-                              np.random.rand() * (x_mean - self.g_best[self.ID_POS])
+            if epoch <= (2 / 3) * self.epoch:  # Eq. 3, 4
+                if self.generator.random() < 0.5:
+                    pos_new = self.g_best.solution * (1 - epoch / self.epoch) + self.generator.random() * (x_mean - self.g_best.solution)
                 else:
-                    idx = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
-                    pos_new = self.g_best[self.ID_POS] * levy_step + self.pop[idx][self.ID_POS] + np.random.rand() * (y - x)  # Eq. 5
+                    idx = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
+                    pos_new = self.g_best.solution * levy_step + self.pop[idx].solution + self.generator.random() * (y - x)  # Eq. 5
             else:
-                if np.random.rand() < 0.5:
-                    pos_new = alpha * (self.g_best[self.ID_POS] - x_mean) - np.random.rand() * \
-                              (np.random.rand() * (self.problem.ub - self.problem.lb) + self.problem.lb) * delta  # Eq. 13
+                if self.generator.random() < 0.5:
+                    pos_new = alpha * (self.g_best.solution - x_mean) - self.generator.random() * \
+                              (self.generator.random() * (self.problem.ub - self.problem.lb) + self.problem.lb) * delta  # Eq. 13
                 else:
-                    pos_new = QF * self.g_best[self.ID_POS] - (g2 * self.pop[idx][self.ID_POS] * np.random.rand()) - \
-                              g2 * levy_step + np.random.rand() * g1  # Eq. 14
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                    pos_new = QF * self.g_best.solution - (g2 * self.pop[idx].solution * self.generator.random()) - \
+                              g2 * levy_step + self.generator.random() * g1  # Eq. 14
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx])
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.update_target_for_population(pop_new)
             self.pop = self.greedy_selection_population(self.pop, pop_new)
