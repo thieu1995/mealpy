accaec2192838ce453c811fd67199708373cc3f1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-21 08:58:37 +0700

    Convert ARO

--- mealpy/swarm_based/ARO.py
+++ mealpy/swarm_based/ARO.py
@@ -19,23 +19,21 @@ class OriginalARO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.ARO import OriginalARO
+    >>> from mealpy import FloatVar, ARO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalARO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = ARO.OriginalARO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -51,7 +49,7 @@ class OriginalARO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -62,33 +60,34 @@ class OriginalARO(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        theta = 2 * (1 - (epoch+1)/self.epoch)
+        theta = 2 * (1 - epoch/self.epoch)
         pop_new = []
         for idx in range(0, self.pop_size):
-            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*np.random.rand()))
+            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*self.generator.random()))
             temp = np.zeros(self.problem.n_dims)
-            rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand()*self.problem.n_dims)), replace=False)
+            rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random()*self.problem.n_dims)), replace=False)
             temp[rd_index] = 1
             R = L * temp        # Eq 2
-            A = 2 * np.log(1.0 / np.random.rand()) * theta      # Eq. 15
+            A = 2 * np.log(1.0 / self.generator.random()) * theta      # Eq. 15
             if A > 1:   # detour foraging strategy
-                rand_idx = np.random.randint(0, self.pop_size)
-                pos_new = self.pop[rand_idx][self.ID_POS] + R * (self.pop[idx][self.ID_POS] - self.pop[rand_idx][self.ID_POS]) + \
-                    np.round(0.5 * (0.05 + np.random.rand())) * np.random.normal(0, 1)      # Eq. 1
+                rand_idx = self.generator.integers(0, self.pop_size)
+                pos_new = self.pop[rand_idx].solution + R * (self.pop[idx].solution - self.pop[rand_idx].solution) + \
+                    np.round(0.5 * (0.05 + self.generator.random())) * self.generator.normal(0, 1)      # Eq. 1
             else:       # Random hiding stage
                 gr = np.zeros(self.problem.n_dims)
-                rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand() * self.problem.n_dims)), replace=False)
+                rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random() * self.problem.n_dims)), replace=False)
                 gr[rd_index] = 1        # Eq. 12
-                H = np.random.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
-                b = self.pop[idx][self.ID_POS] + H * gr * self.pop[idx][self.ID_POS]        # Eq. 13
-                pos_new = self.pop[idx][self.ID_POS] + R * (np.random.rand() * b - self.pop[idx][self.ID_POS])      # Eq. 11
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                H = self.generator.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
+                b = self.pop[idx].solution + H * gr * self.pop[idx].solution       # Eq. 13
+                pos_new = self.pop[idx].solution + R * (self.generator.random() * b - self.pop[idx].solution)      # Eq. 11
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.update_target_for_population(pop_new)
             self.pop = self.greedy_selection_population(self.pop, pop_new)
 
 
@@ -102,23 +101,21 @@ class LARO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.ARO import LARO
+    >>> from mealpy import FloatVar, ARO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalARO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = ARO.LARO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -134,7 +131,7 @@ class LARO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -148,49 +145,52 @@ class LARO(Optimizer):
         theta = 2 * (1 - (epoch+1)/self.epoch)
         pop_new = []
         for idx in range(0, self.pop_size):
-            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*np.random.rand()))
+            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*self.generator.random()))
             temp = np.zeros(self.problem.n_dims)
-            rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand()*self.problem.n_dims)), replace=False)
+            rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random()*self.problem.n_dims)), replace=False)
             temp[rd_index] = 1
             R = L * temp        # Eq 2
-            A = 2 * np.log(1.0 / np.random.rand()) * theta      # Eq. 15
+            A = 2 * np.log(1.0 / self.generator.random()) * theta      # Eq. 15
             if A > 1:   # # detour foraging strategy
-                rand_idx = np.random.randint(0, self.pop_size)
-                pos_new = self.pop[rand_idx][self.ID_POS] + R * (self.pop[idx][self.ID_POS] - self.pop[rand_idx][self.ID_POS]) + \
-                    np.round(0.5 * (0.05 + np.random.rand())) * np.random.normal(0, 1)      # Eq. 1
+                rand_idx = self.generator.integers(0, self.pop_size)
+                pos_new = self.pop[rand_idx].solution + R * (self.pop[idx].solution - self.pop[rand_idx].solution) + \
+                    np.round(0.5 * (0.05 + self.generator.random())) * self.generator.normal(0, 1)      # Eq. 1
             else:       # Random hiding stage
                 gr = np.zeros(self.problem.n_dims)
-                rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand() * self.problem.n_dims)), replace=False)
+                rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random() * self.problem.n_dims)), replace=False)
                 gr[rd_index] = 1        # Eq. 12
-                H = np.random.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
-                b = self.pop[idx][self.ID_POS] + H * gr * self.pop[idx][self.ID_POS]        # Eq. 13
+                H = self.generator.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
+                b = self.pop[idx].solution + H * gr * self.pop[idx].solution        # Eq. 13
                 levy = self.get_levy_flight_step(beta=1.5, multiplier=0.1)
-                pos_new = self.pop[idx][self.ID_POS] + R * (levy * b - self.pop[idx][self.ID_POS])      # Eq. 11
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                pos_new = self.pop[idx].solution + R * (levy * b - self.pop[idx].solution)      # Eq. 11
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.update_target_for_population(pop_new)
             self.pop = self.greedy_selection_population(self.pop, pop_new)
-
         # Selective Opposition (SO) Strategy
-        TS = 2 - (2*(epoch+1) / self.epoch)
+        TS = 2 - (2 * epoch / self.epoch)
         for idx in range(0, self.pop_size):
-            if self.pop[idx][self.ID_TAR][self.ID_FIT] != self.g_best[self.ID_TAR][self.ID_FIT]:
-                dd = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
+            if self.pop[idx].target.fitness != self.g_best.target.fitness:
+                dd = np.abs(self.g_best.solution - self.pop[idx].solution)
                 idx_far = np.sign(dd - TS) < 0
                 n_df = np.sum(idx_far)
                 n_dc = np.sum(np.sign(dd - TS) > 0)
                 src = 1 - 6*np.sum(dd**2) / np.dot(dd, (dd**2 - 1))
-                df_lb, df_ub = np.min(dd[idx_far]), np.max(dd[idx_far])
+                if len(dd[idx_far]) == 0:
+                    df_lb, df_ub = np.min(dd), np.max(dd)
+                else:
+                    df_lb, df_ub = np.min(dd[idx_far]), np.max(dd[idx_far])
                 if src <= 0 and n_df > n_dc:
-                    pos_new = df_lb + df_ub - self.pop[idx][self.ID_POS]
-                    pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-                    tar_new = self.get_target_wrapper(pos_new)
-                    if self.compare_agent([pos_new, tar_new], self.pop[idx]):
-                        self.pop[idx] = [pos_new, tar_new]
+                    pos_new = df_lb + df_ub - self.pop[idx].solution
+                    pos_new = self.correct_solution(pos_new)
+                    target = self.get_target(pos_new)
+                    if self.compare_target(target, self.pop[idx].target):
+                        self.pop[idx].update(solution=pos_new, target=target)
 
 
 class IARO(Optimizer):
@@ -204,23 +204,21 @@ class IARO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.ARO import IARO
+    >>> from mealpy import FloatVar, ARO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalARO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = ARO.IARO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -250,28 +248,29 @@ class IARO(Optimizer):
         theta = 2 * (1 - (epoch+1)/self.epoch)
         pop_new = []
         for idx in range(0, self.pop_size):
-            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*np.random.rand()))
+            L = (np.exp(1) - np.exp((epoch / self.epoch)**2)) * (np.sin(2*np.pi*self.generator.random()))
             temp = np.zeros(self.problem.n_dims)
-            rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand()*self.problem.n_dims)), replace=False)
+            rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random()*self.problem.n_dims)), replace=False)
             temp[rd_index] = 1
             R = L * temp        # Eq 2
-            A = 2 * np.log(1.0 / np.random.rand()) * theta      # Eq. 15
+            A = 2 * np.log(1.0 / self.generator.random()) * theta      # Eq. 15
             if A > 1:   # # detour foraging strategy
-                rand_idx = np.random.randint(0, self.pop_size)
-                pos_new = self.pop[rand_idx][self.ID_POS] + R * (self.pop[idx][self.ID_POS] - self.pop[rand_idx][self.ID_POS]) + \
-                    np.round(0.5 * (0.05 + np.random.rand())) * np.random.normal(0, 1)      # Eq. 1
+                rand_idx = self.generator.integers(0, self.pop_size)
+                pos_new = self.pop[rand_idx].solution + R * (self.pop[idx].solution - self.pop[rand_idx].solution) + \
+                    np.round(0.5 * (0.05 + self.generator.random())) * self.generator.normal(0, 1)      # Eq. 1
             else:       # Random hiding stage
                 gr = np.zeros(self.problem.n_dims)
-                rd_index = np.random.choice(np.arange(0, self.problem.n_dims), int(np.ceil(np.random.rand() * self.problem.n_dims)), replace=False)
+                rd_index = self.generator.choice(np.arange(0, self.problem.n_dims), int(np.ceil(self.generator.random() * self.problem.n_dims)), replace=False)
                 gr[rd_index] = 1        # Eq. 12
-                H = np.random.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
-                b = self.pop[idx][self.ID_POS] + H * gr * self.pop[idx][self.ID_POS]        # Eq. 13
-                pos_new = self.pop[idx][self.ID_POS] + R * (np.random.rand() * b - self.pop[idx][self.ID_POS])      # Eq. 11
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                H = self.generator.normal(0, 1) * (epoch / self.epoch)       # Eq. 8
+                b = self.pop[idx].solution + H * gr * self.pop[idx].solution        # Eq. 13
+                pos_new = self.pop[idx].solution + R * (self.generator.random() * b - self.pop[idx].solution)      # Eq. 11
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.update_target_for_population(pop_new)
             self.pop = self.greedy_selection_population(self.pop, pop_new)
