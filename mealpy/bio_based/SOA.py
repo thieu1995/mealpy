f35d68b6dbf2357ec41338313f5b6d8c4943b83c
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 13:03:48 +0700

    Convert SOA

--- mealpy/bio_based/SOA.py
+++ mealpy/bio_based/SOA.py
@@ -26,32 +26,29 @@ class DevSOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.bio_based.SOA import DevSOA
+    >>> from mealpy import FloatVar, SOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> fc = 2
-    >>> model = DevSOA(epoch, pop_size, fc)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SOA.DevSOA(epoch=1000, pop_size=50, fc = 2)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
     """
 
-    def __init__(self, epoch=10000, pop_size=100, fc=2,  **kwargs):
+    def __init__(self, epoch=10000, pop_size=100, fc=2, **kwargs):
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.fc = self.validator.check_float("fc", fc, [1.0, 10.])
-        self.set_parameters(["epoch", "pop_size"])
+        self.set_parameters(["epoch", "pop_size", "fc"])
         self.sort_flag = False
 
     def evolve(self, epoch):
@@ -61,28 +58,29 @@ class DevSOA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        A = self.fc - (epoch+1)*self.fc / self.epoch    # Eq. 6
+        A = self.fc - epoch*self.fc / self.epoch    # Eq. 6
         uu = vv = 1
         pop_new = []
         for idx in range(0, self.pop_size):
-            B = 2 * A**2 * np.random.random()                                   # Eq. 8
-            M = B * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])     # Eq. 7
-            C = A * self.pop[idx][self.ID_POS]                                  # Eq. 5
-            D = np.abs(C + M)                                                   # Eq. 9
-            k = np.random.uniform(0, 2*np.pi)
+            B = 2 * A**2 * self.generator.random()                          # Eq. 8
+            M = B * (self.g_best.solution - self.pop[idx].solution)         # Eq. 7
+            C = A * self.pop[idx].solution                                  # Eq. 5
+            D = np.abs(C + M)                                               # Eq. 9
+            k = self.generator.uniform(0, 2*np.pi)
             r = uu * np.exp(k*vv)
             xx = r * np.cos(k)
             yy = r * np.sin(k)
             zz = r * k
-            pos_new = xx * yy * zz * D + np.random.normal(0, 1) * self.g_best[self.ID_POS]                 # Eq. 14
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = xx * yy * zz * D + self.generator.normal(0, 1) * self.g_best.solution                 # Eq. 14
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
 
 
 class OriginalSOA(Optimizer):
@@ -98,23 +96,21 @@ class OriginalSOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.bio_based.SOA import OriginalSOA
+    >>> from mealpy import FloatVar, SOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalSOA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SOA.OriginalSOA(epoch=1000, pop_size=50, fc = 2)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -122,12 +118,12 @@ class OriginalSOA(Optimizer):
     for large-scale industrial engineering problems. Knowledge-based systems, 165, 169-196.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, fc=2,  **kwargs):
+    def __init__(self, epoch=10000, pop_size=100, fc=2, **kwargs):
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.fc = self.validator.check_float("fc", fc, [1.0, 10.])
-        self.set_parameters(["epoch", "pop_size"])
+        self.set_parameters(["epoch", "pop_size", "fc"])
         self.sort_flag = False
 
     def evolve(self, epoch):
@@ -137,22 +133,23 @@ class OriginalSOA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        A = self.fc - (epoch+1)*self.fc / self.epoch    # Eq. 6
+        A = self.fc - epoch*self.fc / self.epoch    # Eq. 6
         uu = vv = 1
         pop_new = []
         for idx in range(0, self.pop_size):
-            B = 2 * A**2 * np.random.random()                                   # Eq. 8
-            M = B * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])     # Eq. 7
-            C = A * self.pop[idx][self.ID_POS]                                  # Eq. 5
-            D = np.abs(C + M)                                                   # Eq. 9
-            k = np.random.uniform(0, 2*np.pi)
+            B = 2 * A**2 * self.generator.random()                      # Eq. 8
+            M = B * (self.g_best.solution - self.pop[idx].solution)     # Eq. 7
+            C = A * self.pop[idx].solution                              # Eq. 5
+            D = np.abs(C + M)                                           # Eq. 9
+            k = self.generator.uniform(0, 2*np.pi)
             r = uu * np.exp(k*vv)
             xx = r * np.cos(k)
             yy = r * np.sin(k)
             zz = r * k
-            pos_new = xx * yy * zz * D + self.g_best[self.ID_POS]                 # Eq. 14
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = xx * yy * zz * D + self.g_best.solution           # Eq. 14
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        self.pop = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        self.pop = self.update_target_for_population(pop_new)
