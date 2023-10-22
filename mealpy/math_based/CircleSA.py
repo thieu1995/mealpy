800f2ea25223c668e005e876631ea2fd494c8c16
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 16:05:07 +0700

    Convert CircleSA

--- mealpy/math_based/CircleSA.py
+++ mealpy/math_based/CircleSA.py
@@ -19,23 +19,21 @@ class OriginalCircleSA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.math_based.CircleSA import OriginalCircleSA
+    >>> from mealpy import FloatVar, CircleSA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalCircleSA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CircleSA.OriginalCircleSA(epoch=1000, pop_size=50, c_factor=0.8)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -46,7 +44,7 @@ class OriginalCircleSA(Optimizer):
     def __init__(self, epoch=10000, pop_size=100, c_factor=0.8, **kwargs):
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.c_factor = self.validator.check_float("c_factor", c_factor, (0, 1.0))
         self.set_parameters(["epoch", "pop_size", "c_factor"])
         self.sort_flag = False
@@ -58,18 +56,19 @@ class OriginalCircleSA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        a = np.pi - np.pi * ((epoch+1)/self.epoch)**2       # Eq. 8
-        p = 1 - 0.9 * ((epoch + 1) / self.epoch) ** 0.5
+        a = np.pi - np.pi * (epoch/self.epoch)**2       # Eq. 8
+        p = 1 - 0.9 * (epoch / self.epoch) ** 0.5
         threshold = self.c_factor * self.epoch
         pop_new = []
         for idx in range(0, self.pop_size):
-            w = a * np.random.rand() - a
-            if (epoch+1) > threshold:
-                x_new = self.g_best[self.ID_POS] + (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]) * np.tan(w * np.random.rand())
+            w = a * self.generator.random() - a
+            if epoch > threshold:
+                x_new = self.g_best.solution + (self.g_best.solution - self.pop[idx].solution) * np.tan(w * self.generator.random())
             else:
-                x_new = self.g_best[self.ID_POS] - (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]) * np.tan(w * p)
-            x_new = self.amend_position(x_new, self.problem.lb, self.problem.ub)
-            pop_new.append([x_new, None])
+                x_new = self.g_best.solution - (self.g_best.solution - self.pop[idx].solution) * np.tan(w * p)
+            pos_new = self.correct_solution(x_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(x_new)
-        self.pop = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        self.pop = self.update_target_for_population(pop_new)
