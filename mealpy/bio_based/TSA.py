5bff0c59afc984de2155c76ec8c91071313d3754
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 13:39:33 +0700

    Convert TSA

--- mealpy/bio_based/TSA.py
+++ mealpy/bio_based/TSA.py
@@ -25,23 +25,21 @@ class OriginalTSA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.bio_based.TSA import OriginalTSA
+    >>> from mealpy import FloatVar, TSA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalTSA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = TSA.OriginalTSA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -49,12 +47,11 @@ class OriginalTSA(Optimizer):
     based metaheuristic paradigm for global optimization. Engineering Applications of Artificial Intelligence, 90, 103541.
     """
 
-    ID_WEI = 2
-
     def __init__(self, epoch=10000, pop_size=100, **kwargs):
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
+        self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
     def evolve(self, epoch):
@@ -67,18 +64,19 @@ class OriginalTSA(Optimizer):
         pmin, pmax = 1, 4
         pop_new = []
         for idx in range(0, self.pop_size):
-            c3 = np.random.random(self.problem.n_dims)
-            c2 = np.random.random(self.problem.n_dims)
-            c1 = np.random.random(self.problem.n_dims)
-            M = np.fix(pmin + np.random.rand() * (pmax - pmin))
+            c3 = self.generator.random(self.problem.n_dims)
+            c2 = self.generator.random(self.problem.n_dims)
+            c1 = self.generator.random(self.problem.n_dims)
+            M = np.fix(pmin + self.generator.random() * (pmax - pmin))
             A = (c2 + c3 - 2 * c1) / M
-            t1 = self.g_best[self.ID_POS] + A * np.abs(self.g_best[self.ID_POS] - c2 * self.pop[idx][self.ID_POS])
-            t2 = self.g_best[self.ID_POS] - A * np.abs(self.g_best[self.ID_POS] - c2 * self.pop[idx][self.ID_POS])
-            new_pos = np.where(c3 >= 0.5, t1, t2)
+            t1 = self.g_best.solution + A * np.abs(self.g_best.solution - c2 * self.pop[idx].solution)
+            t2 = self.g_best.solution - A * np.abs(self.g_best.solution - c2 * self.pop[idx].solution)
+            pos_new = np.where(c3 >= 0.5, t1, t2)
             if idx != 0:
-                new_pos = (new_pos + self.pop[idx-1][self.ID_POS]) / 2
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            pop_new.append([new_pos, None])
+                pos_new = (pos_new + self.pop[idx-1].solution) / 2
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(new_pos)
-        self.pop = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        self.pop = self.update_target_for_population(pop_new)
