862635308e0349ed9ce2b98cc3ce9e769ca0e4f3
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-23 00:46:09 +0700

    Convert TS

--- mealpy/math_based/TS.py
+++ mealpy/math_based/TS.py
@@ -12,11 +12,10 @@ class OriginalTS(Optimizer):
     """
     The original version of: Tabu Search (TS)
 
-    Notes
-    ~~~~~
-    + The pop_size is not an official parameter in this algorithm. However, we need it here to adapt to Mealpy library.
-    + You should set pop_size = 2 to reduce the initial computation for the initial population of this algorithm.
-    + The perturbation_scale is important parameter that effect the most to this algorithm.
+    Notes:
+        + The pop_size is not an official parameter in this algorithm. However, we need it here to adapt to Mealpy library.
+        + You should set pop_size = 2 to reduce the initial computation for the initial population of this algorithm.
+        + The perturbation_scale is important parameter that effect the most to this algorithm.
 
     Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + tabu_size (int): [5, 10] Maximum size of the tabu list.
@@ -26,26 +25,21 @@ class OriginalTS(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.math_based.TS import OriginalTS
+    >>> from mealpy import FloatVar, TS
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 2
-    >>> tabu_size = 5
-    >>> neighbour_size = 20
-    >>> perturbation_scale = 0.05
-    >>> model = OriginalTS(epoch, pop_size, tabu_size, neighbour_size, perturbation_scale)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = TS.OriginalTS(epoch=1000, pop_size=50, tabu_size = 5, neighbour_size = 20, perturbation_scale = 0.05)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -53,7 +47,8 @@ class OriginalTS(Optimizer):
     with continuous parameters. IEEE Transactions on Magnetics, 40(2), 1184-1187.
     """
 
-    def __init__(self, epoch=10000, pop_size=2, tabu_size=5, neighbour_size=10, perturbation_scale=0.05, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 2, tabu_size: int = 5,
+                 neighbour_size: int = 10, perturbation_scale: float = 0.05, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -65,14 +60,14 @@ class OriginalTS(Optimizer):
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [2, 10000])
-        self.neighbour_size = self.validator.check_int("neighbour_size", neighbour_size, [2, 10000])
         self.tabu_size = self.validator.check_int("tabu_size", tabu_size, [2, 10000])
+        self.neighbour_size = self.validator.check_int("neighbour_size", neighbour_size, [2, 10000])
         self.perturbation_scale = self.validator.check_float("perturbation_scale", perturbation_scale, (0, 100))
-        self.set_parameters(["epoch", "tabu_size", "neighbour_size", "perturbation_scale"])
+        self.set_parameters(["epoch", "pop_size", "tabu_size", "neighbour_size", "perturbation_scale"])
         self.sort_flag = False
 
     def before_main_loop(self):
-        self.x = self.g_best[self.ID_POS].copy()
+        self.x = self.g_best.solution.copy()
         self.tabu_list = []
 
     def evolve(self, epoch):
@@ -83,23 +78,22 @@ class OriginalTS(Optimizer):
             epoch (int): The current iteration
         """
         # Generate candidate solutions by perturbing the current solution
-        candidates = np.random.normal(loc=self.x, scale=self.perturbation_scale,
-                                      size=(self.neighbour_size, self.problem.n_dims))
+        candidates = self.generator.normal(loc=self.x, scale=self.perturbation_scale, size=(self.neighbour_size, self.problem.n_dims))
         # Evaluate candidate solutions and select best move
         list_candidates = []
         for candidate in candidates:
-            pos_new = self.amend_position(candidate, self.problem.lb, self.problem.ub)
+            pos_new = self.correct_solution(candidate)
             if np.allclose(pos_new, self.x):
                 continue
             if tuple(pos_new) in self.tabu_list:
                 continue
-            list_candidates.append([pos_new, None])
+            agent = self.generate_empty_agent(pos_new)
+            list_candidates.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                list_candidates[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        list_candidates = self.update_target_wrapper_population(list_candidates)
-        self.pop, best_candidate = self.get_global_best_solution(list_candidates)
-        self.x = best_candidate[self.ID_POS]
-
+                list_candidates[-1].target = self.get_target(pos_new)
+        list_candidates = self.update_target_for_population(list_candidates)
+        best_candidate = self.get_best_agent(list_candidates, self.problem.minmax)
+        self.x = best_candidate.solution
         # Update tabu list
         self.tabu_list.append(tuple(self.x))
         if len(self.tabu_list) > self.tabu_size:
