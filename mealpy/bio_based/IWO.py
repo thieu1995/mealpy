47da32a43371be79cfbfd3eb9fedb67b999382ac
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 10:11:25 +0700

    Convert IWO

--- mealpy/bio_based/IWO.py
+++ mealpy/bio_based/IWO.py
@@ -30,28 +30,21 @@ class OriginalIWO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.bio_based.IWO import OriginalIWO
+    >>> from mealpy import FloatVar, EOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> seed_min = 3
-    >>> seed_max = 9
-    >>> exponent = 3
-    >>> sigma_start = 0.6
-    >>> sigma_end = 0.01
-    >>> model = OriginalIWO(epoch, pop_size, seed_min, seed_max, exponent, sigma_start, sigma_end)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = EOA.OriginalEOA(epoch=1000, pop_size=50, seed_min = 3, seed_max = 9, exponent = 3, sigma_start = 0.6, sigma_end = 0.01)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -59,7 +52,8 @@ class OriginalIWO(Optimizer):
     Ecological informatics, 1(4), pp.355-366.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, seed_min=2, seed_max=10, exponent=2, sigma_start=1.0, sigma_end=0.01, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, seed_min: int = 2, seed_max: int = 10,
+                 exponent: int = 2, sigma_start: float = 1.0, sigma_end: float = 0.01, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -72,7 +66,7 @@ class OriginalIWO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.seed_min = self.validator.check_int("seed_min", seed_min, [1, 3])
         self.seed_max = self.validator.check_int("seed_max", seed_max, [4, int(self.pop_size/2)])
         self.exponent = self.validator.check_int("exponent", exponent, [2, 4])
@@ -90,26 +84,28 @@ class OriginalIWO(Optimizer):
         """
         # Update Standard Deviation
         sigma = ((self.epoch - epoch) / (self.epoch - 1)) ** self.exponent * (self.sigma_start - self.sigma_end) + self.sigma_end
-        pop, best, worst = self.get_special_solutions(self.pop)
+        pop, list_best, list_worst = self.get_special_agents(self.pop, n_best=1, n_worst=1, minmax=self.problem.minmax)
+        best, worst = list_best[0], list_worst[0]
         pop_new = []
         for idx in range(0, self.pop_size):
-            temp = best[0][self.ID_TAR][self.ID_FIT] - worst[0][self.ID_TAR][self.ID_FIT]
+            temp = best.target.fitness - worst.target.fitness
             if temp == 0:
-                ratio = np.random.rand()
+                ratio = self.generator.random()
             else:
-                ratio = (pop[idx][self.ID_TAR][self.ID_FIT] - worst[0][self.ID_TAR][self.ID_FIT]) / temp
+                ratio = (pop[idx].target.fitness - worst.target.fitness) / temp
             s = int(np.ceil(self.seed_min + (self.seed_max - self.seed_min) * ratio))
             if s > int(np.sqrt(self.pop_size)):
                 s = int(np.sqrt(self.pop_size))
             pop_local = []
-            for j in range(s):
+            for jdx in range(s):
                 # Initialize Offspring and Generate Random Location
-                pos_new = pop[idx][self.ID_POS] + sigma * np.random.normal(0, 1, self.problem.n_dims)
-                pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-                pop_local.append([pos_new, None])
+                pos_new = pop[idx].solution + sigma * self.generator.normal(0, 1, self.problem.n_dims)
+                pos_new = self.correct_solution(pos_new)
+                agent = self.generate_empty_agent(pos_new)
+                pop_local.append(agent)
                 if self.mode not in self.AVAILABLE_MODES:
-                    pop_local[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                    pop_local[-1].target = self.get_target(pos_new)
             if self.mode in self.AVAILABLE_MODES:
-                pop_local = self.update_target_wrapper_population(pop_local)
+                pop_local = self.update_target_for_population(pop_local)
             pop_new += pop_local
-        self.pop = self.get_sorted_strim_population(pop_new, self.pop_size)
+        self.pop = self.get_sorted_and_trimmed_population(pop_new, self.pop_size)
