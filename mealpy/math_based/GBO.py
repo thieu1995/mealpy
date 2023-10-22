d1b50244b8b3d721443b4d8cdf98535e82351169
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 16:18:09 +0700

    Convert GBO

--- mealpy/math_based/GBO.py
+++ mealpy/math_based/GBO.py
@@ -20,26 +20,21 @@ class OriginalGBO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.math_based.GBO import OriginalGBO
+    >>> from mealpy import FloatVar, GBO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> pr = 0.5
-    >>> beta_min = 0.2
-    >>> beta_max = 1.2
-    >>> model = OriginalGBO(epoch, pop_size, pr, beta_min, beta_max)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = GBO.OriginalGBO(epoch=1000, pop_size=50, pr = 0.5, beta_min = 0.2, beta_max = 1.2)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -47,7 +42,7 @@ class OriginalGBO(Optimizer):
     A new metaheuristic optimization algorithm. Information Sciences, 540, pp.131-159.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, pr=0.5, beta_min=0.2, beta_max=1.2, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, pr: float = 0.5, beta_min: float = 0.2, beta_max: float = 1.2, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -58,7 +53,7 @@ class OriginalGBO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.pr = self.validator.check_float("pr", pr, (0, 1.0))
         self.beta_min = self.validator.check_float("beta_min", beta_min, (0, 2.0))
         self.beta_max = self.validator.check_float("beta_max", beta_max, (0, 5.0))
@@ -73,68 +68,64 @@ class OriginalGBO(Optimizer):
             epoch (int): The current iteration
         """
         # Eq.(14.2), Eq.(14.1)
-        beta = self.beta_min + (self.beta_max - self.beta_min) * (1 - ((epoch + 1) / self.epoch) ** 3) ** 2
+        beta = self.beta_min + (self.beta_max - self.beta_min) * (1 - (epoch / self.epoch) ** 3) ** 2
         alpha = np.abs(beta * np.sin(3 * np.pi / 2 + np.sin(beta * 3 * np.pi / 2)))
 
         pop_new = []
         for idx in range(0, self.pop_size):
-            p1 = 2 * np.random.rand() * alpha - alpha
-            p2 = 2 * np.random.rand() * alpha - alpha
+            p1 = 2 * self.generator.random() * alpha - alpha
+            p2 = 2 * self.generator.random() * alpha - alpha
             #  Four positions randomly selected from population
-            r1, r2, r3, r4 = np.random.choice(list(set(range(0, self.pop_size)) - {idx}), 4, replace=False)
+            r1, r2, r3, r4 = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}), 4, replace=False)
             # Average of Four positions randomly selected from population
-            r0 = (self.pop[r1][self.ID_POS] + self.pop[r2][self.ID_POS] + self.pop[r3][self.ID_POS] + self.pop[r4][self.ID_POS]) / 4
+            r0 = (self.pop[r1].solution + self.pop[r2].solution + self.pop[r3].solution + self.pop[r4].solution) / 4
             # Randomization Epsilon
-            epsilon = 5e-3 * np.random.rand()
+            epsilon = 5e-3 * self.generator.random()
+            delta = 2 * self.generator.random() * np.abs(r0 - self.pop[idx].solution)
+            step = (self.g_best.solution - self.pop[r1].solution + delta) / 2
+            delta_x = self.generator.choice(range(0, self.pop_size)) * np.abs(step)
+            x1 = self.pop[idx].solution - self.generator.normal() * p1 * 2 * delta_x * \
+                 self.pop[idx].solution / (self.g_worst.solution - self.g_best.solution + epsilon) + \
+                 self.generator.random() * p2 * (self.g_best.solution - self.pop[idx].solution)
+            z = self.pop[idx].solution - self.generator.normal() * 2 * delta_x * \
+                self.pop[idx].solution / (self.g_worst.solution - self.g_best.solution + epsilon)
+            y_p = self.generator.random() * ((z + self.pop[idx].solution) / 2 + self.generator.random() * delta_x)
+            y_q = self.generator.random() * ((z + self.pop[idx].solution) / 2 - self.generator.random() * delta_x)
+            x2 = self.g_best.solution - self.generator.normal() * p1 * 2 * delta_x * self.pop[idx].solution / (y_p - y_q + epsilon) + \
+                 self.generator.random() * p2 * (self.pop[r1].solution - self.pop[r2].solution)
 
-            delta = 2 * np.random.rand() * np.abs(r0 - self.pop[idx][self.ID_POS])
-            step = (self.g_best[self.ID_POS] - self.pop[r1][self.ID_POS] + delta) / 2
-            delta_x = np.random.choice(range(0, self.pop_size)) * np.abs(step)
-            x1 = self.pop[idx][self.ID_POS] - np.random.normal() * p1 * 2 * delta_x * \
-                 self.pop[idx][self.ID_POS] / (self.g_worst[self.ID_POS] - self.g_best[self.ID_POS] + epsilon) + \
-                 np.random.rand() * p2 * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-
-            z = self.pop[idx][self.ID_POS] - np.random.normal() * 2 * delta_x * \
-                self.pop[idx][self.ID_POS] / (self.g_worst[self.ID_POS] - self.g_best[self.ID_POS] + epsilon)
-            y_p = np.random.rand() * ((z + self.pop[idx][self.ID_POS]) / 2 + np.random.rand() * delta_x)
-            y_q = np.random.rand() * ((z + self.pop[idx][self.ID_POS]) / 2 - np.random.rand() * delta_x)
-            x2 = self.g_best[self.ID_POS] - np.random.normal() * p1 * 2 * delta_x * self.pop[idx][self.ID_POS] / (y_p - y_q + epsilon) + \
-                 np.random.rand() * p2 * (self.pop[r1][self.ID_POS] - self.pop[r2][self.ID_POS])
-
-            x3 = self.pop[idx][self.ID_POS] - p1 * (x2 - x1)
-            ra = np.random.rand()
-            rb = np.random.rand()
+            x3 = self.pop[idx].solution - p1 * (x2 - x1)
+            ra = self.generator.random()
+            rb = self.generator.random()
             pos_new = ra * (rb * x1 + (1 - rb) * x2) + (1 - ra) * x3
 
             # Local escaping operator
-            if np.random.rand() < self.pr:
-                f1 = np.random.uniform(-1, 1)
-                f2 = np.random.normal(0, 1)
-                L1 = np.round(1 - np.random.rand())
-                u1 = L1 * 2 * np.random.rand() + (1 - L1)
-                u2 = L1 * np.random.rand() + (1 - L1)
-                u3 = L1 * np.random.rand() + (1 - L1)
-
-                L2 = np.round(1 - np.random.rand())
-                x_rand = self.generate_position(self.problem.lb, self.problem.ub)
-                x_p = self.pop[np.random.choice(range(0, self.pop_size))][self.ID_POS]
+            if self.generator.random() < self.pr:
+                f1 = self.generator.uniform(-1, 1)
+                f2 = self.generator.normal(0, 1)
+                L1 = np.round(1 - self.generator.random())
+                u1 = L1 * 2 * self.generator.random() + (1 - L1)
+                u2 = L1 * self.generator.random() + (1 - L1)
+                u3 = L1 * self.generator.random() + (1 - L1)
+                L2 = np.round(1 - self.generator.random())
+                x_rand = self.problem.generate_solution()
+                x_p = self.pop[self.generator.choice(range(0, self.pop_size))].solution
                 x_m = L2 * x_p + (1 - L2) * x_rand
-
-                if np.random.rand() < 0.5:
-                    pos_new = pos_new + f1 * (u1 * self.g_best[self.ID_POS] - u2 * x_m) + \
-                              f2 * p1 * (u3 * (x2 - x1) + u2 * (self.pop[r1][self.ID_POS] - self.pop[r2][self.ID_POS])) / 2
+                if self.generator.random() < 0.5:
+                    pos_new = pos_new + f1 * (u1 * self.g_best.solution - u2 * x_m) + \
+                              f2 * p1 * (u3 * (x2 - x1) + u2 * (self.pop[r1].solution - self.pop[r2].solution)) / 2
                 else:
-                    pos_new = self.g_best[self.ID_POS] + f1 * (u1 * self.g_best[self.ID_POS] - u2 * x_m) + f2 * p1 * (
-                                u3 * (x2 - x1) + u2 * (self.pop[r1][self.ID_POS] - self.pop[r2][self.ID_POS])) / 2
-
+                    pos_new = self.g_best.solution + f1 * (u1 * self.g_best.solution - u2 * x_m) + f2 * p1 * \
+                              (u3 * (x2 - x1) + u2 * (self.pop[r1].solution - self.pop[r2].solution)) / 2
             # Check if solutions go outside the search space and bring them back
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(pop_new, self.pop)
-        _, best, worst = self.get_special_solutions(self.pop, best=1, worst=1)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
+        _, best, worst = self.get_special_agents(self.pop, n_best=1, n_worst=1, minmax=self.problem.minmax)
         self.g_best, self.g_worst = best[0], worst[0]
