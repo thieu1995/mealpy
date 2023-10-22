f98973fa745ac254994e84b2017f47badb3555fc
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-22 15:15:39 +0700

    Convert CGO

--- mealpy/math_based/CGO.py
+++ mealpy/math_based/CGO.py
@@ -15,32 +15,30 @@ class OriginalCGO(Optimizer):
     Links:
         1. https://doi.org/10.1007/s10462-020-09867-w
 
-    Notes
-    ~~~~~
-    + 4th seed is mutation process, but it is not clear mutation on multiple variables or 1 variable
-    + There is no usage of the variable alpha 4th in the paper
-    + The replacement of the worst solutions by generated seed are not clear (Lots of grammar errors in this section)
+    Notes:
+        + 4th seed is mutation process, but it is not clear mutation on multiple variables or 1 variable
+        + There is no usage of the variable alpha 4th in the paper
+        + The replacement of the worst solutions by generated seed are not clear (Lots of grammar errors in this section)
 
     Examples
     ~~~~~~~~
+
     >>> import numpy as np
-    >>> from mealpy.math_based.CGO import OriginalCGO
+    >>> from mealpy import FloatVar, CGO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalCGO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CGO.OriginalCGO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -48,7 +46,7 @@ class OriginalCGO(Optimizer):
     Artificial Intelligence Review, 54(2), pp.917-1004.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -58,6 +56,7 @@ class OriginalCGO(Optimizer):
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
         self.set_parameters(["epoch", "pop_size"])
+        self.is_parallelizable = False
         self.sort_flag = False
 
     def evolve(self, epoch):
@@ -70,44 +69,34 @@ class OriginalCGO(Optimizer):
         pop_new = []
         for idx in range(0, self.pop_size):
             s1, s2, s3 = np.random.choice(range(0, self.pop_size), 3, replace=False)
-            MG = (self.pop[s1][self.ID_POS] + self.pop[s2][self.ID_POS] + self.pop[s3][self.ID_POS]) / 3
-
+            MG = (self.pop[s1].solution + self.pop[s2].solution + self.pop[s3].solution) / 3
             ## Calculating alpha based on Eq. 7
-            alpha1 = np.random.rand()
-            alpha2 = 2 * np.random.rand()
-            alpha3 = 1 + np.random.random() * np.random.rand()
-            esp = np.random.random()
+            alpha1 = self.generator.random()
+            alpha2 = 2 * self.generator.random()
+            alpha3 = 1 + self.generator.random() * self.generator.random()
+            esp = self.generator.random()
             # There is no usage of this variable in the paper
-            alpha4 = esp + esp * np.random.rand()
-
-            beta = np.random.randint(0, 2, 3)
-            gama = np.random.randint(0, 2, 3)
+            alpha4 = esp + esp * self.generator.random()
+            beta = self.generator.integers(0, 2, 3)
+            gama = self.generator.integers(0, 2, 3)
             ## The seed4 is mutation process, but not sure k is multiple variables or 1 variable.
             ## In the text said, multiple variables, but the defination of k is 1 variable. So confused
-            k = np.random.randint(0, self.problem.n_dims)
-            k_idx = np.random.choice(range(0, self.problem.n_dims), k, replace=False)
-
-            seed1 = self.pop[idx][self.ID_POS] + alpha1 * (beta[0] * self.g_best[self.ID_POS] - gama[0] * MG)  # Eq. 3
-            seed2 = self.g_best[self.ID_POS] + alpha2 * (beta[1] * self.pop[idx][self.ID_POS] - gama[1] * MG)  # Eq. 4
-            seed3 = MG + alpha3 * (beta[2] * self.pop[idx][self.ID_POS] - gama[2] * self.g_best[self.ID_POS])  # Eq. 5
-            seed4 = self.pop[idx][self.ID_POS].copy().astype(float)
+            k = self.generator.integers(0, self.problem.n_dims)
+            k_idx = self.generator.choice(range(0, self.problem.n_dims), k, replace=False)
+            seed1 = self.pop[idx].solution + alpha1 * (beta[0] * self.g_best.solution - gama[0] * MG)  # Eq. 3
+            seed2 = self.g_best.solution + alpha2 * (beta[1] * self.pop[idx].solution - gama[1] * MG)  # Eq. 4
+            seed3 = MG + alpha3 * (beta[2] * self.pop[idx].solution - gama[2] * self.g_best.solution)  # Eq. 5
+            seed4 = self.pop[idx].solution.copy().astype(float)
             seed4[k_idx] += np.random.uniform(0, 1, k)
-
             # Check if solutions go outside the search space and bring them back
-            seed1 = self.amend_position(seed1, self.problem.lb, self.problem.ub)
-            seed2 = self.amend_position(seed2, self.problem.lb, self.problem.ub)
-            seed3 = self.amend_position(seed3, self.problem.lb, self.problem.ub)
-            seed4 = self.amend_position(seed4, self.problem.lb, self.problem.ub)
-
-            sol1 = [seed1, self.get_target_wrapper(seed1)]
-            sol2 = [seed2, self.get_target_wrapper(seed2)]
-            sol3 = [seed3, self.get_target_wrapper(seed3)]
-            sol4 = [seed4, self.get_target_wrapper(seed4)]
-
+            seed1 = self.correct_solution(seed1)
+            seed2 = self.correct_solution(seed2)
+            seed3 = self.correct_solution(seed3)
+            seed4 = self.correct_solution(seed4)
+            agent1 = self.generate_agent(seed1)
+            agent2 = self.generate_agent(seed2)
+            agent3 = self.generate_agent(seed3)
+            agent4 = self.generate_agent(seed4)
             ## Lots of grammar errors in this section, so confused to understand which strategy they are using
-            _, best_seed = self.get_global_best_solution([sol1, sol2, sol3, sol4])
-            pop_new.append(best_seed)
-            if self.mode not in self.AVAILABLE_MODES:
-                self.pop[idx] = self.get_better_solution(best_seed, self.pop[idx])
-        if self.mode in self.AVAILABLE_MODES:
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            best_seed = self.get_best_agent([agent1, agent2, agent3, agent4], self.problem.minmax)
+            self.pop[idx] = self.get_better_agent(best_seed, self.pop[idx], self.problem.minmax)
