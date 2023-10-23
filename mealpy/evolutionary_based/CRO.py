69367149846cecd31669fb7c5e627dec081bd4ff
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-23 11:24:05 +0700

    Convert CRO

--- mealpy/evolutionary_based/CRO.py
+++ mealpy/evolutionary_based/CRO.py
@@ -29,32 +29,21 @@ class OriginalCRO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.evolutionary_based.CRO import OriginalCRO
+    >>> from mealpy import FloatVar, CRO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> po = 0.4
-    >>> Fb = 0.9
-    >>> Fa = 0.1
-    >>> Fd = 0.1
-    >>> Pd = 0.5
-    >>> GCR = 0.1
-    >>> gamma_min = 0.02
-    >>> gamma_max = 0.2
-    >>> n_trials = 5
-    >>> model = OriginalCRO(epoch, pop_size, po, Fb, Fa, Fd, Pd, GCR, gamma_min, gamma_max, n_trials)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CRO.OriginalCRO(epoch=1000, pop_size=50, po = 0.4, Fb = 0.9, Fa = 0.1, Fd = 0.1, Pd = 0.5, GCR = 0.1, gamma_min = 0.02, gamma_max = 0.2, n_trials = 5)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -62,8 +51,8 @@ class OriginalCRO(Optimizer):
     The coral reefs optimization algorithm: a novel metaheuristic for efficiently solving optimization problems. The Scientific World Journal, 2014.
     """
 
-    def __init__(self, epoch=10000, pop_size=100,po=0.4, Fb=0.9, Fa=0.1, Fd=0.1, Pd=0.5, GCR=0.1,
-                 gamma_min=0.02, gamma_max=0.2, n_trials=3, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, po: float = 0.4, Fb: float = 0.9, Fa: float = 0.1, Fd: float = 0.1,
+                 Pd: float = 0.5, GCR: float = 0.1, gamma_min: float = 0.02, gamma_max: float = 0.2, n_trials: int = 3, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -80,7 +69,7 @@ class OriginalCRO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])  # ~ number of space
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])  # ~ number of space
         self.po = self.validator.check_float("po", po, (0, 1.0))
         self.Fb = self.validator.check_float("Fb", Fb, (0, 1.0))
         self.Fa = self.validator.check_float("Fa", Fa, (0, 1.0))
@@ -95,7 +84,7 @@ class OriginalCRO(Optimizer):
 
     def initialization(self):
         if self.pop is None:
-            self.pop = self.create_population(self.pop_size)
+            self.pop = self.generate_population(self.pop_size)
         self.reef = np.array([])
         self.occupied_position = []  # after a gen, you should update the occupied_position
         self.G1 = self.gamma_max
@@ -104,61 +93,63 @@ class OriginalCRO(Optimizer):
         self.num_occupied = int(self.pop_size / (1 + self.po))
         self.dyn_Pd = 0
         self.occupied_list = np.zeros(self.pop_size)
-        self.occupied_idx_list = np.random.choice(list(range(self.pop_size)), self.num_occupied, replace=False)
+        self.occupied_idx_list = self.generator.choice(list(range(self.pop_size)), self.num_occupied, replace=False)
         self.occupied_list[self.occupied_idx_list] = 1
 
     def gaussian_mutation__(self, position):
-        random_pos = position + self.G1 * (self.problem.ub - self.problem.lb) * np.random.normal(0, 1, self.problem.n_dims)
-        condition = np.random.random(self.problem.n_dims) < self.GCR
+        random_pos = position + self.G1 * (self.problem.ub - self.problem.lb) * self.generator.normal(0, 1, self.problem.n_dims)
+        condition =self.generator.random(self.problem.n_dims) < self.GCR
         pos_new = np.where(condition, random_pos, position)
-        return self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+        return self.correct_solution(pos_new)
 
     ### Crossover
     def multi_point_cross__(self, pos1, pos2):
-        p1, p2 = np.random.choice(list(range(len(pos1))), 2, replace=False)
+        p1, p2 = self.generator.choice(list(range(len(pos1))), 2, replace=False)
         start, end = min(p1, p2), max(p1, p2)
-        return np.concatenate((pos1[:start], pos2[start:end], pos1[end:]), axis=0)
+        pos_new = np.concatenate((pos1[:start], pos2[start:end], pos1[end:]), axis=0)
+        return self.correct_solution(pos_new)
 
     def larvae_setting__(self, larvae):
         # Trial to land on a square of reefs
         for larva in larvae:
-            for i in range(self.n_trials):
-                p = np.random.randint(0, self.pop_size - 1)
-                if self.occupied_list[p] == 0:
-                    self.pop[p] = larva
-                    self.occupied_idx_list = np.append(self.occupied_idx_list, p)  # Update occupied id
-                    self.occupied_list[p] = 1  # Update occupied list
+            for idx in range(self.n_trials):
+                pdx = self.generator.integers(0, self.pop_size - 1)
+                if self.occupied_list[pdx] == 0:
+                    self.pop[pdx] = larva
+                    self.occupied_idx_list = np.append(self.occupied_idx_list, pdx)  # Update occupied id
+                    self.occupied_list[pdx] = 1  # Update occupied list
                     break
                 else:
-                    if self.compare_agent(larva, self.pop[p]):
-                        self.pop[p] = larva
+                    if self.compare_target(larva.target, self.pop[pdx].target, self.problem.minmax):
+                        self.pop[pdx] = larva
                         break
 
     def sort_occupied_reef__(self):
         def reef_fitness(idx):
-            return self.pop[idx][self.ID_TAR][self.ID_FIT]
-        idx_list_sorted = sorted(self.occupied_idx_list, key=reef_fitness)
-        return idx_list_sorted
+            return self.pop[idx].target.fitness
+        return sorted(self.occupied_idx_list, key=reef_fitness)
 
     def broadcast_spawning_brooding__(self):
         # Step 1a
         larvae = []
-        selected_corals = np.random.choice(self.occupied_idx_list, int(len(self.occupied_idx_list) * self.Fb), replace=False)
-        for i in self.occupied_idx_list:
-            if i not in selected_corals:
-                pos_new = self.gaussian_mutation__(self.pop[i][self.ID_POS])
-                larvae.append([pos_new, None])
+        selected_corals = self.generator.choice(self.occupied_idx_list, int(len(self.occupied_idx_list) * self.Fb), replace=False)
+        for idx in self.occupied_idx_list:
+            if idx not in selected_corals:
+                pos_new = self.gaussian_mutation__(self.pop[idx].solution)
+                agent = self.generate_empty_agent(pos_new)
+                larvae.append(agent)
                 if self.mode not in self.AVAILABLE_MODES:
-                    larvae[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                    larvae[-1].target = self.get_target(pos_new)
         # Step 1b
         while len(selected_corals) >= 2:
-            id1, id2 = np.random.choice(range(len(selected_corals)), 2, replace=False)
-            pos_new = self.multi_point_cross__(self.pop[selected_corals[id1]][self.ID_POS], self.pop[selected_corals[id2]][self.ID_POS])
-            larvae.append([pos_new, None])
+            id1, id2 = self.generator.choice(range(len(selected_corals)), 2, replace=False)
+            pos_new = self.multi_point_cross__(self.pop[selected_corals[id1]].solution, self.pop[selected_corals[id2]].solution)
+            agent = self.generate_empty_agent(pos_new)
+            larvae.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                larvae[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                larvae[-1].target = self.get_target(pos_new)
             selected_corals = np.delete(selected_corals, [id1, id2])
-        return self.update_target_wrapper_population(larvae)
+        return self.update_target_for_population(larvae)
 
     def evolve(self, epoch):
         """
@@ -170,15 +161,13 @@ class OriginalCRO(Optimizer):
         ## Broadcast Spawning Brooding
         larvae = self.broadcast_spawning_brooding__()
         self.larvae_setting__(larvae)
-
         ## Asexual Reproduction
         num_duplicate = int(len(self.occupied_idx_list) * self.Fa)
         pop_best = [self.pop[idx] for idx in self.occupied_idx_list]
-        pop_best = self.get_sorted_strim_population(pop_best, num_duplicate)
+        pop_best = self.get_sorted_and_trimmed_population(pop_best, num_duplicate, self.problem.minmax)
         self.larvae_setting__(pop_best)
-
         ## Depredation
-        if np.random.random() < self.dyn_Pd:
+        if self.generator.random() < self.dyn_Pd:
             num__depredation__ = int(len(self.occupied_idx_list) * self.Fd)
             idx_list_sorted = self.sort_occupied_reef__()
             selected_depredator = idx_list_sorted[-num__depredation__:]
@@ -213,33 +202,21 @@ class OCRO(OriginalCRO):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.evolutionary_based.CRO import OCRO
+    >>> from mealpy import FloatVar, CRO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> po = 0.4
-    >>> Fb = 0.9
-    >>> Fa = 0.1
-    >>> Fd = 0.1
-    >>> Pd = 0.5
-    >>> GCR = 0.1
-    >>> gamma_min = 0.02
-    >>> gamma_max = 0.2
-    >>> n_trials = 5
-    >>> restart_count = 50
-    >>> model = OCRO(epoch, pop_size, po, Fb, Fa, Fd, Pd, GCR, gamma_min, gamma_max, n_trials, restart_count)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CRO.OCRO(epoch=1000, pop_size=50, po = 0.4, Fb = 0.9, Fa = 0.1, Fd = 0.1, Pd = 0.5, GCR = 0.1, gamma_min = 0.02, gamma_max = 0.2, n_trials = 5, restart_count = 50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -248,8 +225,8 @@ class OCRO(OriginalCRO):
     Intelligence Systems, 12(2), p.1144.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, po=0.4, Fb=0.9, Fa=0.1, Fd=0.1, Pd=0.5,
-                 GCR=0.1, gamma_min=0.02, gamma_max=0.2, n_trials=3, restart_count=20, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, po: float = 0.4, Fb: float = 0.9, Fa: float = 0.1, Fd: float = 0.1, Pd: float = 0.5,
+                 GCR: float = 0.1, gamma_min: float = 0.02, gamma_max: float = 0.2, n_trials: int = 3, restart_count: int = 20, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -276,14 +253,15 @@ class OCRO(OriginalCRO):
     def local_search__(self, pop=None):
         pop_new = []
         for idx in range(0, len(pop)):
-            random_pos = np.random.uniform(self.problem.lb, self.problem.ub)
-            condition = np.random.random(self.problem.n_dims) < 0.5
-            pos_new = np.where(condition, self.g_best[self.ID_POS], random_pos)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            random_pos = self.generator.uniform(self.problem.lb, self.problem.ub)
+            condition = self.generator.random(self.problem.n_dims) < 0.5
+            pos_new = np.where(condition, self.g_best.solution, random_pos)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        return self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        return self.update_target_for_population(pop_new)
 
     def evolve(self, epoch):
         """
@@ -295,43 +273,37 @@ class OCRO(OriginalCRO):
         ## Broadcast Spawning Brooding
         larvae = self.broadcast_spawning_brooding__()
         self.larvae_setting__(larvae)
-
         ## Asexual Reproduction
         num_duplicate = int(len(self.occupied_idx_list) * self.Fa)
         pop_best = [self.pop[idx] for idx in self.occupied_idx_list]
-        pop_best = self.get_sorted_strim_population(pop_best, num_duplicate)
+        pop_best = self.get_sorted_and_trimmed_population(pop_best, num_duplicate, self.problem.minmax)
         pop_local_search = self.local_search__(pop_best)
         self.larvae_setting__(pop_local_search)
-
         ## Depredation
-        if np.random.random() < self.dyn_Pd:
+        if self.generator.random() < self.dyn_Pd:
             num__depredation__ = int(len(self.occupied_idx_list) * self.Fd)
             idx_list_sorted = self.sort_occupied_reef__()
             selected_depredator = idx_list_sorted[-num__depredation__:]
             for idx in selected_depredator:
                 ### Using opposition-based leanring
-                oppo_pos = self.create_opposition_position(self.pop[idx], self.g_best)
-                oppo_pos = self.amend_position(oppo_pos, self.problem.lb, self.problem.ub)
-                oppo_reef = [oppo_pos, self.get_target_wrapper(oppo_pos)]
-                if self.compare_agent(oppo_reef, self.pop[idx]):
-                    self.pop[idx] = oppo_reef
+                pos_oppo = self.generate_opposition_solution(self.pop[idx], self.g_best)
+                agent = self.generate_agent(pos_oppo)
+                if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                    self.pop[idx] = agent
                 else:
                     self.occupied_idx_list = self.occupied_idx_list[~np.isin(self.occupied_idx_list, [idx])]
                     self.occupied_list[idx] = 0
-
         if self.dyn_Pd <= self.Pd:
             self.dyn_Pd += self.alpha
         if self.G1 >= self.gamma_min:
             self.G1 -= self.gama
-
         self.reset_count += 1
-        _, local_best = self.get_global_best_solution(self.pop)
-        if self.compare_agent(local_best, self.g_best):
+        local_best = self.get_best_agent(self.pop, self.problem.minmax)
+        if self.compare_target(local_best.target, self.g_best.target, self.problem.minmax):
             self.reset_count = 0
-
         if self.reset_count == self.restart_count:
-            self.pop = self.create_population(self.pop_size)
+            self.pop = self.generate_population(self.pop_size)
             self.occupied_list = np.zeros(self.pop_size)
-            self.occupied_idx_list = np.random.choice(range(self.pop_size), self.num_occupied, replace=False)
+            self.occupied_idx_list = self.generator.choice(range(self.pop_size), self.num_occupied, replace=False)
             self.occupied_list[self.occupied_idx_list] = 1
             self.reset_count = 0
