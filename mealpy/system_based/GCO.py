f23d58c582949be73fc541fb15ba7d8584ce8a7a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-23 09:45:46 +0700

    Convert GCO

--- mealpy/system_based/GCO.py
+++ mealpy/system_based/GCO.py
@@ -8,13 +8,12 @@ import numpy as np
 from mealpy.optimizer import Optimizer
 
 
-class BaseGCO(Optimizer):
+class DevGCO(Optimizer):
     """
     The developed version: Germinal Center Optimization (GCO)
 
-    Notes
-    ~~~~~
-    + The global best solution and 2 random solutions are used instead of randomizing 3 solutions
+    Notes:
+        + The global best solution and 2 random solutions are used instead of randomizing 3 solutions
 
     Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + cr (float): [0.5, 0.95], crossover rate, default = 0.7 (Same as DE algorithm)
@@ -23,28 +22,24 @@ class BaseGCO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.system_based.GCO import BaseGCO
+    >>> from mealpy import FloatVar, GCO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> cr = 0.7
-    >>> wf = 1.25
-    >>> model = BaseGCO(epoch, pop_size, cr, wf)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = GCO.DevGCO(epoch=1000, pop_size=50, cr = 0.7, wf = 1.25)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
     """
 
-    def __init__(self, epoch=10000, pop_size=100, cr=0.7, wf=1.25, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, cr: float = 0.7, wf: float = 1.25, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -54,7 +49,7 @@ class BaseGCO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.cr = self.validator.check_float("cr", cr, (0, 1.0))
         self.wf = self.validator.check_float("wf", wf, (0, 3.0))
         self.set_parameters(["epoch", "pop_size", "cr", "wf"])
@@ -74,36 +69,35 @@ class BaseGCO(Optimizer):
         ## Dark-zone process    (can be parallelization)
         pop_new = []
         for idx in range(0, self.pop_size):
-            if np.random.uniform(0, 100) < self.dyn_list_life_signal[idx]:
+            if self.generator.uniform(0, 100) < self.dyn_list_life_signal[idx]:
                 self.dyn_list_cell_counter[idx] += 1
             else:
                 self.dyn_list_cell_counter[idx] = 1
-
             # Mutate process
-            r1, r2 = np.random.choice(list(set(range(0, self.pop_size)) - {idx}), 2, replace=False)
-            pos_new = self.g_best[self.ID_POS] + self.wf * (self.pop[r2][self.ID_POS] - self.pop[r1][self.ID_POS])
-            condition = np.random.random(self.problem.n_dims) < self.cr
-            pos_new = np.where(condition, pos_new, self.pop[idx][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            r1, r2 = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}), 2, replace=False)
+            pos_new = self.g_best.solution + self.wf * (self.pop[r2].solution - self.pop[r1].solution)
+            condition = self.generator.random(self.problem.n_dims) < self.cr
+            pos_new = np.where(condition, pos_new, self.pop[idx].solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         for idx in range(0, self.pop_size):
-            if self.compare_agent(pop_new[idx], self.pop[idx]):
+            if self.compare_target(pop_new[idx].target, self.pop[idx].target, self.problem.minmax):
                 self.dyn_list_cell_counter[idx] += 10
                 self.pop[idx] = pop_new[idx].copy()
-
         ## Light-zone process   (no needs parallelization)
-        for i in range(0, self.pop_size):
-            self.dyn_list_cell_counter[i] = 10
-            fit_list = np.array([item[self.ID_TAR][self.ID_FIT] for item in self.pop])
-            fit_max = max(fit_list)
-            fit_min = min(fit_list)
-            self.dyn_list_cell_counter[i] += 10 * (self.pop[i][self.ID_TAR][self.ID_FIT] - fit_max) / (fit_min - fit_max + self.EPSILON)
+        for idx in range(0, self.pop_size):
+            self.dyn_list_cell_counter[idx] = 10
+            fit_list = np.array([agent.target.fitness for agent in self.pop])
+            fit_max = np.max(fit_list)
+            fit_min = np.min(fit_list)
+            self.dyn_list_cell_counter[idx] += 10 * (self.pop[idx].target.fitness - fit_max) / (fit_min - fit_max + self.EPSILON)
 
 
-class OriginalGCO(BaseGCO):
+class OriginalGCO(DevGCO):
     """
     The original version of: Germinal Center Optimization (GCO)
 
@@ -118,25 +112,21 @@ class OriginalGCO(BaseGCO):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.system_based.GCO import OriginalGCO
+    >>> from mealpy import FloatVar, GCO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> cr = 0.7
-    >>> wf = 1.25
-    >>> model = OriginalGCO(epoch, pop_size, cr, wf)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = GCO.OriginalGCO(epoch=1000, pop_size=50, cr = 0.7, wf = 1.25)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -144,7 +134,7 @@ class OriginalGCO(BaseGCO):
     Germinal center optimization algorithm. International Journal of Computational Intelligence Systems, 12(1), p.13.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, cr=0.7, wf=1.25, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, cr: float = 0.7, wf: float = 1.25, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -153,7 +143,7 @@ class OriginalGCO(BaseGCO):
             wf (float): weighting factor (f in the paper), default = 1.25 (Same as DE algorithm)
         """
         super().__init__(epoch, pop_size, cr, wf, **kwargs)
-        self.support_parallel_modes = False
+        self.is_parallelizable = False
 
     def evolve(self, epoch):
         """
@@ -162,29 +152,27 @@ class OriginalGCO(BaseGCO):
         Args:
             epoch (int): The current iteration
         """
-        ## Dark-zone process    (can be parallelization)
+        ## Dark-zone process (can't be parallelization)
         for idx in range(0, self.pop_size):
-            if np.random.uniform(0, 100) < self.dyn_list_life_signal[idx]:
+            if self.generator.uniform(0, 100) < self.dyn_list_life_signal[idx]:
                 self.dyn_list_cell_counter[idx] += 1
             elif self.dyn_list_cell_counter[idx] > 1:
                 self.dyn_list_cell_counter[idx] -= 1
-
             # Mutate process
             p = self.dyn_list_cell_counter / np.sum(self.dyn_list_cell_counter)
-            r1, r2, r3 = np.random.choice(list(set(range(0, self.pop_size))), 3, replace=False, p=p)
-            pos_new = self.pop[r1][self.ID_POS] + self.wf * (self.pop[r2][self.ID_POS] - self.pop[r3][self.ID_POS])
-            condition = np.random.random(self.problem.n_dims) < self.cr
-            pos_new = np.where(condition, pos_new, self.pop[idx][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            r1, r2, r3 = self.generator.choice(list(set(range(0, self.pop_size))), 3, replace=False, p=p)
+            pos_new = self.pop[r1].solution + self.wf * (self.pop[r2].solution - self.pop[r3].solution)
+            condition = self.generator.random(self.problem.n_dims) < self.cr
+            pos_new = np.where(condition, pos_new, self.pop[idx].solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_agent(pos_new)
             # for each pos_new, generate the fitness
-            target = self.get_target_wrapper(pos_new)
-            if self.compare_agent([pos_new, target], self.pop[idx]):
-                self.pop[idx] = [pos_new, target]
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
                 self.dyn_list_life_signal[idx] += 10
-
         ## Light-zone process   (no needs parallelization)
         self.dyn_list_life_signal -= 10
-        fit_list = np.array([item[self.ID_TAR][self.ID_FIT] for item in self.pop])
+        fit_list = np.array([agent.target.fitness for agent in self.pop])
         fit_max = np.max(fit_list)
         fit_min = np.min(fit_list)
         fit = (fit_list - fit_max) / (fit_min - fit_max)
