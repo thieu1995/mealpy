05d79a457abb2217dbb53d3db3fd770d345c2285
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-25 22:10:10 +0700

    Convert BSO

--- mealpy/human_based/BSO.py
+++ mealpy/human_based/BSO.py
@@ -5,18 +5,16 @@
 # --------------------------------------------------%
 
 import numpy as np
-from copy import deepcopy
 from mealpy.optimizer import Optimizer
 
 
 class ImprovedBSO(Optimizer):
     """
-    The improved version: Brain Storm Optimization (BSO)
+    The improved version: Improved Brain Storm Optimization (IBSO)
 
-    Notes
-    ~~~~~
-    + Remove some probability parameters, and some unnecessary equations.
-    + The Levy-flight technique is employed to enhance the algorithm's robustness and resilience in challenging environments.
+    Notes:
+        + Remove some probability parameters, and some unnecessary equations.
+        + The Levy-flight technique is employed to enhance the algorithm's robustness and resilience in challenging environments.
 
     Hyper-parameters should fine-tune in approximate range to get faster convergence toward the global optimum:
         + m_clusters (int): [3, 10], number of clusters (m in the paper)
@@ -28,32 +26,29 @@ class ImprovedBSO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.human_based.BSO import ImprovedBSO
+    >>> from mealpy import FloatVar, BSO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> m_clusters = 5
-    >>> p1 = 0.25
-    >>> p2 = 0.5
-    >>> p3 = 0.75
-    >>> p4 = 0.6
-    >>> model = ImprovedBSO(epoch, pop_size, m_clusters, p1, p2, p3, p4)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = BSO.ImprovedBSO(epoch=1000, pop_size=50, m_clusters = 5, p1 = 0.25, p2 = 0.5, p3 = 0.75, p4 = 0.6)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
+
+    References
+    ~~~~~~~~~~
+    [1] El-Abd, M. (2017). Global-best brain storm optimization algorithm. Swarm and evolutionary computation, 37, 27-44.
     """
 
-    def __init__(self, epoch=10000, pop_size=100,
-                 m_clusters=5, p1=0.25, p2=0.5, p3=0.75, p4=0.5, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, m_clusters: int = 5,
+                 p1: float = 0.25, p2: float = 0.5, p3: float = 0.75, p4: float = 0.5, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -79,15 +74,15 @@ class ImprovedBSO(Optimizer):
 
     def find_cluster__(self, pop_group):
         centers = []
-        for i in range(0, self.m_clusters):
-            _, local_best = self.get_global_best_solution(pop_group[i])
-            centers.append(local_best)
+        for idx in range(0, self.m_clusters):
+            local_best = self.get_best_agent(pop_group[idx])
+            centers.append(local_best.copy())
         return centers
 
     def initialization(self):
         if self.pop is None:
-            self.pop = self.create_population(self.pop_size)
-        self.pop_group = self.create_pop_group(self.pop, self.m_clusters, self.m_solution)
+            self.pop = self.generate_population(self.pop_size)
+        self.pop_group = self.generate_group_population(self.pop, self.m_clusters, self.m_solution)
         self.centers = self.find_cluster__(self.pop_group)
 
     def evolve(self, epoch):
@@ -97,43 +92,40 @@ class ImprovedBSO(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        epsilon = 1 - 1 * (epoch + 1) / self.epoch  # 1. Changed here, no need: k
+        epsilon = 1. - 1. * epoch / self.epoch  # 1. Changed here, no need: k
+        if self.generator.uniform() < self.p1:  # p_5a
+            idx = self.generator.integers(0, self.m_clusters)
+            self.centers[idx] = self.generate_agent()
+        pop_group = self.pop_group
+        for idx in range(0, self.pop_size):  # Generate new individuals
+            cluster_id = int(idx / self.m_solution)
+            location_id = int(idx % self.m_solution)
 
-        if np.random.uniform() < self.p1:  # p_5a
-            idx = np.random.randint(0, self.m_clusters)
-            solution_new = self.create_solution(self.problem.lb, self.problem.ub)
-            self.centers[idx] = solution_new
-
-        pop_group = deepcopy(self.pop_group)
-        for i in range(0, self.pop_size):  # Generate new individuals
-            cluster_id = int(i / self.m_solution)
-            location_id = int(i % self.m_solution)
-
-            if np.random.uniform() < self.p2:  # p_6b
-                if np.random.uniform() < self.p3:
-                    pos_new = self.centers[cluster_id][self.ID_POS] + epsilon * np.random.normal(0, 1, self.problem.n_dims)
+            if self.generator.uniform() < self.p2:  # p_6b
+                if self.generator.uniform() < self.p3:
+                    pos_new = self.centers[cluster_id].solution + epsilon * self.generator.normal(0, 1, self.problem.n_dims)
                 else:  # 2. Using levy flight here
                     levy_step = self.get_levy_flight_step(beta=1.0, multiplier=0.001, size=self.problem.n_dims, case=-1)
-                    pos_new = self.pop_group[cluster_id][location_id][self.ID_POS] + levy_step
+                    pos_new = self.pop_group[cluster_id][location_id].solution + levy_step
             else:
-                id1, id2 = np.random.choice(range(0, self.m_clusters), 2, replace=False)
-                if np.random.uniform() < self.p4:
-                    pos_new = 0.5 * (self.centers[id1][self.ID_POS] + self.centers[id2][self.ID_POS]) + \
-                              epsilon * np.random.normal(0, 1, self.problem.n_dims)
+                id1, id2 = self.generator.choice(range(0, self.m_clusters), 2, replace=False)
+                if self.generator.uniform() < self.p4:
+                    pos_new = 0.5 * (self.centers[id1].solution + self.centers[id2].solution) + epsilon * self.generator.normal(0, 1, self.problem.n_dims)
                 else:
-                    rand_id1 = np.random.randint(0, self.m_solution)
-                    rand_id2 = np.random.randint(0, self.m_solution)
-                    pos_new = 0.5 * (self.pop_group[id1][rand_id1][self.ID_POS] + self.pop_group[id2][rand_id2][self.ID_POS]) + \
-                              epsilon * np.random.normal(0, 1, self.problem.n_dims)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_group[cluster_id][location_id] = [pos_new, None]
+                    rand_id1 = self.generator.integers(0, self.m_solution)
+                    rand_id2 = self.generator.integers(0, self.m_solution)
+                    pos_new = 0.5 * (self.pop_group[id1][rand_id1].solution + self.pop_group[id2][rand_id2].solution) + \
+                              epsilon * self.generator.normal(0, 1, self.problem.n_dims)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_group[cluster_id][location_id] = agent
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                pop_group[cluster_id][location_id] = self.get_better_solution([pos_new, target], self.pop_group[cluster_id][location_id])
+                agent.target = self.get_target(pos_new)
+                pop_group[cluster_id][location_id] = self.get_better_agent(agent, self.pop_group[cluster_id][location_id], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
             for idx in range(0, self.m_clusters):
-                pop_group[idx] = self.update_target_wrapper_population(pop_group[idx])
-                pop_group[idx] = self.greedy_selection_population(self.pop_group[idx], pop_group[idx])
+                pop_group[idx] = self.update_target_for_population(pop_group[idx])
+                pop_group[idx] = self.greedy_selection_population(self.pop_group[idx], pop_group[idx], self.problem.minmax)
 
         # Needed to update the centers and population
         self.centers = self.find_cluster__(pop_group)
@@ -160,29 +152,21 @@ class OriginalBSO(ImprovedBSO):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.human_based.BSO import OriginalBSO
+    >>> from mealpy import FloatVar, BSO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> m_clusters = 5
-    >>> p1 = 0.2
-    >>> p2 = 0.8
-    >>> p3 = 0.4
-    >>> p4 = 0.5
-    >>> slope = 20
-    >>> model = OriginalBSO(epoch, pop_size, m_clusters, p1, p2, p3, p4, slope)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = BSO.OriginalBSO(epoch=1000, pop_size=50, m_clusters = 5, p1 = 0.2, p2 = 0.8, p3 = 0.4, p4 = 0.5, slope = 20)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -190,7 +174,8 @@ class OriginalBSO(ImprovedBSO):
     conference in swarm intelligence (pp. 303-309). Springer, Berlin, Heidelberg.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, m_clusters=5, p1=0.2, p2=0.8, p3=0.4, p4=0.5, slope=20, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, m_clusters: int = 5, p1: float = 0.2,
+                 p2: float = 0.8, p3: float = 0.4, p4: float = 0.5, slope: int = 20, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -206,10 +191,10 @@ class OriginalBSO(ImprovedBSO):
         self.slope = self.validator.check_int("slope", slope, [10, 50])
         self.set_parameters(["epoch", "pop_size", "m_clusters", "p1", "p2", "p3", "p4", "slope"])
 
-    def bounded_position(self, position=None, lb=None, ub=None):
-        rand_pos = np.random.uniform(lb, ub)
-        condition = np.logical_and(lb <= position, position <= ub)
-        return np.where(condition, position, rand_pos)
+    def amend_solution(self, solution: np.ndarray) -> np.ndarray:
+        rp = self.generator.uniform(self.problem.lb, self.problem.ub)
+        condition = np.logical_and(self.problem.lb <= solution, solution <= self.problem.ub)
+        return np.where(condition, solution, rp)
 
     def evolve(self, epoch):
         """
@@ -218,47 +203,42 @@ class OriginalBSO(ImprovedBSO):
         Args:
             epoch (int): The current iteration
         """
-        x = (0.5 * self.epoch - (epoch + 1)) / self.slope
-        epsilon = np.random.uniform() * (1 / (1 + np.exp(-x)))
-
-        if np.random.rand() < self.p1:  # p_5a
-            idx = np.random.randint(0, self.m_clusters)
-            solution_new = self.create_solution(self.problem.lb, self.problem.ub)
-            self.centers[idx] = solution_new
-
-        pop_group = deepcopy(self.pop_group)
-        for i in range(0, self.pop_size):  # Generate new individuals
-            cluster_id = int(i / self.m_solution)
-            location_id = int(i % self.m_solution)
-
-            if np.random.uniform() < self.p2:  # p_6b
-                if np.random.uniform() < self.p3:  # p_6i
-                    cluster_id = np.random.randint(0, self.m_clusters)
-                if np.random.uniform() < self.p3:
-                    pos_new = self.centers[cluster_id][self.ID_POS] + epsilon * np.random.normal(0, 1, self.problem.n_dims)
+        x = (0.5 * self.epoch - epoch) / self.slope
+        epsilon = self.generator.uniform() * (1 / (1 + np.exp(-x)))
+        if self.generator.random() < self.p1:  # p_5a
+            idx = self.generator.integers(0, self.m_clusters)
+            self.centers[idx] = self.generate_agent()
+        pop_group = self.pop_group
+        for idx in range(0, self.pop_size):  # Generate new individuals
+            cluster_id = int(idx / self.m_solution)
+            location_id = int(idx % self.m_solution)
+            if self.generator.uniform() < self.p2:  # p_6b
+                if self.generator.uniform() < self.p3:  # p_6i
+                    cluster_id = self.generator.integers(0, self.m_clusters)
+                if self.generator.uniform() < self.p3:
+                    pos_new = self.centers[cluster_id].solution + epsilon * self.generator.normal(0, 1, self.problem.n_dims)
                 else:
-                    rand_idx = np.random.randint(0, self.m_solution)
-                    pos_new = self.pop_group[cluster_id][rand_idx][self.ID_POS] + np.random.normal(0, 1, self.problem.n_dims)
+                    rand_idx = self.generator.integers(0, self.m_solution)
+                    pos_new = self.pop_group[cluster_id][rand_idx].solution + self.generator.normal(0, 1, self.problem.n_dims)
             else:
-                id1, id2 = np.random.choice(range(0, self.m_clusters), 2, replace=False)
-                if np.random.uniform() < self.p4:
-                    pos_new = 0.5 * (self.centers[id1][self.ID_POS] + self.centers[id2][self.ID_POS]) + \
-                              epsilon * np.random.normal(0, 1, self.problem.n_dims)
+                id1, id2 = self.generator.choice(range(0, self.m_clusters), 2, replace=False)
+                if self.generator.uniform() < self.p4:
+                    pos_new = 0.5 * (self.centers[id1].solution + self.centers[id2].solution) + epsilon * self.generator.normal(0, 1, self.problem.n_dims)
                 else:
-                    rand_id1 = np.random.randint(0, self.m_solution)
-                    rand_id2 = np.random.randint(0, self.m_solution)
-                    pos_new = 0.5 * (self.pop_group[id1][rand_id1][self.ID_POS] + self.pop_group[id2][rand_id2][self.ID_POS]) + \
-                              epsilon * np.random.normal(0, 1, self.problem.n_dims)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_group[cluster_id][location_id] = [pos_new, None]
+                    rand_id1 = self.generator.integers(0, self.m_solution)
+                    rand_id2 = self.generator.integers(0, self.m_solution)
+                    pos_new = 0.5 * (self.pop_group[id1][rand_id1].solution + self.pop_group[id2][rand_id2].solution) + \
+                              epsilon * self.generator.normal(0, 1, self.problem.n_dims)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_group[cluster_id][location_id] = agent
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                pop_group[cluster_id][location_id] = self.get_better_solution([pos_new, target], self.pop_group[cluster_id][location_id])
+                agent.target = self.get_target(pos_new)
+                pop_group[cluster_id][location_id] = self.get_better_agent(agent, self.pop_group[cluster_id][location_id], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
             for idx in range(0, self.m_clusters):
-                pop_group[idx] = self.update_target_wrapper_population(pop_group[idx])
-                pop_group[idx] = self.greedy_selection_population(self.pop_group[idx], pop_group[idx])
-
+                pop_group[idx] = self.update_target_for_population(pop_group[idx])
+                pop_group[idx] = self.greedy_selection_population(self.pop_group[idx], pop_group[idx], self.problem.minmax)
         # Needed to update the centers and population
         self.centers = self.find_cluster__(pop_group)
         self.pop = []
