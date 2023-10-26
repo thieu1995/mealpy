5723991106ca288c0c93a502615f7e12e53d8dd9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-26 21:46:41 +0700

    Convert SARO

--- mealpy/human_based/SARO.py
+++ mealpy/human_based/SARO.py
@@ -8,7 +8,7 @@ import numpy as np
 from mealpy.optimizer import Optimizer
 
 
-class BaseSARO(Optimizer):
+class DevSARO(Optimizer):
     """
     The developed version: Search And Rescue Optimization (SARO)
 
@@ -19,28 +19,24 @@ class BaseSARO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.human_based.SARO import BaseSARO
+    >>> from mealpy import FloatVar, SARO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> se = 0.5
-    >>> mu = 50
-    >>> model = BaseSARO(epoch, pop_size, se, mu)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SARO.DevSARO(epoch=1000, pop_size=50, se = 0.5, mu = 50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
     """
 
-    def __init__(self, epoch=10000, pop_size=100, se=0.5, mu=15, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, se: float = 0.5, mu: int = 15, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -50,7 +46,7 @@ class BaseSARO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.se = self.validator.check_float("se", se, (0, 1.0))
         self.mu = self.validator.check_int("mu", mu, [2, 2+int(self.pop_size/2)])
         self.set_parameters(["epoch", "pop_size", "se", "mu"])
@@ -61,14 +57,14 @@ class BaseSARO(Optimizer):
 
     def initialization(self):
         if self.pop is None:
-            self.pop = self.create_population(2 * self.pop_size)
+            self.pop = self.generate_population(2 * self.pop_size)
         else:
-            self.pop = self.pop + self.create_population(self.pop_size)
+            self.pop = self.pop + self.generate_population(self.pop_size)
 
-    def bounded_position(self, position=None, lb=None, ub=None):
-        condition = np.logical_and(lb <= position, position <= ub)
-        rand_pos = np.random.uniform(lb, ub)
-        return np.where(condition, position, rand_pos)
+    def amend_solution(self, solution: np.ndarray) -> np.ndarray:
+        condition = np.logical_and(self.problem.lb <= solution, solution <= self.problem.ub)
+        rand_pos = self.generator.uniform(self.problem.lb, self.problem.ub)
+        return np.where(condition, solution, rand_pos)
 
     def evolve(self, epoch):
         """
@@ -77,60 +73,58 @@ class BaseSARO(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        pop_x = self.pop[:self.pop_size].copy()
-        pop_m = self.pop[self.pop_size:].copy()
-
+        pop_x = [agent.copy() for agent in self.pop[:self.pop_size]]
+        pop_m = [agent.copy() for agent in self.pop[self.pop_size:]]
         pop_new = []
         for idx in range(self.pop_size):
             ## Social Phase
-            k = np.random.choice(list(set(range(0, 2 * self.pop_size)) - {idx}))
-            sd = pop_x[idx][self.ID_POS] - self.pop[k][self.ID_POS]
-
+            k = self.generator.choice(list(set(range(0, 2 * self.pop_size)) - {idx}))
+            sd = pop_x[idx].solution - self.pop[k].solution
             #### Remove third loop here, also using random flight back when out of bound
-            pos_new_1 = self.pop[k][self.ID_POS] + np.random.uniform() * sd
-            pos_new_2 = pop_x[idx][self.ID_POS] + np.random.uniform() * sd
-            pos_new = np.where(np.logical_and(np.random.uniform(0, 1, self.problem.n_dims) < self.se,
-                                              self.pop[k][self.ID_TAR] < pop_x[idx][self.ID_TAR]), pos_new_1, pos_new_2)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new_1 = self.pop[k].solution + self.generator.uniform() * sd
+            pos_new_2 = pop_x[idx].solution + self.generator.uniform() * sd
+            condition = np.logical_and(self.generator.uniform(0, 1, self.problem.n_dims) < self.se, self.pop[k].target.fitness < pop_x[idx].target.fitness)
+            pos_new = np.where(condition, pos_new_1, pos_new_2)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         for idx in range(self.pop_size):
-            if self.compare_agent(pop_new[idx], pop_x[idx]):
-                pop_m[np.random.randint(0, self.pop_size)] = pop_x[idx].copy()
+            if self.compare_target(pop_new[idx].target, pop_x[idx].target, self.problem.minmax):
+                pop_m[self.generator.integers(0, self.pop_size)] = pop_x[idx].copy()
                 pop_x[idx] = pop_new[idx].copy()
                 self.dyn_USN[idx] = 0
             else:
                 self.dyn_USN[idx] += 1
-
         pop = pop_x.copy() + pop_m.copy()
         pop_new = []
         for idx in range(self.pop_size):
             ## Individual phase
-            k1, k2 = np.random.choice(list(set(range(0, 2 * self.pop_size)) - {idx}), 2, replace=False)
+            k1, k2 = self.generator.choice(list(set(range(0, 2 * self.pop_size)) - {idx}), 2, replace=False)
             #### Remove third loop here, and flight back strategy now be a random
-            pos_new = self.g_best[self.ID_POS] + np.random.uniform() * (pop[k1][self.ID_POS] - pop[k2][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.g_best.solution + self.generator.uniform() * (pop[k1].solution - pop[k2].solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         for idx in range(0, self.pop_size):
-            if self.compare_agent(pop_new[idx], pop_x[idx]):
-                pop_m[np.random.randint(0, self.pop_size)] = pop_x[idx].copy()
+            if self.compare_target(pop_new[idx].target, pop_x[idx].target, self.problem.minmax):
+                pop_m[self.generator.integers(0, self.pop_size)] = pop_x[idx].copy()
                 pop_x[idx] = pop_new[idx].copy()
                 self.dyn_USN[idx] = 0
             else:
                 self.dyn_USN[idx] += 1
-
             if self.dyn_USN[idx] > self.mu:
-                pop_x[idx] = self.create_solution(self.problem.lb, self.problem.ub)
+                pop_x[idx] = self.generate_agent()
                 self.dyn_USN[idx] = 0
         self.pop = pop_x + pop_m
 
 
-class OriginalSARO(BaseSARO):
+class OriginalSARO(DevSARO):
     """
     The original version of: Search And Rescue Optimization (SARO)
 
@@ -144,25 +138,21 @@ class OriginalSARO(BaseSARO):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.human_based.SARO import OriginalSARO
+    >>> from mealpy import FloatVar, SARO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> se = 0.5
-    >>> mu = 50
-    >>> model = OriginalSARO(epoch, pop_size, se, mu)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SARO.OriginalSARO(epoch=1000, pop_size=50, se = 0.5, mu = 50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -170,7 +160,7 @@ class OriginalSARO(BaseSARO):
     algorithm based on search and rescue operations. Mathematical Problems in Engineering, 2019.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, se=0.5, mu=15, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, se: float = 0.5, mu: int = 15, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -187,36 +177,36 @@ class OriginalSARO(BaseSARO):
         Args:
             epoch (int): The current iteration
         """
-        pop_x = self.pop[:self.pop_size].copy()
-        pop_m = self.pop[self.pop_size:].copy()
-
+        pop_x = [agent.copy() for agent in self.pop[:self.pop_size]]
+        pop_m = [agent.copy() for agent in self.pop[self.pop_size:]]
         pop_new = []
         for idx in range(self.pop_size):
             ## Social Phase
-            k = np.random.choice(list(set(range(0, 2 * self.pop_size)) - {idx}))
-            sd = pop_x[idx][self.ID_POS] - self.pop[k][self.ID_POS]
-            j_rand = np.random.randint(0, self.problem.n_dims)
-            r1 = np.random.uniform(-1, 1)
+            k = self.generator.choice(list(set(range(0, 2 * self.pop_size)) - {idx}))
+            sd = pop_x[idx].solution - self.pop[k].solution
+            j_rand = self.generator.integers(0, self.problem.n_dims)
+            r1 = self.generator.uniform(-1, 1)
 
-            pos_new = pop_x[idx][self.ID_POS].copy()
+            pos_new = pop_x[idx].solution.copy()
             for j in range(0, self.problem.n_dims):
-                if np.random.uniform() < self.se or j == j_rand:
-                    if self.compare_agent(self.pop[k], pop_x[idx]):
-                        pos_new[j] = self.pop[k][self.ID_POS][j] + r1 * sd[j]
+                if self.generator.uniform() < self.se or j == j_rand:
+                    if self.compare_target(self.pop[k].target, pop_x[idx].target, self.problem.minmax):
+                        pos_new[j] = self.pop[k].solution[j] + r1 * sd[j]
                     else:
-                        pos_new[j] = pop_x[idx][self.ID_POS][j] + r1 * sd[j]
+                        pos_new[j] = pop_x[idx].solution[j] + r1 * sd[j]
                 if pos_new[j] < self.problem.lb[j]:
-                    pos_new[j] = (pop_x[idx][self.ID_POS][j] + self.problem.lb[j]) / 2
+                    pos_new[j] = (pop_x[idx].solution[j] + self.problem.lb[j]) / 2
                 if pos_new[j] > self.problem.ub[j]:
-                    pos_new[j] = (pop_x[idx][self.ID_POS][j] + self.problem.ub[j]) / 2
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                    pos_new[j] = (pop_x[idx].solution[j] + self.problem.ub[j]) / 2
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         for idx in range(0, self.pop_size):
-            if self.compare_agent(pop_new[idx], pop_x[idx]):
-                pop_m[np.random.randint(0, self.pop_size)] = pop_x[idx].copy()
+            if self.compare_target(pop_new[idx].target, pop_x[idx].target, self.problem.minmax):
+                pop_m[self.generator.integers(0, self.pop_size)] = pop_x[idx].copy()
                 pop_x[idx] = pop_new[idx].copy()
                 self.dyn_USN[idx] = 0
             else:
@@ -226,27 +216,28 @@ class OriginalSARO(BaseSARO):
         pop = pop_x.copy() + pop_m.copy()
         pop_new = []
         for idx in range(0, self.pop_size):
-            k, m = np.random.choice(list(set(range(0, 2 * self.pop_size)) - {idx}), 2, replace=False)
-            pos_new = pop_x[idx][self.ID_POS] + np.random.uniform() * (pop[k][self.ID_POS] - pop[m][self.ID_POS])
+            k, m = self.generator.choice(list(set(range(0, 2 * self.pop_size)) - {idx}), 2, replace=False)
+            pos_new = pop_x[idx].solution + self.generator.uniform() * (pop[k].solution - pop[m].solution)
             for j in range(0, self.problem.n_dims):
                 if pos_new[j] < self.problem.lb[j]:
-                    pos_new[j] = (pop_x[idx][self.ID_POS][j] + self.problem.lb[j]) / 2
+                    pos_new[j] = (pop_x[idx].solution[j] + self.problem.lb[j]) / 2
                 if pos_new[j] > self.problem.ub[j]:
-                    pos_new[j] = (pop_x[idx][self.ID_POS][j] + self.problem.ub[j]) / 2
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                    pos_new[j] = (pop_x[idx].solution[j] + self.problem.ub[j]) / 2
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         for idx in range(0, self.pop_size):
-            if self.compare_agent(pop_new[idx], pop_x[idx]):
-                pop_m[np.random.randint(0, self.pop_size)] = pop_x[idx]
+            if self.compare_target(pop_new[idx].target, pop_x[idx].target, self.problem.minmax):
+                pop_m[self.generator.integers(0, self.pop_size)] = pop_x[idx]
                 pop_x[idx] = pop_new[idx].copy()
                 self.dyn_USN[idx] = 0
             else:
                 self.dyn_USN[idx] += 1
 
             if self.dyn_USN[idx] > self.mu:
-                pop_x[idx] = self.create_solution(self.problem.lb, self.problem.ub)
+                pop_x[idx] = self.generate_agent()
                 self.dyn_USN[idx] = 0
         self.pop = pop_x + pop_m
