c7a7ff4690ffcc7f22de21a591440e489a6c077d
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-27 00:03:59 +0700

    Convert CDO

--- mealpy/physics_based/CDO.py
+++ mealpy/physics_based/CDO.py
@@ -19,23 +19,21 @@ class OriginalCDO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.physics_based.CDO import OriginalCDO
+    >>> from mealpy import FloatVar, CDO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalCDO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CDO.OriginalCDO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -43,7 +41,7 @@ class OriginalCDO(Optimizer):
     for global optimization. Neural Computing and Applications, 1-17.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -51,7 +49,7 @@ class OriginalCDO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -62,39 +60,40 @@ class OriginalCDO(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        _, (b1, b2, b3), _ = self.get_special_solutions(self.pop, best=3, worst=1)
-        a = 3 - (epoch+1)*3/self.epoch
-        a1 = np.log10((16000-1) * np.random.rand()+16000)
-        a2 = np.log10((270000-1)*np.random.rand() + 270000)
-        a3 = np.log10((300000-1)*np.random.rand() + 300000)
+        _, (b1, b2, b3), _ = self.get_special_agents(self.pop, n_best=3, n_worst=1)
+        a = 3. - 3.*epoch/self.epoch
+        a1 = np.log10((16000-1) * self.generator.random() + 16000)
+        a2 = np.log10((270000-1) * self.generator.random() + 270000)
+        a3 = np.log10((300000-1) * self.generator.random() + 300000)
         pop_new = []
         for idx in range(0, self.pop_size):
-            r1 = np.random.rand(self.problem.n_dims)
-            r2 = np.random.rand(self.problem.n_dims)
-            pa = np.pi * r1*r1 / (0.25 * a1) - a*np.random.rand(self.problem.n_dims)
+            r1 = self.generator.random(self.problem.n_dims)
+            r2 = self.generator.random(self.problem.n_dims)
+            pa = np.pi * r1*r1 / (0.25 * a1) - a * self.generator.random(self.problem.n_dims)
             c1 = r2 * r2 * np.pi
-            alpha = np.abs(c1*b1[self.ID_POS] - self.pop[idx][self.ID_POS])
-            pos_a = 0.25 * (b1[self.ID_POS] - pa * alpha)
+            alpha = np.abs(c1*b1.solution - self.pop[idx].solution)
+            pos_a = 0.25 * (b1.solution - pa * alpha)
 
-            r3 = np.random.rand(self.problem.n_dims)
-            r4 = np.random.rand(self.problem.n_dims)
-            pb = np.pi * r3 * r3 / (0.5 * a2) - a * np.random.rand(self.problem.n_dims)
+            r3 = self.generator.random(self.problem.n_dims)
+            r4 = self.generator.random(self.problem.n_dims)
+            pb = np.pi * r3 * r3 / (0.5 * a2) - a * self.generator.random(self.problem.n_dims)
             c2 = r4 * r4 * np.pi
-            beta = np.abs(c2 * b2[self.ID_POS] - self.pop[idx][self.ID_POS])
-            pos_b = 0.5 * (b2[self.ID_POS] - pb * beta)
+            beta = np.abs(c2 * b2.solution - self.pop[idx].solution)
+            pos_b = 0.5 * (b2.solution - pb * beta)
 
-            r5 = np.random.rand(self.problem.n_dims)
-            r6 = np.random.rand(self.problem.n_dims)
-            pc = np.pi * r5 * r5 / a3 - a * np.random.rand(self.problem.n_dims)
+            r5 = self.generator.random(self.problem.n_dims)
+            r6 = self.generator.random(self.problem.n_dims)
+            pc = np.pi * r5 * r5 / a3 - a * self.generator.random(self.problem.n_dims)
             c3 = r6 * r6 * np.pi
-            gama = np.abs(c3 * b3[self.ID_POS] - self.pop[idx][self.ID_POS])
-            pos_c = b3[self.ID_POS] - pc * gama
+            gama = np.abs(c3 * b3.solution - self.pop[idx].solution)
+            pos_c = b3.solution - pc * gama
 
             pos_new = (pos_a + pos_b + pos_c) / 3
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                pop_new[-1].target = self.get_target(pos_new)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
+            pop_new = self.update_target_for_population(pop_new)
         self.pop = pop_new
