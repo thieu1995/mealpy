0f6e64f069970d6c7df470e204cb59e3c2635975
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-26 18:02:43 +0700

    Convert SSDO

--- mealpy/human_based/SSDO.py
+++ mealpy/human_based/SSDO.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 from mealpy.optimizer import Optimizer
+from mealpy.utils.agent import Agent
 
 
 class OriginalSSDO(Optimizer):
@@ -19,23 +20,21 @@ class OriginalSSDO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.human_based.SSDO import OriginalSSDO
+    >>> from mealpy import FloatVar, SSDO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalSSDO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SSDO.OriginalSSDO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -43,10 +42,7 @@ class OriginalSSDO(Optimizer):
     data using social ski driver algorithm. Neural Computing and Applications, 32(11), pp.6925-6938.
     """
 
-    ID_VEL = 2
-    ID_LOC = 3
-
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -54,24 +50,17 @@ class OriginalSSDO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
-    def create_solution(self, lb=None, ub=None, pos=None):
-        """
-        Overriding method in Optimizer class
-
-        Returns:
-            list: wrapper of solution with format [position, target, velocity, best_local_position]
-        """
-        if pos is None:
-            pos = self.generate_position(lb, ub)
-        position = self.amend_position(pos, lb, ub)
-        target = self.get_target_wrapper(position)
-        velocity = np.random.uniform(lb, ub)
-        pos_local = position.copy()
-        return [position, target, velocity, pos_local]
+    def generate_agent(self, solution: np.ndarray = None) -> Agent:
+        if solution is None:
+            solution = self.problem.generate_solution(encoded=True)
+        target = self.get_target(solution)
+        velocity = self.generator.uniform(self.problem.lb, self.problem.ub)
+        pos_local = solution.copy()
+        return Agent(solution=solution, target=target, velocity=velocity, local_solution=pos_local)
 
     def evolve(self, epoch):
         """
@@ -81,32 +70,28 @@ class OriginalSSDO(Optimizer):
             epoch (int): The current iteration
         """
         c = 2 - epoch * (2.0 / self.epoch)  # a decreases linearly from 2 to 0
-
         ## Calculate the mean of the best three solutions in each dimension. Eq 9
-        _, pop_best3, _ = self.get_special_solutions(self.pop, best=3)
-        pos_mean = np.mean(np.array([item[self.ID_POS] for item in pop_best3]))
-
-        pop_new = self.pop.copy()
+        _, pop_best3, _ = self.get_special_agents(self.pop, n_best=3, minmax=self.problem.minmax)
+        pos_mean = np.mean(np.array([agent.solution for agent in pop_best3]))
+        pop_new = [agent.copy() for agent in self.pop]
         # Updating velocity vectors
-        r1 = np.random.uniform()  # r1, r2 is a random number in [0,1]
-        r2 = np.random.uniform()
+        r1 = self.generator.uniform()  # r1, r2 is a random number in [0,1]
+        r2 = self.generator.uniform()
         for i in range(0, self.pop_size):
             if r2 <= 0.5:  ## Use Sine function to move
-                vel_new = c * np.sin(r1) * (self.pop[i][self.ID_LOC] - self.pop[i][self.ID_POS]) + (2-c)*np.sin(r1) * (pos_mean - self.pop[i][self.ID_POS])
+                vel_new = c * np.sin(r1) * (self.pop[i].local_solution - self.pop[i].solution) + (2-c)*np.sin(r1) * (pos_mean - self.pop[i].solution)
             else:  ## Use Cosine function to move
-                vel_new = c * np.cos(r1) * (self.pop[i][self.ID_LOC] - self.pop[i][self.ID_POS]) + (2-c)*np.cos(r1) * (pos_mean - self.pop[i][self.ID_POS])
-            pop_new[i][self.ID_VEL] = vel_new
-
+                vel_new = c * np.cos(r1) * (self.pop[i].local_solution - self.pop[i].solution) + (2-c)*np.cos(r1) * (pos_mean - self.pop[i].solution)
+            pop_new[i].velocity = vel_new
         ## Reproduction
         for idx in range(0, self.pop_size):
-            pos_new = np.random.normal(0, 1, self.problem.n_dims) * pop_new[idx][self.ID_POS] + np.random.rand() * pop_new[idx][self.ID_VEL]
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new[idx][self.ID_LOC] = self.pop[idx][self.ID_POS]
-            pop_new[idx][self.ID_POS] = pos_new
+            pos_new = self.generator.normal(0, 1, self.problem.n_dims) * pop_new[idx].solution + self.generator.random() * pop_new[idx].velocity
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            agent.local_solution = self.pop[idx].solution.copy()
             if self.mode not in self.AVAILABLE_MODES:
-                old = pop_new[idx].copy()
-                old[self.ID_TAR] = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution(pop_new[idx], old)
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, pop_new[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
