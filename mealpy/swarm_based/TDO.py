7751335e33df936fd8b6c72b235ff2932844b390
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-27 23:16:26 +0700

    Convert TDO

--- mealpy/swarm_based/TDO.py
+++ mealpy/swarm_based/TDO.py
@@ -25,30 +25,28 @@ class OriginalTDO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.TDO import OriginalTDO
+    >>> from mealpy import FloatVar, TDO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalTDO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = TDO.OriginalTDO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] Dehghani, M., Hubálovský, Š., & Trojovský, P. (2022). Tasmanian devil optimization: a new bio-inspired
     optimization algorithm for solving optimization algorithm. IEEE Access, 10, 19599-19620.
     """
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -56,9 +54,9 @@ class OriginalTDO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
-        self.support_parallel_modes = False
+        self.is_parallelizable = False
         self.sort_flag = False
 
     def evolve(self, epoch):
@@ -70,35 +68,35 @@ class OriginalTDO(Optimizer):
         """
         for idx in range(0, self.pop_size):
             # PHASE1: Hunting Feeding
-            if np.random.rand() > 0.5:
+            if self.generator.random() > 0.5:
                 # STRATEGY 1: FEEDING BY EATING CARRION (EXPLORATION PHASE)
                 # CARRION selection using (3)
-                kk = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
-                if self.compare_agent(self.pop[kk], self.pop[idx]):
-                    pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * (self.pop[kk][self.ID_POS] - np.random.randint(1, 3)*self.pop[idx][self.ID_POS])
+                kk = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
+                if self.compare_target(self.pop[kk].target, self.pop[idx].target, self.problem.minmax):
+                    pos_new = self.pop[idx].solution + self.generator.random(self.problem.n_dims) * (self.pop[kk].solution - self.generator.integers(1, 3)*self.pop[idx].solution)
                 else:
-                    pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * (self.pop[idx][self.ID_POS] - self.pop[kk][self.ID_POS])
-                pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-                tar_new = self.get_target_wrapper(pos_new)
-                if self.compare_agent([pos_new, tar_new], self.pop[idx]):
-                    self.pop[idx] = [pos_new, tar_new]
+                    pos_new = self.pop[idx].solution + self.generator.random(self.problem.n_dims) * (self.pop[idx].solution - self.pop[kk].solution)
+                pos_new = self.correct_solution(pos_new)
+                agent = self.generate_agent(pos_new)
+                if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                    self.pop[idx] = agent
             else:
             # STRATEGY 2: FEEDING BY EATING PREY (EXPLOITATION PHASE)
             # stage1: prey selection and attack it
-                kk = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
-                if self.compare_agent(self.pop[kk], self.pop[idx]):
-                    pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * (self.pop[kk][self.ID_POS] - np.random.randint(1, 3) * self.pop[idx][self.ID_POS])
+                kk = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
+                if self.compare_target(self.pop[kk].target, self.pop[idx].target, self.problem.minmax):
+                    pos_new = self.pop[idx].solution + self.generator.random(self.problem.n_dims) * (self.pop[kk].solution - self.generator.integers(1, 3) * self.pop[idx].solution)
                 else:
-                    pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * (self.pop[idx][self.ID_POS] - self.pop[kk][self.ID_POS])
-                pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-                tar_new = self.get_target_wrapper(pos_new)
-                if self.compare_agent([pos_new, tar_new], self.pop[idx]):
-                    self.pop[idx] = [pos_new, tar_new]
+                    pos_new = self.pop[idx].solution + self.generator.random(self.problem.n_dims) * (self.pop[idx].solution - self.pop[kk].solution)
+                pos_new = self.correct_solution(pos_new)
+                agent = self.generate_agent(pos_new)
+                if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                    self.pop[idx] = agent
 
             # stage2: prey chasing
-            rr = 0.01 * (1 - (epoch+1)/self.epoch)      # Calculating the neighborhood radius using(9)
-            pos_new = self.pop[idx][self.ID_POS] + (-rr + 2 * rr * np.random.rand(self.problem.n_dims)) * self.pop[idx][self.ID_POS]
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            tar_new = self.get_target_wrapper(pos_new)
-            if self.compare_agent([pos_new, tar_new], self.pop[idx]):
-                self.pop[idx] = [pos_new, tar_new]
+            rr = 0.01 * (1 - epoch/self.epoch)      # Calculating the neighborhood radius using(9)
+            pos_new = self.pop[idx].solution + (-rr + 2 * rr * self.generator.random(self.problem.n_dims)) * self.pop[idx].solution
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_agent(pos_new)
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
