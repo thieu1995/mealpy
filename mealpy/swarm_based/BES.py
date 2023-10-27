5a0f2871bf7f6d3124fb2ea2aa631953e6357316
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-27 16:49:52 +0700

    Convert BES

--- mealpy/swarm_based/BES.py
+++ mealpy/swarm_based/BES.py
@@ -25,28 +25,21 @@ class OriginalBES(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.BES import OriginalBES
+    >>> from mealpy import FloatVar, BES
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> a_factor = 10
-    >>> R_factor = 1.5
-    >>> alpha = 2.0
-    >>> c1 = 2.0
-    >>> c2 = 2.0
-    >>> model = OriginalBES(epoch, pop_size, a_factor, R_factor, alpha, c1, c2)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = BES.OriginalBES(epoch=1000, pop_size=50, a_factor = 10, R_factor = 1.5, alpha = 2.0, c1 = 2.0, c2 = 2.0)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -54,7 +47,8 @@ class OriginalBES(Optimizer):
     search optimisation algorithm. Artificial Intelligence Review, 53(3), pp.2237-2264.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, a_factor=10, R_factor=1.5, alpha=2.0, c1=2.0, c2=2.0, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, a_factor: int = 10, R_factor: float = 1.5,
+                 alpha: float = 2.0, c1: float = 2.0, c2: float = 2.0, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -79,14 +73,12 @@ class OriginalBES(Optimizer):
     def create_x_y_x1_y1__(self):
         """ Using numpy vector for faster computational time """
         ## Eq. 2
-        phi = self.a_factor * np.pi * np.random.uniform(0, 1, self.pop_size)
-        r = phi + self.R_factor * np.random.uniform(0, 1, self.pop_size)
+        phi = self.a_factor * np.pi * self.generator.uniform(0, 1, self.pop_size)
+        r = phi + self.R_factor * self.generator.uniform(0, 1, self.pop_size)
         xr, yr = r * np.sin(phi), r * np.cos(phi)
-
         ## Eq. 3
-        r1 = phi1 = self.a_factor * np.pi * np.random.uniform(0, 1, self.pop_size)
+        r1 = phi1 = self.a_factor * np.pi * self.generator.uniform(0, 1, self.pop_size)
         xr1, yr1 = r1 * np.sinh(phi1), r1 * np.cosh(phi1)
-
         x_list = xr / np.max(xr)
         y_list = yr / np.max(yr)
         x1_list = xr1 / np.max(xr1)
@@ -105,51 +97,53 @@ class OriginalBES(Optimizer):
 
         # Three parts: selecting the search space, searching within the selected search space and swooping.
         ## 1. Select space
-        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_list = np.array([agent.solution for agent in self.pop])
         pos_mean = np.mean(pos_list, axis=0)
 
         pop_new = []
         for idx in range(0, self.pop_size):
-            pos_new = self.g_best[self.ID_POS] + self.alpha * np.random.uniform() * (pos_mean - self.pop[idx][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.g_best.solution + self.alpha * self.generator.uniform() * (pos_mean - self.pop[idx].solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
 
         ## 2. Search in space
-        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_list = np.array([agent.solution for agent in self.pop])
         pos_mean = np.mean(pos_list, axis=0)
-
         pop_child = []
         for idx in range(0, self.pop_size):
-            idx_rand = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
-            pos_new = self.pop[idx][self.ID_POS] + y_list[idx] * (self.pop[idx][self.ID_POS] - self.pop[idx_rand][self.ID_POS]) + \
-                      x_list[idx] * (self.pop[idx][self.ID_POS] - pos_mean)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_child.append([pos_new, None])
+            idx_rand = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
+            pos_new = self.pop[idx].solution + y_list[idx] * (self.pop[idx].solution - self.pop[idx_rand].solution) + \
+                      x_list[idx] * (self.pop[idx].solution - pos_mean)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_child.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_child = self.update_target_wrapper_population(pop_child)
-            self.pop = self.greedy_selection_population(self.pop, pop_child)
+            pop_child = self.update_target_for_population(pop_child)
+            self.pop = self.greedy_selection_population(self.pop, pop_child, self.problem.minmax)
 
         ## 3. Swoop
-        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_list = np.array([agent.solution for agent in self.pop])
         pos_mean = np.mean(pos_list, axis=0)
         pop_new = []
         for idx in range(0, self.pop_size):
-            pos_new = np.random.uniform() * self.g_best[self.ID_POS] + x1_list[idx] * (self.pop[idx][self.ID_POS] - self.c1 * pos_mean) \
-                      + y1_list[idx] * (self.pop[idx][self.ID_POS] - self.c2 * self.g_best[self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.generator.uniform() * self.g_best.solution + x1_list[idx] * (self.pop[idx].solution - self.c1 * pos_mean) \
+                      + y1_list[idx] * (self.pop[idx].solution - self.c2 * self.g_best.solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
