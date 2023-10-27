57554ca8dd8de5429503dbcca672581a4ffd26fb
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-27 22:55:54 +0700

    Convert WOA

--- mealpy/swarm_based/WOA.py
+++ mealpy/swarm_based/WOA.py
@@ -18,31 +18,27 @@ class OriginalWOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.WOA import OriginalWOA
+    >>> from mealpy import FloatVar, WOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalWOA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = WOA.OriginalWOA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
-    [1] Mirjalili, S. and Lewis, A., 2016. The whale optimization algorithm.
-    Advances in engineering software, 95, pp.51-67.
+    [1] Mirjalili, S. and Lewis, A., 2016. The whale optimization algorithm. Advances in engineering software, 95, pp.51-67.
     """
-
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -50,7 +46,7 @@ class OriginalWOA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -61,36 +57,36 @@ class OriginalWOA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        a = 2 - 2 * epoch / (self.epoch - 1)  # linearly decreased from 2 to 0
+        a = 2 - 2 * epoch / self.epoch  # linearly decreased from 2 to 0
         pop_new = []
         for idx in range(0, self.pop_size):
-            r = np.random.rand()
+            r = self.generator.random()
             A = 2 * a * r - a
             C = 2 * r
-            l = np.random.uniform(-1, 1)
+            l = self.generator.uniform(-1, 1)
             p = 0.5
             b = 1
-            if np.random.uniform() < p:
+            if self.generator.uniform() < p:
                 if np.abs(A) < 1:
-                    D = np.abs(C * self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = self.g_best[self.ID_POS] - A * D
+                    D = np.abs(C * self.g_best.solution - self.pop[idx].solution)
+                    pos_new = self.g_best.solution - A * D
                 else:
-                    # x_rand = pop[np.random.np.random.randint(self.pop_size)]         # select random 1 position in pop
-                    x_rand = self.create_solution(self.problem.lb, self.problem.ub)
-                    D = np.abs(C * x_rand[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = x_rand[self.ID_POS] - A * D
+                    # x_rand = pop[self.generator.self.generator.randint(self.pop_size)]         # select random 1 position in pop
+                    x_rand = self.problem.generate_solution()
+                    D = np.abs(C * x_rand - self.pop[idx].solution)
+                    pos_new = x_rand - A * D
             else:
-                D1 = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                pos_new = self.g_best[self.ID_POS] + np.exp(b * l) * np.cos(2 * np.pi * l) * D1
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                D1 = np.abs(self.g_best.solution - self.pop[idx].solution)
+                pos_new = self.g_best.solution + np.exp(b * l) * np.cos(2 * np.pi * l) * D1
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution(self.pop[idx], [pos_new, target])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            print(len(pop_new))
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
 
 
 class HI_WOA(Optimizer):
@@ -106,32 +102,28 @@ class HI_WOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.WOA import HI_WOA
+    >>> from mealpy import FloatVar, WOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> feedback_max = 10
-    >>> model = HI_WOA(epoch, pop_size, feedback_max)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = WOA.HI_WOA(epoch=1000, pop_size=50, feedback_max = 10)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] Tang, C., Sun, W., Wu, W. and Xue, M., 2019, July. A hybrid improved whale optimization algorithm.
     In 2019 IEEE 15th International Conference on Control and Automation (ICCA) (pp. 362-367). IEEE.
     """
-
-    def __init__(self, epoch=10000, pop_size=100, feedback_max=10, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, feedback_max: int = 10, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -140,7 +132,7 @@ class HI_WOA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.feedback_max = self.validator.check_int("feedback_max", feedback_max, [2, 2+int(self.epoch/2)])
         # The maximum of times g_best doesn't change -> need to change half of population
         self.set_parameters(["epoch", "pop_size", "feedback_max"])
@@ -160,42 +152,43 @@ class HI_WOA(Optimizer):
         a = 2 + 2 * np.cos(np.pi / 2 * (1 + epoch / self.epoch))  # Eq. 8
         pop_new = []
         for idx in range(0, self.pop_size):
-            r = np.random.rand()
+            r = self.generator.random()
             A = 2 * a * r - a
             C = 2 * r
-            l = np.random.uniform(-1, 1)
+            l = self.generator.uniform(-1, 1)
             p = 0.5
             b = 1
-            if np.random.uniform() < p:
+            if self.generator.uniform() < p:
                 if np.abs(A) < 1:
-                    D = np.abs(C * self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = self.g_best[self.ID_POS] - A * D
+                    D = np.abs(C * self.g_best.solution - self.pop[idx].solution)
+                    pos_new = self.g_best.solution - A * D
                 else:
-                    # x_rand = pop[np.random.np.random.randint(self.pop_size)]         # select random 1 position in pop
-                    x_rand = self.create_solution(self.problem.lb, self.problem.ub)
-                    D = np.abs(C * x_rand[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = x_rand[self.ID_POS] - A * D
+                    # x_rand = pop[self.generator.self.generator.randint(self.pop_size)]         # select random 1 position in pop
+                    x_rand = self.problem.generate_solution()
+                    D = np.abs(C * x_rand - self.pop[idx].solution)
+                    pos_new = x_rand - A * D
             else:
-                D1 = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                pos_new = self.g_best[self.ID_POS] + np.exp(b * l) * np.cos(2 * np.pi * l) * D1
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                D1 = np.abs(self.g_best.solution - self.pop[idx].solution)
+                pos_new = self.g_best.solution + np.exp(b * l) * np.cos(2 * np.pi * l) * D1
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution(self.pop[idx], [pos_new, target])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
 
         ## Feedback Mechanism
-        _, current_best = self.get_global_best_solution(self.pop)
-        if current_best[self.ID_TAR][self.ID_FIT] == self.g_best[self.ID_TAR][self.ID_FIT]:
+        current_best = self.get_best_agent(self.pop, self.problem.minmax)
+        if current_best.target.fitness == self.g_best.target.fitness:
             self.dyn_feedback_count += 1
         else:
             self.dyn_feedback_count = 0
 
         if self.dyn_feedback_count >= self.feedback_max:
-            idx_list = np.random.choice(range(0, self.pop_size), self.n_changes, replace=False)
-            pop_child = self.create_population(self.n_changes)
+            idx_list = self.generator.choice(range(0, self.pop_size), self.n_changes, replace=False)
+            pop_child = self.generate_population(self.n_changes)
             for idx_counter, idx in enumerate(idx_list):
                 self.pop[idx] = pop_child[idx_counter]
