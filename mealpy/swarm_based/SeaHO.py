5dbddf432d3cc86a77b69df94d532c2876afc3f6
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-29 18:35:00 +0700

    Convert SeaHO

--- mealpy/swarm_based/SeaHO.py
+++ mealpy/swarm_based/SeaHO.py
@@ -19,23 +19,21 @@ class OriginalSeaHO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.SeaHO import OriginalSeaHO
+    >>> from mealpy import FloatVar, SeaHO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalSeaHO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = SeaHO.OriginalSeaHO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -43,7 +41,7 @@ class OriginalSeaHO(Optimizer):
     meta-heuristic for global optimization problems. Applied Intelligence, 1-28.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -51,7 +49,7 @@ class OriginalSeaHO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -71,49 +69,48 @@ class OriginalSeaHO(Optimizer):
         step_length = self.get_levy_flight_step(beta=1.5, multiplier=0.01, size=(self.pop_size, self.problem.n_dims), case=-1)
         pop_new = []
         for idx in range(0, self.pop_size):
-            beta = np.random.normal(0, 1, self.problem.n_dims)
-            theta = 2 * np.pi * np.random.rand(self.problem.n_dims)
+            beta = self.generator.normal(0, 1, self.problem.n_dims)
+            theta = 2 * np.pi * self.generator.random(self.problem.n_dims)
             row = self.uu * np.exp(theta * self.vv)
             xx, yy, zz = row * np.cos(theta), row * np.sin(theta), row * theta
-            if np.random.normal(0, 1) > 0:      # Eq. 4
-                pos_new = self.pop[idx][self.ID_POS] + step_length[idx] * ((self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]) * xx * yy * zz + self.g_best[self.ID_POS])
+            if self.generator.normal(0, 1) > 0:      # Eq. 4
+                pos_new = self.pop[idx].solution + step_length[idx] * ((self.g_best.solution - self.pop[idx].solution) * xx * yy * zz + self.g_best.solution)
             else:                               # Eq. 7
-                pos_new = self.pop[idx][self.ID_POS] + np.random.rand(self.problem.n_dims) * self.ll * beta * (self.g_best[self.ID_POS] - beta * self.g_best[self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                pos_new = self.pop[idx].solution + self.generator.random(self.problem.n_dims) * self.ll * beta * (self.g_best.solution - beta * self.g_best.solution)
+            pos_new = self.correct_solution(pos_new)
+            pop_new.append(pos_new)
 
         # The predation behavior of sea horses
         pop_child = []
-        alpha = (1 - (epoch+1)/self.epoch) ** (2 * (epoch+1) / self.epoch)
+        alpha = (1 - epoch/self.epoch) ** (2 * epoch / self.epoch)
         for idx in range(0, self.pop_size):
-            r1 = np.random.rand(self.problem.n_dims)
-            if np.random.rand() >= 0.1:
-                pos_new = alpha * (self.g_best[self.ID_POS] - np.random.rand(self.problem.n_dims) * pop_new[idx][self.ID_POS]) + \
-                          (1 - alpha) * self.g_best[self.ID_POS]        # Eq. 10
+            r1 = self.generator.random(self.problem.n_dims)
+            if self.generator.random() >= 0.1:
+                pos_new = alpha * (self.g_best.solution - r1 * pop_new[idx]) + (1 - alpha) * self.g_best.solution        # Eq. 10
             else:
-                pos_new = (1 - alpha) * (pop_new[idx][self.ID_POS] - np.random.rand(self.problem.n_dims) * self.g_best[self.ID_POS])  + \
-                        alpha * pop_new[idx][self.ID_POS]               # Eq. 11
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_child.append([pos_new, None])
+                pos_new = (1 - alpha) * (pop_new[idx] - r1 * self.g_best.solution) + alpha * pop_new[idx]               # Eq. 11
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_child.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_child[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                pop_child[-1].target = self.get_target(pos_new)
         if self.mode in self.AVAILABLE_MODES:
-            pop_child = self.update_target_wrapper_population(pop_child)
-        pop_child, _ = self.get_global_best_solution(pop_child)         # Sorted population
+            pop_child = self.update_target_for_population(pop_child)
+        pop_child = self.get_sorted_population(pop_child)         # Sorted population
 
         # The reproductive behavior of sea horses
         dads = pop_child[:int(self.pop_size/2)]
         moms = pop_child[int(self.pop_size/2):]
         pop_offspring = []
         for kdx in range(0, int(self.pop_size/2)):
-            r3 = np.random.rand()
-            pos_new = r3 * dads[kdx][self.ID_POS] + (1 - r3) * moms[kdx][self.ID_POS]       # Eq. 13
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_offspring.append([pos_new, None])
+            r3 = self.generator.random()
+            pos_new = r3 * dads[kdx].solution + (1 - r3) * moms[kdx].solution           # Eq. 13
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_offspring.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_offspring[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                pop_offspring[-1].target = self.get_target(pos_new)
         if self.mode in self.AVAILABLE_MODES:
-            pop_offspring = self.update_target_wrapper_population(pop_offspring)
-
+            pop_offspring = self.update_target_for_population(pop_offspring)
         # Sea horses selection
-        self.pop = self.get_sorted_strim_population(pop_child + pop_offspring, self.pop_size)
+        self.pop = self.get_sorted_and_trimmed_population(pop_child + pop_offspring, self.pop_size, self.problem.minmax)
