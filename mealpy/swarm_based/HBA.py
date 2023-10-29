304df69eaae5db0926f513f191c043e4bdd74963
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-29 21:50:36 +0700

    Convert HBA

--- mealpy/swarm_based/HBA.py
+++ mealpy/swarm_based/HBA.py
@@ -19,23 +19,21 @@ class OriginalHBA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.HBA import OriginalHBA
+    >>> from mealpy import FloatVar, HBA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalHBA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = HBA.OriginalHBA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -43,7 +41,7 @@ class OriginalHBA(Optimizer):
     algorithm for solving optimization problems. Mathematics and Computers in Simulation, 192, 84-110.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -51,7 +49,7 @@ class OriginalHBA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -64,12 +62,12 @@ class OriginalHBA(Optimizer):
         di = np.zeros(size)
         si = np.zeros(size)
         for idx in range(0, size):
-            di[idx] = (np.linalg.norm(pop[idx][self.ID_POS] - best[self.ID_POS]) + self.EPSILON) ** 2
+            di[idx] = (np.linalg.norm(pop[idx].solution - best.solution) + self.EPSILON) ** 2
             if idx == size - 1:
-                si[idx] = (np.linalg.norm(pop[idx][self.ID_POS] - self.pop[0][self.ID_POS]) + self.EPSILON) ** 2
+                si[idx] = (np.linalg.norm(pop[idx].solution - self.pop[0].solution) + self.EPSILON) ** 2
             else:
-                si[idx] = (np.linalg.norm(pop[idx][self.ID_POS] - self.pop[idx + 1][self.ID_POS]) + self.EPSILON) ** 2
-        r2 = np.random.rand(size)
+                si[idx] = (np.linalg.norm(pop[idx].solution - self.pop[idx + 1].solution) + self.EPSILON) ** 2
+        r2 = self.generator.random(size)
         return r2 * si / (4 * np.pi * di)
 
     def evolve(self, epoch):
@@ -79,29 +77,28 @@ class OriginalHBA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        t = self.epoch + 1
-        alpha= self.C * np.exp(-t/self.epoch)   # density factor in Eq. (3)
+        tt = self.epoch
+        alpha= self.C * np.exp(-tt/self.epoch)   # density factor in Eq. (3)
         I = self.get_intensity__(self.g_best, self.pop)        # intensity in Eq. (2)
-
         pop_new = []
         for idx in range(0, self.pop_size):
-            r = np.random.rand()
-            F = np.random.choice([1, -1])
-            di = self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS]
-            r3 = np.random.rand(self.problem.n_dims)
-            r4 = np.random.rand(self.problem.n_dims)
-            r5 = np.random.rand(self.problem.n_dims)
-            r6 = np.random.rand(self.problem.n_dims)
-            r7 = np.random.rand(self.problem.n_dims)
-            temp1 = self.g_best[self.ID_POS] + F * self.beta * I[idx] * self.g_best[self.ID_POS] + \
-                F*r3*alpha*di*np.abs(np.cos(2*np.pi*r4) * (1 - np.cos(2*np.pi*r5)))
-            temp2 = self.g_best[self.ID_POS] + F * r7 * alpha * di
+            r = self.generator.random()
+            F = self.generator.choice([1, -1])
+            di = self.g_best.solution - self.pop[idx].solution
+            r3 = self.generator.random(self.problem.n_dims)
+            r4 = self.generator.random(self.problem.n_dims)
+            r5 = self.generator.random(self.problem.n_dims)
+            r6 = self.generator.random(self.problem.n_dims)
+            r7 = self.generator.random(self.problem.n_dims)
+            temp1 = self.g_best.solution + F * self.beta * I[idx] * self.g_best.solution + F*r3*alpha*di*np.abs(np.cos(2*np.pi*r4) * (1 - np.cos(2*np.pi*r5)))
+            temp2 = self.g_best.solution + F * r7 * alpha * di
             pos_new = np.where(r6 < 0.5, temp1, temp2)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
