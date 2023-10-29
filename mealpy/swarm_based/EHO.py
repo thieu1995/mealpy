c6f200242736b3ffb9e4c847d63fa4017d93f70a
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-30 00:15:49 +0700

    Convert EHO

--- mealpy/swarm_based/EHO.py
+++ mealpy/swarm_based/EHO.py
@@ -23,26 +23,21 @@ class OriginalEHO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.EHO import OriginalEHO
+    >>> from mealpy import FloatVar, EHO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> alpha = 0.5
-    >>> beta = 0.5
-    >>> n_clans = 5
-    >>> model = OriginalEHO(epoch, pop_size, alpha, beta, n_clans)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = EHO.OriginalEHO(epoch=1000, pop_size=50, alpha = 0.5, beta = 0.5, n_clans = 5)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -50,7 +45,7 @@ class OriginalEHO(Optimizer):
     In 2015 3rd international symposium on computational and business intelligence (ISCBI) (pp. 1-5). IEEE.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, alpha=0.5, beta=0.5, n_clans=5, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, alpha: float = 0.5, beta: float = 0.5, n_clans: int = 5, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -61,7 +56,7 @@ class OriginalEHO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.alpha = self.validator.check_float("alpha", alpha, (0, 3.0))
         self.beta = self.validator.check_float("beta", beta, (0, 1.0))
         self.n_clans = self.validator.check_int("n_clans", n_clans, [2, int(self.pop_size/5)])
@@ -71,8 +66,8 @@ class OriginalEHO(Optimizer):
 
     def initialization(self):
         if self.pop is None:
-            self.pop = self.create_population(self.pop_size)
-        self.pop_group = self.create_pop_group(self.pop, self.n_clans, self.n_individuals)
+            self.pop = self.generate_population(self.pop_size)
+        self.pop_group = self.generate_group_population(self.pop, self.n_clans, self.n_individuals)
 
     def evolve(self, epoch):
         """
@@ -83,27 +78,27 @@ class OriginalEHO(Optimizer):
         """
         # Clan updating operator
         pop_new = []
-        for i in range(0, self.pop_size):
-            clan_idx = int(i / self.n_individuals)
-            pos_clan_idx = int(i % self.n_individuals)
-
+        for idx in range(0, self.pop_size):
+            clan_idx = int(idx / self.n_individuals)
+            pos_clan_idx = int(idx % self.n_individuals)
             if pos_clan_idx == 0:  # The best in clan, because all clans are sorted based on fitness
-                center = np.mean(np.array([item[self.ID_POS] for item in self.pop_group[clan_idx]]), axis=0)
+                center = np.mean(np.array([agent.solution for agent in self.pop_group[clan_idx]]), axis=0)
                 pos_new = self.beta * center
             else:
-                pos_new = self.pop_group[clan_idx][pos_clan_idx][self.ID_POS] + self.alpha * np.random.uniform() * \
-                          (self.pop_group[clan_idx][0][self.ID_POS] - self.pop_group[clan_idx][pos_clan_idx][self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+                pos_new = self.pop_group[clan_idx][pos_clan_idx].solution + self.alpha * np.random.uniform() * \
+                          (self.pop_group[clan_idx][0].solution - self.pop_group[clan_idx][pos_clan_idx].solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[i] = self.get_better_solution([pos_new, target], self.pop[i])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(pop_new, self.pop)
-        self.pop_group = self.create_pop_group(self.pop, self.n_clans, self.n_individuals)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
+        self.pop_group = self.generate_group_population(self.pop, self.n_clans, self.n_individuals)
         # Separating operator
-        for i in range(0, self.n_clans):
-            self.pop_group[i], _ = self.get_global_best_solution(self.pop_group[i])
-            self.pop_group[i][-1] = self.create_solution(self.problem.lb, self.problem.ub)
+        for idx in range(0, self.n_clans):
+            self.pop_group[idx] = self.get_sorted_population(self.pop_group[idx], self.problem.minmax)
+            self.pop_group[idx][-1] = self.generate_agent()
         self.pop = [agent for pack in self.pop_group for agent in pack]
