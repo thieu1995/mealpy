79c4973dd1a0aad471f7343cb805e29a1a907a03
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-29 23:27:06 +0700

    Convert GJO

--- mealpy/swarm_based/GJO.py
+++ mealpy/swarm_based/GJO.py
@@ -19,30 +19,28 @@ class OriginalGJO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.GJO import OriginalGJO
+    >>> from mealpy import FloatVar, GJO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalGJO(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = GJO.OriginalGJO(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] Chopra, N., & Ansari, M. M. (2022). Golden jackal optimization: A novel nature-inspired
     optimizer for engineering applications. Expert Systems with Applications, 198, 116924.
     """
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -50,7 +48,7 @@ class OriginalGJO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -61,33 +59,33 @@ class OriginalGJO(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        E1 = 1.5*(1-((1+epoch)/self.epoch))
+        E1 = 1.5*(1.-(epoch/self.epoch))
         RL = self.get_levy_flight_step(beta=1.5, multiplier=0.05, size=(self.pop_size, self.problem.n_dims), case=-1)
-        _, (male, female), _ = self.get_special_solutions(self.pop, best=2, worst=1)
+        _, (male, female), _ = self.get_special_agents(self.pop, n_best=2, n_worst=1, minmax=self.problem.minmax)
         pop_new = []
         for idx in range(0, self.pop_size):
-            male_pos = male[self.ID_POS].copy()
-            female_pos = female[self.ID_POS].copy()
-
+            male_pos = male.solution.copy()
+            female_pos = female.solution.copy()
             for jdx in range(0, self.problem.n_dims):
-                r1 = np.random.rand()
+                r1 = self.generator.random()
                 E0 = 2*r1 - 1
                 E = E1 * E0
                 if np.abs(E) < 1:       # EXPLOITATION
-                    t1 = np.abs( (RL[idx, jdx] * male[self.ID_POS][jdx] - self.pop[idx][self.ID_POS][jdx]) )
-                    male_pos[jdx] = male[self.ID_POS][jdx] - E*t1
-                    t2 = np.abs( (RL[idx, jdx] * female[self.ID_POS][jdx] - self.pop[idx][self.ID_POS][jdx]) )
-                    female_pos[jdx] = female[self.ID_POS][jdx] - E*t2
+                    t1 = np.abs( (RL[idx, jdx] * male.solution[jdx] - self.pop[idx].solution[jdx]) )
+                    male_pos[jdx] = male.solution[jdx] - E*t1
+                    t2 = np.abs( (RL[idx, jdx] * female.solution[jdx] - self.pop[idx].solution[jdx]) )
+                    female_pos[jdx] = female.solution[jdx] - E*t2
                 else:                   # EXPLORATION
-                    t1 = np.abs((male[self.ID_POS][jdx] - RL[idx, jdx] * self.pop[idx][self.ID_POS][jdx]))
-                    male_pos[jdx] = male[self.ID_POS][jdx] - E * t1
-                    t2 = np.abs((female[self.ID_POS][jdx] - RL[idx, jdx] * self.pop[idx][self.ID_POS][jdx]))
-                    female_pos[jdx] = female[self.ID_POS][jdx] - E * t2
+                    t1 = np.abs((male.solution[jdx] - RL[idx, jdx] * self.pop[idx].solution[jdx]))
+                    male_pos[jdx] = male.solution[jdx] - E * t1
+                    t2 = np.abs((female.solution[jdx] - RL[idx, jdx] * self.pop[idx].solution[jdx]))
+                    female_pos[jdx] = female.solution[jdx] - E * t2
             pos_new = (male_pos + female_pos) / 2
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = [pos_new, target]
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = agent
         if self.mode in self.AVAILABLE_MODES:
-            self.pop = self.update_target_wrapper_population(pop_new)
+            self.pop = self.update_target_for_population(pop_new)
