df7c87f8bb14c2dcbfc09cccd3f6c51f9a072d86
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-29 23:39:35 +0700

    Convert FOA

--- mealpy/swarm_based/FOA.py
+++ mealpy/swarm_based/FOA.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 from mealpy.optimizer import Optimizer
+from mealpy.utils.agent import Agent
 
 
 class OriginalFOA(Optimizer):
@@ -18,31 +19,28 @@ class OriginalFOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.FOA import OriginalFOA
+    >>> from mealpy import FloatVar, FOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalFOA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = FOA.OriginalFOA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] Pan, W.T., 2012. A new fruit fly optimization algorithm: taking the financial distress model
     as an example. Knowledge-Based Systems, 26, pp.69-74.
     """
-
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -50,7 +48,7 @@ class OriginalFOA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = False
 
@@ -58,19 +56,11 @@ class OriginalFOA(Optimizer):
         return np.array([np.linalg.norm([position[x], position[x + 1]]) for x in range(0, self.problem.n_dims - 1)] + \
                         [np.linalg.norm([position[-1], position[0]])])
 
-    def create_solution(self, lb=None, ub=None, pos=None):
-        """
-        Overriding method in Optimizer class
-
-        Returns:
-            list: a solution with format [position, target]
-        """
-        if pos is None:
-            pos = self.generate_position(self.problem.lb, self.problem.ub)
-        s = self.norm_consecutive_adjacent__(pos)
-        pos = self.amend_position(s, self.problem.lb, self.problem.ub)
-        target = self.get_target_wrapper(pos)
-        return [pos, target]
+    def generate_empty_agent(self, solution: np.ndarray = None) -> Agent:
+        if solution is None:
+            solution = self.problem.generate_solution(encoded=True)
+        solution = self.norm_consecutive_adjacent__(solution)
+        return Agent(solution=solution)
 
     def evolve(self, epoch):
         """
@@ -81,51 +71,49 @@ class OriginalFOA(Optimizer):
         """
         pop_new = []
         for idx in range(0, self.pop_size):
-            pos_new = self.pop[idx][self.ID_POS] + np.random.rand() * np.random.normal(self.problem.lb, self.problem.ub)
+            pos_new = self.pop[idx].solution + self.generator.random() * self.generator.normal(self.problem.lb, self.problem.ub)
             pos_new = self.norm_consecutive_adjacent__(pos_new)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(pop_new, self.pop)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop, self.problem.minmax)
 
 
-class BaseFOA(OriginalFOA):
+class DevFOA(OriginalFOA):
     """
     The developed version: Fruit-fly Optimization Algorithm (FOA)
 
-    Notes
-    ~~~~~
-    + The fitness function (small function) is changed by taking the distance each 2 adjacent dimensions
-    + Update the position if only new generated solution is better
-    + The updated position is created by norm distance * gaussian random number
+    Notes:
+        + The fitness function (small function) is changed by taking the distance each 2 adjacent dimensions
+        + Update the position if only new generated solution is better
+        + The updated position is created by norm distance * gaussian random number
 
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.FOA import BaseFOA
+    >>> from mealpy import FloatVar, FOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = BaseFOA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = FOA.DevFOA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -143,16 +131,17 @@ class BaseFOA(OriginalFOA):
         c = 1 - epoch / self.epoch
         pop_new = []
         for idx in range(0, self.pop_size):
-            pos_new = self.pop[idx][self.ID_POS] + np.random.normal(self.problem.lb, self.problem.ub)
-            pos_new = c * np.random.rand() * self.norm_consecutive_adjacent__(pos_new)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.pop[idx].solution + self.generator.normal(self.problem.lb, self.problem.ub)
+            pos_new = c * self.generator.random() * self.norm_consecutive_adjacent__(pos_new)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(pop_new, self.pop)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop, self.problem.minmax)
 
 
 class WhaleFOA(OriginalFOA):
@@ -165,23 +154,21 @@ class WhaleFOA(OriginalFOA):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.FOA import WhaleFOA
+    >>> from mealpy import FloatVar, FOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = WhaleFOA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = FOA.WhaleFOA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -189,7 +176,7 @@ class WhaleFOA(OriginalFOA):
     fruit fly optimization and advances in real-world problems. Expert Systems with Applications, 159, p.113502.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -204,34 +191,34 @@ class WhaleFOA(OriginalFOA):
         Args:
             epoch (int): The current iteration
         """
-        a = 2 - 2 * epoch / (self.epoch - 1)  # linearly decreased from 2 to 0
-
+        a = 2. - 2. * epoch / self.epoch  # linearly decreased from 2 to 0
         pop_new = []
         for idx in range(0, self.pop_size):
-            r = np.random.rand()
+            r = self.generator.random()
             A = 2 * a * r - a
             C = 2 * r
-            l = np.random.uniform(-1, 1)
+            l = self.generator.uniform(-1, 1)
             p = 0.5
             b = 1
-            if np.random.rand() < p:
+            if self.generator.random() < p:
                 if np.abs(A) < 1:
-                    D = np.abs(C * self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = self.g_best[self.ID_POS] - A * D
+                    D = np.abs(C * self.g_best.solution - self.pop[idx].solution)
+                    pos_new = self.g_best.solution - A * D
                 else:
                     # select random 1 position in pop
-                    x_rand = self.pop[np.random.randint(self.pop_size)]
-                    D = np.abs(C * x_rand[self.ID_POS] - self.pop[idx][self.ID_POS])
-                    pos_new = (x_rand[self.ID_POS] - A * D)
+                    x_rand = self.pop[self.generator.integers(self.pop_size)]
+                    D = np.abs(C * x_rand.solution - self.pop[idx].solution)
+                    pos_new = (x_rand.solution - A * D)
             else:
-                D1 = np.abs(self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                pos_new = D1 * np.exp(b * l) * np.cos(2 * np.pi * l) + self.g_best[self.ID_POS]
+                D1 = np.abs(self.g_best.solution - self.pop[idx].solution)
+                pos_new = D1 * np.exp(b * l) * np.cos(2 * np.pi * l) + self.g_best.solution
             smell = self.norm_consecutive_adjacent__(pos_new)
-            pos_new = self.amend_position(smell, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+            pos_new = self.correct_solution(smell)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                target = self.get_target_wrapper(pos_new)
-                self.pop[idx] = self.get_better_solution([pos_new, target], self.pop[idx])
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-            self.pop = self.greedy_selection_population(pop_new, self.pop)
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(pop_new, self.pop, self.problem.minmax)
