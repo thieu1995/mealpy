6087be6cd48a6e10d9cdea8cd973c1b0c838ceb7
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-29 19:47:18 +0700

    Convert PFA

--- mealpy/swarm_based/PFA.py
+++ mealpy/swarm_based/PFA.py
@@ -18,31 +18,28 @@ class OriginalPFA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.PFA import OriginalPFA
+    >>> from mealpy import FloatVar, PFA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> model = OriginalPFA(epoch, pop_size)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = PFA.OriginalPFA(epoch=1000, pop_size=50)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
     [1] Yapici, H. and Cetinkaya, N., 2019. A new meta-heuristic optimizer: Pathfinder algorithm.
     Applied soft computing, 78, pp.545-568.
     """
-
-    def __init__(self, epoch=10000, pop_size=100, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -50,7 +47,7 @@ class OriginalPFA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.set_parameters(["epoch", "pop_size"])
         self.sort_flag = True
 
@@ -61,35 +58,32 @@ class OriginalPFA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        alpha, beta = np.random.uniform(1, 2, 2)
-        A = np.random.uniform(self.problem.lb, self.problem.ub) * np.exp(-2 * (epoch + 1) / self.epoch)
-        t = 1 - (epoch + 1) * 1.0 / self.epoch
+        alpha, beta = self.generator.uniform(1, 2, 2)
+        A = self.generator.uniform(self.problem.lb, self.problem.ub) * np.exp(-2 * epoch / self.epoch)
+        t = 1. - epoch * 1.0 / self.epoch
         space = self.problem.ub - self.problem.lb
-
         ## Update the position of pathfinder and check the bound
-        pos_new = self.pop[0][self.ID_POS] + 2 * np.random.uniform() * (self.g_best[self.ID_POS] - self.pop[0][self.ID_POS]) + A
-        pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-        target = self.get_target_wrapper(pos_new)
-        pop_new = [[pos_new, target], ]
-
+        pos_new = self.pop[0].solution + 2 * self.generator.uniform() * (self.g_best.solution - self.pop[0].solution) + A
+        pos_new = self.correct_solution(pos_new)
+        agent = self.generate_agent(pos_new)
+        pop_new = [agent, ]
         ## Update positions of members, check the bound and calculate new fitness
         for idx in range(1, self.pop_size):
-            agent = self.pop[idx].copy()
-            pos_new = self.pop[idx][self.ID_POS].copy().astype(float)
+            pos_new = self.pop[idx].solution.copy().astype(float)
             for k in range(1, self.pop_size):
-                dist = np.sqrt(np.sum((self.pop[k][self.ID_POS] - self.pop[idx][self.ID_POS]) ** 2)) / self.problem.n_dims
-                t2 = alpha * np.random.uniform() * (self.pop[k][self.ID_POS] - self.pop[idx][self.ID_POS])
+                dist = np.sqrt(np.sum((self.pop[k].solution - self.pop[idx].solution) ** 2)) / self.problem.n_dims
+                t2 = alpha * self.generator.uniform() * (self.pop[k].solution - self.pop[idx].solution)
                 ## First stabilize the distance
-                t3 = np.random.uniform() * t * (dist / space)
+                t3 = self.generator.uniform() * t * (dist / space)
                 pos_new += t2 + t3
             ## Second stabilize the population size
-            t1 = beta * np.random.uniform() * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
+            t1 = beta * self.generator.uniform() * (self.g_best.solution - self.pop[idx].solution)
             pos_new = (pos_new + t1) / self.pop_size
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            agent[self.ID_POS] = pos_new
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
             pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
+                pop_new[-1].target = self.get_target(pos_new)
         if self.mode in self.AVAILABLE_MODES:
-            pop_new = self.update_target_wrapper_population(pop_new)
-        self.pop = self.greedy_selection_population(self.pop, pop_new)
+            pop_new = self.update_target_for_population(pop_new)
+        self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
