113c2c84efae9ad1f25aaa1e77cce478f26048d9
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-30 09:49:56 +0700

    Convert CSO

--- mealpy/swarm_based/CSO.py
+++ mealpy/swarm_based/CSO.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 from mealpy.optimizer import Optimizer
+from mealpy.utils.agent import Agent
 
 
 class OriginalCSO(Optimizer):
@@ -30,32 +31,21 @@ class OriginalCSO(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.CSO import OriginalCSO
+    >>> from mealpy import FloatVar, CSO
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> mixture_ratio = 0.15
-    >>> smp = 5
-    >>> spc = False
-    >>> cdc = 0.8
-    >>> srd = 0.15
-    >>> c1 = 0.4
-    >>> w_min = 0.4
-    >>> w_max = 0.9
-    >>> selected_strategy = 1
-    >>> model = OriginalCSO(epoch, pop_size, mixture_ratio, smp, spc, cdc, srd, c1, w_min, w_max, selected_strategy)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CSO.OriginalCSO(epoch=1000, pop_size=50, mixture_ratio = 0.15, smp = 5, spc = False, cdc = 0.8, srd = 0.15, c1 = 0.4, w_min = 0.4, w_max = 0.9)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -68,8 +58,9 @@ class OriginalCSO(Optimizer):
     ID_VEL = 2  # velocity
     ID_FLAG = 3  # status
 
-    def __init__(self, epoch=10000, pop_size=100, mixture_ratio=0.15, smp=5, spc=False,
-                 cdc=0.8, srd=0.15, c1=0.4, w_min=0.5, w_max=0.9, selected_strategy=1, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, mixture_ratio: float = 0.15, smp: int = 5,
+                 spc: bool = False, cdc: float = 0.8, srd: float = 0.15, c1: float = 0.4,
+                 w_min: float = 0.5, w_max: float = 0.9, selected_strategy: int = 1, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -86,7 +77,7 @@ class OriginalCSO(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.mixture_ratio = self.validator.check_float("mixture_ratio", mixture_ratio, (0, 1.0))
         self.smp = self.validator.check_int("smp", smp, [2, 10000])
         self.spc = self.validator.check_bool("spc", spc, [True, False])
@@ -99,59 +90,53 @@ class OriginalCSO(Optimizer):
         self.set_parameters(["epoch", "pop_size", "mixture_ratio", "smp", "spc", "cdc", "srd", "c1", "w_min", "w_max", "selected_strategy"])
         self.sort_flag = False
 
-    def create_solution(self, lb=None, ub=None, pos=None):
+    def generate_empty_agent(self, solution: np.ndarray = None) -> Agent:
         """
-        Overriding method in Optimizer class
         + x: current position of cat
         + v: vector v of cat (same amount of dimension as x)
         + flag: the stage of cat, seeking (looking/finding around) or tracing (chasing/catching) => False: seeking mode , True: tracing mode
-
-        Returns:
-            list: wrapper of solution with format [position, target, velocity, flag]
         """
-        if pos is None:
-            pos = self.generate_position(lb, ub)
-        position = self.amend_position(pos, lb, ub)
-        target = self.get_target_wrapper(position)
-        velocity = np.random.uniform(lb, ub)
-        flag = True if np.random.uniform() < self.mixture_ratio else False
-        return [position, target, velocity, flag]
+        if solution is None:
+            solution = self.problem.generate_solution(encoded=True)
+        velocity = self.generator.uniform(self.problem.lb, self.problem.ub)
+        flag = True if self.generator.uniform() < self.mixture_ratio else False
+        return Agent(solution=solution, velocity=velocity, flag=flag)
 
     def seeking_mode__(self, cat):
         candidate_cats = []
-        clone_cats = self.create_population(self.smp)
+        clone_cats = self.generate_population(self.smp)
         if self.spc:
             candidate_cats.append(cat.copy())
             clone_cats = [cat.copy() for _ in range(self.smp - 1)]
-
         for clone in clone_cats:
-            idx = np.random.choice(range(0, self.problem.n_dims), int(self.cdc * self.problem.n_dims), replace=False)
-            pos_new1 = clone[self.ID_POS] * (1 + self.srd)
-            pos_new2 = clone[self.ID_POS] * (1 - self.srd)
-
-            pos_new = np.where(np.random.random(self.problem.n_dims) < 0.5, pos_new1, pos_new2)
-            pos_new[idx] = clone[self.ID_POS][idx]
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            candidate_cats.append([pos_new, None, clone[self.ID_VEL], clone[self.ID_FLAG]])
+            idx = self.generator.choice(range(0, self.problem.n_dims), int(self.cdc * self.problem.n_dims), replace=False)
+            pos_new1 = clone.solution * (1 + self.srd)
+            pos_new2 = clone.solution * (1 - self.srd)
+            pos_new = np.where(self.generator.random(self.problem.n_dims) < 0.5, pos_new1, pos_new2)
+            pos_new[idx] = clone.solution[idx]
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            agent.update(velocity=clone.velocity, flag=clone.flag)
+            candidate_cats.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                candidate_cats[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        candidate_cats = self.update_target_wrapper_population(candidate_cats)
+                candidate_cats[-1].target = self.get_target(pos_new)
+        candidate_cats = self.update_target_for_population(candidate_cats)
 
         if self.selected_strategy == 0:  # Best fitness-self
-            _, cat = self.get_global_best_solution(candidate_cats)
+            cat = self.get_best_agent(candidate_cats, self.problem.minmax)
         elif self.selected_strategy == 1:  # Tournament
             k_way = 4
-            idx = np.random.choice(range(0, self.smp), k_way, replace=False)
+            idx = self.generator.choice(range(0, self.smp), k_way, replace=False)
             cats_k_way = [candidate_cats[_] for _ in idx]
-            _, cat = self.get_global_best_solution(cats_k_way)
+            cat = self.get_best_agent(cats_k_way, self.problem.minmax)
         elif self.selected_strategy == 2:  ### Roul-wheel selection
-            list_fitness = [candidate_cats[u][self.ID_TAR][self.ID_FIT] for u in range(0, len(candidate_cats))]
+            list_fitness = [candidate_cats[u].target.fitness for u in range(0, len(candidate_cats))]
             idx = self.get_index_roulette_wheel_selection(list_fitness)
             cat = candidate_cats[idx]
         else:
-            idx = np.random.choice(range(0, len(candidate_cats)))
+            idx = self.generator.choice(range(0, len(candidate_cats)))
             cat = candidate_cats[idx]  # Random
-        return cat[self.ID_POS]
+        return cat.solution
 
     def evolve(self, epoch):
         """
@@ -165,15 +150,15 @@ class OriginalCSO(Optimizer):
         for idx in range(0, self.pop_size):
             agent = self.pop[idx].copy()
             # tracing mode
-            if self.pop[idx][self.ID_FLAG]:
-                pos_new = self.pop[idx][self.ID_POS] + w * self.pop[idx][self.ID_VEL] + \
-                          np.random.uniform() * self.c1 * (self.g_best[self.ID_POS] - self.pop[idx][self.ID_POS])
-                pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
+            if self.pop[idx].flag:
+                pos_new = self.pop[idx].solution + w * self.pop[idx].velocity + \
+                          self.generator.uniform() * self.c1 * (self.g_best.solution - self.pop[idx].solution)
+                pos_new = self.correct_solution(pos_new)
             else:
                 pos_new = self.seeking_mode__(self.pop[idx])
-            agent[self.ID_POS] = pos_new
-            agent[self.ID_FLAG] = True if np.random.uniform() < self.mixture_ratio else False
+            agent.solution = pos_new
+            agent.flag = True if self.generator.uniform() < self.mixture_ratio else False
             pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        self.pop = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        self.pop = self.update_target_for_population(pop_new)
