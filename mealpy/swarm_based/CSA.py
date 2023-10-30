335496287df06a6c25291bfefb866e76c8bb8c72
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-30 09:55:43 +0700

    Convert CSA

--- mealpy/swarm_based/CSA.py
+++ mealpy/swarm_based/CSA.py
@@ -21,24 +21,21 @@ class OriginalCSA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.CSA import OriginalCSA
+    >>> from mealpy import FloatVar, CSA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
+    >>>     "obj_func": objective_function,
     >>>     "minmax": "min",
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> p_a = 0.3
-    >>> model = OriginalCSA(epoch, pop_size, p_a)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = CSA.OriginalCSA(epoch=1000, pop_size=50, p_a = 0.3)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -46,7 +43,7 @@ class OriginalCSA(Optimizer):
     congress on nature & biologically inspired computing (NaBIC) (pp. 210-214). Ieee.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, p_a=0.3, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, p_a: float = 0.3, **kwargs: object) -> None:
         """
         Args:
             epoch (int): maximum number of iterations, default = 10000
@@ -55,7 +52,7 @@ class OriginalCSA(Optimizer):
         """
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
-        self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
+        self.pop_size = self.validator.check_int("pop_size", pop_size, [5, 10000])
         self.p_a = self.validator.check_float("p_a", p_a, (0, 1.0))
         self.set_parameters(["epoch", "pop_size", "p_a"])
         self.n_cut = int(self.p_a * self.pop_size)
@@ -68,25 +65,30 @@ class OriginalCSA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        pop_new = self.pop.copy()
-        for i in range(0, self.pop_size):
+        pop_new = []
+        for idx in range(0, self.pop_size):
             ## Generate levy-flight solution
             levy_step = self.get_levy_flight_step(multiplier=0.001, case=-1)
-            pos_new = self.pop[i][self.ID_POS] + 1.0 / np.sqrt(epoch + 1) * np.sign(np.random.random() - 0.5) * \
-                      levy_step * (self.pop[i][self.ID_POS] - self.g_best[self.ID_POS])
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            target = self.get_target_wrapper(pos_new)
-            if self.compare_agent([pos_new, target], self.pop[i]):
-                pop_new[i] = [pos_new, target]
+            pos_new = self.pop[idx].solution + 1.0 / np.sqrt(epoch) * np.sign(self.generator.random() - 0.5) * \
+                      levy_step * (self.pop[idx].solution - self.g_best.solution)
+            pos_new = self.correct_solution(pos_new)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
+            if self.mode not in self.AVAILABLE_MODES:
+                agent.target = self.get_target(pos_new)
+                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_for_population(pop_new)
+            self.pop = self.greedy_selection_population(self.pop, pop_new, self.problem.minmax)
 
         ## Abandoned some worst nests
-        pop = self.get_sorted_strim_population(pop_new, self.pop_size)
+        pop = self.get_sorted_and_trimmed_population(self.pop, self.pop_size, self.problem.minmax)
         pop_new = []
-        for i in range(0, self.n_cut):
-            pos_new = np.random.uniform(self.problem.lb, self.problem.ub)
-            pos_new = self.amend_position(pos_new, self.problem.lb, self.problem.ub)
-            pop_new.append([pos_new, None])
+        for idx in range(0, self.n_cut):
+            pos_new = self.generator.uniform(self.problem.lb, self.problem.ub)
+            agent = self.generate_empty_agent(pos_new)
+            pop_new.append(agent)
             if self.mode not in self.AVAILABLE_MODES:
-                pop_new[-1][self.ID_TAR] = self.get_target_wrapper(pos_new)
-        pop_new = self.update_target_wrapper_population(pop_new)
+                pop_new[-1].target = self.get_target(pos_new)
+        pop_new = self.update_target_for_population(pop_new)
         self.pop = pop[:(self.pop_size - self.n_cut)] + pop_new
