68bdbe8cfe3a9ce6c744f0bee9a5803200a5c81e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-30 09:40:12 +0700

    Convert DMOA

--- mealpy/swarm_based/DMOA.py
+++ mealpy/swarm_based/DMOA.py
@@ -24,25 +24,21 @@ class OriginalDMOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.DMOA import OriginalDMOA
+    >>> from mealpy import FloatVar, DMOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> n_baby_sitter = 3
-    >>> peep = 2
-    >>> model = OriginalDMOA(epoch, pop_size, n_baby_sitter, peep)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = DMOA.OriginalDMOA(epoch=1000, pop_size=50, n_baby_sitter = 3, peep = 2)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
     References
     ~~~~~~~~~~
@@ -50,14 +46,14 @@ class OriginalDMOA(Optimizer):
     Computer methods in applied mechanics and engineering, 391, 114570.
     """
 
-    def __init__(self, epoch=10000, pop_size=100, n_baby_sitter=3, peep=2, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, n_baby_sitter: int = 3, peep: float = 2, **kwargs: object) -> None:
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
         self.n_baby_sitter = self.validator.check_int("n_baby_sitter", n_baby_sitter, [2, 10])
         self.peep = self.validator.check_float("peep", peep, [1, 10.])
         self.n_scout = self.pop_size - self.n_baby_sitter
-        self.support_parallel_modes = False
+        self.is_parallelizable = False
         self.set_parameters(["epoch", "pop_size", "n_baby_sitter", "peep"])
         self.sort_flag = False
 
@@ -65,7 +61,6 @@ class OriginalDMOA(Optimizer):
         self.C = np.zeros(self.pop_size)
         self.tau = -np.inf
         self.L = np.round(0.6 * self.problem.n_dims * self.n_baby_sitter)
-
 
     def evolve(self, epoch):
         """
@@ -75,56 +70,53 @@ class OriginalDMOA(Optimizer):
             epoch (int): The current iteration
         """
         ## Abandonment Counter
-        CF = (1 - (epoch+1)/self.epoch)**(2*(epoch+1)/self.epoch)
-        fit_list = np.array([agent[self.ID_TAR][self.ID_FIT] for agent in self.pop])
+        CF = (1. - epoch/self.epoch)**(2.*epoch/self.epoch)
+        fit_list = np.array([agent.target.fitness for agent in self.pop])
         mean_cost = np.mean(fit_list)
         fi = np.exp(-fit_list / mean_cost)
         for idx in range(0, self.pop_size):
             alpha = self.get_index_roulette_wheel_selection(fi)
-            k = np.random.choice(list(set(range(0, self.pop_size)) - {idx, alpha}))
+            k = self.generator.choice(list(set(range(0, self.pop_size)) - {idx, alpha}))
             ## Define Vocalization Coeff.
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
-            new_pos = self.pop[alpha][self.ID_POS] + phi * (self.pop[alpha][self.ID_POS] - self.pop[k][self.ID_POS])
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
-            if self.compare_agent([new_pos, new_tar], self.pop[idx]):
-                self.pop[idx] = [new_pos, new_tar]
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
+            new_pos = self.pop[alpha].solution + phi * (self.pop[alpha].solution - self.pop[k].solution)
+            new_pos = self.correct_solution(new_pos)
+            agent = self.generate_agent(new_pos)
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
             else:
                 self.C[idx] += 1
-
         SM = np.zeros(self.pop_size)
         for idx in range(0, self.pop_size):
-            k = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
+            k = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
             ## Define Vocalization Coeff.
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
-            new_pos = self.pop[idx][self.ID_POS] + phi * (self.pop[idx][self.ID_POS] - self.pop[k][self.ID_POS])
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
+            new_pos = self.pop[idx].solution + phi * (self.pop[idx].solution - self.pop[k].solution)
+            new_pos = self.correct_solution(new_pos)
+            agent = self.generate_agent(new_pos)
             ## Sleeping mould
-            SM[idx] = (new_tar[self.ID_FIT] - self.pop[idx][self.ID_TAR][self.ID_FIT])/np.max([new_tar[self.ID_FIT], self.pop[idx][self.ID_TAR][self.ID_FIT]])
-            if self.compare_agent([new_pos, new_tar], self.pop[idx]):
-                self.pop[idx] = [new_pos, new_tar]
+            SM[idx] = (agent.target.fitness - self.pop[idx].target.fitness)/np.max([agent.target.fitness, self.pop[idx].target.fitness])
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
             else:
                 self.C[idx] += 1
         ## Baby sitters
         for idx in range(0, self.n_baby_sitter):
             if self.C[idx] >= self.L:
-                self.pop[idx] = self.create_solution(self.problem.lb, self.problem.ub)
+                self.pop[idx] = self.generate_agent()
                 self.C[idx] = 0
-
         ## Next Mongoose position
         new_tau = np.mean(SM)
         for idx in range(0, self.pop_size):
-            M = SM[idx] * self.pop[idx][self.ID_POS] / self.pop[idx][self.ID_POS]
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
+            M = SM[idx] * self.pop[idx].solution / self.pop[idx].solution
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
             if new_tau > self.tau:
-                new_pos = self.pop[idx][self.ID_POS] - CF * phi * np.random.rand() * (self.pop[idx][self.ID_POS] - M)
+                new_pos = self.pop[idx].solution - CF * phi * self.generator.random() * (self.pop[idx].solution - M)
             else:
-                new_pos = self.pop[idx][self.ID_POS] + CF * phi * np.random.rand() * (self.pop[idx][self.ID_POS] - M)
+                new_pos = self.pop[idx].solution + CF * phi * self.generator.random() * (self.pop[idx].solution - M)
             self.tau = new_tau
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
-            self.pop[idx] = [new_pos, new_tar]
+            new_pos = self.correct_solution(new_pos)
+            self.pop[idx] = self.generate_agent(new_pos)
 
 
 class DevDMOA(Optimizer):
@@ -139,33 +131,30 @@ class DevDMOA(Optimizer):
     Examples
     ~~~~~~~~
     >>> import numpy as np
-    >>> from mealpy.swarm_based.DMOA import DevDMOA
+    >>> from mealpy import FloatVar, DMOA
     >>>
-    >>> def fitness_function(solution):
+    >>> def objective_function(solution):
     >>>     return np.sum(solution**2)
     >>>
-    >>> problem_dict1 = {
-    >>>     "fit_func": fitness_function,
-    >>>     "lb": [-10, -15, -4, -2, -8],
-    >>>     "ub": [10, 15, 12, 8, 20],
+    >>> problem_dict = {
+    >>>     "bounds": FloatVar(n_vars=30, lb=(-10.,) * 30, ub=(10.,) * 30, name="delta"),
     >>>     "minmax": "min",
+    >>>     "obj_func": objective_function
     >>> }
     >>>
-    >>> epoch = 1000
-    >>> pop_size = 50
-    >>> peep = 2
-    >>> model = DevDMOA(epoch, pop_size, peep)
-    >>> best_position, best_fitness = model.solve(problem_dict1)
-    >>> print(f"Solution: {best_position}, Fitness: {best_fitness}")
+    >>> model = DMOA.DevDMOA(epoch=1000, pop_size=50, peep = 2)
+    >>> g_best = model.solve(problem_dict)
+    >>> print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+    >>> print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
     """
 
-    def __init__(self, epoch=10000, pop_size=100, peep=2, **kwargs):
+    def __init__(self, epoch: int = 10000, pop_size: int = 100, peep: float = 2, **kwargs: object) -> None:
         super().__init__(**kwargs)
         self.epoch = self.validator.check_int("epoch", epoch, [1, 100000])
         self.pop_size = self.validator.check_int("pop_size", pop_size, [10, 10000])
         self.peep = self.validator.check_float("peep", peep, [1, 10.])
         self.set_parameters(["epoch", "pop_size", "peep"])
-        self.support_parallel_modes = False
+        self.is_parallelizable = False
         self.sort_flag = False
 
     def initialize_variables(self):
@@ -180,56 +169,56 @@ class DevDMOA(Optimizer):
             epoch (int): The current iteration
         """
         ## Abandonment Counter
-        CF = (1 - (epoch + 1) / self.epoch) ** (2 * (epoch + 1) / self.epoch)
-        fit_list = np.array([agent[self.ID_TAR][self.ID_FIT] for agent in self.pop])
+        CF = (1. - epoch / self.epoch) ** (2. * epoch / self.epoch)
+        fit_list = np.array([agent.target.fitness for agent in self.pop])
         mean_cost = np.mean(fit_list)
         fi = np.exp(-fit_list / mean_cost)
 
         ## Foraging led by Alpha female
         for idx in range(0, self.pop_size):
             alpha = self.get_index_roulette_wheel_selection(fi)
-            k = np.random.choice(list(set(range(0, self.pop_size)) - {idx, alpha}))
+            k = self.generator.choice(list(set(range(0, self.pop_size)) - {idx, alpha}))
             ## Define Vocalization Coeff.
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
-            new_pos = self.pop[alpha][self.ID_POS] + phi * (self.pop[alpha][self.ID_POS] - self.pop[k][self.ID_POS])
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
-            if self.compare_agent([new_pos, new_tar], self.pop[idx]):
-                self.pop[idx] = [new_pos, new_tar]
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
+            new_pos = self.pop[alpha].solution + phi * (self.pop[alpha].solution - self.pop[k].solution)
+            new_pos = self.correct_solution(new_pos)
+            agent = self.generate_agent(new_pos)
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
             else:
                 self.C[idx] += 1
 
         ## Scout group
         SM = np.zeros(self.pop_size)
         for idx in range(0, self.pop_size):
-            k = np.random.choice(list(set(range(0, self.pop_size)) - {idx}))
+            k = self.generator.choice(list(set(range(0, self.pop_size)) - {idx}))
             ## Define Vocalization Coeff.
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
-            new_pos = self.pop[idx][self.ID_POS] + phi * (self.pop[idx][self.ID_POS] - self.pop[k][self.ID_POS])
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
+            new_pos = self.pop[idx].solution + phi * (self.pop[idx].solution - self.pop[k].solution)
+            new_pos = self.correct_solution(new_pos)
+            agent = self.generate_agent(new_pos)
             ## Sleeping mould
-            SM[idx] = (new_tar[self.ID_FIT] - self.pop[idx][self.ID_TAR][self.ID_FIT]) / np.max([new_tar[self.ID_FIT], self.pop[idx][self.ID_TAR][self.ID_FIT]])
-            if self.compare_agent([new_pos, new_tar], self.pop[idx]):
-                self.pop[idx] = [new_pos, new_tar]
+            SM[idx] = (agent.target.fitness - self.pop[idx].target.fitness) / np.max([agent.target.fitness, self.pop[idx].target.fitness])
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
             else:
                 self.C[idx] += 1
 
         ## Baby sitters
         for idx in range(0, self.pop_size):
             if self.C[idx] >= self.L:
-                self.pop[idx] = self.create_solution(self.problem.lb, self.problem.ub)
+                self.pop[idx] = self.generate_agent()
                 self.C[idx] = 0
 
         ## Next Mongoose position
         new_tau = np.mean(SM)
         for idx in range(0, self.pop_size):
-            phi = (self.peep / 2) * np.random.uniform(-1, 1, self.problem.n_dims)
+            phi = (self.peep / 2) * self.generator.uniform(-1, 1, self.problem.n_dims)
             if new_tau > SM[idx]:
-                new_pos = self.g_best[self.ID_POS] - CF * phi * (self.g_best[self.ID_POS] - SM[idx] * self.pop[idx][self.ID_POS])
+                new_pos = self.g_best.solution - CF * phi * (self.g_best.solution - SM[idx] * self.pop[idx].solution)
             else:
-                new_pos = self.pop[idx][self.ID_POS] + CF * phi * (self.g_best[self.ID_POS] - SM[idx] * self.pop[idx][self.ID_POS])
-            new_pos = self.amend_position(new_pos, self.problem.lb, self.problem.ub)
-            new_tar = self.get_target_wrapper(new_pos)
-            if self.compare_agent([new_pos, new_tar], self.pop[idx]):
-                self.pop[idx] = [new_pos, new_tar]
+                new_pos = self.pop[idx].solution + CF * phi * (self.g_best.solution - SM[idx] * self.pop[idx].solution)
+            new_pos = self.correct_solution(new_pos)
+            agent = self.generate_agent(new_pos)
+            if self.compare_target(agent.target, self.pop[idx].target, self.problem.minmax):
+                self.pop[idx] = agent
