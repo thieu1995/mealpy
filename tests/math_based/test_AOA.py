fcf7a4dc5935647ed1366e66a9cf3aef503752a1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-30 16:09:55 +0700

    Update test for math_based module

--- tests/math_based/test_AOA.py
+++ tests/math_based/test_AOA.py
@@ -4,21 +4,19 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
-from mealpy.math_based import AOA
-from mealpy.optimizer import Optimizer
+from mealpy import FloatVar, AOA, Optimizer
 import numpy as np
 import pytest
 
 
 @pytest.fixture(scope="module")  # scope: Call only 1 time at the beginning
 def problem():
-    def fitness_function(solution):
+    def objective_function(solution):
         return np.sum(solution ** 2)
 
     problem = {
-        "fit_func": fitness_function,
-        "lb": [-10, -10, -10, -10, -10],
-        "ub": [10, 10, 10, 10, 10],
+        "obj_func": objective_function,
+        "bounds": FloatVar(lb=[-10, -15, -4, -2, -8], ub=[10, 15, 12, 8, 20]),
         "minmax": "min",
     }
     return problem
@@ -29,7 +27,7 @@ def test_AOA_results(problem):
         AOA.OriginalAOA(epoch=100, pop_size=50, alpha=5, miu=0.5, moa_min=0.2, moa_max=0.9),
     ]
     for model in models:
-        best_position, best_fitness = model.solve(problem)
+        g_best = model.solve(problem)
         assert isinstance(model, Optimizer)
-        assert isinstance(best_position, np.ndarray)
-        assert len(best_position) == len(problem["lb"])
+        assert isinstance(g_best.solution, np.ndarray)
+        assert len(g_best.solution) == len(model.problem.lb)
