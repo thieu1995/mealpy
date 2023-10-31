e6f66de6190a46bd5cba02a51193c81a54ef8c97
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-10-31 23:34:43 +0700

    Remove space for BA testing

--- tests/swarm_based/test_BA.py
+++ tests/swarm_based/test_BA.py
@@ -4,9 +4,10 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
-from mealpy import FloatVar, BA, Optimizer
 import numpy as np
 import pytest
+
+from mealpy import FloatVar, BA, Optimizer
 
 
 @pytest.fixture(scope="module")  # scope: Call only 1 time at the beginning
@@ -26,7 +27,7 @@ def problem():
 def test_BA_results(problem):
     models = [
         BA.OriginalBA(epoch=10, pop_size=50, loudness=0.8, pulse_rate=0.95, pf_min=0.1, pf_max=10.0),
-        BA.AdaptiveBA(epoch=10, pop_size=50, loudness_min = 1.0, loudness_max = 2.0, pr_min = 0.15, pr_max = 0.85, pf_min = -2.5, pf_max = 10.),
+        BA.AdaptiveBA(epoch=10, pop_size=50, loudness_min=1.0, loudness_max=2.0, pr_min=0.15, pr_max=0.85, pf_min=-2.5, pf_max=10.),
         BA.DevBA(epoch=10, pop_size=50, pulse_rate=0.95, pf_min=0., pf_max=10.)
     ]
     for model in models:
