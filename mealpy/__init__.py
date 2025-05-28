8782c8caeb3ee984c36b9e5939fa9b0852cbf3a4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-28 17:01:12 +0700

    Update get_optimizer_by_class() and get_optimizer_by_name() functions in mealpy module to support new classes.

--- mealpy/__init__.py
+++ mealpy/__init__.py
@@ -52,9 +52,12 @@ from .utils.space import (IntegerVar, FloatVar, StringVar, BinaryVar, BoolVar, C
 __EXCLUDE_MODULES = ["__builtins__", "current_module", "inspect", "sys"]
 
 
-def get_all_optimizers():
+def get_all_optimizers(verbose=True):
     """
     Get all available optimizer classes in Mealpy library
+
+    Args:
+        verbose (bool): whether to print the optimizer information
 
     Returns:
         dict_optimizers (dict): key is the string optimizer class name, value is the actual optimizer class
@@ -66,22 +69,58 @@ def get_all_optimizers():
                 if inspect.isclass(cls_obj) and issubclass(cls_obj, Optimizer):
                     cls[cls_name] = cls_obj
     del cls['Optimizer']
+    if verbose:
+        for name, optimizer in cls.items():
+            print(f"Optimizer: {name} - {optimizer} - {optimizer()}")
     return cls
 
 
-def get_optimizer_by_name(name):
+def get_optimizer_by_class(class_name, verbose=False):
     """
-    Get an optimizer class by name
+    Get an optimizer class by its class name
 
     Args:
-        name (str): the classname of the optimizer (e.g, OriginalGA, OriginalWOA), don't pass the module name (e.g, ABC, WOA, GA)
+        class_name (str): the classname of the optimizer (e.g, C_PSO, OriginalGA), don't pass the module name (e.g, PSO, GA)
+        verbose (bool): whether to print the optimizer information
 
     Returns:
         optimizer (Optimizer): the actual optimizer class or None if the classname is not supported
     """
     try:
-        return get_all_optimizers()[name]
+        all_optimizers = get_all_optimizers(verbose=verbose)
+        return all_optimizers[class_name]
     except KeyError:
-        print(f"Mealpy doesn't support optimizer named: {name}.\n"
+        print(f"Mealpy doesn't support optimizer named: {class_name}.\n"
               f"Please see the supported Optimizer name from here: https://mealpy.readthedocs.io/en/latest/pages/support.html#classification-table")
         return None
+
+
+def get_optimizer_by_name(name, verbose=False):
+    """
+    Get an optimizer class by name
+
+    Args:
+        name (str): the classname of the optimizer (e.g, OriginalGA, OriginalWOA), don't pass the module name (e.g, ABC, WOA, GA)
+        verbose (bool): whether to print the optimizer information
+
+    Returns:
+        dict_optimizers (dict): key is the string optimizer class name, value is the actual optimizer class
+    """
+    cls = {}
+    flag = False
+    for module_name, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.ismodule(obj) and (name not in __EXCLUDE_MODULES) and (module_name == name):
+            flag = True
+            for cls_name, cls_obj in inspect.getmembers(obj):
+                if inspect.isclass(cls_obj) and issubclass(cls_obj, Optimizer):
+                    cls[cls_name] = cls_obj
+    if verbose:
+        if not flag:
+            print(f"Mealpy doesn't support optimizer named: {name}.\n"
+                  f"Please see the supported Optimizer name from here: https://mealpy.readthedocs.io/en/latest/pages/support.html#classification-table")
+            return None
+        del cls['Optimizer']
+        print(f"Found algorithm: {name}, the supported variants are:")
+        for name, optimizer in cls.items():
+            print(f"Optimizer: {name} - {optimizer} - {optimizer()}")
+    return cls
