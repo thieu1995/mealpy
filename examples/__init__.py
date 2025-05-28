fbd69fbc2d41fe494ce96358f5dbdc6237108d4f
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-28 17:52:34 +0700

    Update examples for search functions

--- examples/__init__.py
+++ examples/__init__.py
@@ -3,3 +3,12 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+from mealpy import get_all_optimizers, get_optimizer_by_class, get_optimizer_by_name
+
+get_all_optimizers(verbose=True)
+
+get_optimizer_by_class(class_name="BaseGA", verbose=True)
+
+get_optimizer_by_name("GA", verbose=True)
+get_optimizer_by_name("PSO", verbose=True)
