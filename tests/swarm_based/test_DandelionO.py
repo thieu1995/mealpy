3ae2281e12b5bae6e633023682666a9517e0f0e0
Halil Akbas <halilakbas11@outlook.com>
2026-01-03 21:51:35 +0300

    Add Dandelion Optimizer (OriginalDandelionO) implementation to swarm_based module

---
+++ tests/swarm_based/test_DandelionO.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 14:07, 20/03/2022 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
+
+import numpy as np
+import pytest
+
+from mealpy import FloatVar, Optimizer
+from mealpy.swarm_based.DandelionO import OriginalDandelionO
+
+
+@pytest.fixture(scope="module")  # scope: Call only 1 time at the beginning
+def problem():
+    def objective_function(solution):
+        return np.sum(solution ** 2)
+
+    problem = {
+        "obj_func": objective_function,
+        "bounds": FloatVar(lb=[-10, -15, -4, -2, -8], ub=[10, 15, 12, 8, 20]),
+        "minmax": "min",
+        "log_to": None
+    }
+    return problem
+
+
+def test_OriginalDandelionO_results(problem):
+    models = [
+        OriginalDandelionO(epoch=10, pop_size=50)
+    ]
+    for model in models:
+        g_best = model.solve(problem)
+        assert isinstance(model, Optimizer)
+        assert isinstance(g_best.solution, np.ndarray)
+        assert len(g_best.solution) == len(model.problem.lb)
