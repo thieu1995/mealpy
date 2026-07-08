4f233ae7c6032b94ff88a05dc1d47308fc98c9c2
Nguyen Van Thieu <12763861+thieu1995@users.noreply.github.com>
2026-07-08 17:16:39 +0200

    Refactor imports and update optimizer error messages

--- mealpy/__init__.py
+++ mealpy/__init__.py
@@ -31,194 +31,28 @@ __version__ = "3.0.3"
 
 import sys
 import inspect
-
-# Bio-based algorithms
-from .bio_based import (
-    BBO,
-    BBOA,
-    BMO,
-    EOA,
-    IWO,
-    SBO,
-    SMA,
-    SOA,
-    SOS,
-    TPO,
-    TSA,
-    VCS,
-    WHO,
-    BCO,
-    EAO,
-    SFOA,
-)
-
-# Evolutionary algorithms (without BWO – moved to swarm_based)
-from .evolutionary_based import (
-    CRO,
-    DE,
-    EP,
-    ES,
-    FPA,
-    GA,
-    MA,
-    SHADE,
-)
-
-# Human-based algorithms
-from .human_based import (
-    BRO,
-    BSO,
-    CA,
-    CHIO,
-    FBIO,
-    GSKA,
-    HBO,
-    HCO,
-    ICA,
-    LCO,
-    QSA,
-    SARO,
-    SPBO,
-    SSDO,
-    TLO,
-    TOA,
-    WarSO,
-    AFT,
-    CDDO,
-    DOA,
-)
-
-# Math-based algorithms
-from .math_based import (
-    AOA,
-    CEM,
-    CGO,
-    CircleSA,
-    GBO,
-    HC,
-    INFO,
-    PSS,
-    RUN,
-    SCA,
-    SHIO,
-    TS,
-)
-
-# Physics-based algorithms (including MSO)
-from .physics_based import (
-    ArchOA,
-    ASO,
-    CDO,
-    EFO,
-    EO,
-    EVO,
-    FLA,
-    HGSO,
-    MVO,
-    NRO,
-    RIME,
-    SA,
-    TWO,
-    WDO,
-    ESO,
-    SOO,
-    MSO,
-)
-
-# Swarm-based algorithms, including Beluga Whale Optimization (BWO)
-from .swarm_based import (
-    ABC,
-    ACOR,
-    AGTO,
-    ALO,
-    AO,
-    ARO,
-    AVOA,
-    BA,
-    BeesA,
-    BES,
-    BFO,
-    BSA,
-    BWO,
-    COA,
-    CoatiOA,
-    CSA,
-    CSO,
-    DMOA,
-    DO,
-    EHO,
-    ESOA,
-    FA,
-    FFA,
-    FFO,
-    FOA,
-    FOX,
-    GJO,
-    GOA,
-    GTO,
-    GWO,
-    HBA,
-    HGS,
-    HHO,
-    JA,
-    MFO,
-    MGO,
-    MPA,
-    MRFO,
-    MSA,
-    MShOA,
-    NGO,
-    NMRA,
-    OOA,
-    PFA,
-    POA,
-    PSO,
-    SCSO,
-    SeaHO,
-    ServalOA,
-    SFO,
-    SHO,
-    SLO,
-    SRSR,
-    SSA,
-    SSO,
-    SSpiderA,
-    SSpiderO,
-    STO,
-    TDO,
-    TSO,
-    WaOA,
-    WOA,
-    ZOA,
-    EPC,
-    SMO,
-    SquirrelSA,
-    FDO,
-)
-
-# System, music, game, and other categories
+from .bio_based import (BBO, BBOA, BMO, EOA, IWO, SBO, SMA, SOA, SOS, TPO, TSA, VCS, WHO, BCO, EAO, SFOA)
+from .evolutionary_based import (BWOA, CRO, DE, EP, ES, FPA, GA, MA, SHADE)
+from .human_based import (BRO, BSO, CA, CHIO, FBIO, GSKA, HBO, HCO, ICA, LCO, QSA, SARO, SPBO, SSDO, TLO, TOA, WarSO,
+                          AFT, CDDO, DOA)
+from .math_based import (AOA, CEM, CGO, CircleSA, GBO, HC, INFO, PSS, RUN, SCA, SHIO, TS)
+from .physics_based import (ArchOA, ASO, CDO, EFO, EO, EVO, FLA, HGSO, MVO, NRO, RIME, SA, TWO, WDO, ESO, SOO, MSO)
+from .swarm_based import (ABC, ACOR, AGTO, ALO, AO, ARO, AVOA, BA, BeesA, BES, BFO, BSA, COA, CoatiOA, CSA, CSO,
+                          DMOA, DO, EHO, ESOA, FA, FFA, FFO, FOA, FOX, GJO, GOA, GTO, GWO, HBA, HGS, HHO, JA,
+                          MFO, MGO, MPA, MRFO, MSA, MShOA, NGO, NMRA, OOA, PFA, POA, PSO, SCSO, SeaHO, ServalOA, SFO,
+                          SHO, SLO, SRSR, SSA, SSO, SSpiderA, SSpiderO, STO, TDO, TSO, WaOA, WOA, ZOA,
+                          EPC, SMO, SquirrelSA, FDO, ChOA, RSA, GJA, BWO)
 from .system_based import AEO, GCO, WCA
 from .music_based import HS
 from .game_based import THRO
 from .sota_based import LSHADEcnEpSin, IMODE
-
 from .utils.problem import Problem
 from .utils.termination import Termination
 from .tuner import Tuner, ParameterGrid
 from .multitask import Multitask
 from .optimizer import Optimizer
-from .utils.space import (
-    IntegerVar,
-    FloatVar,
-    StringVar,
-    BinaryVar,
-    BoolVar,
-    CategoricalVar,
-    SequenceVar,
-    PermutationVar,
-    TransferBinaryVar,
-    TransferBoolVar,
-)
+from .utils.space import (IntegerVar, FloatVar, StringVar, BinaryVar, BoolVar, CategoricalVar,
+                          SequenceVar, PermutationVar, TransferBinaryVar, TransferBoolVar)
 
 __EXCLUDE_MODULES = ["__builtins__", "current_module", "inspect", "sys"]
 
@@ -239,7 +73,6 @@ def get_all_optimizers(verbose: bool = True):
             for cls_name, cls_obj in inspect.getmembers(obj):
                 if inspect.isclass(cls_obj) and issubclass(cls_obj, Optimizer):
                     cls[cls_name] = cls_obj
-    # Remove base class
     cls.pop("Optimizer", None)
     if verbose:
         for name, optimizer in cls.items():
@@ -252,8 +85,7 @@ def get_optimizer_by_class(class_name: str, verbose: bool = False):
     Get an optimizer class by its class name
 
     Args:
-        class_name (str): the classname of the optimizer (e.g., C_PSO, OriginalGA);
-            don't pass the module name (e.g., PSO, GA)
+        class_name (str): the classname of the optimizer (e.g, C_PSO, OriginalGA), don't pass the module name (e.g, PSO, GA)
         verbose (bool): whether to print the optimizer information
 
     Returns:
@@ -263,11 +95,9 @@ def get_optimizer_by_class(class_name: str, verbose: bool = False):
         all_optimizers = get_all_optimizers(verbose=verbose)
         return all_optimizers[class_name]
     except KeyError:
-        print(
-            f"Mealpy doesn't support optimizer named: {class_name}.\n"
-            f"Please see the supported Optimizer name from here: "
-            f"https://mealpy.readthedocs.io/en/latest/pages/support.html#classification-table"
-        )
+        print(f"Mealpy doesn't support optimizer named: {class_name}.\n"
+              f"Please see the supported Optimizer name from here:\n"
+              f"https://github.com/thieu1995/mealpy?tab=readme-ov-file#-optimizer-classification-table")
         return None
 
 
@@ -276,8 +106,7 @@ def get_optimizer_by_name(name: str, verbose: bool = False):
     Get an optimizer class by name
 
     Args:
-        name (str): the classname of the optimizer (e.g., OriginalGA, OriginalWOA);
-            don't pass the module name (e.g., ABC, WOA, GA)
+        name (str): the classname of the optimizer (e.g, OriginalGA, OriginalWOA), don't pass the module name (e.g, ABC, WOA, GA)
         verbose (bool): whether to print the optimizer information
 
     Returns:
@@ -293,15 +122,12 @@ def get_optimizer_by_name(name: str, verbose: bool = False):
                     cls[cls_name] = cls_obj
     if verbose:
         if not flag:
-            print(
-                f"Mealpy doesn't support optimizer named: {name}.\n"
-                f"Please see the supported Optimizer name from here: "
-                f"https://mealpy.readthedocs.io/en/latest/pages/support.html#classification-table"
-            )
+            print(f"Mealpy doesn't support optimizer named: {name}.\n"
+                  f"Please see the supported Optimizer name from here:\n"
+                  f"https://github.com/thieu1995/mealpy?tab=readme-ov-file#-optimizer-classification-table")
             return None
-        # Remove base class
-        cls.pop("Optimizer", None)
+        del cls['Optimizer']
         print(f"Found algorithm: {name}, the supported variants are:")
-        for algo_name, optimizer in cls.items():
-            print(f"Optimizer: {algo_name} - {optimizer} - {optimizer()}")
+        for name, optimizer in cls.items():
+            print(f"Optimizer: {name} - {optimizer} - {optimizer()}")
     return cls
