5e2af3f8da0e81031d94dc43df402c157c949ab4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2026-07-09 19:38:19 +0200

    Update tests

--- tests/swarm_based/test_ChOA.py
+++ tests/swarm_based/test_ChOA.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pytest
-from mealpy.swarm_based import ChOA
+
 from mealpy import FloatVar
+from mealpy.swarm_based import ChOA
 
 
 @pytest.fixture(scope="module")
 def problem():
     def objective_function(solution):
-        return np.sum(solution**2)
+        return np.sum(solution ** 2)
 
     return {
         "obj_func": objective_function,
@@ -18,11 +19,9 @@ def problem():
 
 
 def test_original_choa(problem):
-    # Senin yazdığın sınıfı çağırıyoruz
     model = ChOA.OriginalChOA(epoch=10, pop_size=20)
     g_best = model.solve(problem)
-    
-    # Kütüphanenin beklediği standart doğrulamalar (Assertions)
+
     assert g_best.target.fitness < 100
     assert isinstance(g_best.solution, np.ndarray)
     assert len(g_best.solution) == 3
