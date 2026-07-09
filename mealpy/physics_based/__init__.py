467671f1f5e67ddc9af2dd7133cbd0cb5687eab3
Nguyen Van Thieu <12763861+thieu1995@users.noreply.github.com>
2026-07-09 15:33:52 +0200

    Remove unused import in  __init__.py

--- mealpy/physics_based/__init__.py
+++ mealpy/physics_based/__init__.py
@@ -1,2 +1,5 @@
-from . import (ArchOA, ASO, CDO, EFO, EO, EVO, FLA, HGSO, MVO, NRO, RIME, SA, TWO, WDO, ESO, SOO, CEO)
-
+#!/usr/bin/env python
+# Created by "Thieu" at 21:14, 17/03/2020 ----------%
+#       Email: nguyenthieu2102@gmail.com            %
+#       Github: https://github.com/thieu1995        %
+# --------------------------------------------------%
