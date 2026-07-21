df3c51f06f1e19ba1e43ee3e8a0503349f6f2166
Thieu Nguyen <nguyenthieu2102@gmail.com>
2026-07-21 23:35:46 +0200

    Update OriginalEOA optimizer

--- mealpy/bio_based/EOA.py
+++ mealpy/bio_based/EOA.py
@@ -10,7 +10,7 @@ from mealpy.optimizer import Optimizer
 
 class OriginalEOA(Optimizer):
     """
-    Our developed version: Earthworm Optimisation Algorithm (EOA)
+    The original version of: Earthworm Optimisation Algorithm (EOA)
 
     Parameters
     ----------
@@ -31,14 +31,16 @@ class OriginalEOA(Optimizer):
     gama : float
         A constant that is similar to cooling factor of a cooling schedule in the simulated annealing, in range (0.0, 1.0). Default is 0.9.
 
-    Note
-    ----
-    The original version from matlab code above will not work well, even with small dimensions.
-    I change updating process, change cauchy process using x_mean, use global best solution
+
+    .. attention::
+       1. The author's MATLAB source code differs from the equations and parameters presented in the paper.
+       2. This algorithm updates the population twice per epoch, resulting in double the number of function
+          evaluations (NFEs) per epoch compared to standard algorithms.
+       3. Users should be cautious when using algorithms published in low-quality journals like this.
 
     Links
     -----
-    1. http://doi.org/10.1504/IJBIC.2015.10004283
+    1. https://doi.org/10.1504/IJBIC.2018.093328
     2. https://www.mathworks.com/matlabcentral/fileexchange/53479-earthworm-optimization-algorithm-ewa
 
     References
@@ -90,7 +92,7 @@ class OriginalEOA(Optimizer):
         self.beta = self.validator.check_float("beta", beta, (0, 1.0))
         self.gama = self.validator.check_float("gama", gama, (0, 1.0))
         self.set_parameters(["epoch", "pop_size", "p_c", "p_m", "n_best", "alpha", "beta", "gama"])
-        self.sort_flag = False
+        self.sort_flag = True
 
     def initialize_variables(self):
         self.dyn_beta = self.beta
@@ -102,66 +104,64 @@ class OriginalEOA(Optimizer):
         Args:
             epoch (int): The current iteration
         """
-        ## Update the pop best
-        pop_elites, _, _ = self.get_special_agents(self.pop, n_best=1, minmax=self.problem.minmax)
-        pop = []
-        for idx in range(0, self.pop_size):
-            ### Reproduction 1: the first way of reproducing
-            x_t1 = self.problem.lb + self.problem.ub - self.alpha * self.pop[idx].solution
-
-            ### Reproduction 2: the second way of reproducing
-            if idx >= self.n_best:  ### Select two parents to mate and create two children
-                idx = int(self.pop_size * 0.2)
-                if self.generator.uniform() < 0.5:  ## 80% parents selected from best population
-                    idx1, idx2 = self.generator.choice(range(0, idx), 2, replace=False)
-                else:  ## 20% left parents selected from worst population (make more diversity)
-                    idx1, idx2 = self.generator.choice(range(idx, self.pop_size), 2, replace=False)
-                r = self.generator.uniform()
-                x_child = r * self.pop[idx2].solution + (1 - r) * self.pop[idx1].solution
-            else:
-                r1 = self.generator.integers(0, self.pop_size)
-                x_child = self.pop[r1].solution
-            x_t1 = self.dyn_beta * x_t1 + (1.0 - self.dyn_beta) * x_child
-            pos_new = self.correct_solution(x_t1)
-            agent = self.generate_empty_agent(pos_new)
-            pop.append(agent)
-            if self.mode not in self.AVAILABLE_MODES:
-                agent.target = self.get_target(pos_new)
-                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
-        if self.mode in self.AVAILABLE_MODES:
-            pop = self.update_target_for_population(pop)
-            self.pop = self.greedy_selection_population(self.pop, pop, self.problem.minmax)
+        x_keep = np.array([self.pop[idx].solution for idx in range(0, self.n_best)])
+        fit_list = np.array([agent.target.fitness for agent in self.pop])
+        _, best_fit, worst_fit = self.get_special_fitness(self.pop, self.problem.minmax)
+        if self.problem.minmax == "min":
+            fits = worst_fit - fit_list + self.EPSILON
+        else:
+            fits = fit_list - worst_fit + self.EPSILON
+        prob_selection = fits / np.sum(fits)
         self.dyn_beta = self.gama * self.beta
-        self.pop = self.get_sorted_and_trimmed_population(self.pop, self.pop_size, self.problem.minmax)
 
-        pos_list = np.array([agent.solution for agent in self.pop])
-        x_mean = np.mean(pos_list, axis=0)
-        ## Cauchy mutation (CM)
-        cauchy_w = self.g_best.solution.copy()
+        # =========================================================
+        # Begin Earthworm Reproduction Process
         pop_new = []
-        for idx in range(self.n_best, self.pop_size):  # Don't allow the elites to be mutated
-            condition = self.generator.random(self.problem.n_dims) < self.p_m
-            cauchy_w = np.where(condition, x_mean, cauchy_w)
-            x_t1 = (cauchy_w + self.g_best.solution) / 2
-            pos_new = self.correct_solution(x_t1)
-            agent = self.generate_empty_agent(pos_new)
-            pop_new.append(agent)
+        for idx in range(self.pop_size):
+            # Reproduction 1
+            x0 = self.problem.lb + self.problem.ub - self.alpha * self.pop[idx].solution
+            # Reproduction 2
+            r1 = self.generator.integers(0, self.pop_size)
+            if idx >= self.n_best:
+                mate1 = self.generator.integers(0, self.pop_size)
+                mate2 = self.generator.choice(self.pop_size, p=prob_selection)
+                # Uniform Crossover (Vectorized numVar loop)
+                rand_cross = self.generator.random(self.problem.n_dims)
+                rand_select = self.generator.random(self.problem.n_dims)
+                mask_p1 = ((rand_cross < self.p_c) & (rand_select > 0.5)) | ((rand_cross >= self.p_c) & (rand_select <= 0.5))
+                x1 = np.where(mask_p1, self.pop[mate1].solution, self.pop[mate2].solution)
+            else:
+                x1 = self.pop[r1].solution
+            # Combine Reproduction 1 and 2
+            pos_new = self.dyn_beta * x0 + (1.0 - self.dyn_beta) * x1
+            pos_new = self.correct_solution(pos_new)
+            pop_new.append(self.generate_empty_agent(pos_new))
             if self.mode not in self.AVAILABLE_MODES:
-                agent.target = self.get_target(pos_new)
-                self.pop[idx] = self.get_better_agent(agent, self.pop[idx], self.problem.minmax)
+                pop_new[-1].target = self.get_target(pos_new)
+        ## Update fitness in parallel
         if self.mode in self.AVAILABLE_MODES:
             pop_new = self.update_target_for_population(pop_new)
-            self.pop[self.n_best:] = self.greedy_selection_population(pop_new, self.pop[self.n_best:], self.problem.minmax)
 
-        ## Elitism Strategy: Replace the worst with the previous generation's elites.
-        self.pop, _, _ = self.get_special_agents(self.pop, minmax=self.problem.minmax)
-        for idx in range(0, self.n_best):
-            self.pop[self.pop_size - idx - 1] = pop_elites[idx].copy()
-
-        ## Make sure the population does not have duplicates.
-        new_set = set()
-        for idx, agent in enumerate(self.pop):
-            if tuple(agent.solution.tolist()) in new_set:
-                self.pop[idx] = self.generate_agent()
+        # ---------------------------------------------------------
+        # Cauchy Mutation (CM)
+        # Update and sort
+        self.pop = self.get_sorted_population(pop_new, self.problem.minmax)
+        self.pop, best, _ = self.get_special_agents(pop_new, n_best=1, n_worst=1, minmax=self.problem.minmax)
+        pop_mean = np.mean([agent.solution for agent in self.pop], axis=0)
+        pop_new = [self.pop[idx].copy() for idx in range(self.n_best)]
+        for idx in range(self.n_best, self.pop_size):
+            if idx < self.pop_size - self.n_best:
+                mutation_mask = self.generator.random(self.problem.n_dims) < self.p_m
+                cauchy_w = best[0].solution.copy()
+                cauchy_w[mutation_mask] = pop_mean[mutation_mask]
+                pos_new = (cauchy_w + best[0].solution) / 2.0
             else:
-                new_set.add(tuple(agent.solution.tolist()))
+                pos_new = x_keep[idx - (self.pop_size - self.n_best)]
+            pos_new = self.correct_solution(pos_new)
+            pop_new.append(self.generate_empty_agent(pos_new))
+            if self.mode not in self.AVAILABLE_MODES:
+                pop_new[-1].target = self.get_target(pos_new)
+        ## Update fitness in parallel
+        if self.mode in self.AVAILABLE_MODES:
+            pop_new = self.update_target_for_population(pop_new)
+        self.pop = pop_new
